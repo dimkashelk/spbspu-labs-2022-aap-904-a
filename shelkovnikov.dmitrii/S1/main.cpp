cfe876f0748e3805b8ca233490821b113b667c6f
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-10-17 18:52:28 +0300

    create Counter

--- shelkovnikov.dmitrii/S1/main.cpp
+++ shelkovnikov.dmitrii/S1/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+
+struct Counter
+{
+    void operator()(int previous, int current, int next)
+    {
+      if (previous < current and current < next)
+      {
+        count++;
+      }
+    }
+    int count = 0;
+};
+
 int main()
 {
-
 }

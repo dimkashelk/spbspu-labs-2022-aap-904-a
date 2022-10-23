bf586849e7808bfee7fd0ea4f2a48d7a6bd4ae2d
Chemodurovart <chemodurovart@gmail.com>
2022-10-23 23:20:20 +0300

    Lab 1 complete!

---
+++ chemodurov.artem/I1/main.cpp
@@ -0,0 +1,32 @@
+#include "count.h"
+
+int main()
+{
+  MinCounter min_cnt{0};
+  MaxEvenCounter max_even_cnt{0};
+  int curr_value = 0;
+
+  do
+  {
+    std::cin >> curr_value;
+    try
+    {
+      min_cnt(curr_value, std::cin);
+      max_even_cnt(curr_value, std::cin);
+    }
+    catch (const std::overflow_error & e)
+    {
+      std::cout << e.what() << "\n";
+      return 1;
+    }
+    if (!std::cin)
+    {
+      std::cerr << "You didn`t enter an integer number\n";
+      return 1;
+    }
+  }
+  while(std::cin && curr_value);
+
+  std::cout << min_cnt.min_count << " " << max_even_cnt.max_even_count << "\n";
+  return 0;
+}

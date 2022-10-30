dc9ad505b311725e643f50f1a5451a40842444a5
xeniaTa <kseniatarasenko1@gmail.com>
2022-10-30 19:35:28 +0300

    first commit I1

---
+++ tarasenko.ksenia/I1/secondmax.cpp
@@ -0,0 +1,14 @@
+#include "secondmax.hpp"
+
+void SecondMax::operator()(int curr_value)
+{
+    if (curr_value >= first_max)
+    {
+	second_max = first_max;
+	first_max = curr_value;
+    }
+    else if (curr_value > second_max)
+    {
+        second_max = curr_value;
+    }
+}

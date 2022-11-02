bab13e44e3987c075ce816df33824e8532f20863
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-02 18:49:01 +0300

    update counterorderedelements.cpp

--- shelkovnikov.dmitrii/I2/counterorderedelements.cpp
+++ shelkovnikov.dmitrii/I2/counterorderedelements.cpp
@@ -1,1 +1,17 @@
 #include "counterorderedelements.h"
+CounterOrderedElements::CounterOrderedElements(int *arr, int length):
+  length(length),
+  arr(arr)
+{}
+unsigned int CounterOrderedElements::get_count() const
+{
+  unsigned int count = 0;
+  for (int i = 0; i < n - 1; i++)
+  {
+    if (arr[i] < arr[i + 1])
+    {
+      count++;
+    }
+  }
+  return count;
+}

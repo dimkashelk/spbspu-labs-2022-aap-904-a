0d6a4d83fe38e3fbf36d34a381c8781788aba6cf
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-02 18:31:49 +0300

    add constructor and functions: get_maximum_count, get_maximum

--- shelkovnikov.dmitrii/I2/countermaximumelements.cpp
+++ shelkovnikov.dmitrii/I2/countermaximumelements.cpp
@@ -1,1 +1,31 @@
 #include "countermaximumelements.h"
+CounterMaximumElements::CounterMaximumElements(int *arr, int count):
+  max_count(0),
+  length(count),
+  arr(arr)
+{}
+unsigned int CounterMaximumElements::get_maximum_count() const
+{
+  int count = 0;
+  const int maximum = get_maximum();
+  for (int i = 0; i < length; i++)
+  {
+    if (maximum == arr[i])
+    {
+      count++;
+    }
+  }
+  return count;
+}
+int CounterMaximumElements::get_maximum() const
+{
+  int maximum = arr[0];
+  for (int i = 0; i < length; i++)
+  {
+    if (maximum < arr[i])
+    {
+      maximum = arr[i];
+    }
+  }
+  return maximum;
+}

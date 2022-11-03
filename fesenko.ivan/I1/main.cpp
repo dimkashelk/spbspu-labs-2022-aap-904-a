1ff0f48b58b86c72cbd2ff59a1eb40c15d05bb17
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2022-11-03 16:02:01 +0300

    Lab 1 complete

--- fesenko.ivan/I1/main.cpp
+++ fesenko.ivan/I1/main.cpp
@@ -3,13 +3,19 @@ int main()
 {
   int value = 0;
   int predvalue = 0;
+  int count = 0;
   int seqcount = 1;
   int maxseq = 0;
-  int count = 0;
   std::cin >> value;
   while (value != 0) {
     predvalue = value;
     std::cin >> value;
+    if (value == 0) {
+      if (seqcount > maxseq) {
+        maxseq = seqcount;
+      }
+      break;
+    }
     if (predvalue < value) {
       count++;
     }

7d2ad74ca96e15176a562b8886c4bfe4bdd95487
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-04 16:18:43 +0300

    fixup! get_count in counterorderedelements.cpp

--- shelkovnikov.dmitrii/I2/counterorderedelements.cpp
+++ shelkovnikov.dmitrii/I2/counterorderedelements.cpp
@@ -6,7 +6,7 @@ CounterOrderedElements::CounterOrderedElements(int *arr, int length):
 unsigned int CounterOrderedElements::get_count() const
 {
   unsigned int count = 0;
-  for (int i = 0; i < n - 1; i++)
+  for (int i = 0; i < length - 1; i++)
   {
     if (arr[i] < arr[i + 1])
     {

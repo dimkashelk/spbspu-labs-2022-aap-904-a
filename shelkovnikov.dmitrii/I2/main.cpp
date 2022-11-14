5ec221ca835ebd64eee4a2f831c1c707a6c8d4f9
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-14 23:01:12 +0300

    add array from file

--- shelkovnikov.dmitrii/I2/main.cpp
+++ shelkovnikov.dmitrii/I2/main.cpp
@@ -14,13 +14,6 @@ int main(int argc, char *argv[])
     std::cout << "No file name....";
     return 1;
   }
-  std::ifstream in;
-  in.open(argv[1]);
-  if (!in.is_open())
-  {
-    std::cout << "Error while opening file";
-    return 1;
-  }
   int arr1[] = {1, 2, 3, 4, 5, 6, 7};
   CounterMaximumElements counterMaximumElements1(arr1, 7);
   CounterOrderedElements counterOrderedElements1(arr1, 7);
@@ -34,16 +27,15 @@ int main(int argc, char *argv[])
     std::cout << e.what();
     return 2;
   }
-  srand(time(0));
   int n;
   std::cin >> n;
-  int *arr = new int[n];
+  int *arr2 = new int[n];
   for (int i = 0; i < n; i++)
   {
-    arr[i] = rand();
+    arr2[i] = rand();
   }
-  CounterMaximumElements counterMaximumElements2(arr, n);
-  CounterOrderedElements counterOrderedElements2(arr, n);
+  CounterMaximumElements counterMaximumElements2(arr2, n);
+  CounterOrderedElements counterOrderedElements2(arr2, n);
   try
   {
     std::cout << counterMaximumElements2.get_maximum_count() << std::endl;
@@ -52,8 +44,53 @@ int main(int argc, char *argv[])
   catch (const std::overflow_error &e)
   {
     std::cout << e.what();
+    delete[] arr2;
     return 2;
   }
-  delete[] arr;
+  delete[] arr2;
+  std::ifstream in;
+  in.open(argv[1]);
+  if (!in.is_open())
+  {
+    std::cout << "Error while opening file";
+    return 1;
+  }
+  while (!in.eof())
+  {
+    int size;
+    in >> size;
+    if (!in)
+    {
+      std::cout << "Error... =(";
+      delete[] arr2;
+      return 1;
+    }
+    int* arr3 = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+      in >> arr3[i];
+      if (!in)
+      {
+        std::cout << "Error... =(";
+        delete[] arr2;
+        delete[] arr3;
+        return 1;
+      }
+    }
+    CounterMaximumElements counterMaximumElements3(arr3, size);
+    CounterOrderedElements counterOrderedElements3(arr3, size);
+    try
+    {
+      std::cout << counterMaximumElements3.get_maximum_count() << std::endl;
+      std::cout << counterOrderedElements3.get_count() << std::endl;
+    }
+    catch (const std::overflow_error &e)
+    {
+      std::cout << e.what();
+      delete[] arr2;
+      delete[] arr3;
+      return 2;
+    }
+  }
   return 0;
 }

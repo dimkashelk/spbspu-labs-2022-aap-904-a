3cb0d17aa6962603ed130fc4ed79d872d2703d2c
Chemodurovart <chemodurovart@gmail.com>
2022-11-14 22:11:11 +0300

    fix

--- chemodurov.artem/I2/main.cpp
+++ chemodurov.artem/I2/main.cpp
@@ -21,6 +21,7 @@ int main(int argc, char ** argv)
   size_t arr_size = 5;
   size_t new_arr_size = 5;
   int * arr = new int[arr_size];
+  int * new_arr = nullptr;
   for (size_t i = 0; i < arr_size; ++i)
   {
     int x = 0;
@@ -34,7 +35,7 @@ int main(int argc, char ** argv)
     if (i == arr_size - 1)
     {
       new_arr_size = ceil(sqrt(2) * arr_size);
-      int * new_arr = chemodurov::extendDynArray(arr, arr_size, new_arr_size);
+      new_arr = chemodurov::extendDynArray(arr, arr_size, new_arr_size);
       delete [] arr;
       arr = new_arr;
       new_arr = nullptr;
@@ -50,7 +51,6 @@ int main(int argc, char ** argv)
   unsigned int arr_cnt_div = chemodurov::countDivisible(arr, arr_size, divider);
   std::cout << arr_cnt_dec << " " << arr_cnt_div << "\n";
   delete [] arr;
-
   size_t n = 0;
   std::cin >> n;
   if (!std::cin)

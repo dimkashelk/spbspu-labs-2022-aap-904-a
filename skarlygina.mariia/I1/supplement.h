74cd62b0f58f875c912d895f0750a582efaf8c4b
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-11-14 15:16:32 +0300

    fixed

--- skarlygina.mariia/I1/supplement.h
+++ skarlygina.mariia/I1/supplement.h
@@ -3,7 +3,7 @@
 
 struct MaxSameValue
 {
-  unsigned int number_of_same_value = 1;
+  unsigned int number_of_same_value = 0;
   unsigned int max_number_of_same_value = 0;
   void operator()(int value);
   int value_before = 0;
@@ -11,7 +11,7 @@ struct MaxSameValue
 
 struct MaxGrowingSequence
 {
-  unsigned int number_of_growing_sequence = 1;
+  unsigned int number_of_growing_sequence = 0;
   unsigned int max_number_of_growing_sequence = 0;
   void operator()(int value);
   int value_before = 0;

59f831116cd005187025bc992d24395c85998c5b
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-14 23:47:18 +0300

    add remove_extra_space func

--- shelkovnikov.dmitrii/I3/main.cpp
+++ shelkovnikov.dmitrii/I3/main.cpp
@@ -1,4 +1,34 @@
 #include <iostream>
+
+char* remove_extra_spaces(char *destination, const char *source, int size)
+{
+  int j = 0;
+  while (source[j] == ' ')
+  {
+    j++;
+  }
+  int i = 0;
+  while (j < size)
+  {
+    if (source[j] == ' ')
+    {
+      while (source[j] == ' ')
+      {
+        j++;
+      }
+      destination[i] = ' ';
+      j--;
+    }
+    else
+    {
+      destination[i] = source[j];
+    }
+    i++;
+    j++;
+  }
+  return destination;
+}
+
 int main()
 {
   return 0;

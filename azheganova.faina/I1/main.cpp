66b4f7ce419599caa45e727076046b60e49c0c16
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-11-15 23:33:36 +0300

    labbI1

--- azheganova.faina/I1/main.cpp
+++ azheganova.faina/I1/main.cpp
@@ -5,12 +5,12 @@ struct Localminimals
   {
     if (in)
     {
-	  if ((current < previous) && (current < next))
-	  {
-	    countlocalmin++;
-	  }
-	  next = current;
-	  current = previous;
+	 if ((current < previous) && (current < next))
+	 {
+	  countlocalmin++;
+	 }
+	 next = current;
+	 current = previous;
     }
   }
   int countlocalmin;
@@ -23,13 +23,13 @@ struct Minsrmax
   {
     if (in)
     {
-      i++;
-      if ((i>2) && (current < previous) && (current > next))
-      {
-        countminsrmax++;
-      }
-      next = current;
-      current = previous;
+     i++;
+     if ((i>2) && (current < previous) && (current > next))
+     {
+     countminsrmax++;
+     }
+     next = current;
+     current = previous;
     }
   }
   int i = 0;
@@ -44,14 +44,14 @@ int main()
   Minsrmax count2{0};
   do
   {
-    std::cin >> previous;
-    count1(previous, std::cin);
-    count2(previous, std::cin);
-  } 
+   std::cin >> previous;
+   count1(previous, std::cin);
+   count2(previous, std::cin);
+  }
   while (previous && std::cin);
   if (!std::cin)
   {
-    std::cout << "Not a number";
+   std::cout << "Not a number";
   }
   std::cout << count1.countlocalmin << '\n';
   std::cout << count2.countminsrmax << '\n';

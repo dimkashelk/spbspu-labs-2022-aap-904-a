27a5c0f8f3b8e055c880c5d36928b4f0cb80c901
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-16 12:36:03 +0300

    create remove_extra_spaces func

--- shelkovnikov.dmitrii/I3/main.cpp
+++ shelkovnikov.dmitrii/I3/main.cpp
@@ -1,14 +1,15 @@
+#include "stringbuilder.h"
 #include <iostream>
-
-char* remove_extra_spaces(char *destination, const char *source, int size)
+char* remove_extra_spaces(char *destination, const char *source, int *res_size)
 {
+  StringBuilder stringBuilder;
   int j = 0;
   while (source[j] == ' ')
   {
     j++;
   }
   int i = 0;
-  while (j < size)
+  while (source[j])
   {
     if (source[j] == ' ')
     {
@@ -16,20 +17,41 @@ char* remove_extra_spaces(char *destination, const char *source, int size)
       {
         j++;
       }
-      destination[i] = ' ';
+      stringBuilder.add_char(' ');
       j--;
     }
     else
     {
-      destination[i] = source[j];
+      stringBuilder.add_char(source[j]);
     }
     i++;
     j++;
   }
+  int new_size = stringBuilder.get_size();
+  while (stringBuilder.get_char(new_size - 1) == ' ')
+  {
+    stringBuilder.remove_char(new_size - 1);
+    new_size--;
+  }
+  *res_size = stringBuilder.get_size();
+  char *dop = stringBuilder.get_string();
+  for (int k = 0; k < stringBuilder.get_size(); k++)
+  {
+    destination[k] = dop[k];
+  }
+  delete[] dop;
   return destination;
 }
-
 int main()
 {
+  int size = 5;
+  char* str = {'d', ' ', ' ', ' ', 'a', ' '};
+  char* res = new char[size];
+  remove_extra_spaces(res, str, &size);
+  std::cout << size;
+  for (int i = 0; i < size; i++)
+  {
+    std::cout << res[i];
+  }
   return 0;
 }

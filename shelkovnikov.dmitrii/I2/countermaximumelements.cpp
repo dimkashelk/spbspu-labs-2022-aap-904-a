e2e4ac4b3a8187aea06316060b612df7688ec288
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-16 10:28:36 +0300

    update maximum element in func get_maximum

--- shelkovnikov.dmitrii/I2/countermaximumelements.cpp
+++ shelkovnikov.dmitrii/I2/countermaximumelements.cpp
@@ -30,7 +30,7 @@ unsigned int CounterMaximumElements::get_maximum_count()
 }
 int CounterMaximumElements::get_maximum()
 {
-  int maximum = arr[0];
+  int maximum = std::numeric_limits<int>::min();
   for (int i = 0; i < length; i++)
   {
     if (maximum < arr[i])

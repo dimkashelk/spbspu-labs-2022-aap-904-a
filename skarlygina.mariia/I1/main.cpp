d8092388a0f1c5a3f9c1ce3fba9c29d8360a5ed8
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-11-18 21:36:17 +0300

    final changes

--- skarlygina.mariia/I1/main.cpp
+++ skarlygina.mariia/I1/main.cpp
@@ -27,6 +27,7 @@ int main()
       return 1;
     }
   while (std::cin && value);
+
   std::cout << "Max number of same value " << max_same_value.max_number_of_same_value << "\n";
   std::cout << "Max number of growing sequence " << max_growing_sequence.max_number_of_growing_sequence << "\n";
   return 0;

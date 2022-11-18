eb4346ddb69522f59a24300430f0e3205c003228
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-18 23:23:32 +0300

    update parameters

--- shelkovnikov.dmitrii/I2/main.cpp
+++ shelkovnikov.dmitrii/I2/main.cpp
@@ -6,9 +6,9 @@ unsigned int count_ordered_elements(int *arr, size_t size)
 {
   unsigned int count = 0;
   constexpr unsigned int max_int = std::numeric_limits< unsigned int >::max();
-  for (size_t i = 0; i < size - 1; i++)
+  for (size_t i = 1; i < size; i++)
   {
-    if (arr[i] < arr[i + 1])
+    if (arr[i - 1] < arr[i])
     {
       if (count == max_int)
       {

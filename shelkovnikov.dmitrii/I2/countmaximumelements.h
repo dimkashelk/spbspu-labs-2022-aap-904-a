22bab256ca0e20e0f19b915b6e7ceaa011e1f128
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-20 00:55:33 +0300

    change initialization

--- shelkovnikov.dmitrii/I2/countmaximumelements.h
+++ shelkovnikov.dmitrii/I2/countmaximumelements.h
@@ -1,5 +1,5 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_COUNTMAXIMUMELEMENTS_H
 #define SPBSPU_LABS_2022_AAP_904_A_COUNTMAXIMUMELEMENTS_H
 #include <cstddef>
-size_t count_maximum_elements(const int *arr, const size_t size);
+size_t count_maximum_elements(int *arr, size_t size);
 #endif

46b649e506a3854d9b1e067f80bf346d85960839
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-20 00:36:14 +0300

    update return types

--- shelkovnikov.dmitrii/I2/countmaximumelements.h
+++ shelkovnikov.dmitrii/I2/countmaximumelements.h
@@ -1,5 +1,5 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_COUNTMAXIMUMELEMENTS_H
 #define SPBSPU_LABS_2022_AAP_904_A_COUNTMAXIMUMELEMENTS_H
 #include <cstddef>
-unsigned int count_maximum_elements(const int *arr, const size_t size);
+size_t count_maximum_elements(const int *arr, const size_t size);
 #endif
--- shelkovnikov.dmitrii/I2/countorderedelements.h
+++ shelkovnikov.dmitrii/I2/countorderedelements.h
@@ -1,5 +1,5 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_COUNTORDEREDELEMENTS_H
 #define SPBSPU_LABS_2022_AAP_904_A_COUNTORDEREDELEMENTS_H
 #include <cstddef>
-unsigned int count_ordered_elements(const int *arr, const size_t size);
+size_t count_ordered_elements(const int *arr, const size_t size);
 #endif

f786e001b2c8362a16e1d37ca381f468d5a71c9a
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-20 02:37:07 +0300

    create getothersymbols.h and getothersymbols.cpp

---
+++ shelkovnikov.dmitrii/I3/getothersymbols.cpp
@@ -0,0 +1,39 @@
+#include "getothersymbols.h"
+char *get_other_symbols(char *destination, const char *source, int *res_size)
+{
+  int alphabet[26];
+  for (int i = 0; i < 26; i++)
+  {
+    alphabet[i] = 0;
+  }
+  for (int i = 0; source[i]; i++)
+  {
+    if (source[i] >= 'a' && source[i] <= 'z')
+    {
+      alphabet[static_cast<int>(source[i]) - static_cast<int>('a')]++;
+    }
+    else if (source[i] >= 'A' && source[i] <= 'Z')
+    {
+      alphabet[static_cast<int>(source[i]) - static_cast<int>('A')]++;
+    }
+  }
+  int j = 0;
+  StringBuilder stringBuilder;
+  for (int i = 0; i < 26; i++)
+  {
+    if (!alphabet[i])
+    {
+      stringBuilder.add_char(static_cast<char>(static_cast<int>('a') + i));
+      j++;
+    }
+  }
+  size_t new_size = stringBuilder.get_size();
+  char *dop = stringBuilder.get_string();
+  for (int i = 0; i < new_size; i++)
+  {
+    destination[i] = dop[i];
+  }
+  delete[] dop;
+  *res_size = stringBuilder.get_size();
+  return destination;
+}
---
+++ shelkovnikov.dmitrii/I3/getothersymbols.h
@@ -0,0 +1,4 @@
+#ifndef SPBSPU_LABS_2022_AAP_904_A_GETOTHERSYMBOLS_H
+#define SPBSPU_LABS_2022_AAP_904_A_GETOTHERSYMBOLS_H
+char *get_other_symbols(char *destination, const char *source, int *res_size);
+#endif

676b3a80ca64bef2fb081c6c2d7c050fc9136727
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-19 14:49:27 +0300

    include cstdio

--- shelkovnikov.dmitrii/I2/countorderedelements.h
+++ shelkovnikov.dmitrii/I2/countorderedelements.h
@@ -1,4 +1,5 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_COUNTORDEREDELEMENTS_H
 #define SPBSPU_LABS_2022_AAP_904_A_COUNTORDEREDELEMENTS_H
+#include <cstdio>
 unsigned int count_ordered_elements(int *arr, size_t size);
 #endif

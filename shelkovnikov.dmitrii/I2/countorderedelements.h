c41f9608e781f5d69065278b7b30af8d62abba83
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-20 01:13:03 +0300

    change initialization

--- shelkovnikov.dmitrii/I2/countorderedelements.h
+++ shelkovnikov.dmitrii/I2/countorderedelements.h
@@ -1,5 +1,5 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_COUNTORDEREDELEMENTS_H
 #define SPBSPU_LABS_2022_AAP_904_A_COUNTORDEREDELEMENTS_H
 #include <cstddef>
-size_t count_ordered_elements(int *arr, size_t size);
+size_t count_ordered_elements(const int *arr, size_t size);
 #endif

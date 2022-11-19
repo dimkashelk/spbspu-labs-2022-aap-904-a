93e92a662d8f33293e54a63f3c2f45ddf41f0c2c
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-19 23:25:24 +0300

    fixing CG and change NULL to nullptr

--- shelkovnikov.dmitrii/I2/main.cpp
+++ shelkovnikov.dmitrii/I2/main.cpp
@@ -29,10 +29,10 @@ int main(int argc, char *argv[])
   int n = 0;
   std::cin >> n;
   int *arr2 = new int[n];
-  srand(time(NULL));
+  std::srand(time(nullptr));
   for (int i = 0; i < n; i++)
   {
-    arr2[i] = rand();
+    arr2[i] = std::rand();
   }
   try
   {

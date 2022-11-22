6f9574b67fe3b96c1c8219824f25b7af5486c647
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-22 17:45:20 +0300

    update main

--- shelkovnikov.dmitrii/I3/main.cpp
+++ shelkovnikov.dmitrii/I3/main.cpp
@@ -4,40 +4,48 @@
 #include "getothersymbols.h"
 int main()
 {
-  StringBuilder stringBuilder;
-  char dop = 'a';
+  size_t capacity = 10;
+  char * str = new char[capacity];
+  size_t size = 0;
   std::cin >> std::noskipws;
+  constexpr size_t max_size_t = std::numeric_limits< size_t >::max();
   do
   {
-    std::cin >> dop;
-    if (!std::cin)
+    if (size == capacity)
     {
-      std::cout << "Error while input\n";
-      return 1;
+      if (capacity == max_size_t)
+      {
+        std::cout << "Too long string";
+        return 1;
+      }
+      if (max_size_t - 20 <= capacity)
+      {
+        capacity = max_size_t;
+      }
+      else
+      {
+        capacity += 20;
+      }
+      try
+      {
+        char * newstr = new char[capacity];
+        for (auto i = str, j = newstr; i != str + size; ++i, ++j)
+        {
+          *j = *i;
+        }
+        delete [] str;
+        str = newstr;
+      }
+      catch (...)
+      {
+        delete [] str;
+        return 1;
+      }
     }
-    try
-    {
-      stringBuilder.add_char(dop);
-    }
-    catch (const std::overflow_error &e)
-    {
-      std::cout << e.what();
-      return 2;
-    }
+    std::cin >> str[size];
   }
-  while (std::cin && stringBuilder.get_char(stringBuilder.get_size() - 1) != '\n');
-  stringBuilder.remove_char(stringBuilder.get_size() - 1);
-  try
-  {
-    stringBuilder.add_char('\0');
-  }
-  catch (const std::overflow_error &e)
-  {
-    std::cout << e.what();
-    return 2;
-  }
-  char *str = stringBuilder.get_string();
-  size_t size = stringBuilder.get_size();
+  while (std::cin && str[size++] != '\n');
+  str[size - 1] = '\0';
   try
   {
     char *res1 = new char[size];
@@ -48,28 +56,20 @@ int main()
     return 2;
   }
   size_t size1 = 0;
-  remove_extra_spaces(res1, str, &size1);
-  for (size_t i = 0; res1[i]; i++)
-  {
-    std::cout << res1[i];
-  }
-  std::cout << '\n';
+  remove_extra_spaces(res1, str, size);
+  std::cout << res1 << '\n';
   delete[] res1;
   try
   {
-    char *res2 = new char[26];
+    char *res2 = new char[27];
   }
   catch (const std::bad_alloc &e)
   {
     std::cout << e.what();
     return 2;
   }
-  size_t size2 = 0;
-  get_other_symbols(res2, str, &size2);
-  for (size_t i = 0; res2[i]; i++)
-  {
-    std::cout << res2[i];
-  }
+  get_other_symbols(res2, str, size);
+  std::cout << res2 << '\n';
   delete[] res2;
   return 0;
 }

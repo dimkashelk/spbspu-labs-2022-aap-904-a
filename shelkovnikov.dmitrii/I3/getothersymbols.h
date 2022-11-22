801608f3bcedd7474486b7acdd6a640ce8346df3
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-22 17:56:09 +0300

    update#2

--- shelkovnikov.dmitrii/I3/getothersymbols.h
+++ shelkovnikov.dmitrii/I3/getothersymbols.h
@@ -1,5 +1,5 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_GETOTHERSYMBOLS_H
 #define SPBSPU_LABS_2022_AAP_904_A_GETOTHERSYMBOLS_H
 #include <cstddef>
-char *get_other_symbols(char *destination, char *source, size_t source_size);
+char *get_other_symbols(char *destination, char *source);
 #endif

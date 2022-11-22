b67e2a67356d6b4c1d53b8f0a052fbf2408d8791
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-22 17:43:58 +0300

    update getothersymbols

--- shelkovnikov.dmitrii/I3/getothersymbols.cpp
+++ shelkovnikov.dmitrii/I3/getothersymbols.cpp
@@ -1,6 +1,6 @@
 #include "getothersymbols.h"
 #include "stringbuilder.h"
-char *get_other_symbols(char *destination, char *source, size_t *res_size)
+char *get_other_symbols(char *destination, char *source, size_t source_size)
 {
   int alphabet[26];
   for (size_t i = 0; i < 26; i++)
@@ -19,22 +19,20 @@ char *get_other_symbols(char *destination, char *source, size_t *res_size)
     }
   }
   size_t j = 0;
-  StringBuilder stringBuilder;
+  char *str = new char[27];
   for (size_t i = 0; i < 26; i++)
   {
     if (!alphabet[i])
     {
-      stringBuilder.add_char(static_cast<char>(static_cast<int>('a') + i));
+      str[j] = static_cast<char>(static_cast< size_t >('a') + i));
       j++;
     }
   }
-  stringBuilder.add_char('\0');
-  char *dop = stringBuilder.get_string();
-  for (size_t i = 0; dop[i]; i++)
+  str[j] = '\0';
+  for (size_t i = 0; i < j; i++)
   {
-    destination[i] = dop[i];
+    destination[i] = str[i];
   }
-  delete[] dop;
-  *res_size = stringBuilder.get_size();
+  delete[] str;
   return destination;
 }

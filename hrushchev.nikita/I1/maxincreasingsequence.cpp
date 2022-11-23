d9c06bba8890c90e0609e6bbfa410743245a4c54
Shuzamada <shuzamada@inbox.ru>
2022-11-23 14:35:06 +0300

    fix exception again

--- hrushchev.nikita/I1/maxincreasingsequence.cpp
+++ hrushchev.nikita/I1/maxincreasingsequence.cpp
@@ -10,20 +10,24 @@ void MaxIncreasingSequence::operator()(int cur_value)
   {
     if (cur_value > pre_value)
     {
+      if (cur_len_seqn == 0)
+      {
+        cur_len_seqn = 1;
+      }
       if (cur_len_seqn == max_unsigned_int)
       {
         throw std::overflow_error("Overflow!");
       }
-      else if (cur_len_seqn == 0)
+      else
       {
-        cur_len_seqn = 1;
+        cur_len_seqn++;
       }
-      cur_len_seqn++;
     }
     else if (cur_len_seqn > 0)
     {
       cur_len_seqn = 1;
     }
+
     if (cur_len_seqn > max_len_seqn)
     {
       max_len_seqn = cur_len_seqn;
--- hrushchev.nikita/I1/minelementcounter.cpp
+++ hrushchev.nikita/I1/minelementcounter.cpp
@@ -9,14 +9,14 @@ void MinElementCounter::operator()(int cur_value)
   {
     if (min_value == cur_value)
     {
-      if (count_min == max_unsigned_int)
-      {
-        throw std::overflow_error("Overflow!");
-      }
-      else if (cur_value < min_value)
+      if (cur_value < min_value)
       {
         min_value = cur_value;
         count_min = 1;
+      }
+      else if (count_min == max_unsigned_int)
+      {
+        throw std::overflow_error("Overflow!");
       }
       else
       {

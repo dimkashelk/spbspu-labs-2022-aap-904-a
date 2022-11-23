0a098a31d683e56e6e91edb6aa4fb20ad36ad2d7
qucha <lecha.romanchuk@gmail.com>
2022-11-23 19:10:36 +0300

    fix cg?!

--- romanchuk.alexey/I1/check.cpp
+++ romanchuk.alexey/I1/check.cpp
@@ -4,30 +4,30 @@
 
 void values::operator()(int value)
 {
-    const int maxValue = std::numeric_limits< int >::max();
-    if (maxNum == maxValue)
-    {
-        std::cout << "overflow";
-    }
-    else if (value == predV)
-    {
-        nValue += 1;
-    }
-    else
-    {
-        maxNum = std::max(nValue, maxNum);
-        nValue = 1;
-    }
-    predV = value;
+  const int maxValue = std::numeric_limits< int >::max();
+  if (maxNum == maxValue)
+  {
+  std::cout << "overflow";
+  }
+  else if (value == predV)
+  {
+  nValue += 1;
+  }
+  else
+  {
+  maxNum = std::max(nValue, maxNum);
+  nValue = 1;
+  }
+  predV = value;
 }
 void localValuesMax::operator()(int value)
 {
-    const int maxLocalVal = std::numeric_limits< int >::max();
-    if (nValueL == maxLocalVal)
-    {
-        std::cout << "overflow";
-    }
-    nValueL += (predVL > value && predVL > predPredVL);
-    predPredVL = predVL;
-    predVL = value;
+  const int maxLocalVal = std::numeric_limits< int >::max();
+  if (nValueL == maxLocalVal)
+  {
+  std::cout << "overflow";
+  }
+  nValueL += (predVL > value && predVL > predPredVL);
+  predPredVL = predVL;
+  predVL = value;
 }
--- romanchuk.alexey/I1/structure.h
+++ romanchuk.alexey/I1/structure.h
@@ -2,16 +2,16 @@
 #define structure_h
 struct values
 {
-	int predV = 0;
-	int nValue = 0;
-	int maxNum = 0;
-	void operator()(int value);
+  int predV = 0;
+  int nValue = 0;
+  int maxNum = 0;
+  void operator()(int value);
 };
 struct localValuesMax
 {
-	int predVL = 0;
-	int predPredVL = 0;
-	int nValueL = 0;
-	void operator()(int currentValue);
+  int predVL = 0;
+  int predPredVL = 0;
+  int nValueL = 0;
+  void operator()(int currentValue);
 };
 #endif

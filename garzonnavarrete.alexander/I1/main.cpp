b057f40b9b0246a8380e6459cfb7286744a573d7
alexg_29 <alexg29@alexgar29.localdomain>
2022-11-24 21:34:58 +0300

    Lab 1 completed!

---
+++ garzonnavarrete.alexander/I1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+int main()
+{
+  int value = 0;
+  int predvalue = 0;
+  int count = 0;
+  int seqcount = 1;
+  int maxseq = 0;
+  std::cin >> value;
+  while (value != 0) {
+    predvalue = value;
+    std::cin >> value;
+      if (value == 0) {
+      if (seqcount > maxseq) {
+        maxseq = seqcount;
+      }
+      break;
+    }
+    if (predvalue < value) {
+      count++;
+    }
+    if (predvalue > value) {
+      seqcount++;
+    } else {
+      if (seqcount > maxseq) {
+        maxseq = seqcount;
+      }
+      seqcount = 1;
+    }
+  }
+  std::cout << maxseq << " " << count << "\n";
+}

9fc11b4afe6edf281a8091424a1dd44c0d04dee5
Aristarkhov Rostislav <aristarhov.ro@edu.spbstu.ru>
2022-11-25 00:31:19 +0300

    CG

--- aristarkhov.rostislav/I1/CountLarge.cpp
+++ aristarkhov.rostislav/I1/CountLarge.cpp
@@ -2,16 +2,16 @@
 #include <iostream>
 #include <limits>
 
-void  CountLarg::operator()(int curr_val)
+void CountLarg::operator()(int curr_val)
 {
 	if (curr_val > larg_val)
 	{
 		larg_val = curr_val;
 		count_amount = 1;
 	}
-	if (curr_val == larg_val)
+	else if (curr_val == larg_val)
 	{
-		count_amount ++;
+		count_amount++;
 	}
 	if (count_amount == std::numeric_limits<int>::max())
 	{

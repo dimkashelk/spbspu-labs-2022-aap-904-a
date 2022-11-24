17fe6c016f0826fa9c81457a58cdc7cb132584a6
alexg_29 <alexander.gar2901@gmail.com>
2022-11-24 23:55:19 +0300

    garzon.alexander/I1

--- garzonnavarrete.alexander/I1/main.cpp
+++ garzonnavarrete.alexander/I1/main.cpp
@@ -3,14 +3,14 @@ int main()
 {
   int value = 0;
   int predvalue = 0;
+  int maxseq = 0;
   int count = 0;
   int seqcount = 1;
-  int maxseq = 0;
   std::cin >> value;
   while (value != 0) {
     predvalue = value;
     std::cin >> value;
-      if (value == 0) {
+    if (value == 0) {
       if (seqcount > maxseq) {
         maxseq = seqcount;
       }

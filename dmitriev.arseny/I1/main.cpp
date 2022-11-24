470f5d601f41c49c685d94e3754d7f62ecd7e603
Dmitriev.Arseny <abstractlevl@gmail.com>
2022-11-24 21:26:17 +0300

    red

--- dmitriev.arseny/I1/main.cpp
+++ dmitriev.arseny/I1/main.cpp
@@ -4,40 +4,38 @@
 
 int main()
 {
-    int p2 = 0, p1 = 0;
-    MaxMono maxMono;
-    DivNoRemainder divNoRemainder;
+  int p2 = 0, p1 = 0;
+  MaxMono maxMono;
+  DivNoRemainder divNoRemainder;
 
-    do
+  do
+  {
+    p1 = p2;
+    std::cin >> p2;
+    if (!std::cin)
     {
-        p1 = p2;
-        std::cin >> p2;
-        if (!std::cin)
-        {
-            std::cout << "Inp error" << "\n";
-            return 1;
-        }
-        if (p2 == 0)
-        {
-            break;
-        }
-        try
-        {
-          maxMono(p1, p2);
-          divNoRemainder(p1, p2);
-        }
-        catch (const std::overflow_error& e)
-        {
-            std::cout << "Error: ";
-            std::cout << e.what() << "\n";
-            return 1;
-        }
+      std::cout << "Inp error" << "\n";
+      return 1;
     }
-    while (std::cin && p2);
+    if (p2 == 0)
+    {
+      break;
+    }
+    try
+    {
+      maxMono(p1, p2);
+      divNoRemainder(p1, p2);
+    }
+    catch (const std::overflow_error& e)
+    {
+      std::cout << "Error: ";
+      std::cout << e.what() << "\n";
+      return 1;
+    }
+  } while (std::cin && p2);
 
-    std::cout << "max mono increace - " << maxMono.quantity << "\n";
-    std::cout << "without remainder divisible by the preceding - " 
-              << divNoRemainder.quantity << "\n";
+  std::cout << "max mono increace - " << maxMono.quantity << "\n";
+  std::cout << "without remainder divisible by the preceding - " << divNoRemainder.quantity << "\n";
 
-    return 0;
+  return 0;
 }

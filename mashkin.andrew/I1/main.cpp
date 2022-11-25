03d08ec7d8ff86223d8e7c4d8df9b88e195bc107
Машкин Андрей <beedzibood@gmail.com>
2022-11-25 23:20:11 +0300

    don't check

--- mashkin.andrew/I1/main.cpp
+++ mashkin.andrew/I1/main.cpp
@@ -5,6 +5,13 @@ int main()
   int value = 0;
   int before_value = 0;
   int count = 0;
+  int countaftermax = 0
   std::cin >> value;
+  do
+  {
+    before_value = value;
+    std::cin >> value;
+  }
+  while (value && std::cin)
   return 0;
 }

f81e527837f2510abe373a279f4c73b6ab535cbd
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-26 16:11:27 +0300

    add input from file

--- shelkovnikov.dmitrii/I4/main.cpp
+++ shelkovnikov.dmitrii/I4/main.cpp
@@ -14,6 +14,32 @@ int main(int argc, char *argv[])
     std::cout << "Pls check parameters";
     return 1;
   }
-
+  size_t n = 0;
+  size_t m = 0;
+  std::ifstream in(argv[2]);
+  in >> n >> m;
+  if (argv[1] == 1)
+  {
+    int arr[n][m];
+    for (size_t i = 0; i < n; i++)
+    {
+      for (size_t j = 0; j < m; j++)
+      {
+        in >> arr[i][j];
+      }
+    }
+  }
+  else
+  {
+    int *arr = new int*[n];
+    for (size_t i = 0; i < n; i++)
+    {
+      arr[i] = new int[m];
+      for (int j = 0; j < m; j++)
+      {
+        in >> arr[i][j];
+      }
+    }
+  }
   return 0;
 }

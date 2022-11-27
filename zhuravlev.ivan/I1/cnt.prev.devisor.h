c087550ff318c33bb611e3bf896b708e001392d0
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-11-27 18:36:03 +0300

    (I1 try)

---
+++ zhuravlev.ivan/I1/cnt.prev.devisor.h
@@ -0,0 +1,12 @@
+#ifndef CNT_PREV_DEVISOR_H
+#define CNT_PREV_DEVISOR_H
+
+struct CntDevPrev
+{
+	void operator()(int value, int previous_value);
+
+	unsigned int cnt_devisor_counter;
+	int devisor_func_value;
+	int devisor_func_previous_value;
+};
+#endif
---
+++ zhuravlev.ivan/I1/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "max.seq.cnt.h"
+#include "cnt.prev.devisor.h"
+
+int main()
+{
+    MaxSeqRow max_sr{1};
+    CntDevPrev cnt_dev{0};
+    int value = 0;
+    int previous_val = 0;
+
+    do
+    {
+        std::cin >> value;
+        try
+        {
+            max_sr(value, previous_val);
+            cnt_dev(value, previous_val);
+        }
+        catch (const std::overflow_error& e)
+        {
+            std::cout << e.what() << "\n";
+            return 1;
+        }
+        previous_val = value;
+
+    } while (value && std::cin);
+    {
+        if (!std::cin)
+        {
+            std::cout << "You've entered something wrong.....";
+            return 1;
+        }
+        std::cout << "Maximum equal elements in the row: " << max_sr.max_seq_row << "\n";
+        std::cout << "Number of elements which have good devisor: " << cnt_dev.cnt_devisor_counter << "\n";
+    }
+}

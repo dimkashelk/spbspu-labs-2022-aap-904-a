be15f550a310af550c58d848230b30486a529801
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-11-27 21:31:50 +0300

    (GG)

---
+++ zhuravlev.ivan/I1/max.seq.cnt.h
@@ -0,0 +1,13 @@
+#ifndef MAX_SEQ_CNT_H
+#define MAX_SEQ_CNT_H
+
+struct MaxSeqRow
+{
+  void operator()(int value, int previous_value);
+
+  int func_value;
+  int func_previous_value;
+  unsigned current_seq_row;
+  unsigned int max_seq_row;
+};
+#endif

d608939eacf0a14233ddb7211d9a805a95b74028
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-11-27 22:41:37 +0300

    (now it's going to pass the tests)

---
+++ zhuravlev.ivan/I1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "max.seq.cnt.h"
+#include "cnt.prev.devisor.h"
+#include <stdexcept>
+#include <limits>
+
+int main()
+{
+  MaxSeqRow max_sr{1};
+  CntDevPrev cnt_dev{0};
+  int value = 0;
+
+  do
+  {
+     std::cin >> value;
+     try
+     {
+       max_sr(value);
+       cnt_dev(value);
+     }
+     catch (const std::overflow_error& e)
+     {
+       std::cout << e.what() << "\n";
+       return 1;
+     }
+
+   }
+   while (value && std::cin);
+   {
+     if (!std::cin)
+     {
+       std::cout << "You've entered something wrong.....";
+       return 2;
+     }
+     std::cout << "Maximum equal elements in the row: " << max_sr.max_seq_row << "\n";
+     std::cout << "Number of elements which have good devisor: " << cnt_dev.cnt_devisor_counter << "\n";
+     return 0;
+   }
+}
---
+++ zhuravlev.ivan/I1/max.seq.cnt.h
@@ -0,0 +1,13 @@
+#ifndef MAX_SEQ_CNT_H
+#define MAX_SEQ_CNT_H
+
+struct MaxSeqRow
+{
+  void operator()(int value);
+
+  int func_value;
+  int func_previous_value;
+  unsigned int current_seq_row;
+  unsigned int max_seq_row;
+};
+#endif

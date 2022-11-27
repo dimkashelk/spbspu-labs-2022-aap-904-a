8b5262267ea3fb78a8585c361b9d1f34475f1d98
yaroslava <aroslavakotova@gmail.com>
2022-11-27 14:32:22 +0300

    1 lab

--- kotova.yaroslava/I1/CounterElAfterMax.cpp
+++ kotova.yaroslava/I1/CounterElAfterMax.cpp
@@ -7,21 +7,21 @@ void CounterElAfterMax::operator()(int curr)
   const int max_int = std::numeric_limits< int >::max();
   if (curr < maxchislo)
   {
-    if (count >= max_int)
-	{
-      throw std::overflow_error("Overflow.");
-    }
-    else
-	{
-      count++;
-    }
+     if (count >= max_int)
+	 {
+        throw std::overflow_error("Overflow.");
+     }
+     else
+	 {
+        count++;
+     }
   }
   else
-  {
-    maxchislo = curr;
-    if (maxchislo == curr)
-    {
-      count = 0;
-    }
+   {
+     maxchislo = curr;
+     if (maxchislo == curr)
+     {
+       count = 0;
+     }
   }
 }
--- kotova.yaroslava/I1/main.cpp
+++ kotova.yaroslava/I1/main.cpp
@@ -14,22 +14,22 @@ int main()
     std::cin >> curr;
     if (std::cin)
 	{
-      try
-	  {
-        el_more_th_prev(curr);
-        cnt_el_af_max(curr);
-      }
-      catch (const std::overflow_error& e)
-	  {
-        std::cout << e.what();
-        return 2;
-      }
+       try
+	   {
+         el_more_th_prev(curr);
+         cnt_el_af_max(curr);
+       }
+       catch (const std::overflow_error& e)
+	   {
+         std::cout << e.what();
+         return 2;
+       }
     }
   } while (curr && std::cin);
   if (!std::cin)
   {
-    std::cout << "Error.";
-    return 1;
+     std::cout << "Error.";
+     return 1;
   }
   std::cout << el_more_th_prev.count - 1 << '\n';
   std::cout << cnt_el_af_max.count<< '\n';

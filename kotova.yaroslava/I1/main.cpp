4e1038392c9654d6c0389ad52c3b341ca81c2520
yaroslava <aroslavakotova@gmail.com>
2022-11-27 03:39:14 +0300

    1 lab

--- kotova.yaroslava/I1/main.cpp
+++ kotova.yaroslava/I1/main.cpp
@@ -1,36 +1,31 @@
+#include "ElMoreThanPrev.h"
+#include "CounterElAfterMax.h"
 #include <iostream>
+#include <limits>
 #include <stdexcept>
-#include "sequence1.h"
-#include "elementsaftermax.h"
-int main()
-{
-  constexpr unsigned int min_int = std::numeric_limits< int >::min();
-  ElementsAfterMax{0,min_int};
-  int curr = 0;
-  ElementsMoreThanPrev(curr);
-  do
-  {
-    std::cin >> curr;
-    if (!std::cin)
-    {
-       std::cout << "Error";
-       return 1;
-    }
-    if (std::cin)
-    {
-       try
-       {
-          ElementsMoreThanPrev(curr);
-          ElementsAfterMax(curr);
-       }
-      catch (const std::overflow_error& err)
-      {
-         std::cout << err.what();
-         return 2;
-      }
-    }
-  } while (curr && std::cin);
-  std::cout << ElementsMoreThanPrev.cnt << std::endl;
-  std::cout << ElementsAfterMax.cnt << std::endl;
-  return 0;
+
+int main() {
+	int curr = 0;
+	ElMoreThanPrev el_more_th_prev{ curr };
+	CounterElAfterMax cnt_el_af_max{ curr };
+	do {
+		std::cin >> curr;
+		if (std::cin) {
+			try {
+				el_more_th_prev(curr);
+				cnt_el_af_max(curr);
+
+			}
+			catch (const std::overflow_error& e) {
+				std::cout << e.what();
+				return 2;
+			}
+		}
+	} while (curr && std::cin);
+	if (!std::cin) {
+		std::cout << "Error.";
+		return 1;
+	}
+	std::cout << el_more_th_prev.count - 1 << '\n';
+	std::cout << cnt_el_af_max.count<< '\n';
 }

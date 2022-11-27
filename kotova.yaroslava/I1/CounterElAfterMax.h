4a69b361e9843dc8408296ee3f3c6c5ce4813af2
yaroslava <aroslavakotova@gmail.com>
2022-11-27 19:04:09 +0300

    1lab

--- kotova.yaroslava/I1/CounterElAfterMax.h
+++
@@ -1,9 +0,0 @@
-#ifndef COUNTERELAFTERMAX_H
-#define COUNTERELAFTERMAX_H
-struct CounterElAfterMax
-{
-  int maxchislo = 0;
-  int unsigned count = 0;
-  void operator()(int curr);
-};
-#endif
--- kotova.yaroslava/I1/ElMoreThanPrev.h
+++
@@ -1,9 +0,0 @@
-#ifndef ELMORETHANPREV_H
-#define ELMORETHANPREV_H
-struct ElMoreThanPrev
-{
-  int prev;
-  int unsigned count = 0;
-  void operator()(int curr);
-};
-#endif

9a6cf5eaec347ac675461f529f92719842d3f9ef
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-29 00:16:06 +0300

    update parameters

--- shelkovnikov.dmitrii/I4/count_lines_with_unique_elements.h
+++ shelkovnikov.dmitrii/I4/count_lines_with_unique_elements.h
@@ -1,5 +1,5 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_COUNT_LINES_WITH_UNIQUE_ELEMENTS_H
 #define SPBSPU_LABS_2022_AAP_904_A_COUNT_LINES_WITH_UNIQUE_ELEMENTS_H
 #include <cstddef>
-size_t count_lines_with_unique_elements(int *arr, size_t n, size_t m);
+size_t count_lines_with_unique_elements(const int *arr, size_t n, size_t m);
 #endif

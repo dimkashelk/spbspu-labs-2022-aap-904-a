9b8605b8b73b4e436cab769bf9dd392b0f5facdc
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-11-29 02:14:44 +0300

    I2 lab

--- azheganova.faina/I2/shiftarrayleft.h
+++ azheganova.faina/I2/shiftarrayleft.h
@@ -1,5 +1,6 @@
 #ifndef SHIFTARRAYLEFT_H
 #define SHIFTARRAYLEFT_H
 #include <cstddef>
+
 size_t shiftarrayleft(int* arr, const size_t size);
 #endif
--- azheganova.faina/I2/transferofvariables.h
+++ azheganova.faina/I2/transferofvariables.h
@@ -1,5 +1,6 @@
 #ifndef TRANSFEROFVARIABLES_H
 #define TRANSFEROFVARIABLES_H
 #include <cstddef>
+
 size_t transferofvariables(int* arr, const size_t size);
 #endif

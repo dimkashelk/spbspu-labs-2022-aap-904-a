70639972d670b244b175b1ebcd15345f4fcab9b0
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-29 00:15:49 +0300

    create minimum_sums_of_diagonals.cpp minimum_sums_of_diagonals.h

---
+++ shelkovnikov.dmitrii/I4/minimum_sums_of_diagonals.cpp
@@ -0,0 +1,36 @@
+#include "minimum_sums_of_diagonals.h"
+size_t minimum_sums_of_diagonals(const int *arr, size_t n, size_t m)
+{
+  int minimum = arr[m - 1];
+  size_t size = 0;
+  if (n > m)
+  {
+    size = n;
+  }
+  else
+  {
+    size = m;
+  }
+  for (size_t i = 0; i < size; i++)
+  {
+    int sum_of_diagonal_down = 0;
+    int sum_of_diagonal_up = 0;
+    for (size_t j = i; j < size; j++)
+    {
+      sum_of_diagonal_down += arr[j * size + (j - i)];
+    }
+    for (size_t j = 0; j < size - i; j++)
+    {
+      sum_of_diagonal_up += arr[(j + i) * size + j];
+    }
+    if (minimum > sum_of_diagonal_up)
+    {
+      minimum = sum_of_diagonal_up;
+    }
+    if (minimum > sum_of_diagonal_down)
+    {
+      minimum = sum_of_diagonal_down;
+    }
+  }
+  return minimum;
+}

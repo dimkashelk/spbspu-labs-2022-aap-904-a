489eb64d2c4292060fa4c5005cb28c455a219c84
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2022-11-29 19:12:07 +0300

    I1 complete

--- danilenko.ekaterina/I1/QuantityLocalMin.h
+++ danilenko.ekaterina/I1/QuantityLocalMin.h
@@ -1,13 +1,10 @@
 #ifndef QUANTITYLOCALMIN_H
 #define QUANTITYLOCALMIN_H
 
-class QuantityLocalMin
+struct QuantityLocalMin
 {
-public:
   unsigned int local_min = 0;
   void operator()(const int current_val);
-
-private:
   int prev_val = 0;
   int before_prev_val = 0;
 };
--- danilenko.ekaterina/I1/QuantitySignChange.h
+++ danilenko.ekaterina/I1/QuantitySignChange.h
@@ -1,13 +1,10 @@
 #ifndef QUANTITYSIGNCHANGE_H
 #define QUANTITYSIGNCHANGE_H
 
-class QuantitySignChange
+struct QuantitySignChange
 {
-public:
   unsigned int sign_change = 0;
   void operator()(const int current_val);
-
-private:
   int prev_val = 0;
   int before_prev_val = 0;
 };

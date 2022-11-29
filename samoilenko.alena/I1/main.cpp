7b791910fc1d836b950ae055c4c7bbcc28118e5a
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2022-11-29 17:53:03 +0300

    Fixed lab

--- samoilenko.alena/I1/main.cpp
+++ samoilenko.alena/I1/main.cpp
@@ -1,30 +1,34 @@
-#include "LenghthSame.h"
-#include "LenghthDecr.h"
 #include <iostream>
+#include "LengthSame.h"
+#include "LengthDecr.h"
 
-int main() {
-    int maxLenghthSame = 0;
-    int lenghthSame = 1;
-    int maxLenghthDecr = 0;
-    int lenghthDecr = 1;
+int main()
+{
+    int maxLengthSame = 0;
+    int maxLengthDecr = 0;
+    int lengthSame = 1;
+    int lengthDecr = 1;
     int predV = 0;
     int value = 0;
 
-    do {
+    do
+    {
         std::cin >> value;
-        lenghthSame = lenghthSameF (predV, value, lenghthSame);
-        if (lenghthSame > maxLenghthSame) {
-            maxLenghthSame = lenghthSame;
+        lengthSame = lengthSameF(predV, value, lengthSame);
+        if (lengthSame > maxLengthSame)
+        {
+            maxLengthSame = lengthSame;
         }
 
-        lenghthDecr = lenghthDecrF (predV, value, lenghthDecr);
-        if (lenghthDecr > maxLenghthDecr) {
-            maxLenghthDecr = lenghthDecr;
+        lengthDecr = lengthDecrF (predV, value, lengthDecr);
+        if (lengthDecr > maxLengthDecr)
+        {
+            maxLengthDecr = lengthDecr;
         }
 
         predV = value;
     }
     while (value && std::cin);
-    std::cout << maxLenghthDecr << "\n";
-    std::cout << maxLenghthSame << "\n";
+    std::cout << maxLengthDecr << "\n";
+    std::cout << maxLengthSame << "\n";
 }

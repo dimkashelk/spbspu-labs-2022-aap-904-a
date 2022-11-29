869bef80213db8db4cea1f42beb66e927986b838
Aristarkhov Rostislav <aristarhov.ro@edu.spbstu.ru>
2022-11-29 15:26:09 +0300

    fix "limits" try 2

--- aristarkhov.rostislav/I1/CountLarge.cpp
+++ aristarkhov.rostislav/I1/CountLarge.cpp
@@ -4,6 +4,10 @@
 
 void CountLargest::operator()(int curr_val)
 {
+  if (count_amount == std::numeric_limits< unsigned >::max() && curr_val == largest_val)
+  {
+    throw std::overflow_error("Overflow");
+  }
   if (curr_val > largest_val)
   {
     largest_val = curr_val;
@@ -12,9 +16,5 @@ void CountLargest::operator()(int curr_val)
   else if (curr_val == largest_val)
   {
     count_amount++;
-  }
-  if (count_amount == std::numeric_limits< unsigned >::max())
-  {
-    throw std::overflow_error("Overflow"); //попытка исправить
   }
 }

ecabf027e34666f0292e455ebc8b55992360f55d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-01 00:28:10 +0300

    add is_digit

--- shelkovnikov.dmitrii/I5/recursion.cpp
+++ shelkovnikov.dmitrii/I5/recursion.cpp
@@ -3,6 +3,10 @@ bool is_sign(char number)
 {
   return number == '-' || number == '+';
 }
+bool is_digit(char number)
+{
+  return number >= '0' && number <= '9';
+}
 bool is_real_number(char *number, size_t size)
 {
 

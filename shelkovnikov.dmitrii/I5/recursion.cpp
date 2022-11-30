1f4f8a5390a7f89653034613ace91a06f8ba5a5f
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-30 17:16:40 +0300

    add mantissa method

--- shelkovnikov.dmitrii/I5/recursion.cpp
+++ shelkovnikov.dmitrii/I5/recursion.cpp
@@ -2,6 +2,7 @@
 bool sign(char *number);
 bool mantissa(char *number);
 bool order(char *number);
+bool is_digit(char digit);
 bool is_real_number(char *number)
 {
   return sign(number) && mantissa(number) && order(number);
@@ -9,4 +10,22 @@ bool is_real_number(char *number)
 bool sign(char* number)
 {
   return number[0] == '+' || number[0] == '-';
+}
+bool mantissa(char *number)
+{
+  bool res = true;
+  size_t count_point = 0;
+  for (size_t i = 1; number[i] && number[i] != 'E'; i++)
+  {
+    if (!is_digit(number[i]) && number[i] != '.' && count_point < 2)
+    {
+      res = false;
+      break;
+    }
+    else if (number[i] == '.')
+    {
+      count_point++;
+    }
+  }
+  return res;
 }

9e41708cfb9732bf54f9caf882fd420cfc2fd631
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-11-30 20:33:21 +0300

    again fixed I2

--- azheganova.faina/I2/main.cpp
+++ azheganova.faina/I2/main.cpp
@@ -8,18 +8,26 @@ int main(int argc, char *argv[])
 {
   if (argc != 2)
   {
-    std::cerr << "Not correct amolunt of CML args\n";
+    std::cout << "Not correct amolunt of CML args\n";
   }
   int staticarray[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-  shiftarrayleft(staticarray, 10);
-  for (size_t i = 0; i < 10; i++)
+  try 
   {
-    std::cout << staticarray[i] << " ";
+    shiftarrayleft(staticarray, 10);
+    for (size_t i = 0; i < 10; i++)
+    {
+      std::cout << staticarray[i] << " ";
+    }
+    transferofvariables(staticarray, 10);
+    for (size_t newi = 0; newi < 10; newi++)
+    {
+      std::cout << staticarray[newi] << ' ';
+    }
   }
-  transferofvariables(staticarray, 10);
-  for (size_t newi = 0; newi < 10; newi++)
+  catch (const std::overflow_error &e)
   {
-    std::cout << staticarray[newi] << ' ';
+    std::cout << e.what();
+    return 2;
   }
   size_t n = 0;
   std::cout << "Dynamic array size:\n";
@@ -35,44 +43,67 @@ int main(int argc, char *argv[])
   {
     dynamicarray[i] = std::rand();
   }
-  shiftarrayleft(dynamicarray, n);
-  for (size_t i = 0; i < n; i++)
+  try
   {
-    std::cout << dynamicarray[i] << " ";
+    shiftarrayleft(dynamicarray, n);
+    for (size_t i = 0; i < n; i++)
+    {
+      std::cout << dynamicarray[i] << " ";
+    }
+    transferofvariables(dynamicarray, n);
+    for (size_t newi = 0; newi < n; newi++)
+    {
+      std::cout << dynamicarray[newi] << ' ';
+    }
   }
-  transferofvariables(dynamicarray, n);
-  for (size_t newi = 0; newi < n; newi++)
+  catch(const std::exception& e)
   {
-    std::cout << dynamicarray[newi] << ' ';
+    std::cout << e.what();
+    delete[] dynamicarray;
+    return 2;
   }
   delete[] dynamicarray;
   size_t size = 0;
   std::ifstream file(argv[1]);
   if (!file.is_open())
   {
-    std::cerr << argv[1] << "file open error";
+    std::cout << "file open error";
     return 2;
   }
   file >> size;
+  if (!file)
+  {
+    std::cout << "file reading error";
+    return 2;
+  }
   int* arrInput = new int[size];
   for (size_t i=0; i < size; i++)
   {
     file >> arrInput[i];
     if (!file)
     {
-      std::cerr << "file reading error";
+      std::cout << "file reading error";
       return 2;
     }
   }
-  shiftarrayleft(arrInput, size);
-  for (size_t i = 0; i < size; i++)
+  try 
   {
-    std::cout << arrInput[i] << " ";
+    shiftarrayleft(arrInput, size);
+    for (size_t i = 0; i < size; i++)
+    {
+      std::cout << arrInput[i] << " ";
+    }
+    transferofvariables(arrInput, size);
+    for (size_t newi = 0; newi < size; newi++)
+    {
+      std::cout << arrInput[newi] << ' ';
+    }
   }
-  transferofvariables(arrInput, size);
-  for (size_t newi = 0; newi < size; newi++)
+  catch (const std::overflow_error &e)
   {
-    std::cout << arrInput[newi] << ' ';
+    std::cout << e.what();
+    delete[] arrInput;
+    return 2;
   }
   delete[] arrInput;
   return 0;

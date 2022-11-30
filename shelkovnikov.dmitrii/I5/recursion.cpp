00231db892c326b8a827f15d74ffbbc46e97a155
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-01 01:10:21 +0300

    add mantissa

--- shelkovnikov.dmitrii/I5/recursion.cpp
+++ shelkovnikov.dmitrii/I5/recursion.cpp
@@ -15,6 +15,44 @@ bool is_unsigned_integer(char *number, size_t start, size_t end)
   }
   return is_digit(number[0]) && is_unsigned_integer(number, ++start, end);
 }
+size_t index_point(char *number, size_t start, size_t end)
+{
+  if (start == end)
+  {
+    return start;
+  }
+  if (number[start] == '.')
+  {
+    return start;
+  }
+  return index_point(number, ++start, end);
+}
+size_t count_point(char *number, size_t start, size_t end)
+{
+  if (start == end)
+  {
+    return start;
+  }
+  if (number[start] == '.')
+  {
+    return 1 + index_point(number, ++start, end);;
+  }
+  return index_point(number, ++start, end);
+}
+bool mantissa(char *number, size_t start, size_t end)
+{
+  size_t count_of_point = count_point(number, start, end);
+  if (count_of_point > 1)
+  {
+    return false;
+  }
+  if (count_of_point == 0)
+  {
+    return is_unsigned_integer(number, start, end);
+  }
+  size_t index_of_point = index_point(number, start, end);
+  return is_unsigned_integer(number, start, index_of_point) && is_unsigned_integer(number, index_of_point + 1, end);
+}
 bool is_real_number(char *number, size_t size)
 {
 }

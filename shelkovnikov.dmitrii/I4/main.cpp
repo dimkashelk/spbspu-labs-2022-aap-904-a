6abf5ce4176772e66398f67bcb9a935e30304632
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-11-30 23:08:50 +0300

    update static array to const size

--- shelkovnikov.dmitrii/I4/main.cpp
+++ shelkovnikov.dmitrii/I4/main.cpp
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
     std::cout << "Pls check parameters";
     return 1;
   }
-  size_t n = 1;
-  size_t m = 1;
+  size_t n = 100;
+  size_t m = 100;
   std::ifstream in(argv[2]);
   if (!strcmp(argv[1], "1"))
   {

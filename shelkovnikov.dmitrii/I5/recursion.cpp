446848f6a860ca2ea343760b9a40bdf27523a946
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-01 00:43:08 +0300

    add is_unsigned_integer

--- shelkovnikov.dmitrii/I5/recursion.cpp
+++ shelkovnikov.dmitrii/I5/recursion.cpp
@@ -7,7 +7,14 @@ bool is_digit(char number)
 {
   return number >= '0' && number <= '9';
 }
+bool is_unsigned_integer(char *number, size_t start, size_t end)
+{
+  if (start == end)
+  {
+    return true;
+  }
+  return is_digit(number[0]) && is_unsigned_integer(number, ++start, end);
+}
 bool is_real_number(char *number, size_t size)
 {
-
 }

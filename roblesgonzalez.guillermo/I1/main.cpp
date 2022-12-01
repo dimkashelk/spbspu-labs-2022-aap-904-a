4c0f3689934feaf112c5cca9821166c2e2d70eaf
guillermo9559 <javier@LAPTOP-D83M1BKQ.localdomain>
2022-12-01 17:30:41 +0300

    First lab fixed

--- roblesgonzalez.guillermo/I1/main.cpp
+++ roblesgonzalez.guillermo/I1/main.cpp
@@ -3,28 +3,27 @@
 int main()
 {
   int n;
-  int numberafter = 0;
-  int currentnumber = 0;
-  int numberbefore = 0;
+  int nafter = 0;
+  int currentn = 0;
+  int nbefore = 0;
   int sum = 0;
-  double small = std::numeric_limits<double>::infinity();
+  double smalln = std::numeric_limits<double>::infinity();
   double possiblesmallest = std::numeric_limits<double>::infinity();
   int counter = 0;
-
   while(n != 0)
   {
    std::cin>>n;
-   numberbefore = currentnumber;
-   currentnumber = numberafter;
-   numberafter = n;
+   nbefore = currentn;
+   currentn = nafter;
+   nafter = n;
 
-   if(numberafter == 0)
+   if(nafter == 0)
    {
     continue;
    }
    else
     {
-     if((currentnumber < numberbefore) && (currentnumber > numberafter))
+     if((currentn < nbefore) && (currentn > nafter))
      {
       sum++;
      }
@@ -44,17 +43,17 @@ int main()
        }
       else
        {
-        small = possiblesmallest;
+        smalln = possiblesmallest;
         possiblesmallest = n;
        }
       }
       else
        {
-        small = possiblesmallest;
+        smalln = possiblesmallest;
        }
      }
    }
-  std::cout << "Amount of numbers that are smaller than the previous one but bigger than the last one:  "<<sum<<std::endl;
-  std::cout << "The smallest number (Excluding first and last number):  "<< small<<std::endl;
+  std::cout << "Amount of numbers that are smaller than the previous one but bigger:  "<<sum<<std::endl;
+  std::cout << "The smallest number (Excluding first and last):  "<< small<<std::endl;
   return 0;
 }

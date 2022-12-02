66d34899c7da61a95a5926c8adcda9fbc2398015
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-12-03 01:36:23 +0300

    fix

--- skarlygina.mariia/I2/shifted_array.cpp
+++ skarlygina.mariia/I2/shifted_array.cpp
@@ -4,7 +4,7 @@ int* makeShiftedArray(int* array, size_t size)
 {
   if (array && size)
   {
-    int flag = array[size-1];
+    int flag = array[size - 1];
     for (size_t i = size - 1; i >= 1; i--)
     {
       array[i] = array[i - 1];

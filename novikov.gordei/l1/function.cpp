b1ad37ee511854503d11d263b6ffb15b248a4291
Gordei <vokivon2142@mail.ru>
2022-12-03 23:53:58 +0300

    trouble with CG

--- novikov.gordei/l1/function.cpp
+++ novikov.gordei/l1/function.cpp
@@ -4,12 +4,12 @@
 
 bool isNumberValid(char* str, long &number)
 {
-	char *endptr;
-	number = strtol(str, &endptr, 10);
-	return (strlen(endptr) == 0);
+  char *endptr;
+  number = strtol(str, &endptr, 10);
+  return (strlen(endptr) == 0);
 }
 
 bool isPythagoreanTriples(long x, long y, long z)
 {
-	return (x * x + y * y == z * z);
+  return (x * x + y * y == z * z);
 }

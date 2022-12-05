b4e98dcd0781712ec99c2ea3b61b57f113c608ae
yaroslava <aroslavakotova@gmail.com>
2022-12-06 01:23:40 +0300

    2lab

--- kotova.yaroslava/I2/arrays.cpp
+++ kotova.yaroslava/I2/arrays.cpp
@@ -1,40 +1,42 @@
+#include <stdexcept>
 #include "arrays.h"
 
-size_t counterDivision(const int* array,const size_t size, unsigned int n) 
+size_t counterDivision(const int* array, const size_t size, unsigned int n)
 {
-	size_t cnt_div = 0;
-	for (size_t i = 0; i < size; i++)
-	{
-		if (array[i] % n == 0) 
-{
-			cnt_div++;
-		}
-	}
-	return cnt_div;
+  size_t cnt_div = 0;
+  for (size_t i = 0; i < size; i++)
+  {
+    if (array[i] % n == 0)
+    {
+      cnt_div++;
+    }
+  }
+  return cnt_div;
 }
 
 
-size_t checkRepeatEl(const int* array,const size_t size, unsigned int index_m, unsigned int index_n)
+size_t checkRepeatEl(const int* array, size_t size, unsigned int index_m, unsigned int index_n)
 {
-	size_t repeat_cnt = 1;
-	if (index_m > index_n)
-	{
-		int permt = index_m;
-		index_m = index_n;
-		index_n = index_m;
-	}
-	else
-	{
-		repeat_cnt = 0;
-	}
-	for (unsigned int i = index_m; i <= index_n; i++)
-		for (unsigned int j = index_m; j < index_n-1; j++)
-		{
-			if (array[i] == array[j] && i != j)
-			{
-				repeat_cnt++;
-				break;
-			}
-		}
-	return repeat_cnt;
+  size_t repeat_cnt = 1;
+  size_t max_of_index = std::max(index_m, index_n);
+  if (max_of_index > size)
+  {
+    throw std::invalid_argument("Errorrr...");
+  }
+  if (index_m == index_n)
+  {
+    repeat_cnt = 0;
+  }
+  for (unsigned int i = index_m; i <= index_n; i++)
+  {
+    for (unsigned int j = index_m; j < index_n - 1; j++)
+    {
+      if (array[i] == array[j] && i != j)
+      {
+        repeat_cnt++;
+        break;
+      }
+    }
+  }
+  return repeat_cnt;
 }
--- kotova.yaroslava/I2/main.cpp
+++ kotova.yaroslava/I2/main.cpp
@@ -4,97 +4,97 @@
 
 int main(int argc, char* argv[])
 {
-    const std::string INPUT_FILE = "lab2.txt";
-
-
-    if (argc != 2)
+  if (argc != 2)
+  {
+    std::cerr << "File is wrong\n";
+    return 2;
+  }
+//1
+  int firstArray[] = { 1, 2, 6, 12, 12, 12, 9, -5, 4, 4 };
+  unsigned int arr1_index_m = 1;
+  unsigned int arr1_index_n = 6;
+  unsigned int divider_n = 0;
+  try
+  {
+    std::cin >> divider_n;
+    std::cout << "Divider: " << divider_n << std::endl;
+    std::cout << "The number of fully divisible: " << counterDivision(firstArray, 10, divider_n) << std::endl;
+    std::cout << "The number of duplicate elements: " << checkRepeatEl(firstArray, 10, arr1_index_m, arr1_index_n) << std::endl;
+  }
+  catch (const std::invalid_argument& e)
+  {
+    std::cout << e.what();
+    return 2;
+  }
+//2
+  size_t size_arr2 = 0;
+  unsigned int arr2_index_m = 1;
+  unsigned int arr2_index_n = 6;
+  std::cin >> size_arr2;
+  if (!std::cin)
+  {
+    std::cerr << "Incorrect data\n";
+    return 1;
+  }
+  else
+  {
+    int* secondArray = new int[size_arr2];
+    std::srand(time(nullptr));
+    for (size_t i = 0; i < size_arr2; i++)
     {
-        std::cerr << "File is wrong\n";
-        return 2;
+      secondArray[i] = std::rand();
     }
-    //1
-    int firstArray[] = { 1, 2, 6, 12, 12, 12, 9, -5, 4, 4 };
-    unsigned int arr1_index_m = 1;
-    unsigned int arr1_index_n = 6;
-    unsigned int divider_n = 0;
-    try {
-        std::cin >> divider_n;
-        std::cout << "Divider: " << divider_n << std::endl;
-        std::cout << "The number of fully divisible: " << counterDivision(firstArray, 10, divider_n) << std::endl;
-        std::cout << "The number of duplicate elements: " << checkRepeatEl(firstArray, 10, arr1_index_m, arr1_index_n) << std::endl;
-    }
-    catch (const std::invalid_argument& e) {
-        std::cout << e.what();
-        return 2;
-    }
-    //2
-    size_t size_arr2 = 0;
-    unsigned int arr2_index_m = 1;
-    unsigned int arr2_index_n = 6;
-    std::cin >> size_arr2;
-    if (!std::cin) {
-        std::cerr << "Incorrect data\n";
-        return 1;
-    }
-    else
+    size_t cnt_div_arr2 = counterDivision(secondArray, size_arr2, divider_n);
+    size_t cnt_check_rep_el = checkRepeatEl(secondArray, size_arr2, arr2_index_m, arr2_index_n);
+    std::cout << cnt_div_arr2 << std::endl;
+    std::cout << cnt_check_rep_el << std::endl;
+    delete[] secondArray;
+  }
+//3
+  unsigned int arr3_index_m = 1, arr3_index_n = 6;
+  std::ifstream file_arr(argv[1]);
+  if (!file_arr)
+  {
+    std::cout << "file open with an error";
+    return 1;
+  }
+  size_t size_arr3 = 0;
+  file_arr >> size_arr3;
+  if (!file_arr)
+  {
+    std::cout << "error while file reading\n";
+    return 1;
+  }
+  if (size_arr3)
+  {
+    while (!file_arr.eof())
     {
-        int* secondArray = new int[size_arr2];
-        std::srand(time(nullptr));
-        for (size_t i = 0; i < size_arr2; i++) {
-            secondArray[i] = std::rand();
-        }
-        size_t cnt_div_arr2 = counterDivision(secondArray, size_arr2, divider_n);
-        size_t cnt_check_rep_el = checkRepeatEl(secondArray, size_arr2, arr2_index_m, arr2_index_n);
-        std::cout << cnt_div_arr2 << std::endl;
-        std::cout << cnt_check_rep_el << std::endl;
-        delete[] secondArray;
-    }
-
-    //3
-    unsigned int arr3_index_m = 1, arr3_index_n = 6;
-    std::ifstream file_arr(argv[1]);
-    if (!file_arr)
-    {
-        std::cout << "file open with an error";
-        return 1;
-    }
-    size_t size_arr3 = 0;
-    file_arr >> size_arr3;
-    if (!file_arr)
-    {
-        std::cout << "error while file reading\n";
-        return 1;
-    }
-    if (size_arr3)
-    {
-        while (!file_arr.eof())
+      int* thirdArray = new int[size_arr3];
+      for (size_t i = 0; i < size_arr3; i++)
+      {
+        file_arr >> thirdArray[i];
+        if (!file_arr)
         {
-            int* thirdArray = new int[size_arr3];
-            for (size_t i = 0; i < size_arr3; i++)
-            {
-                file_arr >> thirdArray[i];
-                if (!file_arr)
-                {
-                    std::cout << "file reading error";
-                    delete[] thirdArray;
-                    return 1;
-                }
-            }
-            try {
-                size_t cnt_div_arr3 = counterDivision(thirdArray, size_arr3, divider_n);
-                size_t cnt_check_rep_el3 = checkRepeatEl(thirdArray, size_arr3, arr3_index_m, arr3_index_n);
-                std::cout << cnt_div_arr3 << std::endl;
-                std::cout << cnt_check_rep_el3 << std::endl;
-            }
-            catch (const std::invalid_argument& e)
-            {
-                std::cout << e.what();
-                delete[] thirdArray;
-                return 1;
-            }
-            delete[] thirdArray;
+          std::cout << "file reading error";
+          delete[] thirdArray;
+          return 1;
         }
-        return 0;
+      }
+      try
+      {
+        size_t cnt_div_arr3 = counterDivision(thirdArray, size_arr3, divider_n);
+        size_t cnt_check_rep_el3 = checkRepeatEl(thirdArray, size_arr3, arr3_index_m, arr3_index_n);
+        std::cout << cnt_div_arr3 << std::endl;
+        std::cout << cnt_check_rep_el3 << std::endl;
+      }
+      catch (const std::invalid_argument& e)
+      {
+        std::cout << e.what();
+        delete[] thirdArray;
+        return 1;
+      }
+      delete[] thirdArray;
     }
+    return 0;
+  }
 }
-

8b473f8ea3dd2d9c819cd8a71158b80d9852ebce
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-12-05 14:44:22 +0300

    fixed nullptr

--- skarlygina.mariia/I2/main.cpp
+++ skarlygina.mariia/I2/main.cpp
@@ -9,8 +9,7 @@ int main(int argc, char* argv[])
 {
   int static_array[] = {5, 4, 3, 4, -6, 2, -1};
   std::cout << countArithmeticMean(static_array, 7) << " ";
-  int* static_array_ptr = nullptr;
-  static_array_ptr = makeShiftedArray(static_array, 7);
+  int* static_array_ptr = makeShiftedArray(static_array, 7);
   for (size_t i = 0; i < 7; i++)
   {
     std::cout << static_array_ptr[i] << " ";
@@ -32,8 +31,7 @@ int main(int argc, char* argv[])
       dynamic_array[i] = std::rand();
     }
     std::cout << countArithmeticMean(dynamic_array, dynamic_array_size) << " ";
-    int* dynamic_array_ptr = nullptr;
-    dynamic_array_ptr = makeShiftedArray(dynamic_array, dynamic_array_size);
+    int* dynamic_array_ptr = makeShiftedArray(dynamic_array, dynamic_array_size);
     for (size_t i = 0; i < dynamic_array_size; i++)
     {
       std::cout << dynamic_array_ptr[i] << " ";
@@ -86,8 +84,7 @@ int main(int argc, char* argv[])
     try
     {
       std::cout << countArithmeticMean(reading_array, reading_array_size) << " ";
-      int* reading_array_ptr = nullptr;
-      reading_array_ptr = makeShiftedArray(reading_array, reading_array_size);
+      int* reading_array_ptr = makeShiftedArray(reading_array, reading_array_size);
       for (size_t i = 0; i < reading_array_size; i++)
       {
         std::cout << reading_array_ptr[i] << " ";

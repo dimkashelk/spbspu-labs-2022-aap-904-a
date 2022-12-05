eabc853d52e4e54eb3e9252dfdfad87b38b8eac3
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-05 12:38:46 +0300

    update size of squad array

--- shelkovnikov.dmitrii/I4/minimum_sums_of_diagonals.cpp
+++ shelkovnikov.dmitrii/I4/minimum_sums_of_diagonals.cpp
@@ -9,11 +9,11 @@ int minimum_sums_of_diagonals(const int *arr, size_t n, size_t m)
   size_t size = 0;
   if (n > m)
   {
-    size = n;
+    size = m;
   }
   else
   {
-    size = m;
+    size = n;
   }
   for (size_t i = 0; i < size; i++)
   {

8aa471e461593e1403aca4456551a64a72c812e3
Chemodurovart <chemodurovart@gmail.com>
2022-12-06 11:36:54 +0300

    growing rows func add

--- chemodurov.artem/I4/arrays.cpp
+++ chemodurov.artem/I4/arrays.cpp
@@ -1,5 +1,23 @@
 #include "arrays.h"
 size_t chemodurov:: countGrowingRows(int * arr, size_t rows, size_t cols)
 {
-  //do brr
+  size_t counter = 0;
+  size_t number_of_item = 0;
+  for (size_t i = 0; i < rows; ++i)
+  {
+    sie_t counter_in_row = 0;
+    for (size_t j = 0; j < cols - 1; ++j)
+    {
+      number_of_item = i * cols + j;
+      if (arr[number_of_item] > arr[number_of_item + 1])
+      {
+        counter_in_row++;
+      }
+    }
+    if (counter_in_row == cols - 1)
+    {
+      counter++;
+    }
+  }
+  return counter;
 }

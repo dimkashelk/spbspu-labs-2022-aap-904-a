1e9a4ead3ef2deef51b16c17c93fc472948d636d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-07 00:49:59 +0300

    fixup!

--- shelkovnikov.dmitrii/I4/minimum_sums_of_diagonals.cpp
+++ shelkovnikov.dmitrii/I4/minimum_sums_of_diagonals.cpp
@@ -1,30 +1,30 @@
 #include "minimum_sums_of_diagonals.h"
 #include <algorithm>
 #include <stdexcept>
-int sum_of_diagonal_down(const int *arr, const size_t size)
-{
-  int sum = 0;
-  for (size_t i = 0; i < size; i++)
-  {
-    for (size_t j = i; j < size; j++)
-    {
-      sum += arr[j * size + (j - i)];
-    }
-  }
-  return sum;
-}
-int sum_of_diagonal_up(const int *arr, const size_t size)
-{
-  int sum = 0;
-  for (size_t i = 0; i < size; i++)
-  {
-    for (size_t j = i; j < size; j++)
-    {
-      sum += arr[(j + i) * size + j];
-    }
-  }
-  return sum;
-}
+//int sum_of_diagonal_down(const int *arr, const size_t size)
+//{
+//  int sum = 0;
+//  for (size_t i = 0; i < size; i++)
+//  {
+//    for (size_t j = i; j < size; j++)
+//    {
+//      sum += arr[j * size + (j - i)];
+//    }
+//  }
+//  return sum;
+//}
+//int sum_of_diagonal_up(const int *arr, const size_t size)
+//{
+//  int sum = 0;
+//  for (size_t i = 0; i < size; i++)
+//  {
+//    for (size_t j = i; j < size; j++)
+//    {
+//      sum += arr[(j + i) * size + j];
+//    }
+//  }
+//  return sum;
+//}
 int minimum_sums_of_diagonals(const int *arr, const size_t n, const size_t m)
 {
   size_t size = std::min(n, m);
@@ -35,8 +35,22 @@ int minimum_sums_of_diagonals(const int *arr, const size_t n, const size_t m)
   int minimum = arr[size - 1];
   for (size_t i = 0; i < size; i++)
   {
-    const int sum_down = sum_of_diagonal_down(arr, size);
-    const int sum_up = sum_of_diagonal_up(arr, size);
+    int sum_down = sum_of_diagonal_down(arr, size);
+    for (size_t i = 0; i < size; i++)
+    {
+      for (size_t j = i; j < size; j++)
+      {
+        sum_down += arr[j * size + (j - i)];
+      }
+    }
+    int sum_up = sum_of_diagonal_up(arr, size);
+    for (size_t i = 0; i < size; i++)
+    {
+      for (size_t j = i; j < size; j++)
+      {
+        sum_up += arr[(j + i) * size + j];
+      }
+    }
     const int min_sum = std::min(sum_down, sum_up);
     minimum = std::min(minimum, min_sum);
   }

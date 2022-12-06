e30ca3ea30d79801a52d7e0c227acc143f1dc235
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-06 21:49:15 +0300

    update functions

--- shelkovnikov.dmitrii/I4/count_lines_with_unique_elements.cpp
+++ shelkovnikov.dmitrii/I4/count_lines_with_unique_elements.cpp
@@ -1,21 +1,35 @@
 #include "count_lines_with_unique_elements.h"
+size_t count_contains_element(const int element, const int *arr, const size_t size)
+{
+  size_t count = 0;
+  for (size_t i = 0; i < size && count < 2; i++)
+  {
+    if (element == arr[i])
+    {
+      count++;
+    }
+  }
+  return count;
+}
+bool is_unique_line(const int *arr, const size_t size)
+{
+  size_t count = 0;
+  bool fl = true;
+  for (size_t j = 0; j < n && fl; j++)
+  {
+    if (count_contains_element(arr[j], arr[i * n], m) != 1)
+    {
+      fl = false;
+    }
+  }
+  return fl;
+}
 size_t count_lines_with_unique_elements(const int *arr, const size_t n, const size_t m)
 {
   size_t count = 0;
   for (size_t i = 0; i < n; i++)
   {
-    bool fl = true;
-    for (size_t j = 0; j < n && fl; j++)
-    {
-      for (size_t k = j + 1; k < m && fl; k++)
-      {
-        if (arr[i * n + j] == arr[i * n + k])
-        {
-          fl = false;
-        }
-      }
-    }
-    if (fl)
+    if (is_unique_line(arr + i * n, m))
     {
       count++;
     }

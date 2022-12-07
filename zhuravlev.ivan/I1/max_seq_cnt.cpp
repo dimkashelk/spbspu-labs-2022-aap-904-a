dc6bef68873d92ab44962becebe5ea2f676540b8
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-07 20:12:57 +0000

    chaged max_seq_cnt

--- zhuravlev.ivan/I1/max_seq_cnt.cpp
+++ zhuravlev.ivan/I1/max_seq_cnt.cpp
@@ -8,13 +8,17 @@ void MaxSeqRow::operator()(int value)
   func_previous_value = func_value;
   if (func_value && func_previous_value)
   {
-    if (current_seq_row > max_seq_row)
+    if (func_previous_value == func_value)
     {
-      max_seq_row = current_seq_row;
-    }
-    else
-    {
-      current_seq_row = 0;
+      current_seq_row++;
+      if (current_seq_row > max_seq_row)
+      {
+        max_seq_row = current_seq_row;
+      }
+      else
+      {
+        current_seq_row = 1;
+      }
     }
   }
 }

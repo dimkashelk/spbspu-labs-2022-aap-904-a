474a032936904138d4fec726f4d6910a3e5c27ba
Lagushka1984 <nikolai.turkin.main@gmail.com>
2022-12-07 23:13:49 +0300

    fix stringTransform.h

--- turkin.nikolai/I3/stringTransform.h
+++ turkin.nikolai/I3/stringTransform.h
@@ -1,37 +1,10 @@
-#include "stringTransform.h"
-#include <cctype>
-
-char * turkin::deleteNumbers(char * destination, const char * source, size_t size)
+#ifndef STRINGTRANSFORM_H
+#define STRINGTRANSFORM_H
+#include <cstddef>
+#include "cString.h"
+namespace turkin
 {
-  size_t q = 0;
-  for (size_t i = 0; i < size; i++)
-  {
-    if (!std::isdigit(source[i]))
-    {
-      destination[q++] = source[i];
-    }
-  }
-  destination[q] = '\0';
-  return destination;
+  char * deleteNumbers(char * destination, const char * source, size_t size);
+  bool isRepeat(const turkin::String & string);
 }
-
-bool turkin::isRepeat(const turkin::String & string)
-{
-  size_t amount;
-  for (size_t i = 0; i < string.size; i++)
-  {
-    amount = 0;
-    for (size_t k = 0; k < string.size; k++)
-    {
-      if (string.data[i] == string.data[k])
-      {
-        amount++;
-      }
-    }
-    if (amount > 1)
-    {
-      return true;
-    }
-  }
-  return false;
-}
+#endif

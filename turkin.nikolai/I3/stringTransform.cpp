2ce7320b5fc4a7cbf9aeed9e2e7da5818f0601d2
Lagushka1984 <nikolai.turkin.main@gmail.com>
2022-12-07 22:43:47 +0300

    rework lab structure

---
+++ turkin.nikolai/I3/stringTransform.cpp
@@ -0,0 +1,37 @@
+#include "stringTransform.h"
+#include <cctype>
+
+char * turkin::deleteNumbers(char * destination, const char * source)
+{
+  size_t q = 0;
+  for (size_t i = 0; i < source[i]; i++)
+  {
+    if (!std::isdigit(source[i]))
+    {
+      destination[q++] = source[i];
+    }
+  }
+  destination[q] = '\0';
+  return destination;
+}
+
+bool turkin::isRepeat(const turkin::String & string)
+{
+  size_t amount;
+  for (size_t i = 0; i < string.size; i++)
+  {
+    amount = 0;
+    for (size_t k = 0; k < string.size; k++)
+    {
+      if (string.data[i] == string.data[k])
+      {
+        amount++;
+      }
+    }
+    if (amount > 1)
+    {
+      return true;
+    }
+  }
+  return false;
+}
---
+++ turkin.nikolai/I3/stringTransform.h
@@ -0,0 +1,9 @@
+#ifndef STRINGTRANSFORM_H
+#define STRINGTRANSFORM_H
+#include "cString.h"
+namespace turkin
+{
+  char * deleteNumbers(char * destination, const char * source);
+  bool isRepeat(const turkin::String & string);
+}
+#endif

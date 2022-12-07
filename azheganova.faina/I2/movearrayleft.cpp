a00fd31311bf814aa27c7cf4fbb60d52f78eba3e
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-07 22:34:50 +0300

    fixed function

--- azheganova.faina/I2/movearrayleft.cpp
+++ azheganova.faina/I2/movearrayleft.cpp
@@ -2,18 +2,16 @@
 
 void moveArrayLeft(int* arr, const size_t size, const int shift)
 {
-  int howmuchshiftnow = 0;
-  while (howmuchshiftnow++ < shift)
-  {
-  int position1 = 0;
-  int position2 = 0;
-  position1 = arr[0];
-  position2 = arr[1];
-  for (size_t i = 0; i < size - 1; i++)
-  {
-    arr[i] = arr[i + 1];
-  }
-  arr[size - 1] = position1;
-  arr[size] = position2;
-  }
+    int howmuchshiftnow = 0;
+    while (howmuchshiftnow++ < shift)
+    {
+        int firstposition = arr[0];
+        int secondposition = arr[1];
+        for (size_t i = 0; i < size - 1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        arr[size - 1] = firstposition;
+        arr[size] = secondposition;
+    }
 }

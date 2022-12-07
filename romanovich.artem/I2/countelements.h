d1a78e37e6b08678c19fc718e5c3681b1fe656f5
Артем Романович <artrom170@gmail.com>
2022-12-07 19:32:33 +0000

    Update countelements.h

--- romanovich.artem/I2/countelements.h
+++ romanovich.artem/I2/countelements.h
@@ -2,6 +2,5 @@
 #define I2_COUNTELEMENTS_H
 #include <cstddef>
 size_t countNegativeRightFromMax(const int *array, size_t size);
-size_t
-countRepetitiveInRange(const int *array, size_t size, size_t beginIndex, size_t endIndex);
+size_t countRepetitiveInRange(const int *array, size_t size, size_t beginIndex, size_t endIndex);
 #endif

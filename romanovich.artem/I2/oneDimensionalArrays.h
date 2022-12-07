d6ad50ed3395d5bcc3634192796a9f296d1eb244
Артем Романович <artrom170@gmail.com>
2022-12-07 19:26:22 +0000

    Delete oneDimensionalArrays.h

--- romanovich.artem/I2/oneDimensionalArrays.h
+++
@@ -1,6 +0,0 @@
-#ifndef I2_ONEDIMENSIONALARRAYS_H
-#define I2_ONEDIMENSIONALARRAYS_H
-#include <cstddef>
-size_t countNegativeRightFromMax(const int *array, size_t size);
-size_t countRepetitiveInRange(const int *array, const size_t size, const size_t beginIndex, const size_t endIndex);
-#endif

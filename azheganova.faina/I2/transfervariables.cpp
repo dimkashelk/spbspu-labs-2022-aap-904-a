e634065ad5af7cc89efc1e3785c094eb035c5ead
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-08 16:45:35 +0300

    I2 laba

--- azheganova.faina/I2/transfervariables.cpp
+++ azheganova.faina/I2/transfervariables.cpp
@@ -2,7 +2,7 @@
 
 void transferVariables(int *arr, const size_t size, const size_t M, const size_t N)
 {
-  std::size_t newi = size - 1;
+  size_t newi = size - 1;
   if (size > N)
   {
     for (size_t i = N; i >= M ; i--)

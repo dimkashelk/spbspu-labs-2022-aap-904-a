e10b159b180e3f580e6ac541d9bad8a8f8ae7ca2
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-09 18:53:27 +0000

    resolveed max_seq_cnt

--- zhuravlev.ivan/I1/max_seq_cnt.cpp
+++ zhuravlev.ivan/I1/max_seq_cnt.cpp
@@ -4,11 +4,16 @@
 
 void MaxSeqRow::operator()(int value)
 {
+  const unsigned int max_unsigned_int = std::numeric_limits< unsigned int >::max();
   func_value = value;
   func_previous_value = func_value;
   if (func_value && func_previous_value)
   {
-    if (func_previous_value == func_value)
+    if (func_previous_value == func_value && current_seq_row > max_unsigned_int - 1 )
+    {
+      throw std::overflow_error("Overflow!");
+    }
+    else if (func_previous_value == func_value)
     {
       current_seq_row++;
       if (current_seq_row > max_seq_row)
@@ -22,3 +27,4 @@ void MaxSeqRow::operator()(int value)
     }
   }
 }
+

1c99d1677531c018d73dbfb7cf69d2089deb032b
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-09 23:50:22 +0300

    dynArr in stack

--- odintsov.kirill/I2/main.cpp
+++ odintsov.kirill/I2/main.cpp
@@ -1,47 +1,45 @@
-#include "array.hpp"
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
 #include <time.h>
+#include "array.hpp"
 
 int main(int argc, char* argv[])
 {
-  if (argc > 2) {
-    std::cout << "Error: too many arguments\n";
-    return 3;
-  } else if (argc == 1) {
-    std::cout << "Error: filename not given\n";
+  if (argc != 2) {
+    std::cout << "Error: Invalid arguments\n";
     return 3;
   }
-  srand(time(NULL));
-  const size_t staticArrSize = 8;
-  const int staticArr[staticArrSize] = {1, -234, -253, 12323, 2353, -2341, 42342, -12312};
+  std::srand(std::time(NULL));
+  constexpr size_t staticArrSize = 8;
+  constexpr int staticArr[staticArrSize] = {1, -234, -253, 12323, 2353, -2341, 42342, -12312};
   int lowBound = -10000;
   int highBound = 10000;
   try {
     std::cout << odintsov::findMax(staticArr, staticArrSize, lowBound, highBound) << '\n';
-  } catch(const std::logic_error& err) {
+  } catch (const std::logic_error& err) {
     std::cout << err.what() << '\n';
   }
   std::cout << odintsov::getAvgEvenIndices(staticArr, staticArrSize) << '\n';
-  size_t dynArrSize = 0;
-  std::cin >> dynArrSize;
-  odintsov::Array* dynArr = new odintsov::Array(dynArrSize);
-  try {
-    for (size_t i = 0; i < dynArrSize; i++) {
-      dynArr->append(rand() % highBound);
+  {
+    size_t dynArrSize = 0;
+    std::cin >> dynArrSize;
+    odintsov::Array dynArr(dynArrSize);
+    try {
+      for (size_t i = 0; i < dynArrSize; i++) {
+        dynArr.append(std::rand() % highBound);
+      }
+    } catch (const std::length_error& err) {
+      std::cout << err.what() << '\n';
+      return 2;
     }
-  } catch (const std::length_error& err) {
-    std::cout << err.what() << '\n';
-    return 2;
+    try {
+      std::cout << odintsov::findMax(dynArr.data, dynArr.size, lowBound, highBound) << '\n';
+    } catch (const std::logic_error& err) {
+      std::cout << err.what() << '\n';
+    }
+    std::cout << odintsov::getAvgEvenIndices(dynArr.data, dynArr.size) << '\n';
   }
-  try {
-    std::cout << odintsov::findMax(dynArr->data, dynArr->size, lowBound, highBound) << '\n';
-  } catch(const std::logic_error& err) {
-    std::cout << err.what() << '\n';
-  }
-  std::cout << odintsov::getAvgEvenIndices(dynArr->data, dynArr->size) << '\n';
-  delete dynArr;
   std::ifstream file(argv[1]);
   if (!file.is_open()) {
     std::cout << "Error: file could not be opened\n";
@@ -54,7 +52,7 @@ int main(int argc, char* argv[])
     return 3;
   }
   while (!file.eof()) {
-    odintsov::Array fileArr = odintsov::Array(fileArrSize);
+    odintsov::Array fileArr(fileArrSize);
     for (size_t i = 0; i < fileArrSize; i++) {
       int val = 0;
       file >> val;
@@ -66,7 +64,7 @@ int main(int argc, char* argv[])
     }
     try {
       std::cout << odintsov::findMax(fileArr.data, fileArr.size, lowBound, highBound) << '\n';
-    } catch(const std::logic_error& err) {
+    } catch (const std::logic_error& err) {
       std::cout << err.what() << '\n';
     }
     std::cout << odintsov::getAvgEvenIndices(fileArr.data, fileArr.size) << '\n';

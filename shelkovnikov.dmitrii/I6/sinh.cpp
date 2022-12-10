d405a861263f45e2bc69f90a5881cadb7d79855e
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-11 01:19:51 +0300

    change do while to while

--- shelkovnikov.dmitrii/I6/sinh.cpp
+++ shelkovnikov.dmitrii/I6/sinh.cpp
@@ -7,21 +7,20 @@ double sinh(double x, double error, unsigned count)
   {
     throw std::out_of_range("X must be in (-1; 1)");
   }
-  double res = x;
-  double summand = 0;
+  double res = 0;
+  double summand = x;
   unsigned count_summand = 1;
   long factorial = 1;
   long counter = 1;
-  do
+  while (std::fabs(summand) > error && count_summand < count)
   {
+    res += summand;
     x *= x * x;
     factorial *= (counter + 1) * (counter + 2);
     counter += 2;
     count_summand++;
     summand = x / static_cast< double >(factorial);
-    res += summand;
   }
-  while (std::fabs(summand) > error && count_summand < count);
   if (count_summand > count && abs(summand) > error)
   {
     throw std::out_of_range("The specified accuracy has not been reached");

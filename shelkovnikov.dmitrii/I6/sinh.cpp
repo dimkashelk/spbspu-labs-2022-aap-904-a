a3a7961411e1b15c6930b26bc5e4ac89704361f2
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-11 00:45:32 +0300

    add taylor series expansion

--- shelkovnikov.dmitrii/I6/sinh.cpp
+++ shelkovnikov.dmitrii/I6/sinh.cpp
@@ -1,3 +1,18 @@
 #include "sinh.h"
+#include <cmath>
 double sinh(double x, double error, unsigned count)
-{}
+{
+  double res = x;
+  unsigned count_summand = 1;
+  long factorial = 1;
+  long counter = 1;
+  do
+  {
+    x *= x * x;
+    factorial *= (counter + 1) * (counter + 2);
+    counter += 2;
+    count_summand++;
+    res += x / static_cast< double >(factorial);
+  }
+  while (abs(x) >= error && count_summand <= count);
+}

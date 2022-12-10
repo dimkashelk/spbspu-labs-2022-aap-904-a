27b1538b22d7d03d002e9c87cd248e4270be4865
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2022-12-10 17:15:22 +0300

    Corrected comments for the fourth time

--- fesenko.ivan/I2/algorithms.cpp
+++ fesenko.ivan/I2/algorithms.cpp
@@ -2,20 +2,20 @@
 
 size_t negativeAfterMax(const int *array, size_t size)
 {
-  int max = 0;
   if (size > 0) {
-    max = array[0];
-  }
-  int count = 0;
-  for (size_t i = 1; i < size; i++) {
-    if (array[i] > max) {
-      max = array[i];
-      count = 0;
-    } else if (array[i] < 0) {
-      count++;
+    int max = array[0], count = 0;
+    for (size_t i = 1; i < size; i++) {
+      if (array[i] > max) {
+        max = array[i];
+        count = 0;
+      } else if (array[i] < 0) {
+        count++;
+      }
     }
+    return count;
+  } else {
+    return 0;
   }
-  return count;
 }
 
 void movingElements(int *array, size_t size, size_t m, size_t n)

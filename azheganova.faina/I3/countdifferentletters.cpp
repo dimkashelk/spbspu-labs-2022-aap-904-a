cf4c2858de6c5bc825c22fccbe9904debfb4d9ee
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-11 00:22:01 +0300

    start function var 7

---
+++ azheganova.faina/I3/countdifferentletters.cpp
@@ -0,0 +1,4 @@
+#include "countdifferentletters.h"
+
+size_t count_different_letters( char* destination, const char* source)
+{}
---
+++ azheganova.faina/I3/countdifferentletters.h
@@ -0,0 +1,6 @@
+#ifndef COUNTDIFFERENTLETTERS_H
+#define COUNTDIFFERENTLETTERS_H
+#include <cstddef>
+
+size_t count_different_letters( char* destination, const char* source);
+#endif

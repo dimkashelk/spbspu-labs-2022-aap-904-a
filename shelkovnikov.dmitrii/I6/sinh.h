e1d91e1629ca13a7dd85eea9ae969792e89fa63b
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-11 01:22:42 +0300

    add print_table func

--- shelkovnikov.dmitrii/I6/sinh.h
+++ shelkovnikov.dmitrii/I6/sinh.h
@@ -1,4 +1,6 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_SINH_H
 #define SPBSPU_LABS_2022_AAP_904_A_SINH_H
+#include <iostream>
 double sinh(double x, double error, unsigned count);
+void print_table(const std::ostream &out, double left, double right, double step, double error, unsigned k);
 #endif

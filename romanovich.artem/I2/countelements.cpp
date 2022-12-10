55f7fa100758763844739475f31190dcddca0754
Артем Романович <artrom170@gmail.com>
2022-12-10 12:38:59 +0000

    changed int <=> size_t

--- romanovich.artem/I2/countelements.cpp
+++ romanovich.artem/I2/countelements.cpp
@@ -4,7 +4,7 @@
 size_t countNegativeRightFromMax(const int *array, size_t size)
 {
   size_t countNegativeRightFromMax = 0;
-  size_t maxElIndex = -1;
+  size_t maxElIndex = 0;
   int maxEl = -1 * std::numeric_limits<unsigned int>::max();
   for (size_t i = 0; i < size; i++)
   {

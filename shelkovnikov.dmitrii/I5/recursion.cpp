bcb9e189fbd8d7113cfa1bebebab82f34273a393
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-10 22:44:57 +0300

    update mantissa func

--- shelkovnikov.dmitrii/I5/recursion.cpp
+++ shelkovnikov.dmitrii/I5/recursion.cpp
@@ -1,5 +1,4 @@
 #include "recursion.h"
-#include <cstddef>
 #include <cctype>
 bool is_sign(const char number)
 {
@@ -11,14 +10,36 @@ bool is_digit(const char number)
 }
 bool is_unsigned_integer(const char *number)
 {
-  if (!number[0])
+  if (!number[0] || number[0] == '.' || number[0] == 'E')
   {
     return true;
   }
   return is_digit(number[0]) && is_unsigned_integer(number + 1);
 }
-
+bool mantissa(const char *number, const bool contains_point, const bool is_prev_point)
+{
+  if (number[0] == '.')
+  {
+    if (contains_point)
+    {
+      return false;
+    }
+    return mantissa(number + 1, true, true);
+  }
+  else if (number[0] == 'E')
+  {
+    if (is_prev_point)
+    {
+      return false;
+    }
+    return true;
+  }
+  else
+  {
+    return is_digit(number[0]) && mantissa(number + 1, contains_point, false);
+  }
+}
 bool is_real_number(const char *number)
 {
-  return is_unsigned_integer(number)
+  return is_sign(number[0]) && mantissa(number + 1, false, false);
 }

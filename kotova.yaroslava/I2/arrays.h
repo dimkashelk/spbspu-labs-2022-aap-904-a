1b357dc79447b0ccb337394a7b77f82d0972e300
yaroslava kotova <aroslavakotova@gmail.com>
2022-12-10 21:30:18 +0300

    2lab

---
+++ kotova.yaroslava/I2/arrays.h
@@ -0,0 +1,8 @@
+#include <cstddef>
+#ifndef ARRAYS
+#define ARRAYS
+
+size_t checkRepeatEl(const int* array, size_t size, unsigned int index_n, unsigned int index_m);
+size_t counterDivision(const int* array, size_t size,unsigned int n);
+
+#endif

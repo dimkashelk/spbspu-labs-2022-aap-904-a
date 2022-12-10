3b8555bbd92d0ea137cc8ed4056a7d622c7e5761
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-11 02:18:00 +0300

    lab I3

--- azheganova.faina/I3/countrepeatedsymbols.cpp
+++ azheganova.faina/I3/countrepeatedsymbols.cpp
@@ -1,6 +1,6 @@
 #include "countrepeatedsymbols.h"
 
-size_t count_repeated_symbols(const char* source, size_t capacity)
+bool count_repeated_symbols(const char* source, size_t capacity)
 {
   int countofrepeatedsymbol = 1;
   for (char i = 'a'; i <= 'z'; i++)
--- azheganova.faina/I3/countrepeatedsymbols.h
+++ azheganova.faina/I3/countrepeatedsymbols.h
@@ -2,5 +2,5 @@
 #define COUNTREPEATEDSYMBOLS_H
 #include <cstddef>
 
-size_t count_repeated_symbols( char* source, size_t capacity);
+bool count_repeated_symbols( char* source, size_t capacity);
 #endif

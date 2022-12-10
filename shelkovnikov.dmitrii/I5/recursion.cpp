4c2d71d2c55c8b9a5e5210fc278aa82a59022216
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-10 22:17:51 +0300

    update functions

--- shelkovnikov.dmitrii/I5/recursion.cpp
+++ shelkovnikov.dmitrii/I5/recursion.cpp
@@ -1,87 +1,24 @@
 #include "recursion.h"
 #include <cstddef>
 #include <cctype>
-bool is_sign(char number)
+bool is_sign(const char number)
 {
   return number == '-' || number == '+';
 }
-bool is_digit(char number)
+bool is_digit(const char number)
 {
   return std::isdigit(number);
 }
-bool is_unsigned_integer(char *number, size_t start, size_t end)
+bool is_unsigned_integer(const char *number)
 {
-  if (start == end)
+  if (!number[0])
   {
     return true;
   }
-  return is_digit(number[0]) && is_unsigned_integer(number, ++start, end);
+  return is_digit(number[0]) && is_unsigned_integer(number + 1);
 }
-size_t index_point(char *number, size_t start, size_t end)
+
+bool is_real_number(const char *number)
 {
-  if (start == end)
-  {
-    return start;
-  }
-  if (number[start] == '.')
-  {
-    return start;
-  }
-  return index_point(number, ++start, end);
-}
-size_t count_point(char *number, size_t start, size_t end)
-{
-  if (start == end)
-  {
-    return start;
-  }
-  if (number[start] == '.')
-  {
-    return 1 + index_point(number, ++start, end);;
-  }
-  return index_point(number, ++start, end);
-}
-bool mantissa(char *number, size_t start, size_t end)
-{
-  size_t count_of_point = count_point(number, start, end);
-  if (count_of_point > 1)
-  {
-    return false;
-  }
-  if (count_of_point == 0)
-  {
-    return is_unsigned_integer(number, start, end);
-  }
-  size_t index_of_point = index_point(number, start, end);
-  return is_unsigned_integer(number, start, index_of_point) && is_unsigned_integer(number, index_of_point + 1, end);
-}
-bool order(char *number, size_t start, size_t end)
-{
-  if (start >= end - 1)
-  {
-    return false;
-  }
-  return number[start] == 'E' && ((is_sign(number[start + 1]) && is_unsigned_integer(number, start + 2, end))
-    || is_unsigned_integer(number, start + 1, end));
-}
-size_t index_E(char *number, size_t start, size_t end)
-{
-  if (start == end)
-  {
-    return start;
-  }
-  if (number[start] == 'E')
-  {
-    return start;
-  }
-  return index_E(number, ++start, end);
-}
-bool is_real_number(char *number, size_t size)
-{
-  size_t index_of_E = index_E(number, 0, size);
-  if (index_of_E == size)
-  {
-    return false;
-  }
-  return is_sign(number[0]) && mantissa(number, 1, index_of_E) && order(number, index_of_E, size);
+  return is_unsigned_integer(number)
 }

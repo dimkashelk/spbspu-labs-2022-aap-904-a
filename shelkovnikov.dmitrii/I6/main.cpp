0f21951c27f54cc83cf8433d45826382f8740641
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 12:30:03 +0300

    add input and print

--- shelkovnikov.dmitrii/I6/main.cpp
+++ shelkovnikov.dmitrii/I6/main.cpp
@@ -1,2 +1,14 @@
+#include <iostream>
+#include "sinh.h"
 int main()
-{}
+{
+  double left = 0.0f, right = 0.0f;
+  unsigned count = 0;
+  std::cin >> left >> right >> count;
+  if (!std::cin)
+  {
+    std::cout << "Error";
+    return 1;
+  }
+  print_table(std::cout, left, right, 0.1, 0.02, count);
+}

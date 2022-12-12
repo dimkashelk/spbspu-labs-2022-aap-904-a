3e1a9c7e66afa12a8043121f39f11df5dee55af8
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 21:34:27 +0300

    create mantissa and mantissa_with_point func

--- shelkovnikov.dmitrii/I5/recursion.cpp
+++ shelkovnikov.dmitrii/I5/recursion.cpp
@@ -16,7 +16,10 @@ bool is_unsigned_integer(const char *number)
   }
   return is_digit(number[0]) && is_unsigned_integer(number + 1);
 }
-
+bool mantissa_with_point(const char *number)
+{}
+bool mantissa(const char *number)
+{}
 bool is_real_number(const char *number)
 {
   return is_sign(number[0]) && mantissa(number + 1);

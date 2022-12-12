df7672f9f593d53dd22b2d40f2c28e69641ca4fa
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 14:23:19 +0300

    update signature

--- shelkovnikov.dmitrii/I6/sinh.h
+++ shelkovnikov.dmitrii/I6/sinh.h
@@ -2,5 +2,5 @@
 #define SPBSPU_LABS_2022_AAP_904_A_SINH_H
 #include <iostream>
 double sinh(double x, double error, unsigned count);
-void print_table(const std::ostream &out, double left, double right, double step, double error, unsigned k);
+void print_table(std::ostream &out, double left, double right, double step, double error, unsigned k);
 #endif

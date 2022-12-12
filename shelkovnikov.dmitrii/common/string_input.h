19cb49d792e0beee257b8ed9471bf70b4ac7d8bf
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 22:36:52 +0300

    update func signature

--- shelkovnikov.dmitrii/common/string_input.h
+++ shelkovnikov.dmitrii/common/string_input.h
@@ -2,5 +2,5 @@
 #define SPBSPU_LABS_2022_AAP_904_A_STRING_INPUT_H
 #include <istream>
 #include <cstddef>
-char* string_input(std::istream &in, char* destination, size_t *size, size_t *capacity);
+char* string_input(std::istream &in, char* destination, size_t capacity);
 #endif

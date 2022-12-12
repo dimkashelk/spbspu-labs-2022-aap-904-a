9413bbbb9bfccbe1ae9b166724ddb7c2e9a326a5
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 22:29:26 +0300

    copying from I3 main.cpp

--- shelkovnikov.dmitrii/common/string_input.cpp
+++ shelkovnikov.dmitrii/common/string_input.cpp
@@ -1,1 +1,51 @@
 #include "string_input.h"
+#include <limits>
+#include <stdexcept>
+char* string_input(std::istream &in, char* destination, size_t *size, size_t *capacity)
+{
+  in >> std::noskipws;
+  constexpr size_t max_size_t = std::numeric_limits< size_t >::max();
+  do
+  {
+    if (*size == *capacity)
+    {
+      if (*capacity == max_size_t)
+      {
+        throw std::overflow_error("Too long string");
+      }
+      if (max_size_t - 20 <= *capacity)
+      {
+        *capacity = max_size_t;
+      }
+      else
+      {
+        *capacity += 20;
+      }
+      try
+      {
+        char *newstr = new char[*capacity];
+        for (auto i = destination, j = newstr; i != destination + *size; ++i, ++j)
+        {
+          *j = *i;
+        }
+        delete[] destination;
+        destination = newstr;
+      }
+      catch (...)
+      {
+        delete[] destination;
+        throw std::bad_alloc("Error while getting new memory");
+      }
+    }
+    in >> destination[*size];
+    *size = *size + 1;
+  }
+  while (in && destination[*size] != '\n');
+  if (!in && !*size)
+  {
+    delete[] destination;
+    throw std::runtime_error("Error while input");
+  }
+  destination[(*size) - 1] = '\0';
+  return destination;
+}

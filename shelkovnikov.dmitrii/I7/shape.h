ec0dd8cb0813593a9b7308bd16d5221d9f5f8fc9
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 15:11:02 +0300

    add getFrameRect to Shape

--- shelkovnikov.dmitrii/I7/shape.h
+++ shelkovnikov.dmitrii/I7/shape.h
@@ -1,7 +1,9 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_SHAPE_H
 #define SPBSPU_LABS_2022_AAP_904_A_SHAPE_H
+#include "base_types.h"
 class Shape
 {
   virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
 };
 #endif

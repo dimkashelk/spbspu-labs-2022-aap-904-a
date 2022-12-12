3d24a98feac8a7837fba7679f0387955cc2444ef
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 22:17:21 +0300

    add func string_input

--- shelkovnikov.dmitrii/common/string_input.h
+++ shelkovnikov.dmitrii/common/string_input.h
@@ -1,3 +1,6 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_STRING_INPUT_H
 #define SPBSPU_LABS_2022_AAP_904_A_STRING_INPUT_H
+#include <istream>
+#include <cstddef>
+char* string_input(std::istream &in, char* destination, size_t *size, size_t *capacity);
 #endif

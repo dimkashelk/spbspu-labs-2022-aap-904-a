54ac4ed172a23aa8572bd8b1a7d503f839442959
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 15:12:10 +0300

    add move to Shape

--- shelkovnikov.dmitrii/I7/shape.h
+++ shelkovnikov.dmitrii/I7/shape.h
@@ -6,5 +6,6 @@ class Shape
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(point_t point) const = 0;
+  virtual void move(double x, double y) const = 0;
 };
 #endif

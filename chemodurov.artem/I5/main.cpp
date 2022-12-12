bd2f0d029737f2af27f39f7f2daafb0bae1fee06
Chemodurovart <chemodurovart@gmail.com>
2022-12-12 18:55:53 +0300

    add reading

--- chemodurov.artem/I5/main.cpp
+++ chemodurov.artem/I5/main.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include "recursion.h"
+#include "extendCString.h"
 
 int main()
 {
-  std::cout << std::boolalpha << chemodurov::isRealNumber("+100.9E3") << "\n";
-  std::cout << std::boolalpha << chemodurov::isRealNumber("+.9E-3") << "\n";
-  std::cout << std::boolalpha << chemodurov::isRealNumber("100.9E+3") << "\n";
-  std::cout << std::boolalpha << chemodurov::isRealNumber("+100.9E-3") << "\n";
-  std::cout << std::boolalpha << chemodurov::isRealNumber("+.8E3") << "\n";
-  std::cout << std::boolalpha << chemodurov::isRealNumber("+8E3") << "\n";
-  std::cout << std::boolalpha << chemodurov::isRealNumber("+3.E3") << "\n";
+  size_t capacity = 20;
+  char * c_string = new char[capacity];
+  c_string[0] = '\0';
+  size_t size = 0;
+
+  std::cin >> std::noskipws;
+  do
+  {
+    if (size == capacity)
+    {
+      try
+      {
+        size_t new_capacity = capacity + 20;
+        char * new_string = chemodurov::extendCString(c_string, capacity, new_capacity);
+        delete [] c_string;
+        c_string = new_string;
+        new_string = nullptr;
+        capacity = new_capacity;
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << e.what() << "\n";
+        delete [] c_string;
+        return 1;
+      }
+    }
+    std::cin >> c_string[size];
+  }
+  while (std::cin && c_string[size] != '\0' && c_string[size++] != '\n');
+
+  if (c_string[0] == '\n' || c_string[0] == '\0')
+  {
+    std::cerr << "Empty string\n";
+    delete [] c_string;
+    return 1;
+  }
+  c_string[size - 1] = '\0';
+
+  std::cout << std::boolalpha << chemodurov::isRealNumber(c_string) << "\n";
+  delete [] c_string;
   return 0;
 }

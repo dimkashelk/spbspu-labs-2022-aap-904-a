d1e12881ac72e0b955371d8e0fae5fe66a0de2e6
KiraGelaz <kiragelaz@yandex.ru>
2022-12-13 00:42:43 +0300

    draft: deleting extra spaces

--- kryuchkova.kira/I3/main.cpp
+++ kryuchkova.kira/I3/main.cpp
@@ -32,7 +32,34 @@ int main()
   }
   while (std::cin && cstring[size++] != '\n');
   cstring[size - 1] = '\0';
+  size--;
 
-  std::cout << cstring << "\n";
+  size_t n = 0;
+  size_t k = 0;
+  char * cstring_2 = new char[size];
+
+  while (std::isspace(cstring[n]))
+  {
+    n++;
+  }
+
+  for (size_t i = n; i < size; i++)
+  {
+    if (!(std::isspace(cstring[i]) && cstring[i] == cstring[i - 1]))
+    {
+      cstring_2[k] = cstring[i];
+      k++;
+    }
+  }
+
+  if (std::isspace(cstring_2[k - 1]))
+  {
+    k--;
+  }
+
+  cstring_2[k] = '\0';
+  std::cout << cstring_2 << '\n';
+
   delete [] cstring;
+  delete [] cstring_2;
 }

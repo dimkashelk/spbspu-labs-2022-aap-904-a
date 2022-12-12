396537eae6d11864331f0e4a0f1b8944a208a173
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 15:20:16 +0300

    add constructor

--- shelkovnikov.dmitrii/I7/rectangle.cpp
+++ shelkovnikov.dmitrii/I7/rectangle.cpp
@@ -1,5 +1,1 @@
-//
-// Created by notebook on 12.12.2022.
-//
-
 #include "rectangle.h"
--- shelkovnikov.dmitrii/I7/rectangle.h
+++ shelkovnikov.dmitrii/I7/rectangle.h
@@ -1,6 +1,9 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_RECTANGLE_H
 #define SPBSPU_LABS_2022_AAP_904_A_RECTANGLE_H
 #include "shape.h"
-class Rectangle: Shape
-{};
+class Rectangle: public Shape
+{
+public:
+  Rectangle(point_t point, double width, double height);
+};
 #endif

9c3f5c3a3b8c1fb94b6620e102e12eb2dda01d17
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 22:38:16 +0300

    update func

--- shelkovnikov.dmitrii/common/string_input.cpp
+++ shelkovnikov.dmitrii/common/string_input.cpp
@@ -1,30 +1,31 @@
 #include "string_input.h"
 #include <limits>
 #include <stdexcept>
-char* string_input(std::istream &in, char* destination, size_t *size, size_t *capacity)
+char* string_input(std::istream &in, char* destination, size_t capacity)
 {
   in >> std::noskipws;
   constexpr size_t max_size_t = std::numeric_limits< size_t >::max();
+  size_t size = 0;
   do
   {
-    if (*size == *capacity)
+    if (size == capacity)
     {
-      if (*capacity == max_size_t)
+      if (capacity == max_size_t)
       {
         throw std::runtime_error("Too long string");
       }
       if (max_size_t - 20 <= *capacity)
       {
-        *capacity = max_size_t;
+        capacity = max_size_t;
       }
       else
       {
-        *capacity += 20;
+        capacity += 20;
       }
       try
       {
-        char *newstr = new char[*capacity];
-        for (auto i = destination, j = newstr; i != destination + *size; ++i, ++j)
+        char *newstr = new char[capacity];
+        for (auto i = destination, j = newstr; i != destination + size; ++i, ++j)
         {
           *j = *i;
         }
@@ -37,15 +38,14 @@ char* string_input(std::istream &in, char* destination, size_t *size, size_t *ca
         throw std::runtime_error("Error while getting new memory");
       }
     }
-    in >> destination[*size];
-    *size = *size + 1;
+    in >> destination[size];
   }
-  while (in && destination[*size] != '\n');
-  if (!in && !*size)
+  while (in && destination[size++] != '\n');
+  if (!in && !size)
   {
     delete[] destination;
     throw std::runtime_error("Error while input");
   }
-  destination[(*size) - 1] = '\0';
+  destination[size - 1] = '\0';
   return destination;
 }

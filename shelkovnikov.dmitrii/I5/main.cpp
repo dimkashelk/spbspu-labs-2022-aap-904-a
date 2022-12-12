a8844e36f1e413218977a3f4b9c693266ea96488
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-12 22:52:48 +0300

    add input strings

--- shelkovnikov.dmitrii/I5/main.cpp
+++ shelkovnikov.dmitrii/I5/main.cpp
@@ -2,8 +2,74 @@
 #include <cstddef>
 #include <limits>
 #include "recursion.h"
+#include "../common/string_input.h"
+#include "../common/free_memory.h"
 int main()
 {
-
+  size_t capacity = 10;
+  size_t size = 0;
+  constexpr size_t max_size_t = std::numeric_limits< size_t >::max();
+  char** strings = new char*[capacity];
+  while (!std::cin)
+  {
+    try
+    {
+      size_t str_capacity = 10;
+      char* str = new char[str_capacity];
+      try
+      {
+        string_input(std::in, str, str_capacity);
+      }
+      catch (const std::runtime_error &e)
+      {
+        std::cout << e.what();
+        free_memory(strings, size);
+        return 1;
+      }
+      strings[size++] = str;
+      if (size == capacity)
+      {
+        if (capacity == max_size_t)
+        {
+          std::cout << "Too much strings";
+          free_memory(strings, size);
+          return 1;
+        }
+        if (max_size_t - 20 <= *capacity)
+        {
+          capacity = max_size_t;
+        }
+        else
+        {
+          capacity += 20;
+        }
+        try
+        {
+          try
+          {
+            char **new_strings = new char*[capacity];
+            for (size_t i = 0; i < size; i++)
+            {
+              new_strings[i] = strings[i];
+            }
+            delete[] strings;
+            strings = new_strings;
+          }
+          catch (...)
+          {
+            std::cout << "Error";
+            free_memory(strings, size);
+            return 2;
+          }
+        }
+      }
+    }
+    catch (const std::runtime_error &e)
+    {
+      std::cout << e.what();
+      free_memory(strings, size);
+      return 2;
+    }
+  }
   return 0;
 }

62c4ef91a703467728a7502ee6e4d91e6c5d96f3
Chemodurovart <chemodurovart@gmail.com>
2022-12-13 17:30:15 +0300

    fixed functions

--- chemodurov.artem/I4/arrays.cpp
+++ chemodurov.artem/I4/arrays.cpp
@@ -1,21 +1,28 @@
 #include "arrays.h"
 #include <stdexcept>
-
+bool isGrowingRow(const int * arr, size_t row, size_t cols)
+{
+  size_t counter_in_row = 0;
+  for (size_t i = 0; i < cols - 1; ++i)
+  {
+    size_t number = row * cols + i;
+    if (arr[number] < arr[number + 1])
+    {
+      counter_in_row++;
+    }
+  }
+  if (counter_in_row == cols - 1)
+  {
+    return true;
+  }
+  return false;
+}
 size_t chemodurov::countGrowingRows(const int * arr, size_t rows, size_t cols)
 {
   size_t counter = 0;
   for (size_t i = 0; i < rows; ++i)
   {
-    size_t counter_in_row = 0;
-    for (size_t j = 0; j < cols - 1; ++j)
-    {
-      size_t number_of_item = (i * cols) + j;
-      if (arr[number_of_item] > arr[number_of_item + 1])
-      {
-        counter_in_row++;
-      }
-    }
-    if (counter_in_row == (cols - 1))
+    if (isGrowingRow(arr, i, cols))
     {
       counter++;
     }
@@ -23,31 +30,35 @@ size_t chemodurov::countGrowingRows(const int * arr, size_t rows, size_t cols)
   return counter;
 }
 
+long long calcSummSecDiagonal(const int * arr, size_t rows, size_t cols, size_t summ_of_index)
+{
+  long long summ = 0;
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for (size_t j = 0; j < cols; ++j)
+    {
+      if ((i + j) == summ_of_index)
+      {
+        summ += arr[(i * cols) + j];
+      }
+    }
+  }
+  return summ;
+}
 long long chemodurov::calcMinSummSecondaryDiagonal(const int * arr, size_t rows, size_t cols)
 {
   if (rows != cols)
   {
     throw std::invalid_argument("No diagonal in not square matrix\n");
   }
-  long long summ = 0;
   long long max_summ = 0;
   for (size_t i = 0; i < (2 * rows) - 1; ++i)
   {
-    for (size_t j = 0; j < rows; ++j)
-    {
-      for (size_t k = 0; k < cols; ++k)
-      {
-        if ((j + k) == i)
-        {
-          summ += arr[(j * cols) + k];
-        }
-      }
-    }
-    if (summ < max_summ)
+    long long summ = calcSummSecDiagonal(arr, rows, cols, i);
+    if (max_summ < summ)
     {
       max_summ = summ;
     }
-    summ = 0;
   }
   return max_summ;
 }

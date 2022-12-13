c57da69941f88d378930b6c9ee6e399dfcddcc59
xeniaTa <kseniatarasenko1@gmail.com>
2022-12-13 18:02:22 +0300

    fix extendCString

--- tarasenko.ksenia/I3/extendcstring.cpp
+++ tarasenko.ksenia/I3/extendcstring.cpp
@@ -4,17 +4,18 @@
 
 char * extendCString(char * cstring, size_t & capacity, size_t size)
 {
-  capacity = static_cast< size_t >(std::ceil(capacity * std::sqrt(2)));
-  if (capacity < size)
+  size_t new_capacity = static_cast< size_t >(std::ceil(capacity * std::sqrt(2)));
+  if (new_capacity < size)
   {
     throw std::invalid_argument("error");
   }
-  char * newstr = new char[capacity];
+  char * newstr = new char[new_capacity];
   for (auto i = cstring, j = newstr; i != cstring + size; ++i, ++j)
   {
     *j = *i;
   }
   delete [] cstring;
   cstring = newstr;
+  capacity = new_capacity;
   return cstring;
 }

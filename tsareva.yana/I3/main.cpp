9204808630a6dbb7a8c56786a0b8f0f5cc200dd9
Царева ЯД <tsarewaya@yandex.ru>
2022-12-13 20:51:39 +0300

    fix try catch

--- tsareva.yana/I3/main.cpp
+++ tsareva.yana/I3/main.cpp
@@ -50,16 +50,37 @@ int main()
     return 2;
   }
   str_1[size_1 - 1] = '\0';
-  const char * str_2 = "abcdefgh\0";
+  const char *str_2 = "abcdefgh\0";
   size_t size_2 = 9;
-  char *res1 = new char[size_1 + size_2];
-  generate_new_line(res1, str_1, str_2);
-  std::cout << res1 << '\n';
-  delete[] res1;
-  char* res2 = new char[size_1];
-  change_chars(res2, str_1);
-  std::cout << res2 << '\n';
-  delete[] res2;
+  try
+  {
+    char *res1 = new char[size_1 + size_2];
+    generate_new_line(res1, str_1, str_2);
+    std::cout << res1 << '\n';
+    delete[] res1;
+  }
+  catch (const std::bad_alloc &e)
+  {
+    std::cout << e.what();
+    delete[] str_1;
+    delete[] str_2;
+    return 2;
+  }
+  try
+  {
+    char* res2 = new char[size_1];
+    change_chars(res2, str_1);
+    std::cout << res2 << '\n';
+    delete[] res2;
+  }
+  catch (const std::bad_alloc &e)
+  {
+    std::cout << e.what();
+    delete[] str_1;
+    delete[] str_2;
+    return 2;
+  }
   delete[] str_1;
+  delete[] str_2;
   return 0;
 }

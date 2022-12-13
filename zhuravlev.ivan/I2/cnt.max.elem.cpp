932c5869323ea48c466a55e976082c884ed4ce29
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-13 17:36:10 +0000

    fixed cg

--- zhuravlev.ivan/I2/cnt.max.elem.cpp
+++ zhuravlev.ivan/I2/cnt.max.elem.cpp
@@ -2,21 +2,21 @@
 
 size_t CounterMaxElement(const int *arr, const int size)
 {
-	size_t cnt_max = 0;
-	size_t max = 0;
-	for (int i = 0; i < size; ++i)
+  size_t cnt_max = 0;
+  size_t max = 0;
+  for (int i = 0; i < size; ++i)
+  {
+	if (arr[i] > max)
 	{
-		if (arr[i] > max)
-		{
-			max = arr[i];
-		}
+	  max = arr[i];
 	}
-	for (int i = 0; i < size; ++i) 
+  }
+  for (int i = 0; i < size; ++i) 
+  {
+	if (arr[i] == max) 
 	{
-		if (arr[i] == max) 
-		{
-			cnt_max++;
-		}
+	  cnt_max++;
 	}
-	return cnt_max;
+  }
+  return cnt_max;
 }

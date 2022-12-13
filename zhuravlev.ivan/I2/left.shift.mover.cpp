782508f34f413c34dc2e312e6577672d78e72915
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-13 17:02:14 +0000

    fix cg

--- zhuravlev.ivan/I2/left.shift.mover.cpp
+++ zhuravlev.ivan/I2/left.shift.mover.cpp
@@ -3,15 +3,15 @@
 
 int* LeftShiftMover(int *arr, const size_t size,const size_t wanted_shift)
 {
-	int current_shifts = 0;
-	while (current_shifts++ < wanted_shift)
+  int current_shifts = 0;
+  while (current_shifts++ < wanted_shift)
+  {
+    int temp = arr[0];
+	for (size_t i = 0; i < size - 1; ++i)
 	{
-		int temp = arr[0];
-		for (size_t i = 0; i < size - 1; ++i)
-		{
-			arr[i] = arr[i + 1];
-		}
-		arr[size - 1] = temp;
+	  arr[i] = arr[i + 1];
 	}
-	return arr;
+	  arr[size - 1] = temp;
+	}
+  return arr;
 }

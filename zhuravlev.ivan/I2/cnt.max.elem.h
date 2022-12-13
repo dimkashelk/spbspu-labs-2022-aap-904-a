dba599d709329f0564fc44f44eb3e7583dfe39c5
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-13 20:26:37 +0300

    fixed

---
+++ zhuravlev.ivan/I2/cnt.max.elem.h
@@ -0,0 +1,7 @@
+#ifndef CNT_MAX_ELEM_H
+#define CNT_MAX_ELEM_H
+#include <cstddef>
+
+size_t CounterMaxElement(const int *arr, int size);
+
+#endif
---
+++ zhuravlev.ivan/I2/left.shift.mover.cpp
@@ -0,0 +1,17 @@
+#include "left.shift.mover.h"
+#include <cstddef>
+
+int* LeftShiftMover(int *arr, const size_t size,const size_t wanted_shift)
+{
+	int current_shifts = 0;
+	while (current_shifts++ < wanted_shift)
+	{
+		int temp = arr[0];
+		for (size_t i = 0; i < size - 1; ++i)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[size - 1] = temp;
+	}
+	return arr;
+}

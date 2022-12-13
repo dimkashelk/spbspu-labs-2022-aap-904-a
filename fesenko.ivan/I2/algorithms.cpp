d218287474027b77cc82cf16641540b529d5919b
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2022-12-13 10:06:39 +0300

    Changed the counter type

--- fesenko.ivan/I2/algorithms.cpp
+++ fesenko.ivan/I2/algorithms.cpp
@@ -3,7 +3,8 @@
 size_t negativeAfterMax(const int *array, size_t size)
 {
   if (size > 0) {
-    int max = array[0], count = 0;
+    int max = array[0];
+    size_t count = 0;
     for (size_t i = 1; i < size; i++) {
       if (array[i] > max) {
         max = array[i];

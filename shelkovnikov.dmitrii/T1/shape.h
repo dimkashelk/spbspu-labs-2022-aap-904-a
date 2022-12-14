e8102d94dc6d257df387c0b4cb92ee942d4b54ad
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-14 18:46:32 +0300

    remove getName func

--- shelkovnikov.dmitrii/T1/shape.h
+++ shelkovnikov.dmitrii/T1/shape.h
@@ -1,6 +1,5 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_SHAPE_H
 #define SPBSPU_LABS_2022_AAP_904_A_SHAPE_H
-#include <string>
 #include "base_types.h"
 class Shape
 {
@@ -10,7 +9,6 @@ public:
   virtual void move(point_t point) const = 0;
   virtual void move(double delta_x, double delta_y) const = 0;
   virtual void scale(double k) const = 0;
-  virtual std::string getName() const = 0;
   virtual ~Shape() = default;
 };
 #endif

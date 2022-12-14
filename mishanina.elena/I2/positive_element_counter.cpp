d15d6b6b3a8df63a74f4f2350b3a47f3e42a9058
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2022-12-14 12:32:51 +0300

    Implementation header positive_element_counter.cpp

---
+++ mishanina.elena/I2/positive_element_counter.cpp
@@ -0,0 +1,32 @@
+#include "positive_element_counter.h"
+
+size_t positive_element_counter(const int* array, size_t size)
+{
+  if (size == 0)
+  {
+    return 0;
+  }
+  else
+  {
+    int min_element = array[0];
+    size_t counter = 0;
+    if (array[0] > 0)
+    {
+      counter = 1;
+    }
+    size_t final_value = 0;
+    for (size_t i = 1;i < size;i++)
+    {
+     if (min_element > array[i])
+     {
+       min_element = array[i];
+       final_value = counter;
+     }
+     if (array[i] > 0)
+     {
+       counter++;
+     }
+    }
+    return final_value;
+  }
+}

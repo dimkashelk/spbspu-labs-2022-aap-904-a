7ffe930f83d5007049ec5f674316112442fffc6c
Gulkov Mikhail <gulkov.mikhail@mail.ru>
2022-12-14 18:14:31 +0300

    matrix wave alg

---
+++ gulkov.mikhail/I4/MakeMatrixWaveIncrement.hpp
@@ -0,0 +1,6 @@
+#ifndef MAKEMATRIXWAVEINCREMENT_HPP
+#define MAKEMATRIXWAVEINCREMENT_HPP
+
+int *make_matrix_wave_increment(int *arr, unsigned int m, unsigned int n, unsigned int row_number_elem, unsigned int col_number_elem);
+
+#endif

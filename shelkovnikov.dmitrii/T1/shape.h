899fef1069b2e2f429b360da78e8e06fd07000a0
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-14 18:46:17 +0300

    update signature of move

--- shelkovnikov.dmitrii/T1/shape.h
+++ shelkovnikov.dmitrii/T1/shape.h
@@ -8,7 +8,7 @@ public:
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(point_t point) const = 0;
-  virtual void move(double x, double y) const = 0;
+  virtual void move(double delta_x, double delta_y) const = 0;
   virtual void scale(double k) const = 0;
   virtual std::string getName() const = 0;
   virtual ~Shape() = default;

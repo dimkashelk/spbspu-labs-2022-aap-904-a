3eabbf2cc22afbedd1b6f3d017c09f6fe8e269d6
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2022-12-14 18:36:57 +0300

    Added input error check

--- fesenko.ivan/I3/main.cpp
+++ fesenko.ivan/I3/main.cpp
@@ -6,7 +6,6 @@ int main()
   size_t capacity = 10;
   char *cstring = new char[capacity];
   size_t size = 0;
-
   std::cin >> std::noskipws;
   do {
     if (size == capacity) {
@@ -15,15 +14,19 @@ int main()
       }
       catch (const std::exception &e) {
         delete [] cstring;
-        std::cerr << e.what();
+        std::cerr << e.what() << "\n";
         return 2;
       }
     }
     std::cin >> cstring[size];
   }
   while (std::cin && cstring[size++] != '\n');
+  if (!std::cin || cstring[0] == '\n') {
+    std::cerr << "Error while reading\n";
+    delete [] cstring;
+    return 2;
+  }
   cstring[size - 1] = '\0';
-
-  std::cout << cstring << "\n";
+  std::cout << "No errors" << "\n";
   delete [] cstring;
 }

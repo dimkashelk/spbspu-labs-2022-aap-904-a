5de0f420d1bcb7d8b1cd712c72a7a4c050e7f4c2
Kirill Tobbo <tobbo.kv@edu.spbstu.ru>
2022-12-14 08:54:50 +0300

    fix problems

--- tobbo.kirill/I1/lab1.cpp
+++ tobbo.kirill/I1/lab1.cpp
@@ -1,45 +1,66 @@
 #include <iostream>
-#include <climits>
 
-const char* ERROR_INVALID_INPUT = "Error:input must only contain integers.";
-
-int main() {
-  int countMax = 0;
-  int countLocalMax = 0;
-  int maxx = INT_MIN;
+int main()
+{
+  unsigned int countMax = 0;
+  unsigned int countLocalMax = 0;
+  int maxx = std::numeric_limits< int >::min();
+  constexpr unsigned int max_uint = std::numeric_limits< unsigned int >::max();
   int third = 0;
   int first = 0;
   int second = 0;
-  std::cout << "Enter numbers:" << std::endl;
-  try {
-    do {
-      std::cin >> third;
-      if (std::cin.fail())
-        throw ERROR_INVALID_INPUT;
-
-      if (third != 0) {
-        if (third > maxx) {
+  std::cout << "Enter numbers:\n";
+  do
+  {
+    std::cin >> third;
+    if (!std::cin)
+    {
+      std::cerr << "Input should be numbers";
+      return 1;
+    }
+    try
+    {
+      if (third != 0)
+      {
+        if (third > maxx)
+        {
           countMax = 0;
           maxx = third;
         }
         if (third == maxx)
+        {
+          if (countMax == max_uint)
+          {
+            throw std::overflow_error("Overflow!");
+          }
           countMax++;
+        }
         if (first < second && second > third)
+        {
+          if (countLocalMax == max_uint)
+          {
+            throw std::overflow_error("Overflow!");
+          }
           countLocalMax++;
+        }
         first = second;
         second = third;
       }
-    } while (third != 0);
-
-    if (countMax == 0)
-      std::cout << "The sequence is empty." << std::endl;
-    else {
-      std::cout << "Count of maximum values: " << countMax << " Max value: " << maxx << std::endl;
-      std::cout << "Count of local maximums: " << countLocalMax << std::endl;
     }
-  }
+    catch (const std::overflow_error& e)
+    {
+      std::cerr << e.what();
+      return 1;
+    }
+  } while (third != 0);
 
-  catch (const char* error) {
-    std::cerr << std::endl << error << std::endl;
+  if (countMax == 0)
+    std::cout << "The sequence is empty.\n";
+  else
+  {
+    std::cout << "Count of maximum values: " << countMax << " Max value: " << maxx << "\n";
+    std::cout << "Count of local maximums: " << countLocalMax << "\n";
   }
+  return 0;
 }
+

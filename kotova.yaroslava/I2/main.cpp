85c2d93951e2f95537850aa10349365857358860
Yaroslava <aroslavakotova@gmail.com>
2022-12-14 20:29:28 +0300

    dslk

--- kotova.yaroslava/I2/main.cpp
+++ kotova.yaroslava/I2/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char* argv[])
     }
     catch (const std::invalid_argument& e)
     {
-      std::cout << e.what()
+      std::cout << e.what();
       delete[] secondArray;
       return 1;
     }

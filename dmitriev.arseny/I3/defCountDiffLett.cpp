eeb9a5fb6b55072de7a5b2344f5b598f6353c0e6
ArsDi <abstractlevl@gmail.com>
2022-12-14 22:06:21 +0300

    try 2 (piplines)

--- dmitriev.arseny/I3/defCountDiffLett.cpp
+++ dmitriev.arseny/I3/defCountDiffLett.cpp
@@ -3,24 +3,24 @@
 
 size_t defCountDiffLett(const char* cStr)
 {
-    bool letExist[26];
-    size_t count = 0;
+  bool letExist[26];
+  size_t count = 0;
 
-    for (size_t i = 0; i < 26; i++)
+  for (size_t i = 0; i < 26; i++)
+  {
+    letExist[i] = 0;
+  }
+  for (size_t i = 0; cStr[i] != '\0'; i++)
+  {
+    if (int(cStr[i]) >= 97 && int(cStr[i]) <= 122)
     {
-        letExist[i] = 0;
+      letExist[int(cStr[i]) - 97] = 1;
     }
-    for (size_t i = 0; cStr[i] != '\0'; i++)
-    {
-        if (int(cStr[i]) >= 97 && int(cStr[i]) <= 122)
-        {
-            letExist[int(cStr[i]) - 97] = 1;
-        }
-    }
-    for (size_t i = 0; i < 26; i++)
-    {
-        count = count + letExist[i];
-    }
+  }
+  for (size_t i = 0; i < 26; i++)
+  {
+    count = count + letExist[i];
+  }
 
-    return count;
+  return count;
 }

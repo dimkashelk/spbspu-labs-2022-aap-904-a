f45d0cde57718e6d803e3d1153c238724c068ece
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2022-12-14 12:48:18 +0300

    Fixed counter_of_elements_divisible_without_remainder.h

--- mishanina.elena/I2/counter_of_elements_divisible_without_remainder.h
+++ mishanina.elena/I2/counter_of_elements_divisible_without_remainder.h
@@ -1,5 +1,6 @@
 #ifndef COUNTER_OF_ELEMENTS_DIVISIBLE_WITHOUT_REMAINDER_H
 #define COUNTER_OF_ELEMENTS_DIVISIBLE_WITHOUT_REMAINDER_H
+#include <cstddef>
 
 size_t counter_of_elements_divisible_without_remainder(const int* array, size_t size, int cur_num);
 

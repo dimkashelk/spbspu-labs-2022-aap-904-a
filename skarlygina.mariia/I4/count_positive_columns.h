7976494ee87b776795daaddd92e2261bcfa69b75
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-12-14 18:24:36 +0300

    try1

--- skarlygina.mariia/I4/count_positive_columns.h
+++ skarlygina.mariia/I4/count_positive_columns.h
@@ -1,5 +1,6 @@
 #ifndef COUNT_POSITIVE_COLUMNS_H
 #define COUNT_POSITIVE_COLUMNS_H
 #include <cstddef>
-size_t countPositiveColumns(const int** array, size_t rows, size_t columns);
+typedef const int* array;
+size_t countPositiveColumns(array*, size_t rows, size_t columns);
 #endif

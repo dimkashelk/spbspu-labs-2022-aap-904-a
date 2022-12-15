0ee0f7b74f97125ded1927dc22fa5f196301ed25
Artem <artrom170@gmail.com>
2022-12-15 12:22:46 +0300

    wrote getting array and cols-rows logic

--- romanovich.artem/I4/main.cpp
+++ romanovich.artem/I4/main.cpp
@@ -1,2 +1,74 @@
-int main(){
+#include <iostream>
+#include <fstream>
+#include <cstring>
+int main(int argc, char **argv)
+{
+  if (argc < 4)
+  {
+    std::cerr << "Not enough parameters.\n";
+    return 2;
+  }
+  if (argc > 4)
+  {
+    std::cerr << "Too much parameters.\n";
+    return 2;
+  }
+  char* taskN = argv[1];
+  std::string fileIn = argv[2];
+  std::string fileOut = argv[3];
+  std::fstream fileInput(fileIn);
+  if (!strcmp(taskN, "1"))
+  {
+    if (!fileInput.is_open())
+    {
+      std::cerr << "Error while opening file " << fileIn << ".\n";
+      return 2;
+    }
+    while (!fileInput.eof())
+    {
+      size_t rows = 0;
+      fileInput >> rows;
+      if (!fileInput)
+      {
+        std::cerr << "Error while reading rows.\n";
+        return 2;
+      }
+      size_t columns = 0;
+      fileInput >> columns;
+      if (!fileInput)
+      {
+        std::cerr << "Error while reading columns.\n";
+        return 2;
+      }
+      size_t arraySize = rows * columns;
+      int array[arraySize];
+      //std::cout << rows << " " << columns << " " << arraySize << "\n";
+      if (arraySize <= 1000)
+      {
+        for (size_t i = 0; i < arraySize; i++)
+        {
+          fileInput >> array[i];
+          //std::cout << array[i] << "\n";
+          if (!fileInput)
+          {
+            std::cerr << "Error while reading file " << fileIn << ".\n";
+            return 2;
+          }
+        }
+        for (size_t i = 0; i < rows; i++)
+        {
+          for (size_t j = 0; j < columns; j++)
+          {
+          //std::cout << i << "-" << j << "-" << i*rows + j << " " << array[i*rows + j] << " ";
+          }
+          //std::cout << "\n";
+        }
+      }
+      else
+      {
+        std::cerr << "Maximum possible array size exceeded.\n";
+        return 2;
+      }
+    }
+  }
 }

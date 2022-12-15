0b3fb40fe667086b5a9a3f5ead918e64dc9e1bc3
Gulkov Mikhail <gulkov.mikhail@mail.ru>
2022-12-15 19:45:33 +0300

    Remove unused var

--- gulkov.mikhail/I3/SumStrings.cpp
+++ gulkov.mikhail/I3/SumStrings.cpp
@@ -6,11 +6,11 @@ char *sum_strings(char *arr_sum, const char *cstring_one, const char *cstring_tw
 
   size_t size_sum = 0;
 
-  for (size_t i = 0, j = 0; *cstring_one || *cstring_two; i++)
+  for (size_t i = 0; *cstring_one || *cstring_two; i++)
   {
     if ((i % 2 == 0 && *cstring_one) || !(*cstring_two))
     {
-      arr_sum[i] = cstring_one[j];
+      arr_sum[i] = *cstring_one;
       size_sum++;
       if (!(*cstring_two))
       {
@@ -19,7 +19,7 @@ char *sum_strings(char *arr_sum, const char *cstring_one, const char *cstring_tw
     }
     else if ((i % 2 != 0 && *cstring_two) || !(*cstring_one))
     {
-      arr_sum[i] = cstring_two[j];
+      arr_sum[i] = *cstring_two;
       size_sum++;
       cstring_two++;
     }

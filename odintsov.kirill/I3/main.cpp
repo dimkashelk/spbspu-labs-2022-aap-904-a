6e0538bce27407b7e17fd008765aad0dbc68b9e1
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-15 23:19:30 +0300

    Rewrote main with struct

--- odintsov.kirill/I3/main.cpp
+++ odintsov.kirill/I3/main.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
+#include <stdexcept>
 #include "cstr.hpp"
 
 int main()
 {
-  size_t size = 0;
-  size_t capacity = 10;
-  char* cstr = new char[capacity];
+  const size_t cap = 10;
+  odintsov::CString cstr(cap);
   std::cin >> std::noskipws;
-  do {
-    if (size == capacity) {
-      try {
-        char* newstr = new char[capacity + 20];
-        for (auto i = newstr, j = cstr; j != cstr + size; i++, j++) {
-          *i = *j;
-        }
-        delete [] cstr;
-        cstr = newstr;
-        capacity += 20;
-      } catch (...) {
-         delete [] cstr;
-         return 1;
+  try {
+    char c = '\0';
+    do {
+      std::cin >> c;
+      if (!std::cin) {
+        std::cout << "Error: Input error\n";
+        return 1;
       }
-    }
-    std::cin >> cstr[size];
-  } while (std::cin && cstr[size++] != '\n');
-  cstr[size - 1] = '\0';
-  std::cout << cstr << '\n';
-  delete [] cstr;
+      if (c == '\n') {
+        break;
+      }
+      cstr.append(c);
+    } while (std::cin && c != '\n');
+  } catch (const std::invalid_argument& err) {
+    std::cout << err.what() << '\n';
+    return 1;
+  }
+  std::cout << cstr.str << '\n';
 }

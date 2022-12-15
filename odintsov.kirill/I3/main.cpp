ae7fd854cdac59b69ce3a5cd5f4cc9a7bb6b1531
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-15 18:05:58 +0300

    Cstring input

--- odintsov.kirill/I3/main.cpp
+++ odintsov.kirill/I3/main.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
+
 int main()
 {
+  size_t size = 0;
+  size_t capacity = 10;
+  char* cstr = new char[capacity];
+  std::cin >> std::noskipws;
+  do {
+    if (size == capacity) {
+      try {
+        char* newstr = new char[capacity + 20];
+        for (auto i = newstr, j = cstr; j != cstr + size; i++, j++) {
+          *i = *j;
+        }
+        delete [] cstr;
+        cstr = newstr;
+        capacity += 20;
+      } catch (...) {
+         delete [] cstr;
+         return 1;
+      }
+    }
+    std::cin >> cstr[size];
+  } while (std::cin && cstr[size++] != '\n');
+  cstr[size - 1] = '\0';
+  std::cout << cstr << '\n';
+  delete [] cstr;
 }

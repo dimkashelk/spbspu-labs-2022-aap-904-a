8991c84fddcd231d7aab602d91671578f3969171
Yaroslava <aroslavakotova@gmail.com>
2022-12-15 22:51:42 +0300

    fixed

--- kotova.yaroslava/I2/main.cpp
+++ kotova.yaroslava/I2/main.cpp
@@ -26,6 +26,12 @@ int main(int argc, char* argv[])
   unsigned int arr2_index_m = 1;
   unsigned int arr2_index_n = 6;
   std::cin >> size_arr2;
+  if (!std::cin)
+  {
+    std::cerr << "Error...\n";
+    return 1;
+  }
+
   if (size_arr2 == 0)
   {
     std::cout<< "Empty array\n";

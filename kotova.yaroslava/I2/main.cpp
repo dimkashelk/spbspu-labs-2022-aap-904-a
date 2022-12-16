e3883fab9075147709be2852feac7fced3f9d88a
Yaroslava <aroslavakotova@gmail.com>
2022-12-16 22:34:10 +0300

    try

--- kotova.yaroslava/I2/main.cpp
+++ kotova.yaroslava/I2/main.cpp
@@ -14,8 +14,8 @@ int main(int argc, char* argv[]){
   size_t arr1_index_m = 1;
   size_t arr1_index_n = 6;
 
-  std::cout<< counterDivision(firstArray, 10, divider) << " ";
-  std::cout << checkRepeatEl(firstArray, 10, arr1_index_m, arr1_index_n) << " ";
+  std::cout<< counterDivision(FirstArray, 10, divider) << " ";
+  std::cout << checkRepeatEl(FirstArray, 10, arr1_index_m, arr1_index_n) << " ";
 //2
   size_t size_arr2 = 0;
   size_t arr2_index_m = 1, arr2_index_n = 6;
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]){
     std::cout << "Empty array\n";
   }
   else{
-    int* SecondArray = new int[size_arr2];
+    int* secondArray = new int[size_arr2];
     std::srand(time(0));
     for (size_t i = 0; i < size_arr2; i++){
       secondArray[i] = std::rand();
@@ -33,6 +33,7 @@ int main(int argc, char* argv[]){
       size_t cnt_div_arr2 = counterDivision(secondArray, size_arr2, divider);
       size_t cnt_check_rep_el = checkRepeatEl(secondArray, size_arr2, arr2_index_m, arr2_index_n);
       std::cout << cnt_div_arr2 << " " << cnt_check_rep_el << " ";
+    }
     catch (const std::invalid_argument& e) {
       std::cout << e.what();
       delete[] secondArray;
@@ -54,7 +55,7 @@ int main(int argc, char* argv[]){
       std::cout << " reading file with an error ";
       return 1;
     }
-    int* thirdArray = new int[size_arr3};
+    int* thirdArray = new int[size_arr3];
     for (size_t i = 0; i < size_arr3; i++){
       file_arr >> thirdArray[i];
       if (!file_arr)
@@ -64,9 +65,10 @@ int main(int argc, char* argv[]){
         return 1;
       }
     }
-  size_t cnt_div_arr3 = counterDivision(thirdArray, size_arr3, divider);
-  size_t cnt_check_rep_el3 =  checkRepeatEl(thirdArray, size_arr3, arr3_index_m, arr3_index_n);
-  std::cout << cnt_div_arr3 << " " << cnt_check_rep_el3 << " ";
-  delete[] thirdArray;
+    size_t cnt_div_arr3 = counterDivision(thirdArray, size_arr3, divider);
+    size_t cnt_check_rep_el3 =  checkRepeatEl(thirdArray, size_arr3, arr3_index_m, arr3_index_n);
+    std::cout << cnt_div_arr3 << " " << cnt_check_rep_el3 << " ";
+    delete[] thirdArray;
+  }
   return 0;
 }

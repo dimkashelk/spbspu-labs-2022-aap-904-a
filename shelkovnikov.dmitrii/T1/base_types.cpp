18b5f632e1f4ca6d11bbf1c986a9a577558b0308
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-17 01:24:37 +0300

    add copying constructor

--- shelkovnikov.dmitrii/T1/base_types.cpp
+++ shelkovnikov.dmitrii/T1/base_types.cpp
@@ -1,5 +1,9 @@
 #include "base_types.h"
 #include <cmath>
+point_t::point_t(const point_t &point):
+  x(point.x),
+  y(point.y)
+{}
 point_t rectangle_t::getLeftDownPoint() const
 {
   return point_t(x - width / 2, y - height / 2);

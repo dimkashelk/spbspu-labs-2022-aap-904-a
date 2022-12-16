671d0d5c0054df0f3cebb92c8b6c084eb5ca7514
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-17 00:44:17 +0300

    add move by delta x and delta y method

--- shelkovnikov.dmitrii/T1/rectangle.cpp
+++ shelkovnikov.dmitrii/T1/rectangle.cpp
@@ -13,6 +13,11 @@ rectangle_t Rectangle::getFrameRect() const
 {
   return rectangle;
 }
+void Rectangle::move(double delta_x, double delta_y)
+{
+  rectangle.point.x += delta_x;
+  rectangle.point.y += delta_y;
+}
 void Rectangle::move(point_t point) const
 {
   rectangle.point = point;

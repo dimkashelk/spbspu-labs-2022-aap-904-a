568477ee57ff5b8980844cda4ed8f2caa85fc336
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-17 00:43:59 +0300

    update virtual method

--- shelkovnikov.dmitrii/T1/shape.h
+++ shelkovnikov.dmitrii/T1/shape.h
@@ -7,7 +7,7 @@ public:
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
   virtual void move(point_t point) const = 0;
-  virtual void move(double delta_x, double delta_y) const = 0;
+  virtual void move(double delta_x, double delta_y) = 0;
   virtual void scale(double k) const = 0;
   virtual ~Shape() = default;
 };

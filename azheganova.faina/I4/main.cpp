9b327dd5afea73dc7a75d06b553308f63e3a1d23
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-16 20:26:34 +0300

    first function completed

--- azheganova.faina/I4/main.cpp
+++ azheganova.faina/I4/main.cpp
@@ -1,2 +1,81 @@
-int main()
-{}
+#include<iostream>
+#include <fstream>
+#include <cstddef>
+
+size_t countRowsWithDifferentElements(int* arr, const size_t m, const size_t n)
+{
+    size_t count = 0;
+    size_t elementscounter = 0;
+    for (size_t i = 0; i < m; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                elementscounter++;
+            }
+        }
+        if (elementscounter == 0)
+        {
+            count++;
+        }
+        elementscounter = 0;
+    }
+    return count;
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc > 4)
+  {
+    std::cout << "Too much parameters";
+    return 1;
+  }
+  if (argc == 1)
+  {
+    std::cout << "No file name";
+    return 1;
+  }
+  size_t m = 0;
+  size_t n = 0;
+  std::ifstream inputfile(argv[2]);
+  if (!inputfile.is_open())
+  {
+    std::cout << "file open error";
+    return 1;
+  }
+  inputfile >> m;
+  inputfile >> n;
+  size_t size = m * n;
+  std::ofstream outputfile(argv[3]);
+  if (!outputfile.is_open())
+  {
+    std::cout << "file open error";
+    return 1;
+  }
+  if (!std::strcmp(argv[1], "1"))
+  {
+    int arr[1000];
+    if (size > 1000)
+    {
+      std::cout << "too much elements";
+      return 1;
+    }
+    for (size_t i = 0; i < size; ++i)
+    {
+      inputfile >> arr[i];
+      if (!inputfile)
+      {
+        std::cerr << "elements read error";
+        return 1;
+      }
+    }
+    size_t countdifferentelements = countRowsWithDifferentElements(arr, m, n);
+    outputfile << countdifferentelements << "\n";
+    if (!outputfile)
+    {
+      std::cout << "number write error";
+      return 1;
+    }
+  }
+}

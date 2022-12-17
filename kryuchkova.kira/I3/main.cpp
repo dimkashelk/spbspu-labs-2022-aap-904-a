9247873d73f4273011b09ec95eb80fabb546f101
KiraGelaz <kiragelaz@yandex.ru>
2022-12-17 19:34:03 +0300

    trying to fix valgrind errors

--- kryuchkova.kira/I3/main.cpp
+++ kryuchkova.kira/I3/main.cpp
@@ -48,7 +48,7 @@ int main()
   try
   {
     cstring_2 = input_string(cstring_2, capacity_2);
-    cstring_2[capacity - 1] = '\0';
+    cstring_2[capacity_2 - 1] = '\0';
   }
   catch(std::exception &e)
   {

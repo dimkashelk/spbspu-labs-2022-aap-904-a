d709ea5ee8b7c488151f90f513737d75839ded7d
Chemodurovart <chemodurovart@gmail.com>
2022-12-17 11:06:31 +0300

    try to fix possiblylost

--- chemodurov.artem/I5/main.cpp
+++ chemodurov.artem/I5/main.cpp
@@ -5,7 +5,16 @@
 
 int main()
 {
-  char * c_string = chemodurov::getCString(std::cin);
+  char * c_string = nullptr;
+  try
+  {
+    c_string = chemodurov::getCString(std::cin);
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
   std::cout << std::boolalpha << chemodurov::isRealNumber(c_string) << "\n";
   delete [] c_string;
   return 0;

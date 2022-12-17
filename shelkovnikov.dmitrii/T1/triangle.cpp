42875413e09a98dee4ada7960dc499add1bcbd29
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-17 12:38:45 +0300

    add constructor

--- shelkovnikov.dmitrii/T1/triangle.cpp
+++ shelkovnikov.dmitrii/T1/triangle.cpp
@@ -1,2 +1,16 @@
 #include "triangle.h"
-
+#include <cmath>
+#include <stdexcept>
+Triangle::Triangle(point_t p1, point_t p2, point_t p3):
+  point1(p1),
+  point2(p2),
+  point3(p3)
+{
+  double side1 = sqrt(pow((point1.x - point2.x), 2) + pow(point1.y - point2.y, 2));
+  double side2 = sqrt(pow((point1.x - point3.x), 2) + pow(point1.y - point3.y, 2));
+  double side3 = sqrt(pow((point3.x - point2.x), 2) + pow(point3.y - point2.y, 2));
+  if (side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2)
+  {
+    throw std::logic_error("It's not a triangle");
+  }
+}
--- shelkovnikov.dmitrii/T1/triangle.h
+++ shelkovnikov.dmitrii/T1/triangle.h
@@ -2,5 +2,12 @@
 #define SPBSPU_LABS_2022_AAP_904_A_TRIANGLE_H
 #include "shape.h"
 class Triangle: public Shape
-{};
+{
+public:
+  Triangle(point_t point1, point_t point2, point_t point3);
+private:
+  point_t point1;
+  point_t point2;
+  point_t point3;
+};
 #endif

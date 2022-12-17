4e53d739a55a946939302b778743befa5ec78801
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-17 16:13:45 +0300

    create Regular class

---
+++ shelkovnikov.dmitrii/T1/regular.cpp
@@ -0,0 +1,1 @@
+#include "regular.h"
---
+++ shelkovnikov.dmitrii/T1/regular.h
@@ -0,0 +1,6 @@
+#ifndef SPBSPU_LABS_2022_AAP_904_A_REGULAR_H
+#define SPBSPU_LABS_2022_AAP_904_A_REGULAR_H
+#include "shape.h"
+class Regular: public Shape
+{};
+#endif

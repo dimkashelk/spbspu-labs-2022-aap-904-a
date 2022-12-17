745f198fe11f2297ad5cdc0d2efb365487f278b7
Chemodurovart <chemodurovart@gmail.com>
2022-12-17 19:13:37 +0300

    fixed operator()

--- chemodurov.artem/I6/sqrt1minusxsquare.cpp
+++ chemodurov.artem/I6/sqrt1minusxsquare.cpp
@@ -1,1 +1,6 @@
 #include "sqrt1minusxsquare.h"
+
+double Sqrt1MinusXSquare::operator()()
+{
+  return 0.0;
+}
--- chemodurov.artem/I6/sqrt1minusxsquare.h
+++ chemodurov.artem/I6/sqrt1minusxsquare.h
@@ -2,12 +2,14 @@
 #define SQRT1MINUSXSQUARE_H
 #include <cstddef>
 
-struct sqrt1minusxsquare
+struct Sqrt1MinusXSquare
 {
-  double operator()(double error);
+  double operator()();
 
   double next;
   size_t members;
+  size_t max_members;
+  double error;
 };
 
 #endif

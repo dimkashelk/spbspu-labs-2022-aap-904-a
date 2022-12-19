18de3beb785493720a7e287a977bac392b796c56
KiraGelaz <kiragelaz@yandex.ru>
2022-12-20 02:48:04 +0300

    Reading matrix from file

--- kryuchkova.kira/I4/main.cpp
+++ kryuchkova.kira/I4/main.cpp
@@ -5,27 +5,83 @@
 #include "unique_elements.h"
 #include "sum_elements_of_new_matrix.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-  // if (argc != 4)
-  // {
-  //   std::cout << "Invalid number of arguments." << '\n';
-  //   return 1;
-  // }
-  // size_t n = 0;
-  // size_t m = 0;
-  // std::ifstream file(argv[2]);
-  // file >> n >> m;
-  // if (!file)
-  // {
-  //   std::cout << "Error";
-  //   return 1;
-  // }
-  int matrix[20] = {0, 1, 2, 3,
-                    4, 5, 6, 7,
-                    8, 9, 10, 11,
-                    12, 13, 14, 15};
-  std::cout << unique_elements(matrix, 4, 4) << '\n';
-  std::cout << sum_elements_of_new_matrix(matrix, 4, 4) << '\n';
-  
+  if (argc != 4)
+  {
+    std::cout << "Invalid number of arguments." << '\n';
+    return 1;
+  }
+  size_t rows = 0;
+  size_t columns = 0;
+  std::ifstream file(argv[2]);
+  file >> rows >> columns;
+  if (!file)
+  {
+    std::cout << "Error";
+    return 1;
+  }
+  if (!strcmp(argv[1], "1"))
+  {
+    int matrix[1000];
+    for (size_t i = 0; i < rows; i++)
+    {
+      for (size_t j = 0; j < columns; j++)
+      {
+        file >> matrix[rows * i + j];
+        if (!file)
+        {
+          std::cout << "Error";
+          return 1;
+        }
+      }
+    }
+    std::ofstream out(argv[3]);
+    try
+    {
+      out << unique_elements(matrix, rows, columns) << "\n";
+    }
+    catch (const std::runtime_error &e)
+    {
+      std::cout << e.what();
+    }
+  }
+  else if (!strcmp(argv[1], "2"))
+  {
+    if (rows != columns)
+    {
+      std::cerr << "Matrix must be square" << '\n';
+      return 1;
+    }
+    int *matrix = new int[rows * columns];
+    for (size_t i = 0; i < rows; i++)
+    {
+      for (size_t j = 0; j < columns; j++)
+      {
+        file >> matrix[i * rows + j];
+        if (!file)
+        {
+          std::cout << "Error";
+          delete[] matrix;
+          return 1;
+        }
+      }
+    }
+    std::ofstream out(argv[3]);
+    try
+    {
+      out << sum_elements_of_new_matrix(matrix, rows, columns) << "\n";
+    }
+    catch (const std::runtime_error &e)
+    {
+      std::cout << e.what();
+    }
+    delete[] matrix;
+  }
+  else
+  {
+    std::cout << "No such parameter";
+    return 1;
+  }
+  return 0;
 }
--- kryuchkova.kira/I4/sum_elements_of_new_matrix.cpp
+++ kryuchkova.kira/I4/sum_elements_of_new_matrix.cpp
@@ -18,29 +18,24 @@ double sum_elements_of_new_matrix(int *matrix, size_t rows, size_t columns)
         {
           neighbors++;
           sum_nei = sum_nei + abs(matrix[(i - 1) * rows + j]);
-          std::cout << matrix[(i - 1) * rows + j] << ' ';
         }
         if (j != 0)
         {
           neighbors++;
           sum_nei = sum_nei + abs(matrix[i * rows + (j - 1)]);
-          std::cout << matrix[i * rows + (j - 1)] << ' ';
         }
         if ((i + 1) < rows)
         {
           neighbors++;
           sum_nei = sum_nei + abs(matrix[(i + 1) * rows + j]);
-          std::cout << matrix[(i + 1) * rows + j] << ' ';
         }
         if ((j + 1) < columns)
         {
           neighbors++;
           sum_nei = sum_nei + abs(matrix[i * rows + (j + 1)]);
-          std::cout << matrix[i * rows + (j + 1)] << ' ';
         }
         element = sum_nei / neighbors;
         sum_elements = sum_elements + element;
-        std::cout << '\n';
       }
     }
   }

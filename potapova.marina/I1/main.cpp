23f4f63c2949f87c427851a87835e2f099dc0319
Marinochka.n_n <marina-potapova04@mail.ru>
2022-12-19 14:42:26 +0300

    4 commit

--- potapova.marina/I1/main.cpp
+++ potapova.marina/I1/main.cpp
@@ -1,11 +1,9 @@
 #include "sequence.h"
 #include <iostream>
-
 int main() {
   CounterPythagoreanTriplets pythcnt{0, 0, 0};
   CounterSignChange signchangecnt{0, 0};
   int currval = 0;
-
   do {
     std::cin >> currval;
     if (!std::cin){
@@ -15,7 +13,6 @@ int main() {
     pythcnt(currval);
     signchangecnt(currval);
   } while (std::cin && currval);
-  
   std::cout << pythcnt.count << "\n";
   std::cout << signchangecnt.count << "\n";
   return 0;

1255c2a332eb2cbc698d66c9bf3d4cb61858b0e7
Yaroslava <aroslavakotova@gmail.com>
2022-12-20 19:56:25 +0300

    fixed

--- kotova.yaroslava/I2/CheckRepeatingElements.cpp
+++ kotova.yaroslava/I2/CheckRepeatingElements.cpp
@@ -13,9 +13,9 @@ size_t checkRepeatEl(const int* array, size_t size, size_t index_m, size_t index
   {
     repeat_cnt = 0;
   }
-  for (unsigned int i = index_m; i <= index_n; i++)
+  for (size_t i = index_m; i <= index_n; i++)
   {
-    for (unsigned int j = index_m; j < index_n - 1; j++)
+    for (size_t j = index_m; j < index_n - 1; j++)
     {
       if (array[i] == array[j] && i != j)
       {

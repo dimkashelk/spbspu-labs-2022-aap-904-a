e0483b7cceefb0231dea18238253ffba13cbc48c
Yaroslava <aroslavakotova@gmail.com>
2022-12-22 23:03:18 +0300

    fixed  max

--- kotova.yaroslava/I2/CheckRepeatingElements.cpp
+++ kotova.yaroslava/I2/CheckRepeatingElements.cpp
@@ -4,9 +4,12 @@
 size_t checkRepeatEl(const int* array, size_t size, size_t index_m, size_t index_n)
 {
   size_t repeat_cnt = 0;
-  if (index_n > size)
   {
-    throw std::invalid_argument("Error");
+    size_t max_of_index = std::max(index_m, index_n);
+    if (max_of_index > size)
+    {
+      throw std::invalid_argument("Error");
+    }
   }
   if (index_m == index_n)
   {

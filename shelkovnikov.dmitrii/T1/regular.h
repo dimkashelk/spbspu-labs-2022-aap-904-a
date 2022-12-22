abff5064142cdfb818f74c9d5985500c8115509a
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-23 01:50:37 +0300

    add func

--- shelkovnikov.dmitrii/T1/regular.h
+++ shelkovnikov.dmitrii/T1/regular.h
@@ -7,6 +7,11 @@ class Regular: public Shape
 {
 public:
   Regular(double x1, double y1, double x2, double y2, double x3, double y3);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(point_t point) override;
+  void move(double delta_x, double delta_y) override;
+  void scale(double k) override;
 private:
   Triangle triangle;
   size_t count;

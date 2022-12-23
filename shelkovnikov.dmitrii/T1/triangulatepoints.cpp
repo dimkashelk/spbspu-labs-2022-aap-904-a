87ae49dab42bbefe0f92bce2a2aec006bddbfaeb
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-23 17:57:12 +0300

    update operator()

--- shelkovnikov.dmitrii/T1/triangulatepoints.cpp
+++ shelkovnikov.dmitrii/T1/triangulatepoints.cpp
@@ -13,20 +13,20 @@ Triangle *TriangulatePoints::operator()() const
   //4. If both conditions are met, then we build a triangle A1A2A3, and exclude vertex A2 from the polygon, without touching vertex A1, shift vertices A2 (A2 to A3), A3 (A3 to A4)
   //5. If at least one condition is not met, we move on to the next three vertices.
   //6. Repeat from step 1 until there are three vertices left.
-  for (size_t i = 0; i < size - 2; i++)
+  point_t *point1 = points;
+  size_t index1 = 0;
+  point_t *point2 = points + 1;
+  size_t index2 = 1;
+  point_t *point3 = points + 2;
+  size_t index3 = 2;
+  while (size > 3)
   {
-    for (size_t j = i + 1; j < size - 1; j++)
+    if (get_mixed_product(vector_t(*point3, *point1), vector_t(*point2, *point1)) > 0)
     {
-      for (size_t k = j + 1; k < size; k++)
+      Triangle triangle(*point1, *point2, *point3);
+      if (!containsAnyPoint(triangle))
       {
-        if (get_mixed_product(vector_t(points[k], points[i]), vector_t(points[j], points[i])) > 0)
-        {
-          Triangle triangle(points[i], points[j], points[k]);
-          if (!containsAnyPoint(triangle))
-          {
 
-          }
-        }
       }
     }
   }

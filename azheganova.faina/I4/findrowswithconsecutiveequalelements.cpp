f4343b72a5d4ae1659a225f583284f5c00dc35d6
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-24 01:37:08 +0300

    Laba I4 completed

--- azheganova.faina/I4/findrowswithconsecutiveequalelements.cpp
+++ azheganova.faina/I4/findrowswithconsecutiveequalelements.cpp
@@ -7,7 +7,7 @@ size_t findRowsWithConsecutiveEqualElements(const int* arr, size_t m, size_t n)
   size_t linenumber = 0;
   for (size_t i = 0; i < m; i++)
   {
-    for (size_t j = 0; j < n; j++)
+    for (size_t j = 0; j < n - 1; j++)
     {
       if (arr[i * m + j] == arr[i * m + j + 1])
       {

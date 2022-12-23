a6d868e352f7f7f63d9e6c259d477a43a3333358
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-24 02:34:33 +0300

    add functions to realize

--- shelkovnikov.dmitrii/T1/polygon.h
+++ shelkovnikov.dmitrii/T1/polygon.h
@@ -6,6 +6,11 @@ class Polygon: public Shape
 {
 public:
   Polygon(point_t *points, size_t size);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(point_t point) override;
+  void move(double delta_x, double delta_y) override;
+  void scale(double k) override;
 private:
   Triangle **triangles;
   size_t count;

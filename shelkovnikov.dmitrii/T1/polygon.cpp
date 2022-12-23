1c2b139425ab4f2ec5424c5af7962523948fe8ac
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-24 02:39:57 +0300

    add getArea func

--- shelkovnikov.dmitrii/T1/polygon.cpp
+++ shelkovnikov.dmitrii/T1/polygon.cpp
@@ -7,3 +7,12 @@ Polygon::Polygon(point_t *points, size_t size):
   TriangulatePoints triangulatePoints(points, size);
   triangles = triangulatePoints(count);
 }
+double Polygon::getArea() const
+{
+  double global_area = 0.0;
+  for (size_t i = 0; i < count; i++)
+  {
+    global_area += triangles[i]->getArea();
+  }
+  return global_area;
+}

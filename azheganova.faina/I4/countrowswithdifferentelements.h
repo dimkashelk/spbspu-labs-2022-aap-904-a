bfe75d8fc704349bf20f9118a7a4c4f1dab9bef9
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-24 01:32:27 +0300

    fixed functions lab I4

--- azheganova.faina/I4/countrowswithdifferentelements.h
+++ azheganova.faina/I4/countrowswithdifferentelements.h
@@ -2,5 +2,5 @@
 #define COUNTROWSWITHDIFFERENTELEMENTS_H
 #include <cstddef>
 
-size_t countRowsWithDifferentElements(int* arr, size_t m, size_t n);
+size_t countRowsWithDifferentElements( const int* arr, size_t m, size_t n);
 #endif
--- azheganova.faina/I4/findrowswithconsecutiveequalelements.h
+++ azheganova.faina/I4/findrowswithconsecutiveequalelements.h
@@ -2,5 +2,5 @@
 #define FINDROWSWITHCONSECUTIVEEQUALELEMENTS_H
 #include <cstddef>
 
-size_t findRowsWithConsecutiveEqualElements(int* arr, size_t m, size_t n);
+size_t findRowsWithConsecutiveEqualElements( const int* arr, size_t m, size_t n);
 #endif

bfcbd8810701d51cb14f47df4951af85117be8bf
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-24 02:28:17 +0300

    add constructor

--- shelkovnikov.dmitrii/T1/polygon.cpp
+++ shelkovnikov.dmitrii/T1/polygon.cpp
@@ -1,1 +1,9 @@
 #include "polygon.h"
+#include "triangulatepoints.h"
+Polygon::Polygon(point_t *points, size_t size):
+  triangles(nullptr),
+  count(0)
+{
+  TriangulatePoints triangulatePoints(points, size);
+  triangles = triangulatePoints(count);
+}

22a7e7945fd7646d9e41ec5df679bad755b524a3
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-23 15:55:58 +0300

    create TriangulatePoints

---
+++ shelkovnikov.dmitrii/T1/triangulatepoints.cpp
@@ -0,0 +1,1 @@
+#include "triangulatepoints.h"
---
+++ shelkovnikov.dmitrii/T1/triangulatepoints.h
@@ -0,0 +1,15 @@
+#ifndef SPBSPU_LABS_2022_AAP_904_A_TRIANGULATEPOINTS_H
+#define SPBSPU_LABS_2022_AAP_904_A_TRIANGULATEPOINTS_H
+#include <cstddef>
+#include "base_types.h"
+#include "triangle.h"
+class TriangulatePoints
+{
+public:
+  TriangulatePoints(point_t *points, size_t size);
+  Triangle* operator()();
+private:
+  point_t *points;
+  size_t size;
+};
+#endif

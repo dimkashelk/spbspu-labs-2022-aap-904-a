55c30f04dd50eb310114af2755d7eeb60e42a092
Artem <artrom170@gmail.com>
2022-12-24 12:52:58 +0300

    ternary operator not postincrement

--- romanovich.artem/I4/smoothedmatrix.cpp
+++ romanovich.artem/I4/smoothedmatrix.cpp
@@ -4,18 +4,10 @@ int sumClose(const int *matrix, size_t row, size_t column, size_t matrixN)
 {
   int sum_ = 0;
   int count = 0;
-  for (size_t k = row; k <= row+2; k++)
+  for (size_t k = (row>0) ? row-1 : row; k <= row+1; k++)
   {
-    if (k > 0)
+    for (size_t l = (column>0) ? column-1 : column; l <= column+1; l++)
     {
-      k--;
-    }
-    for (size_t l = column; l <= column+2; l++)
-    {
-      if (l > 0)
-      {
-        l--;
-      }
       size_t elemIndex = k*matrixN+l;
       bool elemInMatrixRange = (elemIndex <= matrixN*matrixN);
       bool elemNotCenterElem = !(k==row && l==column);

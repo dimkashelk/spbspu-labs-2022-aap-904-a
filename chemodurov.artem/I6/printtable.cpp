3cfa06ebff44b5154c47f85a31085f6ea70842a8
Chemodurovart <chemodurovart@gmail.com>
2022-12-24 14:07:48 +0300

    add std::fixed

--- chemodurov.artem/I6/printtable.cpp
+++ chemodurov.artem/I6/printtable.cpp
@@ -1,22 +1,13 @@
 #include "printtable.h"
 #include <iomanip>
 #include <cmath>
-#include <limits>
 #include "sqrt1minusxsquare.h"
 
 void printRowInTable(std::ostream & out, double x, size_t max_members, double error)
 {
-  const double epsilon = std::numeric_limits< double >::epsilon();
-  if (std::abs(x) < epsilon)
-  {
-    out << std::setw(10) << 0.0 << ' ' << std::setw(10) << 1 << ' ' << std::setw(10) << 1;
-  }
-  else
-  {
-    out << std::setw(10) << std::setprecision(4) << x << ' ';
-    out << std::setw(10) << std::setprecision(5) << chemodurov::sqrt1MinusXSquare(x, max_members, error) << ' ';
-    out << std::setw(10) << std::setprecision(5) << 1 / (std::sqrt(1 - x * x));
-  }
+  out << std::fixed << std::setw(10) << std::setprecision(4) << x << ' ';
+  out << std::setw(10) << std::setprecision(5) << chemodurov::sqrt1MinusXSquare(x, max_members, error) << ' ';
+  out << std::setw(10) << std::setprecision(5) << 1 / (std::sqrt(1 - x * x));
 }
 
 void chemodurov::printTable(std::ostream & out, double left, double right, double step, size_t max_mem, double err)

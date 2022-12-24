46f4d92083ea59069dddfed8302d054d6112bc2c
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-24 22:05:02 +0300

    add getFrameRect

--- shelkovnikov.dmitrii/T1/polygon.cpp
+++ shelkovnikov.dmitrii/T1/polygon.cpp
@@ -1,4 +1,5 @@
 #include "polygon.h"
+#include <algorithm>
 #include "triangulatepoints.h"
 Polygon::Polygon(point_t *points, size_t size):
   triangles(nullptr),
@@ -15,4 +16,20 @@ double Polygon::getArea() const
     global_area += triangles[i]->getArea();
   }
   return global_area;
+}
+rectangle_t Polygon::getFrameRect() const
+{
+  double x_min = triangles[0]->getFrameRect().getLeftDownPoint().x;
+  double y_min = triangles[0]->getFrameRect().getLeftDownPoint().y;
+  double x_max = x_min;
+  double y_max = y_min;
+  for (size_t i = 0; i < count; i++)
+  {
+    rectangle_t rectangle = triangles[i]->getFrameRect();
+    x_min = std::min(x_min, rectangle.getLeftDownPoint().x);
+    y_min = std::min(y_min, rectangle.getLeftDownPoint().y);
+    x_max = std::max(x_max, rectangle.getRightUpPoint().x);
+    y_max = std::max(y_max, rectangle.getRightUpPoint().y);
+  }
+  return rectangle_t(x_min, y_min, x_max, y_max);
 }

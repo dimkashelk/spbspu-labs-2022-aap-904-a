97c8d650ffb86f7110c1f634c786c01bbe07bf82
Chemodurovart <chemodurovart@gmail.com>
2022-12-24 17:53:04 +0300

    add base-types.hpp, point_t, rectangle_t

---
+++ chemodurov.artem/T1/base-types.hpp
@@ -0,0 +1,17 @@
+#ifndef BASE_TYPES_HPP
+#define BASE_TYPES_HPP
+
+struct point_t
+{
+  double x;
+  double y;
+};
+
+struct rectange_t
+{
+  double width;
+  double height;
+  point_t pos;
+};
+
+#endif

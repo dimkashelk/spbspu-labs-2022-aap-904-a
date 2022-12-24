32d3a7765a73c007dd6032b24c30ee39075b4df6
Artem <artrom170@gmail.com>
2022-12-24 22:05:31 +0300

    checkpoint to save

--- romanovich.artem/I5/main.cpp
+++ romanovich.artem/I5/main.cpp
@@ -1,4 +1,5 @@
 #include "iostream"
+bool isUnsignedInteger1(const char *data);
 bool isZero(char c)
 {
   return (c == '0');
@@ -23,46 +24,44 @@ bool isLetterE(char c)
 {
   return (c == 'E');
 }
-bool isUnsignedInteger(const char *data)
+bool isUnsignedInteger2(const char *data)
 {
-  return isDigit(*data) && (isDigit(*data) && isUnsignedInteger(data + 1) || isStringEnd(*(data + 1)));
+  return isDigit(*data) && (isDigit(*data) && isUnsignedInteger2(data + 1) || isStringEnd(*(data + 1)));
 }
 bool isSignificand(const char *data)
 {
-  return isUnsignedInteger(data + 1) || (isDot(*data) && isUnsignedInteger(data + 1));
+  return isUnsignedInteger1(data + 1) || isUnsignedInteger2(data + 1) ||
+         (isDot(*data) && isUnsignedInteger1(data + 1));
 }
 bool isBase(const char *data)
 {
-  return (isLetterE(*data) && isNumberSign(*(data + 1)) && isUnsignedInteger(data + 2)) ||
-         (isLetterE(*data) && isUnsignedInteger(data + 1));
+  return isLetterE(*data) && ((isNumberSign(*(data + 1)) && isUnsignedInteger2(data + 2)) || isUnsignedInteger2(data + 1));
+}
+bool isUnsignedInteger1(const char *data)
+{
+  return isDigit(*data) && (isDigit(*data) && isUnsignedInteger1(data + 1) || isBase(data + 1));
 }
 bool isFloat(const char *data)
 {
-  return !isZero(*data) && (isSignificand(data) && isBase(data)) ||
-         (isNumberSign(*data) && isSignificand(data) && isBase(data));
+  return !isZero(*data) && (isSignificand(data)) || (isNumberSign(*data) && isSignificand(data + 1));
 }
 int main()
 {
   const char *testArr[] = {
-    "5",
-    "-6",
-    "333333335",
-    ".33335",
-    "3.33335",
-    "555.5555",
-    "555.55.55",
-    ".123E2",
-    ".123E-9",
-    ".123E+12",
-    "E-9",
-    "E-",
-    "E3",
-    "E33245364",
-    "E",
-    "E+12"
+    "", ".92", ".4", "4", "92", "+", "+.92", "+.4", "+4",
+    "+92",
+    "E+92", ".92E+92", ".4E+92", "4E+92", "92E+92", "+E+92", "+.92E+92", "+.4E+92", "+4E+92", "+92E+92", "E+4",
+    ".92E+4", ".4E+4",
+    "4E+4",
+    "92E+4",
+    "+E+4", "+.92E+4", "+.4E+4", "+4E+4", "+92E+4", "E4", ".92E4", ".4E4",
+    "4E4",
+    "92E4", "+E4", "+.92E4", "+.4E4", "+4E4", "+92E4", "E92", ".92E92", ".4E92", "4E92",
+    "92E92", "+E92", "+.92E92", "+.4E92", "+4E92",
+    "+92E92",
   };
   for (auto test: testArr)
   {
-    std::cout << isBase(test) << " " << test << "\n";
+    if(!isFloat(test)) std::cout << isFloat(test) << " " << test << "\n";
   }
 }

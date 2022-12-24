7b67bfc0e26e499a212b9adbbcfce2be67e5429d
Chemodurovart <chemodurovart@gmail.com>
2022-12-24 19:22:29 +0300

    add private and constructor in Rectangle

--- chemodurov.artem/T1/rectangle.hpp
+++ chemodurov.artem/T1/rectangle.hpp
@@ -8,13 +8,14 @@ namespace chemodurov
   class Rectangle: public chemodurov::Shape
   {
    public:
+    Rectangle(chemodurov::point_t left_down, chemodurov::point_t right_up);
     double getArea() const;
     chemodurov::rectangle_t getFrameRect() const;
     void move(chemodurov::point_t position);
     void move(double dx, double dy);
     void scale(double k);
    private:
-    
+    chemodurov::point_t left_down_, right_up_, center_;
   };
 }
 

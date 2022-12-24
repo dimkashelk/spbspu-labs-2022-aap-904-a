30a7dda50c86a6366842db8205b4a34fb3cf3acc
Artem <artrom170@gmail.com>
2022-12-24 19:33:36 +0300

    proper isUnsignedInteger func

---
+++ romanovich.artem/I5/main.cpp
@@ -0,0 +1,65 @@
+#include "iostream"
+/*bool isZero(char c)//+
+{
+  return (c == '0');
+}
+bool isNumberSign(char c)//+
+{
+  return (c == '+') || (c == '-');
+}*/
+bool isDigit(char c)//+
+{
+  return std::isdigit(c);
+}
+bool isStringEnd(char c)
+{
+  return (c == '\0');
+}
+/*bool isDot(char c)//+
+{
+  return (c == '.') || (c == ',');
+}
+bool isLetterE(char c)//+
+{
+  return (c == 'E');
+}*/
+bool isUnsignedInteger(const char *data)
+{
+  return isDigit(*data) && (isDigit(*data) && isUnsignedInteger(data + 1) || isStringEnd(*(data + 1)));
+}
+/*bool isSignificand(const char *data)
+{
+  return isUnsignedInteger(data) || (isDot(*data) && isUnsignedInteger(data + 1));
+}
+bool isBase(const char *data)
+{
+  return (isLetterE(*data) && isNumberSign(*data) && isUnsignedInteger(data + 1)) ||
+         (isLetterE(*data) && isUnsignedInteger(data + 1));
+}
+bool isFloat(const char *data)
+{
+  return !isZero(*data) && (isSignificand(data) && isBase(data)) || (isNumberSign(*data) && isSignificand(data) && isBase(data));
+}*/
+int main()
+{
+  const char *testArr[] = {
+    "+",
+    "-",
+    "5",
+    "-6",
+    "333333335",
+    "555.5555",
+    "555.55.55",
+    ".123E2",
+    ".123E-9",
+    ".123E+12",
+    "E-9",
+    "E-",
+    "E",
+    "E+12"
+  };
+  for (auto test: testArr)
+  {
+    std::cout << isUnsignedInteger(test) << " " << test << "\n";
+  }
+}

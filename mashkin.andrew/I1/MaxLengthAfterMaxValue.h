24e1383a2128d15ca7da021b8263d8dd4a241f95
Машкин Андрей <beedzibood@gmail.com>
2022-12-24 17:12:35 +0300

    remove uppercase files

--- mashkin.andrew/I1/MaxLengthAfterMaxValue.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAXLENGTHAFTERMAXVALUE_H
-#define MAXLENGTHAFTERMAXVALUE_H
-#include <cstddef>
-
-void maxLengthAfterMaxValue(int value, int& maxValue, size_t& count, size_t& maxCount);
-#endif
--- mashkin.andrew/I1/MaxLengthDescSeq.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAXLENGTHDESCSEQ_H
-#define MAXLENGTHDESCSEQ_H
-#include <cstddef>
-
-void maxLengthDescSeq(int value, int& predValue, size_t& length, size_t& maxLength);
-#endif

0ee3c6272979dee515b06ce8d78ace716ea65f78
Gulkov Mikhail <gulkov.mikhail@mail.ru>
2022-12-24 17:04:43 +0300

    size_t

--- gulkov.mikhail/I4/CountPositiveCols.cpp
+++ gulkov.mikhail/I4/CountPositiveCols.cpp
@@ -2,7 +2,7 @@
 
 size_t countPositiveCols(const int *array, size_t cols, size_t rows)
 {
-  unsigned int positive_cols = 0;
+  size_t positive_cols = 0;
   for (size_t i = 0; i < cols; i++)
   {
     for (size_t j = 0; j < rows; j++)

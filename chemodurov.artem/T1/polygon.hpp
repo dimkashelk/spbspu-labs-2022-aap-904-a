8ce89921a752033da4b2ae02739a9409de4fc687
Chemodurovart <chemodurovart@gmail.com>
2022-12-25 13:46:07 +0300

    add polygon.cpp (almost empty) and polygon.hpp

---
+++ chemodurov.artem/T1/polygon.hpp
@@ -0,0 +1,26 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+#include <cstddef>
+#include "shape.hpp"
+#include "base-types.hpp"
+
+namespace chemodurov
+{
+  class Polygon: public chemodurov::Shape
+  {
+   public:
+    Polygon(chemodurov::point_t * vertices, size_t number_of_vertices, chemodurov::point_t & center);
+    ~Polygon();
+    double getArea() const;
+    chemodurov::rectangle_t getFrameRect() const;
+    void move(chemodurov::point_t position);
+    void move(double dx, double dy);
+    void scale(double k);
+   private:
+    chemodurov::point_t * vertices_;
+    size_t number_of_vertices_;
+    chemodurov::point_t center_;
+  };
+}
+
+#endif

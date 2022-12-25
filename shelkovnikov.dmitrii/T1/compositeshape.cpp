a9eb962b58952132b5bc8d1e69d9f11668ddafa4
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-26 02:49:59 +0300

    add getArea()

--- shelkovnikov.dmitrii/T1/compositeshape.cpp
+++ shelkovnikov.dmitrii/T1/compositeshape.cpp
@@ -9,3 +9,12 @@ CompositeShape::CompositeShape(Shape **shapes, size_t size):
     shapes_[i] = shapes[i]->clone();
   }
 }
+const double CompositeShape::getArea() const
+{
+  double area = 0.0;
+  for (size_t i = 0; i < size_; i++)
+  {
+    area += shapes_[i]->getArea();
+  }
+  return area;
+}

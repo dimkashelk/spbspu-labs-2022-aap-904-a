2ca2b12ff42bafebf8a48af92596cfde714a83a3
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-25 20:26:45 +0300

    indexing function

--- odintsov.kirill/I4/array2d.cpp
+++ odintsov.kirill/I4/array2d.cpp
@@ -1,15 +1,20 @@
 #include "array2d.hpp"
 #include <cstddef>
 
+int odintsov::get2DArrayVal(const int* arr, size_t rows, size_t, size_t x, size_t y)
+{
+  return arr[y * rows + x];
+}
+
 unsigned odintsov::getColAmtThreeRepeatVals(const int* arr, size_t rows, size_t cols)
 {
   unsigned colsThreeRepeat = 0;
   for (size_t i = 0; i < cols; i++) {
     for (size_t j = 0; j < rows - 2; j++) {
-      int val = arr[j * rows + i];
+      int val = get2DArrayVal(arr, rows, cols, i, j);
       unsigned short count = 1;
       for (size_t k = j + 1; k < rows; k++) {
-        if (val == arr[k * rows + i]) {
+        if (val == get2DArrayVal(arr, rows, cols, i, k)) {
           count++;
           if (count == 3) {
             break;
--- odintsov.kirill/I4/array2d.hpp
+++ odintsov.kirill/I4/array2d.hpp
@@ -4,6 +4,7 @@
 #include <cstddef>
 
 namespace odintsov {
+  int get2DArrayVal(const int* arr, size_t rows, size_t cols, size_t x, size_t y);
   unsigned getColAmtThreeRepeatVals(const int* arr, size_t rows, size_t cols);
   unsigned getRowAmtThreeRepeatVals(const int* arr, size_t rows, size_t cols);
 }

145fee2f0d35eccb977e2a6c7eef87fb21de85ec
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-26 02:34:56 +0300

    var 15 I5

--- azheganova.faina/I5/rowvalidation.cpp
+++ azheganova.faina/I5/rowvalidation.cpp
@@ -9,10 +9,6 @@ bool isDigit(char c)
 {
   return std::isdigit(c);
 }
-bool isNotZero(char c)
-{
-  return isDigit(c) && (c != '0');
-}
 bool isEnd(char c)
 {
   return (c == '\0');
@@ -29,29 +25,35 @@ bool continueWithNextDigit(const char * data)
 {
   return isDigit(*data) && (continueWithNextDigit(data + 1) || isEnd(*(data + 1)));
 }
-bool continueWithUnsignedInteger(const char * data)
-{
-  return isDigit(*data) && (isDigit(*(data + 1)) || continueWithUnsignedInteger(data + 1));
-}
 bool continueWithOrder(const char * data)
 {
-  return isE(*data) && isSign(*(data + 1)) && continueWithNextDigit(data + 2);
+  return isE(*data) && continueWithSign(data + 1) && (continueWithDigit(data + 1) || finishedWithDigit(data + 1));
 }
-bool continueWithDotAndUnsignedDigit(const char * data)
+bool continueWithSign(const char * data)
 {
-  return isDot(*data) && continueWithUnsignedInteger(data + 1 );
+  return isSign(*data) && (continueWithSign(data + 1) || continueWithDigit(data + 1));
 }
-bool continueWithUnsignedDigitDotAndUnsignedDigit(const char * data)
+bool continueWithDigit(const char * data)
 {
-  return continueWithUnsignedInteger(data) && isDot(*(data + 1)) && continueWithUnsignedInteger(data + 2);
+  return isDigit(*data) && (continueWithDigit(data + 1) || finishedWithDigit(data + 1));
 }
-bool isMantisa(const char * data)
+bool finishedWithDigit(const char * data)
 {
-  return continueWithDotAndUnsignedDigit(data) || continueWithUnsignedDigitDotAndUnsignedDigit(data) || continueWithUnsignedInteger (data);
+  return isDigit(*data) && isEnd(*(data + 1));
+}
+bool continueWithMantisaPartOne(const char * data)
+{
+  return isDot(*data) && continueWithDigit(data + 1);
+}
+bool continueWithMantisaPartTwo(const char * data)
+{
+  return continueWithDigit(data) && isDot(*(data + 1)) && continueWithDigit(data + 2);
+}
+bool continueWithMantisa(const char * data)
+{
+  return continueWithMantisaPartOne(data) || continueWithMantisaPartTwo(data) || continueWithDigit(data);
 }
 bool isFloat(const char * data)
 {
-  return ((isMantisa(data) && continueWithOrder(data + 1)) || (isSign(*data) && isMantisa(data + 1) && continueWithOrder(data + 2)));
+  return ((continueWithMantisa(data) && continueWithOrder(data + 1)) || (isSign(*data) && continueWithMantisa(data + 1) && continueWithOrder(data + 2)));
 }
-
-

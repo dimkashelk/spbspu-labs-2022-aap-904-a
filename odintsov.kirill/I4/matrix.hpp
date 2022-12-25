b43e11ae0a4c40440e4b453520dea402f902eac8
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-26 00:33:28 +0300

    fixed typo in header

--- odintsov.kirill/I4/matrix.hpp
+++ odintsov.kirill/I4/matrix.hpp
@@ -10,7 +10,7 @@ namespace odintsov {
   bool isThreeRepeatsInCol(const int* arr, size_t rows, size_t cols, size_t col);
   bool isThreeRepeatsInRow(const int* arr, size_t rows, size_t cols, size_t row);
   size_t countColsWithThreeRepeats(const int* arr, size_t rows, size_t cols);
-  size_t coutnRowsWithThreeRepeats(const int* arr, size_t rows, size_t cols);
+  size_t countRowsWithThreeRepeats(const int* arr, size_t rows, size_t cols);
 
 }
 

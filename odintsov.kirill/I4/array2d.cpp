91eccb75bf7b6797a6196f9dcf1c2816a75e1ccf
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-25 20:18:38 +0300

    getColAmtThreeRepeatVals

--- odintsov.kirill/I4/array2d.cpp
+++ odintsov.kirill/I4/array2d.cpp
@@ -1,9 +1,29 @@
 #include <cstddef>
 
-unsigned getColAmtThreeRepeatVals(const int*, size_t, size_t)
+unsigned getColAmtThreeRepeatVals(const int* arr, size_t rows, size_t cols)
 {
-  return 0;
+  unsigned colsThreeRepeat = 0;
+  for (size_t i = 0; i < cols; i++) {
+    for (size_t j = 0; j < rows - 2; j++) {
+      int val = arr[j * rows + i];
+      unsigned short count = 1;
+      for (size_t k = j + 1; k < rows; k++) {
+        if (val == arr[k * rows + i]) {
+          count++;
+          if (count == 3) {
+            break;
+          }
+        }
+      }
+      if (count == 3) {
+        colsThreeRepeat++;
+        break;
+      }
+    }
+  }
+  return colsThreeRepeat;
 }
+
 unsigned getRowAmtThreeRepeatVals(const int*, size_t, size_t)
 {
   return 0;

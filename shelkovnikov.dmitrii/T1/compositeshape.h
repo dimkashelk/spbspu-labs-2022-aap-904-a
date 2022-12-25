58021744a9d8384c2ea291fa19a40efb5b441901
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-26 02:27:32 +0300

    add new methods

--- shelkovnikov.dmitrii/T1/compositeshape.h
+++ shelkovnikov.dmitrii/T1/compositeshape.h
@@ -1,5 +1,19 @@
-#ifndef T1_COMPOSITESHAPE_H
-#define T1_COMPOSITESHAPE_H
+#ifndef SPBSPU_LABS_2022_AAP_904_A_COMPOSITE_SHAPE_H
+#define SPBSPU_LABS_2022_AAP_904_A_COMPOSITE_SHAPE_H
+#include <cstddef>
+#include "base_types.h"
+#include "shape.h"
 class CompositeShape
-{};
+{
+public:
+  CompositeShape(Shape **shapes, size_t size);
+  const double getArea() const;
+  const rectangle_t getFrameRect() const;
+  void move(point_t point);
+  void move(double delta_x, double delta_y);
+  void scale(double k);
+private:
+  Shape **shapes_;
+  size_t size_;
+};
 #endif

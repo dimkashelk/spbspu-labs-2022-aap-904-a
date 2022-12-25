3edcf3547458363b95fe013f76abe56d986c53ec
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-25 19:32:55 +0300

    main args

--- odintsov.kirill/I4/main.cpp
+++ odintsov.kirill/I4/main.cpp
@@ -1,3 +1,10 @@
-int main()
+#include <iostream>
+
+int main(int argc, char* argv[])
 {
+  if (argc != 4) {
+    std::cout << "Error: Invalid arguments\n";
+    return 1;
+  }
+  std::cout << argv[1];
 }

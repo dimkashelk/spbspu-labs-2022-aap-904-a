0a36e0a3ded08f8e3533ee3cd3cfec3b7077713e
Chemodurovart <chemodurovart@gmail.com>
2022-12-25 21:15:38 +0300

    moved functions to poligon.cpp and add copyArrayOfPoints

---
+++ chemodurov.artem/T1/is-repeating-points-in-array.hpp
@@ -0,0 +1,10 @@
+#ifndef IS_REPEATING_POINTS_IN_ARRAY_HPP
+#define IS_REPEATING_POINTS_IN_ARRAY_HPP
+#include "base-types.hpp"
+
+namespace chemodurov
+{
+  
+}
+
+#endif
--- chemodurov.artem/T1/polygon.cpp
+++ chemodurov.artem/T1/polygon.cpp
@@ -1,14 +1,61 @@
 #include "polygon.hpp"
+#include "find-polygon-center.hpp"
+#include "copy-array-of-points.hpp"
 
-chemodurov::Polygon::Polygon(const chemodurov::point_t * vertices, size_t number_of_vertices)
+namespace chemodurov
 {
-  chemodurov::point_t * temp = new point_t[number_of_vertices];
-  for (size_t i = 0; i < number_of_vertices; ++i)
+  chemodurov::point_t * copyArrayOfPoints(const chemodurov::point_t * arr, size_t arr_size)
   {
-    temp[i] = vertices[i];
+    chemodurov::point_t * temp = new point_t[arr_size];
+    for (size_t i = 0; i < arr_size; ++i)
+    {
+      temp[i] = arr[i];
+    }
+    return temp;
   }
-  vertices_ = temp;
-  number_of_vertices_ = number_of_vertices;
-  chemodurov::point_t center{0, 0};
-  center_ = center;
+
+  double calcPolygonArea(const chemodurov::point_t * verts, size_t num_of_verts)
+  {
+    double area = 0.0;
+    for (size_t i = 0; i < num_of_verts - 1; ++i)
+    {
+      area += verts[i].x * verts[i + 1].y - verts[i + 1].x * verts[i].y;
+    }
+    area *= 0.5;
+    return area;
+  }
+
+  chemodurov::point_t findPolygonCenter(const chemodurov::point_t * verts, size_t num_of_verts)
+  {
+    double area = calcPolygonArea(verts, num_of_verts);
+    double x_of_center = 0.0;
+    double y_of_center = 0.0;
+    for (size_t i = 0; i < num_of_verts - 1; ++i)
+    {
+      x_of_center += (verts[i].x + verts[i + 1].x) * (verts[i].x * verts[i + 1].y - verts[i + 1].x * verts[i].y);
+      y_of_center += (verts[i].y + verts[i + 1].y) * (verts[i].x * verts[i + 1].y - verts[i + 1].x * verts[i].y);
+    }
+    x_of_center /= (6 * area);
+    y_of_center /= (6 * area);
+    chemodurov::point_t temp{x_of_center, y_of_center};
+    return temp;
+  }
+}
+
+chemodurov::Polygon::Polygon(const chemodurov::point_t * verts, size_t num_of_verts):
+ vertices_(chemodurov::copyArrayOfPoints(verts, num_of_verts)),
+ number_of_vertices_(num_of_verts),
+ center_(chemodurov::findPolygonCenter(verts, num_of_verts))
+{
+  
+}
+
+chemodurov::Polygon::~Polygon()
+{
+  delete [] vertices_;
+}
+
+double chemodurov::Polygon::getArea() const
+{
+  return chemodurov::calcPolygonArea(vertices_, number_of_vertices_);
 }

ef84b8f525f623509cdbd908c667ed963b9f9158
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-25 19:57:02 +0300

    task 1 functions

---
+++ odintsov.kirill/I4/array2d.cpp
@@ -0,0 +1,10 @@
+#include <cstddef>
+
+unsigned getColAmtThreeRepeatVals(const int*, size_t, size_t)
+{
+  return 0;
+}
+unsigned getRowAmtThreeRepeatVals(const int*, size_t, size_t)
+{
+  return 0;
+}
--- odintsov.kirill/I4/array2d.hpp
+++ odintsov.kirill/I4/array2d.hpp
@@ -1,3 +1,11 @@
 #ifndef ARRAY2D_HPP
 #define ARRAY2D_HPP
+
+#include <cstddef>
+
+namespace odintsov {
+  unsigned getColAmtThreeRepeatVals(const int* arr, size_t rows, size_t cols);
+  unsigned getRowAmtThreeRepeatVals(const int* arr, size_t rows, size_t cols);
+}
+
 #endif

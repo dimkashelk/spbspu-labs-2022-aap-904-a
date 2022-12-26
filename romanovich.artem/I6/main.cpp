5d546b39d36b6416d127054f2e4c5ed75b1ebbcd
Artem <artrom170@gmail.com>
2022-12-26 13:07:03 +0300

    pow, print, some fixes

--- romanovich.artem/I6/main.cpp
+++ romanovich.artem/I6/main.cpp
@@ -1,9 +1,7 @@
-double sinh(x)(double x, double absError, undigned long numberMax)
-{
-  double result = 0;
-}
+#include "iostream"
+#include <stddef.h>
 
-double factorial(size_t a)
+size_t factorial(size_t a)
 {
   size_t result = 1;
   while (a > 0)
@@ -14,11 +12,33 @@ double factorial(size_t a)
   return result;
 }
 
+size_t pow(size_t a, size_t power)
+{
+  size_t result = 1;
+  for (size_t i = 0; i < power; ++i)
+  {
+    result *= a;
+  }
+  return result;
+}
+
+double sinh(double x, double absError, unsigned long numberMax)
+{
+  double result = 0;
+  size_t precision = 10;
+  for (size_t i = 0; i < precision; ++i)
+  {
+    result += pow(x, 1+2*i)/factorial(1+2*i);
+  }
+  return result;
+}
+
+
 
 //корректное сравнение double
 
 
 int main()
 {
-
+  std::cout << sinh(1, 0, 0) << "\n";
 }

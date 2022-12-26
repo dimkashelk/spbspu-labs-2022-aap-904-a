ede38ebc7c7b672593b62897d5cebaaeb6e11b5c
Chemodurovart <chemodurovart@gmail.com>
2022-12-27 00:59:40 +0300

    add some base in main.cpp

--- chemodurov.artem/T1/main.cpp
+++ chemodurov.artem/T1/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <string>
+
 int main()
 {
+  std::string line = "";
+  do
+  {
+    std::getline(std::cin, line);
+    if (!line.compare(0, 8, "POLYGON"))
+    {
+      
+    }
+  }
+  while (std::cin);
   return 0;
 }

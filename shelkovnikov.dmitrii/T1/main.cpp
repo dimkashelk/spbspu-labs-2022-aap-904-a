c76a1205059d9ed288dadcf7a97bd018ade2ac12
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-27 02:49:04 +0300

    create input of Rectangle

--- shelkovnikov.dmitrii/T1/main.cpp
+++ shelkovnikov.dmitrii/T1/main.cpp
@@ -1,4 +1,58 @@
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <cstddef>
+#include "rectangle.h"
+void expand(Shape *shapes, size_t size, size_t new_capacity)
+{}
+Rectangle input_rectangle(std::istream &in)
+{
+  double *coords = new double[4];
+  for (size_t i = 0; i < 4; i++)
+  {
+    in >> coords[i];
+    if (!in)
+    {
+      throw std::runtime_error("Not enough data to input");
+    }
+  }
+  return Rectangle(coords[0], coords[1], coords[2], coords[3]);
+}
 int main()
 {
+  std::string line = "";
+  std::string name = "";
+  bool contains_errors_with_shapes = false;
+  size_t size = 0;
+  size_t capacity = 10;
+  Shape **shapes = new Shape*[capacity];
+  while (std::cin)
+  {
+    std::getline(std::cin, line);
+    std::istringstream in(line);
+    in >> name;
+    if (name == "RECTANGLE")
+    {
+      try
+      {
+        Rectangle rect = input_rectangle(in);
+        shapes[size] = &rect;
+        if (size == capacity)
+        {
+
+        }
+      }
+      catch (const std::runtime_error &e)
+      {
+        contains_errors_with_shapes = true;
+      }
+    }
+    else if (name == "MOVE")
+    {
+      double x1 = 0.0;
+      double x2 = 0.0;
+      std::cin >> x1 >> x2;
+    }
+  }
   return 0;
 }

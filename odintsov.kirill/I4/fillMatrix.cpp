56f48e524008cb98b63e30d7830a9a2fb7428520
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-26 22:29:36 +0300

    rippleFromPointFill realized

--- odintsov.kirill/I4/fillMatrix.cpp
+++ odintsov.kirill/I4/fillMatrix.cpp
@@ -2,5 +2,11 @@
 
 void rippleFromPointFill(int* const* arr, size_t rows, size_t cols, size_t row, size_t col)
 {
-  return;
+  for (size_t r = 0; r < rows; r++) {
+    for (size_t c = 0; c < cols; c++) {
+      size_t rowDist = row > r ? row - r : r - row;
+      size_t colDist = col > c ? col - c : c - col;
+      arr[r][c] = colDist > rowDist ? colDist : rowDist;
+    }
+  }
 }

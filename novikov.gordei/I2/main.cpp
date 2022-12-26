ad55dc0f7933f50ab050e7de86e1dac5708cd3eb
Novikov Gordei <vokivon2142@mail.ru>
2022-12-26 18:06:23 +0300

    renamed to I2

--- novikov.gordei/I2/main.cpp
+++ novikov.gordei/I2/main.cpp
@@ -13,28 +13,28 @@ const int randMax = 50;
 int main()
 {
   srand(time(0));
-  
+
   int k;
   if (!inputInteger("Enter number of elements for shift: ", k)) {
     return 1;
   }
-  
+
   const int N = 10;
-  int staticArray[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int staticArray[N] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
   cout << "Static array: ";
   processArray(staticArray, N, k);
-  
+
   int n;
   if (!inputInteger("Enter number of elements in dynamic array: ", n)) {
     return 1;
   }
-  int *dynamicArray = new int[n];
+  int* dynamicArray = new int[n];
   for (int i = 0; i < n; i++) {
     dynamicArray[i] = rand() % (randMax - randMin + 1) + randMin;
   }
   processArray(dynamicArray, n, k);
-  delete [] dynamicArray;
-  
+  delete[] dynamicArray;
+
   int numberOfArrays;
   if (!inputInteger("Enter number of files with arrays: ", numberOfArrays)) {
     return 1;
@@ -48,8 +48,8 @@ int main()
       return 1;
     }
     processArray(dynamicArray, n, k);
-    delete [] dynamicArray;
+    delete[] dynamicArray;
   }
-  
+
   return 0;
 }

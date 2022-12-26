39989799302bc66fe7201283c9db1e607b8860df
Disus <mita2004567@gmail.com>
2022-12-26 18:42:07 +0300

    test

--- suslov.dmitriy/I3/main.cpp
+++ suslov.dmitriy/I3/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+#include "formNewStringVarFiveteen.h"
+#include <fstream>
+#include "formNewStringVarFive.h"
 int main()
 {
   size_t capacity = 10;
@@ -30,13 +32,78 @@ int main()
     std::cin >> cstring[size];
   } while (std::cin && cstring[size++] != '\n');
   if (size > 0)
+  {
     cstring[size - 1] = '\0';
+  }
   if (size == 0 or size == 1)
   {
     std::cerr << "Empty input" << '\n';
     delete[] cstring;
     return 2;
   }
-  std::cout << cstring << "\n";
+  size_t capacity2 = 10;
+  char *cstring2 = new char[capacity2];
+  size_t size2 = 0;
+  std::cin >> std::noskipws;
+  do
+  {
+    if (size2 == capacity2)
+    {
+      try
+      {
+        char *newstr = new char[capacity2 + 20];
+        for (auto i = cstring2, j = newstr; i != cstring2 + size2; ++i, ++j)
+        {
+          *j = *i;
+        }
+        delete[] cstring2;
+        delete[] cstring;
+        cstring2 = newstr;
+        capacity2 += 20;
+      }
+      catch (...)
+      {
+        delete[] cstring2;
+        delete[] cstring;
+        return 1;
+      }
+    }
+    std::cin >> cstring2[size2];
+  } while (std::cin && cstring2[size2++] != '\n');
+  if (size2 > 0)
+  {
+    cstring2[size2 - 1] = '\0';
+  }
+  if (size2 == 0 or size2 == 1)
+  {
+    std::cerr << "Empty input" << '\n';
+    delete[] cstring2;
+    delete[] cstring;
+    return 2;
+  }
+  char *result1;
+  try
+  {
+    result1 = new char[size + size2 + 1];
+  } catch (std::bad_alloc &e)
+  {
+    std::cerr << "Fail" << "\n";
+    return 3;
+  }
+  std::cout << varFiveFunction(result1, cstring, cstring2) << "\n";
+  delete[] result1;
+  char *result2;
+  try
+  {
+    result2 = new char[size + 1];
+  } catch (std::bad_alloc &e)
+  {
+    std::cerr << "Fail" << "\n";
+    return 3;
+  }
+  std::cout << varFiveteenFunction(result2, cstring, cstring2) << "\n";
+  delete[] result2;
   delete[] cstring;
+  delete[] cstring2;
+  return 0;
 }

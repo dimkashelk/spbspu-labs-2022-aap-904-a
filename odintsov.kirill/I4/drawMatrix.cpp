b1f52ba8ced28d53a60ba1cc3896db520cc4c1f7
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-26 22:41:38 +0300

    drawMatrix() realized

--- odintsov.kirill/I4/drawMatrix.cpp
+++ odintsov.kirill/I4/drawMatrix.cpp
@@ -4,4 +4,14 @@
 
 std::ostream& drawMatrix(std::ostream& out, const int* const* arr, size_t rows, size_t cols)
 {
+  for (size_t r = 0; r < rows - 1; r++) {
+    for (size_t c = 0; c < cols - 1; c++) {
+      out << arr[r][c] << ' ';
+    }
+    out << arr[r][cols - 1] << '\n';
+  }
+  for (size_t c = 0; c < cols - 1; c++) {
+    out << arr[rows - 1][c] << ' ';
+  }
+  return out << arr[rows - 1][cols - 1];
 }

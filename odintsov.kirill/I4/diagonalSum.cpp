4be9a61323b814d39cd694078b09940064ead7a4
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-26 19:51:47 +0300

    diagonalSum realization

--- odintsov.kirill/I4/diagonalSum.cpp
+++ odintsov.kirill/I4/diagonalSum.cpp
@@ -1,10 +1,32 @@
 #include "diagonalSum.hpp"
+#include <cstddef>
 
 int odintsov::getOffDiagonalSumByPoint(const int* const* arr, size_t rows, size_t cols, size_t row, size_t col)
 {
-
+  int sum = 0;
+  for (size_t r = row, c = col; c < cols; r--, c++) {
+    sum += arr[r][c];
+    if (r == 0) {
+      break;
+    }
+  }
+  for (size_t r = row + 1, c = col - 1; r < rows; r++, c--) {
+    sum += arr[r][c];
+    if (c == 0) {
+      break;
+    }
+  }
+  return sum;
 }
 
 int odintsov::getMinOffDiagonalSum(const int* const* arr, size_t rows, size_t cols)
 {
+  int minSum = getOffDiagonalSumByPoint(arr, rows, cols, 0, 0);
+  for (size_t r = 1; r < rows; r++) {
+    int newSum = getOffDiagonalSumByPoint(arr, rows, cols, r, 0);
+    if (newSum < minSum) {
+      minSum = newSum;
+    }
+  }
+  return minSum;
 }
--- odintsov.kirill/I4/diagonalSum.hpp
+++ odintsov.kirill/I4/diagonalSum.hpp
@@ -1,6 +1,8 @@
 #ifndef DIAGONALSUM_HPP
 #define DIAGONALSUM_HPP
 
+#include <cstddef>
+
 namespace odintsov {
   int getOffDiagonalSumByPoint(const int* const* arr, size_t rows, size_t cols, size_t row, size_t col);
   int getMinOffDiagonalSum(const int* const* arr, size_t rows, size_t cols);

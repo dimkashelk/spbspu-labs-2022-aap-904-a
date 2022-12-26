af85e6be124d790fc34dd09578fcde6aef582431
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2022-12-26 18:57:10 +0300

    fix3

--- chulkov.leontii/I2/outputarray.cpp
+++ chulkov.leontii/I2/outputarray.cpp
@@ -3,6 +3,10 @@
 
 std::ostream& outputArray(std::ostream& out, const int* arr, size_t size)
 {
+  if (size == 0)
+  {
+    return out;
+  }
   for (size_t i = 0; i < size - 1; i++)
   {
     out << arr[i] << '\t';

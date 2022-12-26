2c66c6c4df0791b59d08ec408a4d1368ea6d9a45
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-26 15:53:52 +0000

    fixed 1.3 cg

--- zhuravlev.ivan/I3/finder_same_elements_in_two_strings.cpp
+++ zhuravlev.ivan/I3/finder_same_elements_in_two_strings.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "finder_same_elements_in_two_strings.h"
 
-void finderSameElementsInTwoStrings(const char *first_string, const char *second_string, size_t size_first_string, size_t size_second_string)
+void finderSameElementsInTwoStrings(const char *first_string,const char *second_string, size_t size_first_string, size_t size_sec_string)
 {
-  size_t final_size = size_first_string + size_second_string;
+  size_t final_size = size_first_string + size_sec_string;
   char result_string[final_size];
   size_t cnt = -1;
   for (size_t i = 0; i < size_first_string; ++i)
   {
-    for (size_t j = 0; j < size_second_string; ++j)
+    for (size_t j = 0; j < size_sec_string; ++j)
     {
       if (first_string[i] == second_string[j])
       {

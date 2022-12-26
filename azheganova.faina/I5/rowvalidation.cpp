03e1b8ac0881f6dee8f50ad18d64d28d490d9029
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-26 15:19:08 +0300

    I5 laba completed

--- azheganova.faina/I5/rowvalidation.cpp
+++ azheganova.faina/I5/rowvalidation.cpp
@@ -21,9 +21,9 @@ bool isE(char c)
 {
   return (c == 'E');
 }
-bool continueWithNextDigit(const char * data)
+bool continueWithDigit(const char * data)
 {
-  return isDigit(*data) && (continueWithNextDigit(data + 1) || isEnd(*(data + 1)));
+  return isDigit(*data) && (continueWithDigit(data + 1) || isEnd(*(data + 1)));
 }
 bool finishedWithDigit(const char * data)
 {
@@ -31,25 +31,17 @@ bool finishedWithDigit(const char * data)
 }
 bool continueWithSign(const char * data)
 {
-  return isSign(*data) && (continueWithSign(data + 1) || continueWithNextDigit(data + 1));
+  return isSign(*data) && (continueWithSign(data + 1) || continueWithDigit(data + 1));
 }
 bool continueWithOrder(const char * data)
 {
-  return isE(*data) && continueWithSign(data + 1) && continueWithNextDigit(data + 2);
+  return isE(*data) && continueWithSign(data + 1) && continueWithDigit(data + 2);
 }
-bool continueWithMantisaPartOne(const char * data)
+bool continueWithDot(const char * data)
 {
-  return isDot(*data) && continueWithNextDigit(data + 1);
-}
-bool continueWithMantisaPartTwo(const char * data)
-{
-  return isDigit(*data) && isDot(*(data + 1)) && continueWithNextDigit(data + 2);
-}
-bool isMantisa(const char * data)
-{
-  return continueWithMantisaPartOne(data) || continueWithMantisaPartTwo(data) || continueWithNextDigit(data);
+  return continueWithDigit(data) && (isDot(*(data + 1)) || continueWithDigit(data + 1));
 }
 bool isFloat(const char * data)
 {
-  return ((isMantisa(data) && continueWithOrder(data + 1)) || (isSign(*data) && isMantisa(data + 1) && continueWithOrder(data + 2)));
+  return ((continueWithDot(data) && continueWithOrder(data + 1)) || (isSign(*data) && continueWithDot(data + 1) && continueWithOrder(data + 2)));
 }

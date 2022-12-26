27ea51ea0e40e581794e3661c4fd928c7cbb0ecc
Lagushka1984 <nikolai.turkin.main@gmail.com>
2022-12-26 15:11:14 +0300

    fixed input condition

--- turkin.nikolai/I6/main.cpp
+++ turkin.nikolai/I6/main.cpp
@@ -3,14 +3,13 @@
 
 int main()
 {
-  size_t precision = 0;
   size_t maxAmount = 0;
   double lhs = 0.0;
   double rhs = 0.0;
   const double step = 0.01;
   const double error = 0.0001;
   std::cin >> lhs >> rhs >> maxAmount;
-  if (!std::cin || lhs < -1.0 || rhs > 1.0 || precision > maxAmount)
+  if (!std::cin || lhs < -1.0 || rhs > 1.0 || lhs > rhs)
   {
     std::cerr << "incorrect input\n";
     return 1;

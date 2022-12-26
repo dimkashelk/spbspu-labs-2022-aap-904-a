3beb1c16b57c36a4a217e0f8d846816654cf07e5
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-26 19:10:39 +0300

    getOffDiagonalSumByPoint

--- odintsov.kirill/I4/diagonalSum.cpp
+++ odintsov.kirill/I4/diagonalSum.cpp
@@ -1,5 +1,10 @@
 #include "diagonalSum.hpp"
 
-int odintsov::getMinOffDiagonalSum(const int* const*, size_t, size_t) {
-  return 0
+int odintsov::getOffDiagonalSumByPoint(const int* const* arr, size_t rows, size_t cols, size_t row, size_t col)
+{
+
+}
+
+int odintsov::getMinOffDiagonalSum(const int* const* arr, size_t rows, size_t cols)
+{
 }
--- odintsov.kirill/I4/diagonalSum.hpp
+++ odintsov.kirill/I4/diagonalSum.hpp
@@ -2,6 +2,7 @@
 #define DIAGONALSUM_HPP
 
 namespace odintsov {
+  int getOffDiagonalSumByPoint(const int* const* arr, size_t rows, size_t cols, size_t row, size_t col);
   int getMinOffDiagonalSum(const int* const* arr, size_t rows, size_t cols);
 }
 

cf420964b37b54cf2254fbfa7c444ab148f20465
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-26 16:49:28 +0000

    fixed cg

--- zhuravlev.ivan/I3/finder_same_elements_in_two_strings.cpp
+++ zhuravlev.ivan/I3/finder_same_elements_in_two_strings.cpp
@@ -3,30 +3,30 @@
 
 void finderSameElementsInTwoStrings(const char *first_string, const char *second_string, size_t size_first_string, size_t size_sec_string)
 {
-	size_t final_size = size_first_string + size_sec_string;
-	char tmp_string[final_size];
-	size_t cnt = -1;
-	for (size_t i = 0; i < size_first_string; ++i)
+  size_t final_size = size_first_string + size_sec_string;
+  char tmp_string[final_size];
+  size_t cnt = -1;
+  for (size_t i = 0; i < size_first_string; ++i)
+  {
+    for (size_t j = 0; j < size_sec_string; ++j)
 	{
-		for (size_t j = 0; j < size_sec_string; ++j)
-		{
-			if (first_string[i] == second_string[j])
-			{
-				cnt++;
-				tmp_string[cnt] = first_string[i];
-			}
-		}
-	}
+	  if (first_string[i] == second_string[j])
+	  {
+	    cnt++;
+		tmp_string[cnt] = first_string[i];
+	  }
+  	}
+  }
 
-	char result_string[cnt]{};
-	
+  char result_string[cnt]{};
+
   for (int i = 0; i < cnt; ++i)
-	{
-	  result_string[i] = tmp_string[i];
-	}
+  {
+    result_string[i] = tmp_string[i];
+  }
 
-	for (int i = 0; i <= cnt; ++i)
-	{
-	  std::cout << result_string[i] << " ";
-	}
+  for (int i = 0; i <= cnt; ++i)
+  {
+    std::cout << result_string[i] << " ";
+  }
 }

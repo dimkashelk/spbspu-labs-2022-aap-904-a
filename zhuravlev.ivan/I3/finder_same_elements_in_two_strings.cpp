984e4437d7bf001f6f650c1567c70b02ba93f867
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-26 16:38:46 +0000

    fixed acceptance 4

--- zhuravlev.ivan/I3/finder_same_elements_in_two_strings.cpp
+++ zhuravlev.ivan/I3/finder_same_elements_in_two_strings.cpp
@@ -18,12 +18,12 @@ void finderSameElementsInTwoStrings(const char *first_string, const char *second
 		}
 	}
 
-	char result_string[cnt];
+	char result_string[cnt] = {};
 	for (int i = 0; i < cnt; ++i)
 	{
-		tmp_string[i] = result_string[i];
+		result_string[i] = tmp_string[i];
 	}
-	for (size_t i = 0; i <= final_size; ++i)
+	for (size_t i = 0; i <= cnt; ++i)
 	{
 		std::cout << result_string[i] << " ";
 	}

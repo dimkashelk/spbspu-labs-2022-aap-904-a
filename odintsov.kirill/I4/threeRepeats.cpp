a6096ab5739ebc1e105a756a8e260b88faab9195
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-27 17:35:55 +0300

    threeRepeats fix

--- odintsov.kirill/I4/threeRepeats.cpp
+++ odintsov.kirill/I4/threeRepeats.cpp
@@ -5,7 +5,7 @@
 size_t odintsov::countValRepeatsInCol(int val, const int* arr, size_t rows, size_t cols, size_t col) {
   size_t count = 0;
   for (size_t i = 0; i < rows; i++) {
-    if (val == getMatrixVal(arr, rows, cols, col, i)) {
+    if (val == odintsov::getMatrixValue(arr, rows, cols, i, col)) {
       count++;
     }
   }
@@ -16,7 +16,7 @@ size_t odintsov::countValRepeatsInRow(int val, const int* arr, size_t rows, size
 {
   size_t count = 0;
   for (size_t i = 0; i < cols; i++) {
-    if (val == getMatrixVal(arr, rows, cols, i, row)) {
+    if (val == odintsov::getMatrixValue(arr, rows, cols, row, i)) {
       count++;
     }
   }
@@ -26,7 +26,7 @@ size_t odintsov::countValRepeatsInRow(int val, const int* arr, size_t rows, size
 bool odintsov::isThreeRepeatsInCol(const int* arr, size_t rows, size_t cols, size_t col)
 {
   for (size_t i = 0; i < rows - 2; i++) {
-    int val = getMatrixVal(arr, rows, cols, col, i);
+    int val = odintsov::getMatrixValue(arr, rows, cols, i, col);
     if (countValRepeatsInCol(val, arr, rows, cols, col) >= 3) {
       return true;
     }
@@ -37,7 +37,7 @@ bool odintsov::isThreeRepeatsInCol(const int* arr, size_t rows, size_t cols, siz
 bool odintsov::isThreeRepeatsInRow(const int* arr, size_t rows, size_t cols, size_t row)
 {
   for (size_t i = 0; i < cols - 2; i++) {
-    int val = getMatrixVal(arr, rows, cols, i, row);
+    int val = odintsov::getMatrixValue(arr, rows, cols, row, i);
     if (countValRepeatsInRow(val, arr, rows, cols, row) >= 3) {
       return true;
     }

cb06bc0f4960474abd98b6e659ab8bff050f56bd
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-27 22:37:02 +0300

    I3 and I5 completed

--- azheganova.faina/I3/main.cpp
+++ azheganova.faina/I3/main.cpp
@@ -6,7 +6,16 @@
 
 int main()
 {
-  char *cstring = inputString(std::cin);
+  char * cstring = nullptr;
+  try
+  {
+    cstring = inputString(std::cin);
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
   std::cout << cstring << "\n";
   std::cout << countDifferentLetters(cstring) << '\n';
   std::cout << findRepeatedSymbols(cstring) << '\n';

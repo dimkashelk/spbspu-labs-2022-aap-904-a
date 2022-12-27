513052d5feb03e0d4364013a7e3b1db93b9e61f4
Машкин Андрей <beedzibood@gmail.com>
2022-12-28 02:20:34 +0300

    try 5

--- mashkin.andrew/I2/maxCountOfEqualElements.cpp
+++ mashkin.andrew/I2/maxCountOfEqualElements.cpp
@@ -3,8 +3,8 @@
 
 size_t countMaxQuantityOfEqualElements(const int* arr, const size_t size)
 {
-  size_t count = 0;
-  size_t maxCount = 0;
+  size_t count = 1;
+  size_t maxCount = 1;
   for (size_t i = 0; i < size - 1; ++i)
   {
     if (arr[i] == arr[i + 1])

913c288b509f775f24bcef8d359923252e8a34e5
Yaroslava <aroslavakotova@gmail.com>
2022-12-27 16:07:41 +0300

    try

--- kotova.yaroslava/I3/DelNumbers.cpp
+++ kotova.yaroslava/I3/DelNumbers.cpp
@@ -5,7 +5,7 @@ char* deleteNumbers(char* destination,const char* source)
 {
   size_t k = 0;
   destination[k] = '\0';
-  size_t i = 0;
+  char * i = 0;
   while (source[i] != '\0')
   {
     if (!std::isdigit(source[i]))

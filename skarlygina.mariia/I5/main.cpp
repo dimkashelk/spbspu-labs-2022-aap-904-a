f13be1f65a273057d47fffecbb3f6ed8491949ea
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-12-28 01:34:06 +0300

    exception

--- skarlygina.mariia/I5/main.cpp
+++ skarlygina.mariia/I5/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <iomanip>
+#include <exception>
 #include <cstring_capacity.h>
 #include "is_float_digit.h"
 
 int main()
 {
   char* digit_string = nullptr;
-  digit_string = makeNewCapacityCString(std::cin);
+  try
+  {
+    digit_string = makeNewCapacityCString(std::cin);
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
   std::cout << std::boolalpha << isFloatDigit(digit_string) << '\n';
   delete[] digit_string;
   return 0;

2a3ad35b1a432dd2c7e8a6c56507a5b36a8d1f46
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-28 14:50:45 +0000

    trying fix acceptance

--- zhuravlev.ivan/I3/main.cpp
+++ zhuravlev.ivan/I3/main.cpp
@@ -18,34 +18,37 @@ int main()
     std::cerr << "Error: Something wrong with input!" << "\n";
     return 1;
   }
-  do
+  else
   {
-    if (size == capacity)
+    do
     {
-      try
+      if (size == capacity)
       {
-        char * newstr = new char[capacity + 20];
-        for (auto i = cstring, j = newstr; i != cstring + size; ++i, ++j)
+        try
         {
-          *j = *i;
+          char * newstr = new char[capacity + 20];
+          for (auto i = cstring, j = newstr; i != cstring + size; ++i, ++j)
+          {
+            *j = *i;
+          }
+          delete [] cstring;
+          cstring = newstr;
+          capacity += 20;
         }
-        delete [] cstring;
-        cstring = newstr;
-        capacity += 20;
+        catch (std::bad_alloc &e)
+        {
+          delete [] cstring;
+          std::cerr << e.what();
+        }
       }
-      catch (std::bad_alloc &e)
-      {
-        delete [] cstring;
-        std::cerr << e.what();
-      }
+      std::cin >> cstring[size];
     }
-    std::cin >> cstring[size];
-  }
-  while (std::cin && cstring[size++] != '\n');
-  cstring[size - 1] = '\0';
+    while (std::cin && cstring[size++] != '\n');
+    cstring[size - 1] = '\0';
 
-  finderThreeSameElements(cstring, size);
-  finderSameElementsInTwoStrings(cstring, static_string, size, static_string_size);
-  delete [] cstring;
+    finderThreeSameElements(cstring, size);
+    finderSameElementsInTwoStrings(cstring, static_string, size, static_string_size);
+    delete [] cstring;
+  }
   return 0;
 }

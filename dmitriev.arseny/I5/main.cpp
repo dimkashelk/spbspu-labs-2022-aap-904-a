0f2c3f7c19b2e05a73a32b174f0e61728568fce9
ArsDi <abstractlevl@gmail.com>
2022-12-28 23:26:00 +0300

    input processing

--- dmitriev.arseny/I5/main.cpp
+++ dmitriev.arseny/I5/main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
+#include <cstddef>
 
 int main()
 {
+  size_t capacity = 10;
+  size_t size = 0;
+  char inp = '\0';
+  char* cStr = nullptr;
+  try
+  {
+    cStr = new char[capacity];
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cout << e.what() << '\n';
+    return 1;
+  }
+
+  std::cin >> std::noskipws;
+
+  for (std::cin >> inp; (std::cin && inp) && (inp != '\n'); std::cin >> inp)
+  {
+    if (size == capacity)
+    {
+      capacity = capacity + 10;
+      char* dupStr = nullptr;
+      try
+      {
+        dupStr = new char[capacity];
+      }
+      catch (const std::bad_alloc& e)
+      {
+        std::cout << e.what() << '\n';
+        delete[] cStr;
+        return 1;
+      }
+      for (size_t i = 0; i < size; i++)
+      {
+        dupStr[i] = cStr[i];
+      }
+      delete[] cStr;
+      cStr = dupStr;
+      dupStr = nullptr;
+    }
+    cStr[size++] = inp;
+  }
+
+  if (size == 0)
+  {
+    std::cerr << "Empty string" << '\n';
+    delete[] cStr;
+    return 1;
+  }
+
+  cStr[size] = '\0';
 
   return 0;
 }

e0f1e6edd78a6925ffa517a452704701b1af6d08
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-28 03:54:47 +0300

    isRealNumber realized

--- odintsov.kirill/I5/isRealNumber.cpp
+++ odintsov.kirill/I5/isRealNumber.cpp
@@ -1,3 +1,43 @@
-bool isRealNumber(char*) {
-  return true;
+#include "isRealNumber.hpp"
+#include <cctype>
+
+const char* odintsov::skipUnsigned(const char* string) {
+  if (!isdigit(*(string++))) {
+    return nullptr;
+  }
+  return isdigit(*string) ? skipUnsigned(string) : string;
+}
+
+const char* odintsov::skipExponent(const char* string) {
+  if (string == nullptr || *(string++) != 'E') {
+    return nullptr;
+  }
+  return skipUnsigned(skipSign(string));
+}
+
+const char* odintsov::skipMantissa(const char* string) {
+  string = skipUnsigned(string);
+  if (string == nullptr || *string != '.') {
+    return nullptr;
+  }
+  return skipUnsigned(string + 1);
+}
+
+const char* odintsov::skipSign(const char* string) {
+  if (*string == '+' || *string == '-') {
+    return string + 1;
+  }
+  return string;
+}
+
+const char* odintsov::skipRealNumber(const char* string) {
+  return skipExponent(skipMantissa(skipSign(string)));
+}
+
+bool odintsov::isRealNumber(const char* string) {
+  string = skipRealNumber(string);
+  if (string == nullptr) {
+    return false;
+  }
+  return *string == '\0';
 }
--- odintsov.kirill/I5/isRealNumber.hpp
+++ odintsov.kirill/I5/isRealNumber.hpp
@@ -1,8 +1,13 @@
 #ifndef ISREALNUMBER_HPP
 #define ISREALNUMBER_HPP
 
-namespace odintsov {    
-  bool isRealNumber(char* string);
+namespace odintsov {
+  const char* skipUnsigned(const char* string);
+  const char* skipExponent(const char* string);
+  const char* skipMantissa(const char* string);
+  const char* skipSign(const char* string);
+  const char* skipRealNumber(const char* string);
+  bool isRealNumber(const char* string);
 }
 
 #endif

2c2911744ccd8fe04f497d1f87eb6ca1b7b79ea0
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-28 05:08:47 +0300

    CG correction

--- odintsov.kirill/I5/isRealNumber.cpp
+++ odintsov.kirill/I5/isRealNumber.cpp
@@ -1,21 +1,24 @@
 #include "isRealNumber.hpp"
 #include <cctype>
 
-const char* odintsov::skipUnsigned(const char* string) {
+const char* odintsov::skipUnsigned(const char* string)
+{
   if (!isdigit(*(string++))) {
     return nullptr;
   }
   return isdigit(*string) ? skipUnsigned(string) : string;
 }
 
-const char* odintsov::skipExponent(const char* string) {
+const char* odintsov::skipExponent(const char* string)
+{
   if (string == nullptr || *(string++) != 'E') {
     return nullptr;
   }
   return skipUnsigned(skipSign(string));
 }
 
-const char* odintsov::skipMantissa(const char* string) {
+const char* odintsov::skipMantissa(const char* string)
+{
   string = skipUnsigned(string);
   if (string == nullptr || *string != '.') {
     return nullptr;
@@ -23,18 +26,21 @@ const char* odintsov::skipMantissa(const char* string) {
   return skipUnsigned(string + 1);
 }
 
-const char* odintsov::skipSign(const char* string) {
+const char* odintsov::skipSign(const char* string)
+{
   if (*string == '+' || *string == '-') {
     return string + 1;
   }
   return string;
 }
 
-const char* odintsov::skipRealNumber(const char* string) {
+const char* odintsov::skipRealNumber(const char* string)
+{
   return skipExponent(skipMantissa(skipSign(string)));
 }
 
-bool odintsov::isRealNumber(const char* string) {
+bool odintsov::isRealNumber(const char* string)
+{
   string = skipRealNumber(string);
   if (string == nullptr) {
     return false;

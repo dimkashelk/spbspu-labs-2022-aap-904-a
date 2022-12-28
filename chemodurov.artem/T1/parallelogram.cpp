4f1db9ac7969d1514b4aba86ca53a5b25e523310
Chemodurovart <chemodurovart@gmail.com>
2022-12-28 14:09:54 +0300

    add realisation of parallelogram

--- chemodurov.artem/T1/parallelogram.cpp
+++ chemodurov.artem/T1/parallelogram.cpp
@@ -1,8 +1,9 @@
 #include "parallelogram.hpp"
+#include <stdexcept>
 
 namespace chemodurov
 {
-  chemodurov::point_t * makeArray(const chemodurov::point_t & fst, const chemodurov::point_t & sec, const chemodurov::point_t trd)
+  chemodurov::point_t * makeArray(const chemodurov::point_t & fst, const point_t & sec, const point_t & trd)
   {
     const size_t x = 4;
     chemodurov::point_t * arr = new chemodurov::point_t[x];
@@ -14,6 +15,33 @@ namespace chemodurov
   }
 }
 
-chemodurov::Parallelogram::Parallelogram(const chemodurov::point_t & fst, const chemodurov::point_t & sec, const chemodurov::point_t trd)
+chemodurov::Parallelogram::Parallelogram(const chemodurov::point_t & fst, const point_t & sec, const point_t & trd):
  p(chemodurov::makeArray(fst, sec, trd), 4ull)
+{
+  if (fst.y != sec.y && sec.y != trd.y)
+  {
+    throw std::invalid_argument("Not correct parameters");
+  }
+}
+chemodurov::Parallelogram::~Parallelogram()
 {}
+double chemodurov::Parallelogram::getArea() const
+{
+  return p.getArea();
+}
+chemodurov::rectangle_t chemodurov::Parallelogram::getFrameRect() const
+{
+  return p.getFrameRect();
+}
+void chemodurov::Parallelogram::move(const chemodurov::point_t & pos)
+{
+  p.move(pos);
+}
+void chemodurov::Parallelogram::move(double dx, double dy)
+{
+  p.move(dx, dy);
+}
+void chemodurov::Parallelogram::scale(double k)
+{
+  p.scale(k);
+}
--- chemodurov.artem/T1/parallelogram.hpp
+++ chemodurov.artem/T1/parallelogram.hpp
@@ -7,7 +7,7 @@ namespace chemodurov
   class Parallelogram: public chemodurov::Shape
   {
    public:
-    Parallelogram(const chemodurov::point_t & fst, const chemodurov::point_t & sec, const chemodurov::point_t & trd);
+    Parallelogram(const chemodurov::point_t & fst, const point_t & sec, const point_t & trd);
     ~Parallelogram();
     double getArea() const;
     chemodurov::rectangle_t getFrameRect() const;

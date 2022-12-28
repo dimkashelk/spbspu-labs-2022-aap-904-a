c1efe7e7336f8ba754f216c6765fe174b6040efb
Машкин Андрей <beedzibood@gmail.com>
2022-12-28 13:37:44 +0300

    try7

--- mashkin.andrew/I2/main.cpp
+++ mashkin.andrew/I2/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
   bool ans1 = isThereTheSamePositiveElements(constArr, constSize);
   std::cout << countMaxQuantityOfEqualElements(constArr, constSize) << "\n";
   std::cout << (ans1 ? ansTrue : ansFalse) << "\n";
-  
+
   size_t dinamicSize = 0;
   std::cin >> dinamicSize;
   int* dinamicArr = new int[dinamicSize];

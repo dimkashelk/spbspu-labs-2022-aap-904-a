3ea0e4a8dc2746a3f6a747863d66a63b0edfe46e
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-12-28 15:59:02 +0300

    changes

--- skarlygina.mariia/common/cstring_capacity_prev.cpp
+++ skarlygina.mariia/common/cstring_capacity_prev.cpp
@@ -1,14 +1,10 @@
 #include "cstring_capacity_prev.h"
+#include <cstring>
 char* makeNewCapacityCString(char* cstring, size_t& capacity, size_t size)
 {
   char* newstring = new char[capacity + 20];
-  for (auto i = cstring, j = newstring; i != cstring + size; ++i, ++j)
-  {
-    *j = *i;
-  }
-  delete[] cstring;
+  strcpy (newstring, cstring);
   cstring = newstring;
-  newstring = nullptr;
   capacity += 20;
   return cstring;
 }

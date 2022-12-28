b5cf3356e0c5e48e9fdb9edc182c6f48ae1c4fe4
ArsDi <abstractlevl@gmail.com>
2022-12-29 02:06:14 +0300

    main function redacting

--- dmitriev.arseny/I5/main.cpp
+++ dmitriev.arseny/I5/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cstddef>
+#include <cStrFtomInput.h>
 #include "isExpression.h"
 
 int main()
 {
   size_t capacity = 10;
   size_t size = 0;
-  char inp = '\0';
   char* cStr = nullptr;
   try
   {
@@ -18,43 +18,23 @@ int main()
     return 1;
   }
 
-  std::cin >> std::noskipws;
-
-  for (std::cin >> inp; (std::cin && inp) && (inp != '\n'); std::cin >> inp)
+  try
   {
-    if (size == capacity)
+    cStr = createCStrFromInput(cStr, size, capacity, std::cin);
+    if (size == 0)
     {
-      capacity = capacity + 10;
-      char* dupStr = nullptr;
-      try
-      {
-        dupStr = new char[capacity];
-      }
-      catch (const std::bad_alloc& e)
-      {
-        std::cout << e.what() << '\n';
-        delete[] cStr;
-        return 1;
-      }
-      for (size_t i = 0; i < size; i++)
-      {
-        dupStr[i] = cStr[i];
-      }
+      std::cerr << "Empty string" << '\n';
       delete[] cStr;
-      cStr = dupStr;
-      dupStr = nullptr;
+      return 1;
     }
-    cStr[size++] = inp;
+    cStr[size] = '\0';
   }
-
-  if (size == 0)
+  catch (const std::bad_alloc& e)
   {
-    std::cerr << "Empty string" << '\n';
+    std::cout << e.what() << '\n';
     delete[] cStr;
     return 1;
   }
-
-  cStr[size] = '\0';
 
   std::cout << std::boolalpha << isMathExpr(cStr) << '\n';
 

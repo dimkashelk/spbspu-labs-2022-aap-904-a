82a10f362a5de6c0a03b25959944ca0674d2a15a
Kirill Odintsov <ksodintsov03@gmail.com>
2022-12-28 17:06:31 +0300

    std::boolalpha

--- odintsov.kirill/I5/main.cpp
+++ odintsov.kirill/I5/main.cpp
@@ -20,7 +20,7 @@ int main()
       std::cout << "Error: Empty string\n";
       return 1;
     }
-    std::cout << (odintsov::isRealNumber(string.str) ? "true\n" : "false\n");
+    std::cout << std::boolalpha << odintsov::isRealNumber(string.str) << '\n';
   } catch (const std::exception& err) {
     std::cout << "Error: " << err.what() << '\n';
     return 1;

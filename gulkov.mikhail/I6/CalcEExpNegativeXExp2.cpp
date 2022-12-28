d380cad42cd0aab2b7ad107ea0587418e12830af
Gulkov Mikhail <gulkov.mikhail@mail.ru>
2022-12-28 23:43:57 +0300

    Add class with overloaded operator, add function to count term (countTerm)

--- gulkov.mikhail/I6/CalcEExpNegativeXExp2.cpp
+++ gulkov.mikhail/I6/CalcEExpNegativeXExp2.cpp
@@ -1,5 +1,17 @@
 #include "CalcEExpNegativeXExp2.hpp"
 
+CalcEExpNegativeXExp2::CalcEExpNegativeXExp2(double x, double absError, size_t numberMax) : x(x), absError(absError),
+                                                                                            terms_number(0), numberMax(numberMax)
+{}
+
+double CalcEExpNegativeXExp2::operator()()
+{
+  terms_number = 0;
+  double result = 0;
+
+  return result;
+}
+
 double countFraction(double x, double factorial_of, unsigned int exp)
 {
   int factorial = 0;
@@ -12,4 +24,20 @@ double countFraction(double x, double factorial_of, unsigned int exp)
     factorial *= i;
   }
   return (x / factorial);
+}
+
+double countTerm(double terms_count, double x, double factorial_of, unsigned int exp)
+{
+  if (terms_count == 0)
+  {
+    return 1;
+  }
+  else if (terms_count == 1)
+  {
+    return (x * x);
+  }
+  else
+  {
+    return countFraction(x, factorial_of, exp);
+  }
 }
--- gulkov.mikhail/I6/CalcEExpNegativeXExp2.hpp
+++ gulkov.mikhail/I6/CalcEExpNegativeXExp2.hpp
@@ -3,6 +3,22 @@
 
 #include <cstddef>
 
+class CalcEExpNegativeXExp2
+{
+public:
+  explicit CalcEExpNegativeXExp2(double x, double absError, size_t numberMax);
+
+  double operator()();
+
+private:
+  double x;
+  double absError;
+  size_t terms_number;
+  size_t numberMax;
+};
+
 double countFraction(double x, double factorial_of, unsigned int exp);
+
+double countTerm(double terms_count, double x, double factorial_of, unsigned int exp);
 
 #endif

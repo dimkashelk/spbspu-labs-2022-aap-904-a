02525f87e1e821b0d56550238308d146637ebf41
ArsDi <abstractlevl@gmail.com>
2022-12-28 23:35:02 +0300

    adding elementary functions

--- dmitriev.arseny/I5/isExpression.cpp
+++ dmitriev.arseny/I5/isExpression.cpp
@@ -1,5 +1,45 @@
 #include "isExpression.h"
 
+bool isLetter(const char val)
+{
+  return (val >= 'A') and (val <= 'E');
+}
+
+bool isDigit(const char val)
+{
+  return (val >= '0') and (val <= '9');
+}
+
+bool isOpenBrace(const char val)
+{
+  return (val == '(');
+}
+
+bool isCloseBrace(const char val)
+{
+  return (val == ')');
+}
+
+bool isMuSi(const char val)
+{
+  return (val == '*');
+}
+
+bool isAdSi(const char val)
+{
+  return (val == '+');
+}
+
+bool isSuSi(const char val)
+{
+  return (val == '-');
+}
+
+bool isEnd(const char val)
+{
+  return (val == '\0');
+}
+
 bool isExpr(char* c)
 {
   return

35b177fa7aee505b7840ee45637b6ba494f1624b
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2022-12-28 15:18:06 +0000

    add "/0"

--- zhuravlev.ivan/I3/finder_same_elements_in_two_strings.cpp
+++ zhuravlev.ivan/I3/finder_same_elements_in_two_strings.cpp
@@ -24,10 +24,10 @@ void finderSameElementsInTwoStrings(const char *first_string, const char *second
   {
     result_string[i] = tmp_string[i];
   }
-  result_string[cnt - 1] = '\0';
 
   for (size_t i = 0; i <= cnt; ++i)
   {
     std::cout << result_string[i] << " ";
   }
+  std::cout << '\0';
 }

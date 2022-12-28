16e98e68df284c649dd5d22f28aedb92873b3a4d
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2022-12-28 22:25:33 +0300

    input of variables

--- azheganova.faina/I6/main.cpp
+++ azheganova.faina/I6/main.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
+
 int main()
-{}
+{
+  double borderleft = 0.0;
+  double borderright = 0.0;
+  int maxnumber = 0;
+  std::cin >> borderleft;
+  std::cin >> borderright;
+  std::cin >> maxnumber;
+  if (!std::cin)
+  {
+    std::cout << "error";
+    return 1;
+  }
+
+}

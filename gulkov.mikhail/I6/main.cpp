9fb92e3eda28aef450c7744acf6ea21de4bc816c
Gulkov Mikhail <gulkov.mikhail@mail.ru>
2022-12-28 19:05:16 +0300

    Create base variables, make input

--- gulkov.mikhail/I6/main.cpp
+++ gulkov.mikhail/I6/main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <cstddef>
+
 int main()
 {
+  size_t number_max = 0;
+  double lhs = 0.0;
+  double rhs = 0.0;
+  const double step = 0.01;
+  const double error = 0.0001;
+  std::cin >> lhs >> rhs >> number_max;
 
+  if (!std::cin || lhs < -1.0 || rhs > 1.0 || lhs > rhs)
+  {
+    std::cerr << "Error: incorrect input\n";
+    return 1;
+  }
 }

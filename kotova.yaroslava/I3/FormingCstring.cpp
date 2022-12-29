b2e73b694dce25cee84d544765fd4cc541d07637
Yaroslava <aroslavakotova@gmail.com>
2022-12-29 15:37:41 +0300

    try

--- kotova.yaroslava/I3/FormingCstring.cpp
+++ kotova.yaroslava/I3/FormingCstring.cpp
@@ -16,17 +16,12 @@ char* formingCstring(char* cstring, size_t& capacity, std::istream& input)
       {
         char* newstr = new char[capacity + 20];
         newstr [capacity+19] = '\0';
-        char* i = new char[capacity + 1];
-        i[capacity] = '\0';
-        char * j = new char[capacity + 21];
-        j[capacity + 20] = '\0';
-        strcpy(i, cstring);
-        strcpy(j, newstr);
-        while(i != cstring + size)
+        for (cstring; cstring != cstring + size; cstring++)
         {
-          *j = *i;
-          i++;
-          j++;
+          for (newstr; cstring != cstring + size; newstr++)
+          {
+            strcpy(newstr, cstring);
+          }
         }
         delete [] cstring;
         cstring = newstr;

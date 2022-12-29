7396147c8b8ef7c53438af13c5b86f9582fdbf06
Gulkov Mikhail <gulkov.mikhail@mail.ru>
2022-12-29 19:01:55 +0300

    Fix exp counter

--- gulkov.mikhail/I6/CalcEExpNegativeXExp2.cpp
+++ gulkov.mikhail/I6/CalcEExpNegativeXExp2.cpp
@@ -1,17 +1,17 @@
 #include "CalcEExpNegativeXExp2.hpp"
 
 CalcEExpNegativeXExp2::CalcEExpNegativeXExp2(double x, double absError, size_t numberMax) : x(x), absError(absError), result(0),
-                                                                                            factorial_of(2), exp(2),
+                                                                                            factorial_of(2), exp(3),
                                                                                             terms_count(0), numberMax(numberMax)
 {}
 
 double CalcEExpNegativeXExp2::operator()()
 {
-  while (terms_count <= numberMax)
+  while (terms_count < numberMax)
   {
     result +=
       terms_count % 2 == 0 ? countTerm(terms_count, x, factorial_of, exp) : (-1 * countTerm(terms_count, x, factorial_of, exp));
-    ++terms_count;
+    terms_count++;
   }
 
   return result;
@@ -19,25 +19,26 @@ double CalcEExpNegativeXExp2::operator()()
 
 double countFraction(double x, unsigned int &factorial_of, unsigned int &exp)
 {
-  int factorial = 0;
-  for (unsigned int i = 0; i < exp; ++i)
+  double counted_exp_x = x;
+  int factorial = 1;
+  for (unsigned int i = 0; i < exp; i++)
   {
-    x *= x;
+    counted_exp_x *= x;
   }
-  for (unsigned int i = 0; i < factorial_of; ++i)
+  for (unsigned int i = 1; i <= factorial_of; i++)
   {
     factorial *= static_cast< int >(i);
   }
   exp += 2;
-  ++factorial;
-  return (x / factorial);
+  factorial_of++;
+  return (counted_exp_x / factorial);
 }
 
 double countTerm(size_t terms_count, double x, unsigned int &factorial_of, unsigned int &exp)
 {
   if (terms_count == 0)
   {
-    return 1;
+    return (1);
   }
   else if (terms_count == 1)
   {

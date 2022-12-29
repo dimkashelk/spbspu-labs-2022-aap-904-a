8c7cafb0bcdedd8fd72980731d33d5a0cfc78eaf
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2022-12-29 14:15:00 +0300

    Add some simple functions

--- fesenko.ivan/I5/isFloat.cpp
+++ fesenko.ivan/I5/isFloat.cpp
@@ -1,6 +1,34 @@
 #include "isFloat.h"
 #include <cctype>
 
+bool isSign(char x)
+{
+  return x == '+' || x == '-';
+}
+bool isDigit(char x)
+{
+  return std::isdigit(x);
+}
+bool isNonZero(char x)
+{
+  return x != '0';
+}
+bool isEnd(char x)
+{
+  return x == '\0';
+}
+
+bool isDot(char x)
+{
+  return x == '.';
+}
+
+bool isE(char x)
+{
+  return x == 'E';
+}
+
+
 bool isFloat(const char *x)
 {
   return *x == '\0';

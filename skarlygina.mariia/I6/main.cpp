cbd4d6e27a37a4c566177f656284eb3d6b72ae54
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-12-30 00:54:45 +0300

    input parameters of interval and members

--- skarlygina.mariia/I6/main.cpp
+++ skarlygina.mariia/I6/main.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
 int main()
 {
+  double left_border = 0.0;
+  double right_border = 0.0;
+  unsigned members = 0;
+  std::cin >> left_border >> right_border >> members;
+  if (!std::cin || left_border >= right_border || left_border <= -1.0 || right_border >= 1.0)
+  {
+    std::cerr << "Error: false parameters";
+    return 1;
+  }
   return 0;
 }

02e31c681ed0c5dc1e814cfb0b89fc893ea70efa
KiraGelaz <kiragelaz@yandex.ru>
2022-12-29 03:34:47 +0300

    try mistakes again

--- kryuchkova.kira/I4/sum_elements_of_new_matrix.h
+++ kryuchkova.kira/I4/sum_elements_of_new_matrix.h
@@ -2,6 +2,6 @@
 #define SUM_ELEMENTS_OF_NEW_MATRIX
 #include <cstddef>
 
-double sumElementsOfNewMatrix(int *matrix, size_t rows, size_t columns);
+double sumElementsOfNewMatrix(const int *matrix, size_t rows, size_t columns);
 
 #endif
--- kryuchkova.kira/I4/unique_elements.h
+++ kryuchkova.kira/I4/unique_elements.h
@@ -2,6 +2,6 @@
 #define UNIQUE_ELEMENTS_H
 #include <cstddef>
 
-size_t countColumnsWithUniqueElements(int *matrix, size_t rows, size_t columns);
+size_t countColumnsWithUniqueElements(const int *matrix, size_t rows, size_t columns);
 
 #endif

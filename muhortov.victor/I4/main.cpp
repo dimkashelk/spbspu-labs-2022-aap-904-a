0ab75c83f956e281a05d2470ac6c7df227ed64b1
Victor Muhortov <viktormuhortov26@gmail.com>
2022-12-30 21:28:20 +0300

    create main variables and input

--- muhortov.victor/I4/main.cpp
+++ muhortov.victor/I4/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstddef>
 #include <fstream>
+#include <cstring>
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +11,26 @@ int main(int argc, char *argv[])
     std::cerr << "Error: 4 arguments nof found\n";
     return 1;
   }
+  size_t cols = 0;
+  size_t rows = 0;
+  std::ifstream in(argv[2]);
+  in >> cols;
+  in >> rows;
 
+  if (!in)
+  {
+    std::cerr << "Error: problems with reading file\n";
+    return 1;
+  }
+  if (!std::strcmp(argv[1], "1"))
+  {
+
+    return 1;
+  }
+  else if (!std::strcmp(argv[1], "2"))
+  {
+
+    return 1;
+  }
   return 0;
 }

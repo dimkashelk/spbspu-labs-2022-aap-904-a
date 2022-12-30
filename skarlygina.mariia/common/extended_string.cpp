33a24c06e17f6d4f397eb40d9f28d52d29faeb27
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-12-30 04:12:57 +0300

    try2

--- skarlygina.mariia/common/extended_string.cpp
+++ skarlygina.mariia/common/extended_string.cpp
@@ -6,6 +6,7 @@ char* makeExtendedString(char* cstring, size_t& capacity)
   char* newstring = new char[capacity + 20];
   cstring[capacity - 1] = '\0';
   std::strcpy(newstring, cstring);
+  delete[] cstring;
   capacity += 20;
   return newstring;
 }

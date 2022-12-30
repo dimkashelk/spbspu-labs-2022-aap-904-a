0011fa0325d4c4dea9fcbbc677be2c701b757329
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2022-12-30 10:23:44 +0300

    Function implementation: hasItTheSameSimbols

--- mishanina.elena/I3/hasItTheSameSimbols.cpp
+++ mishanina.elena/I3/hasItTheSameSimbols.cpp
@@ -2,5 +2,15 @@
 
 bool hasItTheSameSimbols(const char* cstring1, const char* cstring2)
 {
+  for (size_t i = 0; i < strlen(cstring1); i++)
+  {
+    for (size_t j = 0; j < strlen(cstring2); j++)
+    {
+      if (cstring1[i] == cstring2[j])
+      {
+        return true;
+      }
+    }
+  }
   return false;
 }

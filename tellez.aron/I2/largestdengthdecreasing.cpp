8d23e4be460ac21ec786609c7a1d0c2b9db4eb66
aront14 <arontellezc14@gmail.com>
2022-12-30 05:01:46 +0300

    2nd lab done

--- tellez.aron/I2/largestdengthdecreasing.cpp
+++ tellez.aron/I2/largestdengthdecreasing.cpp
@@ -1,9 +1,9 @@
 #include "largestdengthdecreasing.h"
-size_t largest_dength_decreasing(const int *arr,const size_t size)
+size_t largest_dength_decreasing(const int *arr, const size_t size)
 {
   int max = 0; 
   int len = 1;
-  for (int i = 0; i < size-1; i++)
+  for (int i = 0; i < size - 1; i++)
   {
     if (arr[i + 1] < arr[i])
       len++;

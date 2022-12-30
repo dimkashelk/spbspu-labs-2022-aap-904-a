6c33a844f2423dbcde47249190a22cab43710c79
ArsDi <abstractlevl@gmail.com>
2022-12-31 02:48:59 +0300

    function realization

--- dmitriev.arseny/I6/defineArcSin.cpp
+++ dmitriev.arseny/I6/defineArcSin.cpp
@@ -3,5 +3,23 @@
 
 double defineArcSin(double x, double absError, unsigned numberMax)
 {
+  if (x * x > 1)
+  {
+    throw std::logic_error("x does not fall within the specified interval");
+  }
+  MemberOfRow defineNewMember(x);
+  double result = x;
+  double member = 0.0;
 
+  for (unsigned n = 0; n < numberMax; n++)
+  {
+    result = result + member;
+    member = defineNewMember();
+    if (member < absError)
+    {
+      return result;
+    }
+  }
+
+  throw std::logic_error("the required accuracy has not been achieved");
 }

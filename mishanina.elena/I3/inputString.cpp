cc4d02acb36868e5dc435ed5e19946da2ed20a5c
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2022-12-30 09:55:14 +0300

    Function implementation: inputString

--- mishanina.elena/I3/inputString.cpp
+++ mishanina.elena/I3/inputString.cpp
@@ -2,5 +2,31 @@
 
 char* inputString(char* cstring, size_t &capacity, std::istream& input)
 {
+  size_t size = 0;
+  input >> std::noskipws;
+  do
+  {
+    if (size == capacity)
+    {
+      try
+      {
+        char* newstr = new char[capacity + 20];
+        for (auto i = cstring, j = newstr; i != cstring + size; ++i, ++j)
+        {
+          *j = *i;
+        }
+        delete[] cstring;
+        cstring = newstr;
+        capacity += 20;
+      }
+      catch (...)
+      {
+        delete[] cstring;
+        throw;
+      }
+    }
+    input >> cstring[size];
+  } while (input && cstring[size++] != '\n');
+  capacity = size;
   return cstring;
 }

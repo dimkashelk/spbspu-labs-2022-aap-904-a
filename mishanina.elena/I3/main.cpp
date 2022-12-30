358ab9f6fa2fa5aba724d7c4aaa96d60a4e518cb
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2022-12-30 10:06:56 +0300

    Create cstring1

--- mishanina.elena/I3/main.cpp
+++ mishanina.elena/I3/main.cpp
@@ -2,4 +2,24 @@
 
 int main()
 {
+  size_t capacity1 = 10;
+  char* cstring1 = new char[capacity1];
+  cstring1[0] = '\0';
+  try
+  {
+    cstring1 = inputString(cstring1, capacity1, std::cin);
+    if (cstring1[0] == '\0')
+    {
+      std::cout << "ERROR: empty string";
+      delete[] cstring1;
+      return 1;
+    }
+    cstring1[capacity1 - 1] = '\0';
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+    delete[] cstring1;
+    return 1;
+  }
 }

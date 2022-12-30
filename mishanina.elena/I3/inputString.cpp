f1a709750673bd89eac662f48883bdc5ddae5393
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2022-12-30 09:50:12 +0300

    Create function inputString

---
+++ mishanina.elena/I3/inputString.cpp
@@ -0,0 +1,6 @@
+#include "inputString.h"
+
+char* inputString(char* cstring, size_t &capacity, std::istream& input)
+{
+  return cstring;
+}
---
+++ mishanina.elena/I3/inputString.h
@@ -0,0 +1,7 @@
+#ifndef INPUT_STRINGS_H
+#define INPUT_STRINGS_H
+#include <istream>
+
+char* inputString(char* cstring, size_t &capacity, std::istream& input);
+
+#endif

ea2b79988d97d40c078e31f4c49519e68b094a97
ArsDi <abstractlevl@gmail.com>
2022-12-31 14:23:55 +0300

    no try catch realization

--- dmitriev.arseny/I6/printTable.cpp
+++ dmitriev.arseny/I6/printTable.cpp
@@ -5,18 +5,8 @@
 
 void printRowInTable(std::ostream& out, double x, double absError, unsigned numberMax)
 {
-  double result = 0.0;
-  try
-  {
-    result = defineArcSin(x, absError, numberMax);
-  }
-  catch (const std::exception& e)
-  {
-    throw;
-  }
-
   out << std::setw(5) << x << " ";
-  out << std::setw(10) << std::setprecision(5) << result << " ";
+  out << std::setw(10) << std::setprecision(5) << defineArcSin(x, absError, numberMax) << " ";
   out << std::setw(10) << std::setprecision(5) << std::asin(x) << "\n";
 }
 
@@ -28,21 +18,7 @@ void printTable(std::ostream& out, double leftBorder, double rightBorder, double
   }
   for (double x = leftBorder; x + step < rightBorder; x = x + step)
   {
-    try
-    {
       printRowInTable(out << "\n", x, absError, numberMax);
-    }
-    catch (const std::exception& e)
-    {
-      throw;
-    }
   }
-  try
-  {
     printRowInTable(out << "\n", rightBorder, absError, numberMax);
-  }
-  catch (const std::exception& e)
-  {
-    throw;
-  }
 }

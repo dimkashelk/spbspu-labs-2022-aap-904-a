fac38e5cf2b191a5882868b6594aa75ee9e217d7
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2022-12-31 11:32:56 +0300

    add throw to rectangle_t constructor

--- shelkovnikov.dmitrii/T1/base_types.cpp
+++ shelkovnikov.dmitrii/T1/base_types.cpp
@@ -22,7 +22,12 @@ rectangle_t::rectangle_t(point_t point_1, point_t point_2):
   point((point_1.x + point_2.x) / 2, (point_1.y + point_2.y) / 2),
   width(point_2.x - point_1.x),
   height(point_2.y - point_1.y)
-{}
+{
+  if (point_2.x < point_1.x || point_2.y < point_1.y)
+  {
+    throw std::logic_error("It isn't rectangle");
+  }
+}
 rectangle_t::rectangle_t(point_t point, double width, double height):
   point(point),
   width(width),

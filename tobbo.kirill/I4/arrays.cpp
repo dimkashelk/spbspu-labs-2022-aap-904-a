78b501dff50dbc5240bf677cd573416b7247a45a
Kirill Tobbo <tobbo.kv@edu.spbstu.ru>
2022-12-31 11:07:15 +0300

    fix warnings

---
+++ tobbo.kirill/I4/arrays.cpp
@@ -0,0 +1,86 @@
+#include "arrays.h"
+#include <cstddef>
+#include <cstring>
+#include <stdexcept>
+
+void fillArray(int* arr, size_t rows, size_t cols, std::ifstream* in)
+{
+  for (size_t i = 0; i < rows; i++)
+  {
+    for (size_t j = 0; j < cols; j++)
+    {
+      *in >> arr[i * cols + j];
+      if (!in)
+      {
+        throw std::runtime_error("Error while readaing from file");
+      }
+    }
+  }
+}
+
+void printArray(std::ostream& out, int* arr, size_t rows, size_t cols)
+{
+  for (size_t row = 0; row < rows; row++)
+  {
+    for (size_t col = 0; col < cols; col++)
+    {
+      out << arr[row * cols + col];
+      if (col == (cols - 1))
+      {
+        out << "\n";
+      }
+      else
+      {
+        out << " ";
+      }
+    }
+  }
+}
+
+size_t getPositiveRowsCount(int* arr, size_t rows, size_t cols)
+{
+  size_t count = 0;
+  for (size_t row = 0; row < rows; row++)
+  {
+    bool foundNegative = false;
+    for (size_t col = 0; col < cols; col++)
+    {
+      if (arr[row * cols + col] < 0)
+      {
+        foundNegative = true;
+        break;
+      }
+    }
+    if (!foundNegative)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+int getMaxSumDiag(int* arr, size_t size)
+{
+  size_t rows = size;
+  size_t cols = size;
+  int* sums = new int[2 * rows - 1] { 0 };
+  for (size_t row = 0; row < rows; row++)
+  {
+    for (size_t col = 0; col < cols; col++)
+    {
+      size_t diagNum = row + col;
+      sums[diagNum] += arr[row * cols + rows - 1 - col];
+    }
+  }
+  size_t maxDiag = 0;
+  for (size_t i = 1; i < rows * 2 - 1; i++)
+  {
+    if (i + 1 != rows && sums[i] > sums[maxDiag])
+    {
+      maxDiag = i;
+    }
+  }
+  int maxSum = sums[maxDiag];
+  delete[] sums;
+  return maxSum;
+}
--- tobbo.kirill/I4/main.cpp
+++ tobbo.kirill/I4/main.cpp
@@ -1,92 +1,5 @@
-#include <stdexcept>
-#include <iostream>
-#include <cstddef>
-#include <fstream>
-#include <cstring>
+#include "arrays.h"
 
-void fillArray(std::ostream& out, int* arr, size_t rows, size_t cols, std::ifstream* in)
-{
-  for (size_t i = 0; i < rows; i++)
-  {
-    for (size_t j = 0; j < cols; j++)
-    {
-      *in >> arr[i * cols + j];
-      if (!in)
-      {
-        out << "Error";
-        return;
-      }
-      out << "row: " << i << " col: " << j << " index= " << i * cols + j << " = " << arr[i * cols + j] << "\n";
-    }
-  }
-}
-
-void printArray(std::ostream& out, int* arr, size_t rows, size_t cols)
-{
-  for (size_t row = 0; row < rows; row++)
-  {
-    for (size_t col = 0; col < cols; col++)
-    {
-      std::cout << arr[row * cols + col];
-      if (col == (cols - 1))
-      {
-        out << "\n";
-      }
-      else
-      {
-        out << " ";
-      }
-    }
-  }
-}
-
-size_t getPositiveRowsCount(int* arr, size_t rows, size_t cols)
-{
-  size_t count = 0;
-  for (size_t row = 0; row < rows; row++)
-  {
-    bool foundNegative = false;
-    for (size_t col = 0; col < cols; col++)
-    {
-      if (arr[row * cols + col] < 0)
-      {
-        foundNegative = true;
-        break;
-      }
-    }
-    if (!foundNegative)
-    {
-      count++;
-    }
-  }
-  return count;
-}
-
-int getMaxSumDiag(int* arr, size_t size)
-{
-  size_t rows = size;
-  size_t cols = size;
-  int* sums = new int[2 * rows - 1] { 0 };
-  for (size_t row = 0; row < rows; row++)
-  {
-    for (size_t col = 0; col < cols; col++)
-    {
-      size_t diagNum = row + col;
-      sums[diagNum] += arr[row * cols + rows - 1 - col];
-    }
-  }
-  size_t maxDiag = 0;
-  for (size_t i = 1; i < rows * 2 - 1; i++)
-  {
-    if (i + 1 != rows && sums[i] > sums[maxDiag])
-    {
-      maxDiag = i;
-    }
-  }
-  int maxSum = sums[maxDiag];
-  delete[] sums;
-  return maxSum;
-}
 
 int main(int argc, char* argv[])
 {
@@ -109,15 +22,29 @@ int main(int argc, char* argv[])
     std::cerr << "Too many rows or columns. Max is 1000 elements\n";
     return 1;
   }
-  if (!strcmp(argv[1], "3"))
+  std::ofstream output(argv[3]);
+  if (!output)
+  {
+    std::cerr << "Cannot open output file\n";
+    return 1;
+  }
+  if (!strcmp(argv[1], "1"))
   {
     int arr[1000];
-    fillArray(std::cout, arr, rows, cols, &in);
-    printArray(std::cout, arr, rows, cols);
-    size_t positiveRowsCount = getPositiveRowsCount(reinterpret_cast< int* >(&arr), rows, cols);
-    std::cout << "Found positive rows count: " << positiveRowsCount << "\n";
+    try
+    {
+      fillArray(arr, rows, cols, &in);
+      printArray(output, arr, rows, cols);
+      size_t positiveRowsCount = getPositiveRowsCount(reinterpret_cast< int* >(&arr), rows, cols);
+      output << "Found positive rows count: " << positiveRowsCount << "\n";
+    }
+    catch (const std::exception& e)
+    {
+      std::cerr << e.what() << "\n";
+      return 1;
+    }
   }
-  else if (!strcmp(argv[1], "14"))
+  else if (!strcmp(argv[1], "2"))
   {
     if (rows != cols)
     {
@@ -125,11 +52,20 @@ int main(int argc, char* argv[])
       return 1;
     }
     int* arr = new int[rows * cols];
-    fillArray(std::cout, arr, rows, cols, &in);
-    printArray(std::cout, arr, rows, cols);
-    int maxSumDiag = getMaxSumDiag(arr, rows);
-    std::cout << "Max sum in diagonals except main one: " << maxSumDiag << "\n";
-    delete[] arr;
+    try
+    {
+      fillArray(arr, rows, cols, &in);
+      printArray(output, arr, rows, cols);
+      int maxSumDiag = getMaxSumDiag(arr, rows);
+      output << "Max sum in diagonals except main one: " << maxSumDiag << "\n";
+      delete[] arr;
+    }
+    catch (const std::exception& e)
+    {
+      std::cerr << e.what() << "\n";
+      delete[] arr;
+      return 1;
+    }
   }
   else
   {

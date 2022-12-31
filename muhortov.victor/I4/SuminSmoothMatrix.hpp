3da31c34be8ac26e7f68da30260cc72dc884821c
Victor Muhortov <viktormuhortov26@gmail.com>
2022-12-31 13:49:36 +0300

    retry, because of problem with lab I3 (gitlab shiza)

--- muhortov.victor/I4/SuminSmoothMatrix.hpp
+++ muhortov.victor/I4/SuminSmoothMatrix.hpp
@@ -1,8 +1,8 @@
 #ifndef LABS_SUMINSMOOTHMATRIX_HPP
 #define LABS_SUMINSMOOTHMATRIX_HPP
 
-#include <cstddef>
 #include <cmath>
+#include <cstddef>
 #include "SmoothingMatrix.hpp"
 
 double summingInSmoothMatrix(const int *arr, size_t cols, size_t rows);

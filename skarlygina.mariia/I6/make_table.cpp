715b31d7297a876ee39b0ba5c585ed2c6f6c32a7
Мария Скарлыгина <skarlygina04@yandex.ru>
2022-12-31 03:20:58 +0300

    fixed cg 1.5

--- skarlygina.mariia/I6/make_table.cpp
+++ skarlygina.mariia/I6/make_table.cpp
@@ -2,23 +2,21 @@
 #include "sinx.h"
 #include <iomanip>
 
-
 void printTableRow(std::ostream& out, double x, double error, unsigned members)
 {
-    out << std::setw(5) << x << ' ';
-    out << std::setw(10) << std::setprecision(5) << sinx(x, error, members) << ' ';
-    out << std::setw(10) << std::setprecision(5) << std::sin(x) << '\n';
+  out << std::setw(5) << x << ' ';
+  out << std::setw(10) << std::setprecision(5) << sinx(x, error, members) << ' ';
+  out << std::setw(10) << std::setprecision(5) << std::sin(x) << '\n';
 }
 
 void printTable(std::ostream& out, double error, unsigned members, double left_border, double right_border, double step)
 {
-    for (double x = left_border; x <= right_border; x = x + step)
+  for (double x = left_border; x <= right_border; x = x + step)
+  {
+    printTableRow(out, x, error, members);
+    if (x + step >= right_border)
     {
-        printTableRow(out, x, error, members);
-        if (x + step >= right_border)
-        {
-            printTableRow(out, right_border, error, members);
-        }
+      printTableRow(out, right_border, error, members);
     }
+  }
 }
-

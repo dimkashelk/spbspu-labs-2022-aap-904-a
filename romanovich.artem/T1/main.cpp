4f01e4836e9b4bfa37e35b723236ddf8a529feb3
Artem <artrom170@gmail.com>
2023-01-01 13:59:20 +0300

    build parallelogram func

--- romanovich.artem/T1/main.cpp
+++ romanovich.artem/T1/main.cpp
@@ -1,3 +1,22 @@
+#include <string>
+#include <iostream>
+void buildParallelogram()
+{
+  double xa, ya, xb, yb, xc, yc = 0;
+  std::cin >> xa >> ya >> xb >> yb >> xc >> yc;
+  double xd = xa - xb + xc;
+  double yd = ya - yb + yc;
+  std::cout << xd << " " << yd;
+}
+// PARALLELOGRAM, CONCAVE
 int main()
 {
+  std::string figureName = "";
+  std::cin >> figureName;
+  std::cout << figureName << "\n";
+  if (figureName == "PARALLELOGRAM")
+  {
+    buildParallelogram();
+  }
+  return 0;
 }

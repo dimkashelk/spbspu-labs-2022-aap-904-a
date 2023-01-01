0acd470f73aacc7c8141c28e99c3c94bfb1102e4
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-01-01 18:22:17 +0300

    update func signature

--- shelkovnikov.dmitrii/T1/base_functions.h
+++ shelkovnikov.dmitrii/T1/base_functions.h
@@ -1,6 +1,6 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_BASE_FUNCTIONS_H
 #define SPBSPU_LABS_2022_AAP_904_A_BASE_FUNCTIONS_H
 #include "base_types.h"
-point_t getLeftDownPoint();
-point_t getRightUpPoint();
+point_t getLeftDownPoint(rectangle_t rect);
+point_t getRightUpPoint(rectangle_t rect);
 #endif

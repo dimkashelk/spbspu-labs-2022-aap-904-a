5a8ae6037e4c06f6d604dbc418adeaf90ca31784
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-01-01 21:23:41 +0300

    add input_regular

--- shelkovnikov.dmitrii/T1/input_shapes.cpp
+++ shelkovnikov.dmitrii/T1/input_shapes.cpp
@@ -13,3 +13,19 @@ dimkashelk::Rectangle* dimkashelk::input_rectangle(std::istream &in)
   }
   return new Rectangle(point_t{x1, y1}, point_t{x2, y2});
 }
+dimkashelk::Regular *dimkashelk::input_regular(std::istream &in)
+{
+  double x = 0.0;
+  double y = 0.0;
+  dimkashelk::point_t point[3];
+  for (size_t i = 0; i < 3; i++)
+  {
+    in >> x >> y;
+    point[i] = dimkashelk::point_t{x, y};
+  }
+  if (!in)
+  {
+    throw std::logic_error("Not enough data for input");
+  }
+  return new Regular(point[0], point[1], point[2]);
+}

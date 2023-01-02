3d45a8bd32e0f8eef02ceff140ecea72ce4b181e
xeniaTa <kseniatarasenko1@gmail.com>
2023-01-02 23:33:44 +0300

    add outputFrameRect

---
+++ tarasenko.ksenia/T1/outputframerect.hpp
@@ -0,0 +1,10 @@
+#ifndef OUTPUTFRAMERECT_HPP
+#define OUTPUTFRAMERECT_HPP
+#include <iostream>
+#include "base-types.hpp"
+
+namespace tarasenko
+{
+  void outputFrameRect(std::ostream & output, const rectangle_t & rect);
+}
+#endif

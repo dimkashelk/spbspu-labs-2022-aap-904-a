2b3d5318a6899d7db9c9b0b907bda7d608b158b8
timofeev maxim <maks.timofeev9834@gmail.com>
2023-01-02 12:49:09 +0400

    brr

--- timofeev.maxim/I2/main.cpp
+++ timofeev.maxim/I2/main.cpp
@@ -17,6 +17,49 @@ int main(int argc, char *argv[])
     std::cerr << "File error" << "\n";
     return 2;
   }
+  int ready_array[5] = {4,5,3,2,1};
+  try
+  {
+    std::cout << "Length of Decrease1: " << LengthOfDecreaseVal(ready_array, 5) << "\n";
+    std::cout << "Sorted array1: " ;
+    Sort_Array(ready_array,5);
+    std::cout << "\n";
+  }
+  catch (std::overflow_error & e)
+  {
+    std::cout << e.what() << "\n";
+    return 0;
+  }
+
+  size_t line;
+  std::cin >> line;
+  if (!std::cin)
+  {
+    std::cout << "Input error" << "\n";
+    return 2;
+  }
+  std::srand(time(NULL));
+  int * dyn_array = new int[line];
+  size_t i;
+  for (i = 0; i < line; i++)
+  {
+    dyn_array[i] = rand() % 100;
+  }
+  try
+  {
+    std::cout << "Length of Decrease2: " << LengthOfDecreaseVal(dyn_array, line) << "\n";
+    std::cout << "Sorted array2: " ;
+    Sort_Array(dyn_array,line);
+    std::cout << "\n";
+  }
+  catch (std::overflow_error & e)
+  {
+    std::cout << e.what() << "\n";
+    delete [] dyn_array;
+    return 2;
+  }
+  delete [] dyn_array;
+
   size_t fsize = 0;
   int * File_Array = new int[fsize];
   std::string fname =argv[1];
@@ -66,52 +109,4 @@ int main(int argc, char *argv[])
     }
     delete [] File_Array;
   }
-  int ready_array[5] = {4,5,3,2,1};
-  try
-  {
-    std::cout << "Length of Decrease1: " << LengthOfDecreaseVal(ready_array, 5) << "\n";
-    std::cout << "Sorted array1: " ;
-    Sort_Array(ready_array,5);
-    std::cout << "\n";
-  }
-  catch (std::overflow_error & e)
-  {
-    std::cout << e.what() << "\n";
-    return 0;
-  }
-
-  size_t line;
-  std::cin >> line;
-  if (!std::cin)
-  {
-    std::cout << "Input error" << "\n";
-    return 2;
-  }
-  std::srand(time(NULL));
-  int * dyn_array = new int[line];
-  size_t i;
-  for (i = 0; i < line; i++)
-  {
-    dyn_array[i] = rand() % 100;
-  }
-  try
-  {
-    std::cout << "Length of Decrease2: " << LengthOfDecreaseVal(dyn_array, line) << "\n";
-    std::cout << "Sorted array2: " ;
-    Sort_Array(dyn_array,line);
-    std::cout << "\n";
-  }
-  catch (std::overflow_error & e)
-  {
-    std::cout << e.what() << "\n";
-    delete [] dyn_array;
-    return 2;
-  }
-  catch (std::length_error & e)
-  {
-    std::cout << e.what() << "\n";
-    delete [] dyn_array;
-    return 0;
-  }
-  delete [] dyn_array;
 }

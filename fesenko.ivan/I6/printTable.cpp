916aca68b0c4a27ca2eaec199e73f05699100b18
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-01-02 17:28:09 +0500

    Complete function printTable

--- fesenko.ivan/I6/printTable.cpp
+++ fesenko.ivan/I6/printTable.cpp
@@ -1,5 +1,16 @@
 #include "printTable.h"
+#include <cmath>
+#include <iomanip>
+
+void printTableRow(std::ostream &out, double x, unsigned numberMax, double absError)
+{
+  out << std::setw(5) << x << " ";
+  out << std::setw(10) << std::setprecision(5) << std::exp(x) << "\n";
+}
 
 void printTable(std::ostream &out, double left, double right, double step, unsigned numberMax, double absError)
 {
+  for (double x = left; x < right; x += step) {
+    printTableRow(out, x, numberMax, absError);
+  }
 }

2cc4aef2f2f9072146ca8b7b07e7addbddab5f09
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-01-02 16:36:06 +0500

    Added variables, input, and exception handlers

--- fesenko.ivan/I6/main.cpp
+++ fesenko.ivan/I6/main.cpp
@@ -2,4 +2,18 @@
 
 int main()
 {
+  size_t numberMax = 0;
+  const double step = 0.01;
+  const double absError = 0.0001;
+  double left = 0.0;
+  double right = 0.0;
+  std::cin >> left >> right >> numberMax;
+  if (!std::cin) {
+    std::cerr << "Error while reading\n";
+    return 2;
+  }
+  if (left > right) {
+    std::cerr << "Invalid range\n";
+    return 2;
+  }
 }

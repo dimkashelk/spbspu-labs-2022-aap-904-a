f20b72468133a167b92fa1582d1d909c4fe74aa9
Yaroslava <aroslavakotova@gmail.com>
2023-01-02 15:50:21 +0300

    fix

--- kotova.yaroslava/I3/main.cpp
+++ kotova.yaroslava/I3/main.cpp
@@ -9,18 +9,7 @@ int main()
   size_t size = 1;
   size_t capacity1 = 10;
   size_t capacity2 = 10;
-  char* cstring1 = nullptr;
-  try
-  {
-    cstring1 = new char[capacity1];
-    delete[] cstring1;
-  }
-  catch (const std::exception &e)
-  {
-    std::cout << e.what();
-    delete[] cstring1;
-    return 1;
-  }
+  char* cstring1 = new char[capacity1];
   cstring1[0] = '\0';
   try
   {

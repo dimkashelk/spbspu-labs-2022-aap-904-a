eb43e9ee771aa9bde42bccfab6bea953c91984f0
Kirill Tobbo <tobbo.kv@edu.spbstu.ru>
2023-01-02 23:13:40 +0300

    polish

---
+++ tobbo.kirill/I5/isreal.cpp
@@ -0,0 +1,50 @@
+#include "isreal.h"
+
+bool isDigit(char c)
+{
+  return c >= '0' && c <= '9';
+}
+bool isSign(char c)
+{
+  if (c == '+' || c == '-')
+    return true;
+  return false;
+}
+bool isNatural(std::string str, int& index)
+{
+  if (isDigit(str[index]))
+  {
+    while (isDigit(str[++index]));
+    return true;
+  }
+  return false;
+}
+bool isOrder(std::string str, int& index)
+{
+  if (str[index] == 'E')
+  {
+    index++;
+    if (isNatural(str, index))
+    {
+      return true;
+    }
+    if (isSign(str[index]))
+    {
+      index++;
+      if (isNatural(str, index))
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+bool isMantissa(std::string str, int& index)
+{
+  return (str[index] == '.' && isNatural(str, ++index) || isNatural(str, index) && str[index] == '.' && isNatural(str, ++index));
+}
+bool isReal(const std::string str)
+{
+  int i = 0;
+  return (isMantissa(str, i) && isOrder(str, i) || isSign(str[i]) && isMantissa(str, ++i) && isOrder(str, i));
+}
---
+++ tobbo.kirill/I5/main.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<fstream>
+#include<string>
+#include "isreal.h"
+
+
+int main(int argc, char* argv[])
+{
+  std::ifstream fin(argv[1]);
+  if (!fin)
+  {
+    std::cerr << "Error while rading from file\n";
+    return 1;
+  }
+
+  std::string str;
+  while (fin >> str)
+  {
+    if (isReal(str))
+      std::cout << str << " True\n";
+    else
+      std::cout << str << " False\n";
+  }
+  fin.close();
+  return 0;
+}

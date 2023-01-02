c6279fa43be27d486f59342dd0b0fc7ac5dcc26d
Kirill Tobbo <tobbo.kv@edu.spbstu.ru>
2023-01-02 23:52:05 +0300

    polish

--- tobbo.kirill/I5/main.cpp
+++ tobbo.kirill/I5/main.cpp
@@ -5,8 +5,8 @@
 int main()
 {
   std::string line;
-
-  while (std::getline(std::cin, line) && !line.empty()) {
+  if (std::getline(std::cin, line) && !line.empty())
+  {
     if (isReal(line))
     {
       std::cout << line << " true\n";
@@ -15,6 +15,11 @@ int main()
     {
       std::cout << line << " false\n";
     }
+  }
+  else
+  {
+    std::cerr << "Error input!\n";
+    return 1;
   }
   return 0;
 }

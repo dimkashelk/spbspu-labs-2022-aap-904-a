9098f89ed3b9bf1b799e8c1abd4e67534aa3b66a
Kirill Tobbo <tobbo.kv@edu.spbstu.ru>
2023-01-02 23:39:29 +0300

    polish

--- tobbo.kirill/I5/main.cpp
+++ tobbo.kirill/I5/main.cpp
@@ -1,29 +1,20 @@
 ﻿#include<iostream>
-#include<fstream>
 #include<string>
 #include "isreal.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-  if (argc != 2)
-  {
-    std::cerr << "Wrong number of parameters\n";
-    return 1;
-  }
-  std::ifstream fin(argv[1]);
-  if (!fin)
-  {
-    std::cerr << "Error while rading from file\n";
-    return 1;
-  }
-  std::string str;
-  while (fin >> str)
-  {
-    if (isReal(str))
-      std::cout << str << " True\n";
+  std::string line;
+
+  while (std::getline(std::cin, line) && !line.empty()) {
+    if (isReal(line))
+    {
+      std::cout << line << " true\n";
+    }
     else
-      std::cout << str << " False\n";
+    {
+      std::cout << line << " false\n";
+    }
   }
-  fin.close();
   return 0;
 }

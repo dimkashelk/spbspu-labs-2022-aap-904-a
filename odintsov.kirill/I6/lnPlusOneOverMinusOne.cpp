501c948aa2159ee57abd4dac54f2022f16591797
Kirill Odintsov <ksodintsov03@gmail.com>
2023-01-04 00:25:43 +0300

    lnPlusONeOverMinusOne

---
+++ odintsov.kirill/I6/lnPlusOneOverMinusOne.cpp
@@ -0,0 +1,6 @@
+#include "lnPlusOneOverMinusOne.hpp"
+
+double lnPlusOneOverMinusOne(double x, double absError, unsigned numberMax)
+{
+  return 0.0;
+}
---
+++ odintsov.kirill/I6/lnPlusOneOverMinusOne.hpp
@@ -0,0 +1,8 @@
+#ifndef LNPLUSONEOVERMINUSONE_HPP
+#define LNPLUSONEOVERMINUSONE_HPP
+
+namespace odintsov {
+  double lnPlusOneOverMinusOne(double x, double absError, unsigned numberMax);
+}
+
+#endif

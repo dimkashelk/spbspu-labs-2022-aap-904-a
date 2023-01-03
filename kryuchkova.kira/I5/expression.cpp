c00087c37fd33b27de5555c3a71ac03e682ab150
KiraGelaz <kiragelaz@yandex.ru>
2023-01-03 03:51:17 +0300

    do first functions

--- kryuchkova.kira/I5/expression.cpp
+++ kryuchkova.kira/I5/expression.cpp
@@ -9,3 +9,19 @@ bool isCloseBrace (const char data)
 {
   return data == ')';
 }
+bool isDigit(const char data)
+{
+  return std::isdigit(data);
+}
+bool isIdentificator(const char data)
+{
+  return data == 'a' || data == 'b' || data == 'c' || data == 'd' || data == 'e' || data == 'f' || data == 'x' || data == 'y' || data == 'z';
+}
+bool isEnd(const char data)
+{
+  return (data == '\0');
+}
+bool isSign(const char data)
+{
+  return data == '+' || data == '-' || data == '/' || data == '*';
+}

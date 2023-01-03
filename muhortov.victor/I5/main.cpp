0d370886dd1f60cc8a852c799eef9b60a522d0c4
Victor Muhortov <viktormuhortov26@gmail.com>
2023-01-03 21:03:20 +0300

    Test makecstring in main

--- muhortov.victor/I5/main.cpp
+++ muhortov.victor/I5/main.cpp
@@ -1,4 +1,36 @@
+#include <iostream>
+#include <makecstring.hpp>
+
 int main()
 {
+  size_t size = 0;
+  size_t capacity = 10;
+  char *c_string = new char[capacity];
+
+  c_string[0] = '\n';
+
+  try
+  {
+    c_string = makeCString(c_string, size, capacity, std::cin);
+  }
+  catch (std::bad_alloc &e)
+  {
+    std::cerr << "Error: " << e.what();
+    delete[] c_string;
+    return 1;
+  }
+
+  if (c_string[0] == '\n')
+  {
+    std::cout << "Empty string";
+    delete[] c_string;
+    return 2;
+  }
+
+  c_string[size] = '\0';
+
+  std::cout << c_string << '\n';
+
+  delete[] c_string;
   return 0;
 }

2f12c7f0055cd6feeb43c0c8b39d16f21a70b214
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2023-01-03 04:34:42 +0300

    lllaba T1

--- azheganova.faina/T1/inputrectangle.cpp
+++ azheganova.faina/T1/inputrectangle.cpp
@@ -3,10 +3,10 @@
 
 rectangle* inputRectangle()
 {
-  point_t x1{};
-  point_t y1{};
-  point_t x2{};
-  point_t y2{};
+  rectangle x1{};
+  rectangle y1{};
+  rectangle x2{};
+  rectangle y2{};
   std::cin >> x1.x;
   std::cin >> y1.y;
   std::cin >> x2.x;

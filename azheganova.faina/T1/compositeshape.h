d4e9aa54e5fd068f1e0411d051c4a43b2ae7cec4
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2023-01-04 02:26:36 +0300

    1

--- azheganova.faina/T1/compositeshape.h
+++ azheganova.faina/T1/compositeshape.h
@@ -2,6 +2,8 @@
 #define COMPOSITESHAPE_H
 #include <cstddef>
 #include "shape.h"
+#include "base_types.h"
+#include "rectangle.h"
 
 class CompositeShape
 {
@@ -16,6 +18,7 @@ class CompositeShape
     void scale(const point_t & position, double k);
     void push_back(shape * shapes);
     CompositeShape *clone() const;
+    ~CompositeShape();
   private:
     shape ** shape_;
     size_t size_;

19f766b95acc36fe8dd712af942e391c10c0f284
Kirill Odintsov <ksodintsov03@gmail.com>
2023-01-04 01:00:09 +0300

    lnTermGenerator

--- odintsov.kirill/I6/lnPlusOneOverMinusOne.cpp
+++ odintsov.kirill/I6/lnPlusOneOverMinusOne.cpp
@@ -1,6 +1,30 @@
 #include "lnPlusOneOverMinusOne.hpp"
 
-double lnPlusOneOverMinusOne(double x, double absError, unsigned numberMax)
+struct LnTermGenerator {
+  double oneOverXSquared;
+  double term;
+  unsigned number;
+
+  explicit LnTermGenerator(double x):
+    oneOverXSquared(1 / (x * x)),
+    term(1 / x),
+    number(0)
+  {}
+
+  double operator()()
+  {
+    if (number == 0) {
+      number++;
+      return term;
+    }
+    term *= (number++) * 2 - 1;
+    term *= oneOverXSquared;
+    term /= number * 2 - 1;
+    return term;
+  }
+};
+
+double odintsov::lnPlusOneOverMinusOne(double x, double absError, unsigned numberMax)
 {
   return 0.0;
 }

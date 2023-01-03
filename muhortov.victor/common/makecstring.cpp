037076cedfd6aedc694471cc344a4daee6f7bd28
Victor Muhortov <viktormuhortov26@gmail.com>
2023-01-03 22:48:46 +0300

    probably fix valgrind v2

--- muhortov.victor/common/makecstring.cpp
+++ muhortov.victor/common/makecstring.cpp
@@ -11,12 +11,11 @@ char *makeCString(char *c_string, size_t &size, size_t &capacity, std::istream &
     if (size == capacity)
     {
       char *newstr = new char[capacity + 10];
-      c_string[size] = '\0';
+      c_string[capacity - 1] = '\0';
       std::strcpy(newstr, c_string);
       delete[] c_string;
       c_string = newstr;
       capacity += 10;
-      std::cout << c_string << "!\n";
     }
     input >> c_string[size];
   }

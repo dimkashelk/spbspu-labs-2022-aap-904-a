8e334d2dc49fbad18ca065e92da8e2a435bf8d87
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2023-01-05 02:32:27 +0300

    main fixed

--- azheganova.faina/T1/main.cpp
+++ azheganova.faina/T1/main.cpp
@@ -9,6 +9,30 @@
 #include "inputtriangle.h"
 #include "inputcomplexquad.h"
 
+void printFramePoints(std::ostream & output, const rectangle_t & rectangle)
+{
+  point_t point1{rectangle.pos.x - 0.5 * rectangle.width, rectangle.pos.y - 0.5 * rectangle.height};
+  point_t point2{rectangle.pos.x + 0.5 * rectangle.width, rectangle.pos.y + 0.5 * rectangle.height};
+  output << point1.x << ' ' << point1.y << ' ' << point2.x << ' ' << point2.y;
+}
+void printAreaAndFrames(std::ostream & output, const CompositeShape & tmp, size_t size)
+{
+  if (!size)
+  {
+    throw std::invalid_argument("error");
+  }
+  double summ_area = 0.0;
+  for (size_t i = 0; i < size; ++i)
+  {
+    summ_area += tmp[i]->getArea();
+  }
+  output << summ_area;
+  for (size_t i = 0; i < size; ++i)
+  {
+    printFramePoints(output << ' ', tmp[i]->getFrameRect());
+  }
+}
+
 int main()
 {
   std::string line;
@@ -66,34 +90,10 @@ int main()
       {
         std::cout << "incorrect value";
       }
-      point_t point {x, y};
     }
   }
   std::cout << "\n";
   printAreaAndFrames(std::cout << std::fixed << std::setprecision(1), rhs, rhs.size());
   std::cout << "\n";
   printAreaAndFrames(std::cout, rhs, rhs.size());
-}
-void printFramePoints(std::ostream & output, const rectangle_t & rectangle)
-{
-  point_t point1{rectangle.pos.x - 0.5 * rectangle.width, rectangle.pos.y - 0.5 * rectangle.height};
-  point_t point2{rectangle.pos.x + 0.5 * rectangle.width, rectangle.pos.y + 0.5 * rectangle.height};
-  output << point1.x << ' ' << point1.y << ' ' << point2.x << ' ' << point2.y;
-}
-void printAreaAndFrames(std::ostream & output, const CompositeShape & tmp, size_t size)
-{
-  if (!size)
-  {
-    throw std::invalid_argument("error");
-  }
-  double summ_area = 0.0;
-  for (size_t i = 0; i < size; ++i)
-  {
-    summ_area += tmp[i]->getArea();
-  }
-  output << summ_area;
-  for (size_t i = 0; i < size; ++i)
-  {
-    printFramePoints(output << ' ', tmp[i]->getFrameRect());
-  }
 }

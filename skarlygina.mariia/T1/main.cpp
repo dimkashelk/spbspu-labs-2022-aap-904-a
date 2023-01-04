c2f0c42587d96028fb4fb1b8bebe7fb1e48420b1
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-01-04 17:51:00 +0300

    input and if for figure

--- skarlygina.mariia/T1/main.cpp
+++ skarlygina.mariia/T1/main.cpp
@@ -1,4 +1,31 @@
+#include <iostream>
 int main()
 {
+  std::string name = "";
+  double x = 0.0;
+  double y = 0.0;
+  std::cin >> name >> x >> y;
+  if (!std::cin)
+  {
+    std::cerr << "Error: wrong input";
+    return 1;
+  }
+  if (name == "RECTANGLE")
+  {
+    return 0;
+  }
+  else if (name == "RING")
+  {
+    return 0;
+  }
+  else if (name == "Ellipse")
+  {
+    return 0;
+  }
+  else
+  {
+    std::cerr << "Error: wrong name of the figure";
+	return 1;
+  }
   return 0;
 }

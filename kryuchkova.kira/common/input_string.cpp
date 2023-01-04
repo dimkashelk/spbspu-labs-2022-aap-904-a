20a5713573955b4d1c12c02d27d8ae31b2fb4576
KiraGelaz <kiragelaz@yandex.ru>
2023-01-04 06:09:31 +0300

    try to find mistake

--- kryuchkova.kira/common/input_string.cpp
+++ kryuchkova.kira/common/input_string.cpp
@@ -22,6 +22,7 @@ char *inputString(char *cstring, size_t &capacity)
           //*j = *i;
         //}
         //delete[] cstring;
+        cstring[capacity] = '\0';
         strcpy(newstr, cstring);
         delete[] cstring;
         cstring = newstr;

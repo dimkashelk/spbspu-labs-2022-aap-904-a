ca8b378b3c670d40b2aa35b041f2a290a6895e70
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-01-04 22:12:37 +0300

    modified catching exceptions by the task

--- skarlygina.mariia/T1/main.cpp
+++ skarlygina.mariia/T1/main.cpp
@@ -16,14 +16,21 @@ int main()
     if (figure == "RECTANGLE")
     {
       point_t left_down_point{}, right_up_point{};
+      Shape* rectangle = nullptr;
       try
       {
         std::cin >> left_down_point.x >> left_down_point.y >> right_up_point.x >> right_up_point.y;
-        Shape* rectangle = new Rectangle(left_down_point, right_up_point);
+        rectangle = new Rectangle(left_down_point, right_up_point);
+      }
+      catch (const std::invalid_argument& e)
+      {
+          std::cerr << e.what() << "\n";
+          delete rectangle;
+          continue;
       }
       catch (...)
       {
-        std::cerr << "Error while reading parameters of the rectangle\n";
+        std::cerr << "Error while reading the rectangle\n";
         return 2;
       }
     }
@@ -32,14 +39,21 @@ int main()
     {
       point_t center{};
       double radius_small, radius_big;
+      Shape* ring = nullptr;
       try
       {
         std::cin >> center.x >> center.y >> radius_small >> radius_big;
-        Shape* ring = new Ring(center, radius_small, radius_big);
+        ring = new Ring(center, radius_small, radius_big);
+      }
+      catch (const std::invalid_argument& e)
+      {
+          std::cerr << e.what() << "\n";
+          delete ring;
+          continue;
       }
       catch (...)
       {
-        std::cerr << "Error while reading parameters of the ring\n";
+        std::cerr << "Error while reading the ring\n";
         return 2;
       }
     }
@@ -48,14 +62,21 @@ int main()
     {
       double radius_vertical, radius_horizontal;
       point_t center;
+      Shape* ellipse = nullptr;
       try
       {
         std::cin >> center.x >> center.y >> radius_vertical >> radius_horizontal;
-        Shape* ellipse = new Ellipse(center, radius_vertical, radius_horizontal);
+        ellipse = new Ellipse(center, radius_vertical, radius_horizontal);
+      }
+      catch (const std::invalid_argument& e)
+      {
+        std::cerr << e.what() << "\n";
+        delete ellipse;
+        continue;
       }
       catch (...)
       {
-          std::cerr << "Error while reading parameters of the ellipse\n";
+          std::cerr << "Error while reading the ellipse\n";
           return 2;
       }
     }

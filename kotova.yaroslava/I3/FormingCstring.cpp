93c9484dceba0d4b89fb874d376d137d6f93280d
Yaroslava <aroslavakotova@gmail.com>
2023-01-04 19:42:28 +0300

    fix

--- kotova.yaroslava/I3/FormingCstring.cpp
+++ kotova.yaroslava/I3/FormingCstring.cpp
@@ -16,12 +16,12 @@ char* formingCstring(size_t & size, std::istream& input)
     {
       try
       {
-        char* newstr = new char[capacity + 20];
+        char* newstr = new char[capacity + 10];
         cstring[capacity - 1] = '\0';
         std::strcpy(newstr, cstring);
         delete[] cstring;
         cstring = newstr;
-        capacity += 20;
+        capacity += 10;
       }
       catch (const std::exception& e)
       {

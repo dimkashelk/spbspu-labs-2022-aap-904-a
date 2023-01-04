c88e82695fc7935d213be343fa8480369ce04732
Kirill Odintsov <ksodintsov03@gmail.com>
2023-01-04 18:07:41 +0300

    main finalized

--- odintsov.kirill/I6/main.cpp
+++ odintsov.kirill/I6/main.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+#include "lnPlusOneOverMinusOne.hpp"
+
 int main()
 {
+  double absError = 0.01;
+  unsigned numberMax = 3;
+  double l = 2.0;
+  double r = 3.0;
+  double step = 0.1;
+  std::cin >> absError >> numberMax >> l >> r >> step;
+  if (!std::cin) {
+    std::cout << "Error: Input error";
+    return 1;
+  }
+  try {
+    odintsov::outputComparisonTable(std::cout, l, r, step, absError, numberMax) << '\n';
+  } catch (const std::invalid_argument& err) {
+    std::cout << err.what() << '\n';
+    return 1;
+  }
 }

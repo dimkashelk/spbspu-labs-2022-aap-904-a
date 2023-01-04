b49ff071090564653a1a3993f97c3da515aa0d96
aront14 <arontellezc14@gmail.com>
2023-01-04 18:09:34 +0300

    sacond lab fixed...

--- tellez.aron/I2/lengthlongerdecreasing.cpp
+++ tellez.aron/I2/lengthlongerdecreasing.cpp
@@ -1,9 +1,11 @@
+#include <cstddef>
 #include "lengthlongerdecreasing.h"
 size_t length_longer_decreasing(const int *arr, const size_t size)
-{ size_t size1 = size - 1;
+{
   size_t max = 0;
   size_t len = 1;
-  for (size_t i = 0; i < size1 ; i++)
+  size_t i = 0;
+  while (i < size - 1)
   {
     if (arr[i + 1] < arr[i])
     {
@@ -17,6 +19,7 @@ size_t length_longer_decreasing(const int *arr, const size_t size)
         len = 1;
       }
     }
+    i++;
   }
   if (max < len)
   {

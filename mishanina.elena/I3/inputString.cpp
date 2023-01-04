25cbaaa59e1bf07f4c88d3692db5a1d537d7424f
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-04 22:44:49 +0300

    Fix function: inputString

--- mishanina.elena/I3/inputString.cpp
+++ mishanina.elena/I3/inputString.cpp
@@ -30,7 +30,7 @@ char* inputString(size_t &capacity, std::istream& input)
     input >> cstring[size];
   }
   while (input && cstring[size++] != '\n');
-  if (cstring[0] == '\0')
+  if (size == 1)
   {
     delete[] cstring;
     throw std::invalid_argument("ERROR: Empty string");

ab67e1df8bd5cd63e347af5b777a91e8fdb0a739
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-05 19:32:20 +0300

    Create function: countDiagonalsParallelToSecondaryWithoutZero

---
+++ mishanina.elena/I4/count_diagonals_parallel_to_secondary_without_zero.cpp
@@ -0,0 +1,6 @@
+#include "count_diagonals_parallel_to_secondary_without_zero.h"
+
+size_t countDiagonalsParallelToSecondaryWithoutZero(const int* matrix, size_t rows)
+{
+  return 0;
+}
---
+++ mishanina.elena/I4/count_diagonals_parallel_to_secondary_without_zero.h
@@ -0,0 +1,6 @@
+#ifndef COUNT_DIAGONALS_PARALLEL_TO_SECONDARY_WITHOUT_ZERO_H
+#define COUNT_DIAGONALS_PARALLEL_TO_SECONDARY_WITHOUT_ZERO_H
+
+size_t countDiagonalsParallelToSecondaryWithoutZero(const int* matrix, size_t rows);
+
+#endif

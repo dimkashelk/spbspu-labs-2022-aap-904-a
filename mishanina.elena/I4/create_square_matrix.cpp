4c8464569b6b51b3b2dd759cd288595097027736
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-06 01:14:54 +0300

    Create function: createSquareMatrix

---
+++ mishanina.elena/I4/create_square_matrix.cpp
@@ -0,0 +1,6 @@
+#include "create_square_matrix.h"
+
+int* createSquareMatrix(const int* dynamicMatrix, size_t rows, size_t columns, int* squareMatrix, size_t squareMatrixSize)
+{
+  return nullptr;
+}
---
+++ mishanina.elena/I4/create_square_matrix.h
@@ -0,0 +1,6 @@
+#ifndef CREATE_SQUARE_MATRIX_H
+#define CREATE_SQUARE_MATRIX_H
+
+int* createSquareMatrix(const int* dynamicMatrix, size_t rows, size_t columns, int* squareMatrix, size_t squareMatrixSize);
+
+#endif CREATE_SQUARE_MATRIX_H

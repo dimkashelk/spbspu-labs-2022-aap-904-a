b390c3b0b09678950f6b6d5dbc6b0386b47f4f9d
Kirill Tobbo <tobbo.kv@edu.spbstu.ru>
2023-01-05 13:52:06 +0300

    polish

--- tobbo.kirill/I5/main.cpp
+++ tobbo.kirill/I5/main.cpp
@@ -9,8 +9,11 @@ int main()
   std::pair< char*, size_t > strPair = std::make_pair(nullptr, 0);
   try
   {
-    std::cout << "Enter real number: ";
     strPair = getCString(std::cin);
+    if (strPair.second == 1)
+    {
+      throw std::invalid_argument("Error! Empty string...");
+    }
     std::cout << std::boolalpha << isReal(strPair.first) << "\n";
   }
   catch (const std::exception& e)

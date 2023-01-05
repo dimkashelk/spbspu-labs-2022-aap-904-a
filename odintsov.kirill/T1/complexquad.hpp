67600930e1c7f7925ff13eeb74632025648c11ff
Kirill Odintsov <ksodintsov03@gmail.com>
2023-01-06 01:48:21 +0300

    complexquad.hpp

---
+++ odintsov.kirill/T1/complexquad.hpp
@@ -0,0 +1,23 @@
+#ifndef COMPLEXQUAD_HPP
+#define COMPLEXQUAD_HPP
+
+#include "shape.hpp"
+
+namespace odintsov {
+  class ComplexQuad: public Shape {
+    public:
+      ComplexQuad(const point_t& p1, const point_t& p2, const point_t& p3, const point_t& p4);
+
+      double getArea() const;
+      rectangle_t getFrameRect() const;
+      void move(double dx, double dy);
+      void move(const point_t& pos);
+      void scale(double k);
+      point_t getMiddlePoint() const;
+      point_t getIntersectionPoint() const;
+    private:
+      point_t p1, p2, p3, p4;
+  };
+}
+
+#endif

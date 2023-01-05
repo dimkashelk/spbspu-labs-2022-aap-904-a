52feb42aa97f860fb8985fa5328c283e72da1968
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-01-06 02:13:25 +0300

    create main

--- danilenko.ekaterina/I3/main.cpp
+++ danilenko.ekaterina/I3/main.cpp
@@ -1,3 +1,59 @@
-﻿int main()
+﻿#include <iostream>
+#include <cstddef>
+#include "makeNewStringWithoutRepeat.h"
+#include "makeNewStringWithMissingLetters.h"
+
+int main()
 {
+  size_t capacity = 10;
+  char* source1 = new char[capacity];
+  size_t size = 0;
+  std::cout << "Enter string: ";
+  std::cin >> std::noskipws;
+  do
+  {
+    if (size == capacity)
+    {
+      try
+      {
+        char* new_string = new char[capacity + 20];
+        for (auto i = source1, j = new_string; i != source1 + size; i++, j++)
+        {
+          *j = *i;
+        }
+        delete[] source1;
+        source1 = new_string;
+        capacity += 20;
+      }
+      catch (...)
+      {
+        delete[] source1;
+        return 1;
+      }
+    }
+    std::cin >> source1[size];
+  } 
+  while (std::cin && source1[size++] != '\n');
+  if (!std::cin && !size)
+  {
+    std::cout << "Error...";
+    delete[] source1;
+    return 1;
+  }
+
+  //1
+  char source2[] = "ahleptgsl";
+  char* destination1 = new char[30];
+  destination1 = makeNewStringWithoutRepeat(destination1, source1, source2, size);
+  std::cout << "New string without repeating letters from two strings: " << destination1 << "\n";
+  
+  //2
+  char* destination2;
+  destination2 = new char[27];
+  destination2 = makeNewStringWithMissingLetters(destination2, source1);
+  std::cout << "New string with missing letters in first string: " << destination2;
+
+  delete[] destination1;
+  delete[] destination2;
+  return 0;
 }

791b9153d3bfeee6424e82fcb0e048b8cf2b4adf
Yaroslava <aroslavakotova@gmail.com>
2023-01-05 22:30:16 +0300

    try to fix acceptance

--- kotova.yaroslava/I4/columns_w_unique_el.cpp
+++ kotova.yaroslava/I4/columns_w_unique_el.cpp
@@ -4,7 +4,7 @@ size_t countUniqueElOfCol(const int* matrix, size_t column, size_t line)
   size_t count = 0;
   for (size_t i = 0; i < column; i++)
   {
-    for (size_t j = 0; j < line; j++)
+    for (size_t j = 0; j < line - 1; j++)
     {
       if (matrix[column * j + i] != matrix[(column * j + 1) + i])
       {

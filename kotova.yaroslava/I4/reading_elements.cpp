27b18f76a45d7e85d49dbb6a4b9a93ec149eb1c7
Yaroslava <aroslavakotova@gmail.com>
2023-01-06 02:11:38 +0300

    try to fix 1 & 2 tests

--- kotova.yaroslava/I4/reading_elements.cpp
+++ kotova.yaroslava/I4/reading_elements.cpp
@@ -5,7 +5,7 @@ void read_el(int* matrix, size_t column, size_t line, std::ifstream& file_matrix
   for (size_t i = 0; i < column * line; i++)
   {
     file_matrix_in >> matrix[i];
-    if (!file_matrix_in.good())
+    if (!file_matrix_in)
     {
       throw std::runtime_error("something went wrong..");
     }

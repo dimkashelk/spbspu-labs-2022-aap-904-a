e0593aa8cb3d870a7011f9f73523cfdbf5ede9a3
xeniaTa <kseniatarasenko1@gmail.com>
2023-01-05 13:48:23 +0300

    fix complexquad.hpp

--- tarasenko.ksenia/T1/complexquad.hpp
+++ tarasenko.ksenia/T1/complexquad.hpp
@@ -6,8 +6,6 @@ namespace tarasenko
   class Complexquad: public Shape
   {
   public:
-   point_t vertexes[4];
-   point_t p_cross;
    Complexquad(point_t point_1, point_t point_2, point_t point_3, point_t point_4);
    double getArea() const override;
    rectangle_t getFrameRect() const override;
@@ -15,6 +13,9 @@ namespace tarasenko
    void move(double dx, double dy) override;
    void scale(double k) override;
    Shape * clone() const override;
+  private:
+   point_t vertexes[4];
+   point_t p_cross;
   };
 }
 #endif

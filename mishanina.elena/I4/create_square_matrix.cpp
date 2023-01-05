3d7945bf6ba2f7e3ca2eee3fd549d528105e4175
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-06 01:16:35 +0300

    Implementation function: createSquareMatrix

--- mishanina.elena/I4/create_square_matrix.cpp
+++ mishanina.elena/I4/create_square_matrix.cpp
@@ -2,5 +2,22 @@
 
 int* createSquareMatrix(const int* dynamicMatrix, size_t rows, size_t columns, int* squareMatrix, size_t squareMatrixSize)
 {
-  return nullptr;
+  if (columns < rows)
+  {
+    for (size_t i = 0; i < (squareMatrixSize * squareMatrixSize); ++i)
+    {
+      squareMatrix[i] = dynamicMatrix[i];
+    }
+  }
+  else if (columns > rows)
+  {
+    for (size_t i = 0; i < squareMatrixSize; ++i)
+    {
+      for (size_t j = 0; j < squareMatrixSize; ++j)
+      {
+        squareMatrix[squareMatrixSize * i + j] = dynamicMatrix[columns * i + j];
+      }
+    }
+  }
+  return squareMatrix;
 }

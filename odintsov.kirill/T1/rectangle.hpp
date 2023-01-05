f1450fcb7f34cccc800623a19336b9f922df9de9
Kirill Odintsov <ksodintsov03@gmail.com>
2023-01-05 20:58:26 +0300

    Basic rectangle methods and params

--- odintsov.kirill/T1/rectangle.hpp
+++ odintsov.kirill/T1/rectangle.hpp
@@ -1,10 +1,23 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
 
+#include "base-types.hpp"
 #include "shape.hpp"
 
 namespace odintsov {
   class Rectangle: public Shape {
+    public:
+      Rectangle(double x, double y, double width, double height);
+      Rectangle(const point_t& pos, double width, double height);
+      explicit Rectangle(const rectangle_t& rect);
+
+      double getArea() const;
+      rectangle_t& getFrameRect() const;
+      void move(double dx, double dy);
+      void move(const point_t& pos);
+      void scale(double k);
+    private:
+      rectangle_t rect;
   };
 }
 

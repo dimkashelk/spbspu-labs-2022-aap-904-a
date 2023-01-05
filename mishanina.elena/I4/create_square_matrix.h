0f336ab8d00b7c8e343058fcd949143838029a20
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-06 01:28:53 +0300

    Fix function: createSquareMatrix, readMatrixFromFile (include cstddef)

--- mishanina.elena/I4/create_square_matrix.h
+++ mishanina.elena/I4/create_square_matrix.h
@@ -1,5 +1,6 @@
 #ifndef CREATE_SQUARE_MATRIX_H
 #define CREATE_SQUARE_MATRIX_H
+#include <cstddef>
 
 int* createSquareMatrix(const int* dynamicMatrix, size_t rows, size_t columns, int* squareMatrix, size_t squareMatrixSize);
 
--- mishanina.elena/I4/read_matrix_from_file.h
+++ mishanina.elena/I4/read_matrix_from_file.h
@@ -2,6 +2,7 @@
 #define READ_MATRIX_FROM_FILE_H
 #include <fstream>
 #include <istream>
+#include <cstddef>
 
 int* readMatrixFromFile(int* matrix, size_t rows, size_t columns, std::ifstream& input);
 

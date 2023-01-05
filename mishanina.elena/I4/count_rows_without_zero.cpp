d3a023f384241727d3178bdfee277127b6c0d34b
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-05 19:14:23 +0300

    Create function: countRowsWithoutZero

---
+++ mishanina.elena/I4/count_rows_without_zero.cpp
@@ -0,0 +1,6 @@
+#include "count_rows_without_zero.h"
+
+size_t countRowsWithoutZero(const int* matrix, size_t rows, size_t columns)
+{
+  return 0;
+}
---
+++ mishanina.elena/I4/count_rows_without_zero.h
@@ -0,0 +1,6 @@
+#ifndef COUNT_ROWS_WITHOUT_ZERO_H
+#define COUNT_ROWS_WITHOUT_ZERO_H
+
+size_t countRowsWithoutZero(const int * matrix, size_t rows, size_t columns);
+
+#endif

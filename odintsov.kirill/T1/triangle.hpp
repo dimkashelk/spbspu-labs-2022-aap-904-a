0b0954cbf8e71ef186a5e5562a045c25f82c0e90
Kirill Odintsov <ksodintsov03@gmail.com>
2023-01-05 22:51:25 +0300

    Triangle header

---
+++ odintsov.kirill/T1/triangle.hpp
@@ -0,0 +1,21 @@
+#ifndef TRIANGLE_HPP
+#define TRIANGLE_HPP
+
+#include "shape.hpp"
+
+namespace odintsov {
+  class Triangle: public Shape {
+    public:
+      Triangle(const point_t& p1, const point_t& p2, const point_t& p3);
+
+      double getArea() const;
+      rectangle_t getFrameRect() const;
+      void move(double dx, double dy);
+      void move(const point_t& pos);
+      void scale(double k);
+    private:
+      point_t p1, p2, p3;
+  };
+}
+
+#endif

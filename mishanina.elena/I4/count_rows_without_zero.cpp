f6ab712de47ead35f78614c9e67a17668194dc87
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-05 19:16:57 +0300

    Implementation function: countRowsWithoutZero

--- mishanina.elena/I4/count_rows_without_zero.cpp
+++ mishanina.elena/I4/count_rows_without_zero.cpp
@@ -2,5 +2,18 @@
 
 size_t countRowsWithoutZero(const int* matrix, size_t rows, size_t columns)
 {
-  return 0;
+  size_t numberOfRowsWithZero = 0;
+  for (size_t i = 0; i < rows; i++)
+  {
+    for (size_t j = 0; j < columns; j++)
+    {
+      if (matrix[rows * i + j] == 0)
+      {
+        numberOfRowsWithZero++;
+        break;
+      }
+    }
+  }
+  size_t numberOfRowsWithoutZero = rows - numberOfRowsWithZero;
+  return numberOfRowsWithoutZero;
 }

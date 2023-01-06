17a7e9964b5e1fc1f77faae472e2ed169884e7ac
Artem <artrom170@gmail.com>
2023-01-06 22:07:16 +0300

    hiding implementation details

--- romanovich.artem/T1/compositeshape.h
+++ romanovich.artem/T1/compositeshape.h
@@ -20,8 +20,6 @@ public:
   void move(point_t position);
   void scale(double k);
   void unsafeScale(double k);
-  void isoScale(Shape &shape, const point_t &position, double k);
-  void unsafeIsoScale(Shape &shape, const point_t &position, double k) noexcept;
   void push_back(Shape *shp);
   void push_back(const Shape *shp);
   void pop_back();
@@ -31,5 +29,7 @@ private:
   Shape **shape_;
   size_t size_;
   size_t capacity_;
+  void isoScale(Shape &shape, const point_t &position, double k);
+  void unsafeIsoScale(Shape &shape, const point_t &position, double k) noexcept;
 };
 #endif

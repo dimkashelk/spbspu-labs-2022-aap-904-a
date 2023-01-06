a63137d9f7b7e75fb92a30602b387fcbf0c0bbc9
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-01-06 20:54:59 +0300

    fix3.2

--- chulkov.leontii/I3/sortstringbyascii.cpp
+++ chulkov.leontii/I3/sortstringbyascii.cpp
@@ -28,10 +28,10 @@ bool compare(const char* i, const char* q)
   }
   return match;
 }
-char* sortStringByAscii(char* finalstr, const char* cstring, const char* newstr)
+char* sortStringByAscii(char* finalstr, const char* cstring, char* newstr)
 {
   size_t elstr = 0;
-  const char* q = newstr;
+  char* q = newstr;
   for (const char* i = cstring; *i; i++)
   {
     if (std::isalpha(*i))

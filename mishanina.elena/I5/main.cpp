88a25acf3329285d8f42428a4cd252c6d344468f
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-06 17:23:56 +0300

    Include function inputString to main

--- mishanina.elena/I5/main.cpp
+++ mishanina.elena/I5/main.cpp
@@ -1,11 +1,20 @@
 ﻿#include <iostream>
-#include <iostream>
-#include <cstddef>
+#include "inputString.h"
 
 int main()
 {
   size_t capacity = 10;
   char* cstring = nullptr;
+  try
+  {
+    cstring = inputString(capacity, std::cin);
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what();
+    delete[] cstring;
+    return 1;
+  }
   delete[] cstring;
   return 0;
 }

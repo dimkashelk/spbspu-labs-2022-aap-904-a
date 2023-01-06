be449f59a470e53b367abde632f3be1fe4af7a04
Kozyrin Semen <garbagemilitia@gmail.com>
2023-01-06 20:27:57 +0300

    Trying to fix inputString

--- kozyrin.semen/I3/StringOperations.cpp
+++ kozyrin.semen/I3/StringOperations.cpp
@@ -18,13 +18,12 @@ char* inputString(std::istream& stream, size_t& size)
   stream >> std::noskipws;
   size_t capacity = 20;
   size = 0;
-  char* str = new char[capacity];
-  str[0] = '\0';
+  char* str = new char[capacity]{0};
 
   do {
     if (size + 1 == capacity) {
       capacity += 20;
-      char* temp = new char[capacity];
+      char* temp = new char[capacity]{0};
       for (size_t i = 0; i <= size; ++i) {
         temp[i] = str[i];
       }

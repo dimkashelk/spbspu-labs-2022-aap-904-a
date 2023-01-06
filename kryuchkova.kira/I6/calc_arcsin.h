7d721b7162eaca1aa8b68e13a8e08ba82bd14f99
KiraGelaz <kiragelaz@yandex.ru>
2023-01-07 00:24:56 +0300

    add cpp file for culc

--- kryuchkova.kira/I6/calc_arcsin.h
+++ kryuchkova.kira/I6/calc_arcsin.h
@@ -1,5 +1,5 @@
-#include <cmath>
-#include <stdexcept>
+#ifndef CALC_ARCSIN_H
+#define CALC_ARCSIN_H
 
 class TaylorTerm
 {
@@ -13,48 +13,6 @@ class TaylorTerm
     size_t number;
 };
 
-TaylorTerm::TaylorTerm(double x):
-  x(x),
-  numerator(x),
-  denominator(1),
-  number(0)
-{}
+double calcArcsin(double x, double error, size_t max_number);
 
-double TaylorTerm::operator()()
-{
-  if (number == 0)
-  {
-    number++;
-    return x;
-  }
-  else if (number == 1)
-  {
-    numerator *= x * x;
-    denominator = 6;
-    number++;
-    return numerator / denominator;
-  }
-  else
-  {
-    numerator *= (number * 2 - 1) * x * x;
-    denominator *= (number * 2) / (number * 2 + 1);
-    number++;
-    return numerator / denominator;
-  }
-}
-
-double calcArcsin(double x, double error, size_t max_number)
-{
-  TaylorTerm TaylorTerm(x);
-  double term = 0;
-  double res = 0;
-  size_t counter = 0;
-  do
-  {
-    term = TaylorTerm();
-    res += term;
-    counter++;
-  }
-  while (counter < max_number && term > error);
-  return res;
-}
+#endif CALC_ARCSIN_H

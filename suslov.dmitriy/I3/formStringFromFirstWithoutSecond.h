de7e4d6333195f1f1cb162d6c0cf22ebbad5ba87
Disus <mita2004567@gmail.com>
2023-01-06 17:04:23 +0300

    fix headers

--- suslov.dmitriy/I3/formStringFromFirstWithoutSecond.h
+++ suslov.dmitriy/I3/formStringFromFirstWithoutSecond.h
@@ -1,2 +1,4 @@
-#pragma once
+#ifndef FORM_STRING_FROM_FIRST_WITHOUT_SECOND_H
+#define FORM_STRING_FROM_FIRST_WITHOUT_SECOND_H
 char *formStringFromFirstWithoutSecond(char *destination, const char *source_1, const char *source_2);
+#endif
--- suslov.dmitriy/I3/formStringFromOtherSymbols.h
+++ suslov.dmitriy/I3/formStringFromOtherSymbols.h
@@ -1,3 +1,4 @@
-#pragma once
+#ifndef FORM_STRING_FROM_OTHER_SYMBOLS_H
+#define FORM_STRING_FROM_OTHER_SYMBOLS_H
 char *formStringFromOtherSymbols(char *destination, const char *source_1, const char *source_2);
-
+#endif

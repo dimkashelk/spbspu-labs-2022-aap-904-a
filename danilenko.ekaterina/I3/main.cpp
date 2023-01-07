cf09654ca3a75b2b769acb96c26f18779a3f1a5e
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-01-07 03:27:21 +0300

    Lab I3 complete (1)

--- danilenko.ekaterina/I3/main.cpp
+++ danilenko.ekaterina/I3/main.cpp
@@ -42,9 +42,9 @@ int main()
   }
 
   //1
-  char source2[] = "ahleptgsl";
+  char source2[] = "abcd";
   char* destination1 = new char[capacity];
-  destination1 = makeNewStringWithoutRepeat(destination1, source1, source2, size);
+  destination1 = makeNewStringWithoutRepeat(destination1, source1, source2);
   std::cout << "New string without repeating letters from two strings: " << destination1 << "\n";
 
   //2

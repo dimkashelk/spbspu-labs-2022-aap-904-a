e51d0e9b9fcef81d09e77c3c19f315cb02b4edf4
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-07 20:28:08 +0300

    Implementation function: main (enter val)

--- mishanina.elena/I6/main.cpp
+++ mishanina.elena/I6/main.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
+  const double step = 0.05;
+  const double error = 0.0001;
+  double left = 0.0;
+  double right = 0.0;
+  unsigned members = 0;
+  std::cin >> left >> right >> members;
+  if (!std::cin || left >= right || left <= -1.0 || right >= 1.0 || !members)
+  {
+    std::cerr << "ERROR: invalid parameters\n";
+    return 1;
+  }
   return 0;
 }

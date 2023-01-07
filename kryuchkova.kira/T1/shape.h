c92247922dd3038f56b0d9760939602fa8d8782a
KiraGelaz <kiragelaz@yandex.ru>
2023-01-07 21:25:21 +0300

    add operator

--- kryuchkova.kira/T1/shape.h
+++ kryuchkova.kira/T1/shape.h
@@ -1,13 +1,15 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 #include "base_types.h"
+#include <iomanip>
+#include <ostream>
 
 namespace kryuchkova
 {
   class Shape
   {
   public:
-    virtual double getArea() = 0;
+    virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(point_t point) = 0;
     virtual void move(double dx, double dy) = 0;
@@ -28,6 +30,23 @@ namespace kryuchkova
     shape->scale(k);
     shape->move(dx, dy);
   }
+
+  std::ostream& operator << (std::ostream &out, point_t &point)
+  {
+    return out << point.x << " " << point.y;
+  }
+
+  std::ostream& operator << (std::ostream &out, const Shape &shape)
+  {
+    std::setprecision(1);
+    std::string name = typeid(shape).name();
+    double area = shape.getArea();
+    rectangle_t rect = shape.getFrameRect();
+    point_t lb(rect.pos.x - rect.width / 2, rect.pos.y - rect.height / 2);
+    point_t ru(rect.pos.x + rect.width / 2, rect.pos.y + rect.height / 2); 
+    return out << name << " " << area << " " << lb << ru << '\n';
+  }
+
 }
 
 #endif

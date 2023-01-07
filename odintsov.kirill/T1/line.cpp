ee571e65fff23497144e521767e6d5d4686f2762
Kirill Odintsov <ksodintsov03@gmail.com>
2023-01-07 18:14:00 +0300

    line intersection math + cg fix

--- odintsov.kirill/T1/line.cpp
+++ odintsov.kirill/T1/line.cpp
@@ -9,12 +9,19 @@ double odintsov::getLineSegLength(const point_t& p1, const point_t& p2)
   return std::sqrt(std::pow(p2.x - p1.x, 2) + std::pow(p2.y - p1.y, 2));
 }
 
+double getDeterminant(double m11, double m12, double m21, double m22)
+{
+  return m11 * m22 - m12 * m21
+}
+
 odintsov::point_t odintsov::getLineIntersectionPoint(const point_t& p1, const point_t& p2, const point_t& p3, const point_t& p4)
 {
-  double Dx = (p1.x * p2.y - p1.y * p2.x) * (p3.x - p4.x) - (p1.x - p2.x) * (p3.x * p4.y - p3.y * p4.x);
-  double Dy = (p1.x * p2.y - p1.y * p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x * p4.y - p3.y * p4.x);
-  double D = (p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x - p4.x);
-  return point_t{Dx/D, Dy/D};
+  double D12 = getDeterminant(p1.x, p1.y, p2.x, p2.y);
+  double D34 = getDeterminant(p3.x, p3.y, p4.x, p4.y);
+  double Dx = getDeterminant(D12, p1.x - p2.x, D34, p3.x - p4.x);
+  double Dy = getDeterminant(D12, p1.y - p2.y, D34, p3.y - p4.y);
+  double D = getDeterminant(p1.x - p2.x, p1.y - p2.y, p3.x - p4.x, p3.y - p4.y);
+  return point_t{Dx / D, Dy / D};
 }
 
 odintsov::point_t odintsov::getLineSegIntersectionPoint(const point_t& p1, const point_t& p2, const point_t& p3, const point_t& p4)

f44de46957ef4b0fc9a5ce766f39ff02e37fa017
KiraGelaz <kiragelaz@yandex.ru>
2023-01-07 15:34:38 +0300

    add first file

---
+++ kryuchkova.kira/T1/base-types.h
@@ -0,0 +1,19 @@
+#ifndef BASE_TYPES_H
+#define BASE_TYPES_H
+
+namespace kiragelaz
+{
+  struct point_t
+  {
+    double x;
+    double y;
+  };
+  struct rectangle_t
+  {
+    point_t pos;
+    double width;
+    double height;
+  };
+}
+
+#endif

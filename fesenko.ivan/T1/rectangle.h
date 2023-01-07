13fec4e2237ea3f6e31e47870ec7ae66b8d4ec18
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-01-07 16:16:20 +0500

    Added override

--- fesenko.ivan/T1/rectangle.h
+++ fesenko.ivan/T1/rectangle.h
@@ -5,11 +5,11 @@ class Rectangle: public Shape {
  public:
   Rectangle(const point_t &point1, const point_t &point2);
   explicit Rectangle(const rectangle_t &rect);
-  double getArea() const;
-  rectangle_t getFrameRect() const;
-  void move(double dx, double dy);
-  void move(const point_t &pos);
-  void scale(double k);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(double dx, double dy) override;
+  void move(const point_t &pos) override;
+  void scale(double k) override;
  private:
   rectangle_t rect;
 };

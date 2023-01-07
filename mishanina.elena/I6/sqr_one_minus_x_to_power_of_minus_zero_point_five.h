eb8c830c21a6bdef29336693be9060e269dbd4e3
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-08 00:18:57 +0300

    Fix function: sqrtOneMinusXToPowerOfMinusZeroPointFive (build)

--- mishanina.elena/I6/sqr_one_minus_x_to_power_of_minus_zero_point_five.h
+++ mishanina.elena/I6/sqr_one_minus_x_to_power_of_minus_zero_point_five.h
@@ -1,5 +1,7 @@
 #ifndef SQR_ONE_MINUS_X_TO_POWER_OF_MINUS_ZERO_POINT_FIVE_H
 #define SQR_ONE_MINUS_X_TO_POWER_OF_MINUS_ZERO_POINT_FIVE_H
+#include <cmath>
+#include <cstddef>
 
 double sqrtOneMinusXToPowerOfMinusZeroPointFive(double x, unsigned members, double error);
 

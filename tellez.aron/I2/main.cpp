75b7eeff94dc8aac60727aa670f14cd7c8cd7be5
aront14 <arontellezc14@gmail.com>
2023-01-07 16:09:13 +0300

    back to good one

--- tellez.aron/I2/main.cpp
+++ tellez.aron/I2/main.cpp
@@ -8,66 +8,54 @@ int main (int argc, char *argv[])
 {
   if (argc != 2)
   {
-    std::cout << "Error";
+    std::cout << "Error" << "\n";
     return 1;
   }
   int arr_1[10] = {10, 8, 6, 4, 2, 8, 5, 7, 3, 1};
   try
   {
-    std::cout << sum_index_elements(arr_1, 10) << "\n";
-    std::cout << length_longer_decreasing(arr_1, 10) << "\n";
+    std::cout << sum_index_elements(arr_1, 10) << std::endl;
+    std::cout << length_longer_decreasing(arr_1, 10) << std::endl;
   }
   catch (const std::overflow_error &e)
   {
     std::cout << e.what();
     return 2;
   }
+  size_t n = 0;
+  std::cin >> n;
+  int * arr_2 = new int[n];
+  std::srand(time(nullptr));
+  for (size_t i = 0; i < static_cast<size_t> (n); i++)
+  {
+    arr_2[i] = (std::rand()%10)+1;
+  }
   try
   {
-    size_t n = 0;
-    std::cin >> n;
-    int * arr_2 = new int[n];
-    std::srand(time(nullptr));
-    for (size_t i = 0; i < n; i++)
-    {
-      arr_2[i] = (std::rand() % 10) + 1;
-    }
-    try
-    {
-      std::cout << sum_index_elements(arr_2, n) << "\n";
-      std::cout << length_longer_decreasing(arr_2, n) << "\n";
-    }
-    catch (const std::overflow_error &e)
-    {
-      std::cout << e.what();
-      delete[] arr_2;
-      return 2;
-    }
-     delete[] arr_2;
+    std::cout << sum_index_elements(arr_2, n) << std::endl;
+    std::cout << length_longer_decreasing(arr_2, n) << std::endl;
   }
-  catch (const std::bad_array_new_length &e)
+  catch (const std::overflow_error &e)
   {
     std::cout << e.what();
+    delete[] arr_2;
     return 2;
   }
-  catch (const std::bad_alloc &e)
-  {
-    std::cout << e.what();
-    return 2;
-  }
+  delete[] arr_2;
   std::ifstream in(argv[1]);
   if (!in.is_open())
   {
-    std::cout << "Error while opening file" << "\n";
+    std::cout << "Error while opening file";
     return 1;
   }
-  size_t size = 0;
-  in >> size;
-  if(in.eof())
+
+  if (in.peek() == std::ifstream::traits_type::eof())
   {
     std::cout << "Error.";
     return 1;
   }
+  size_t size = 0;
+  in >> size;
   if (size == 0)
   {
     return 0;
@@ -76,6 +64,12 @@ int main (int argc, char *argv[])
   for (size_t i = 0; i < size; i++)
   {
     in >> arr_3[i];
+    if (!in)
+    {
+      std::cout << "Error..";
+      delete[] arr_3;
+      return 1;
+    }
   }
   try
   {
@@ -91,3 +85,4 @@ int main (int argc, char *argv[])
   delete[] arr_3;
   return 0;
 }
+

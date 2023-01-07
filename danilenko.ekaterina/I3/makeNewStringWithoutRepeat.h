1223fc2a51913566e47132ff545f20790c331e31
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-01-07 03:26:50 +0300

    fix header for first function

--- danilenko.ekaterina/I3/makeNewStringWithoutRepeat.h
+++ danilenko.ekaterina/I3/makeNewStringWithoutRepeat.h
@@ -2,6 +2,6 @@
 #define MAKENEWSTRINGWITHOUTREPEAT_H
 #include <cstddef>
 
-char *makeNewStringWithoutRepeat(char *destination, const char *source1, const char *source2, size_t size);
+char *makeNewStringWithoutRepeat(char *destination, const char *source1, const char *source2);
 
 #endif

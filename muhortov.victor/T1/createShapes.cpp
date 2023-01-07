fb6097fad98f41b44f06f9c7871dfa7ac4582793
Victor Muhortov <viktormuhortov26@gmail.com>
2023-01-08 02:43:09 +0300

    Create checkingInput in createShapes

--- muhortov.victor/T1/createShapes.cpp
+++ muhortov.victor/T1/createShapes.cpp
@@ -4,10 +4,19 @@
 #include "ring.hpp"
 #include "concave.hpp"
 
+void checkingInput(std::istream  &input)
+{
+  if(!input)
+  {
+    throw std::invalid_argument("Invalid argument");
+  }
+}
+
 Shape *createRectangle(std::istream &input)
 {
   double positions[4] {0.0, 0.0, 0.0, 0.0};
   input >> positions[0] >> positions[1] >> positions[2] >> positions[3];
+  checkingInput(input);
   return new Rectangle({positions[0], positions[1]},{positions[2], positions[3]});
 }
 
@@ -15,6 +24,7 @@ Shape *createRing(std::istream &input)
 {
   double positions[4] {0.0, 0.0, 0.0, 0.0};
   input >> positions[0] >> positions[1] >> positions[2] >> positions[3];
+  checkingInput(input);
   return new Ring({positions[0], positions[1]},positions[2], positions[3]);
 }
 
@@ -22,6 +32,7 @@ Shape *createConcave(std::istream &input)
 {
   double positions[8] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   input >> positions[0] >> positions[1] >> positions[2] >> positions[3] >> positions[4] >> positions[5] >> positions[6] >> positions[7];
+  checkingInput(input);
   return new Concave({positions[0], positions[1]}, {positions[2], positions[3]}, {positions[4], positions[5]}, {positions[6], positions[7]});
 }
 
@@ -29,6 +40,7 @@ scale_t getScale(std::istream &input)
 {
   double positions[3] {0.0, 0.0, 0.0};
   input >> positions[0] >> positions[1] >> positions[2];
+  checkingInput(input);
   if (positions[2] < 0.0)
   {
     throw std::invalid_argument("invalid scale size");
--- muhortov.victor/T1/createShapes.hpp
+++ muhortov.victor/T1/createShapes.hpp
@@ -3,6 +3,7 @@
 #include <istream>
 #include "shape.hpp"
 
+void checkingInput(std::istream  &input);
 Shape *createRectangle(std::istream &input);
 Shape *createRing(std::istream &input);
 Shape *createConcave(std::istream &input);

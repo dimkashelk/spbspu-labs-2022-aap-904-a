e294744e1fe930d0ccf7680dc954caf12bd8ea1c
xeniaTa <kseniatarasenko1@gmail.com>
2023-01-08 00:17:11 +0300

    add output res

---
+++ tarasenko.ksenia/T1/outputareaandfaramerect.cpp
@@ -0,0 +1,25 @@
+#include "outputareaandfaramerect.hpp"
+#include "outputframerect.hpp"
+
+void tarasenko::outputArea(std::ostream & output, tarasenko::CompositeShape & c_shp)
+{
+  double area = 0.0;
+  for (size_t i = 0; i < c_shp.size_; i++)
+  {
+    area += c_shp[i]->getArea();
+  }
+  output << area;
+}
+void tarasenko::outputFrameRect(std::ostream & output, CompositeShape & c_shp)
+{
+  for (size_t i = 0; i < c_shp.size_; i++)
+  {
+    tarasenko::outputFrameRect(output, c_shp[i]->getFrameRect());
+  }
+}
+void outputAreaAndFrameRect(std::ostream & output, tarasenko::CompositeShape & c_shp)
+{
+  tarasenko::outputArea(std::cout, c_shp);
+  output << " ";
+  tarasenko::outputFrameRect(std::cout, c_shp);
+}

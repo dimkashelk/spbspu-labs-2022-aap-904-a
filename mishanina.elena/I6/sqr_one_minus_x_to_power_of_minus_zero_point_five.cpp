ab77df3a1afc58e278d15e9e51630c33d5850312
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-07 20:41:42 +0300

    Create function: sqrtOneMinusXToPowerOfMinusZeroPointFive

---
+++ mishanina.elena/I6/sqr_one_minus_x_to_power_of_minus_zero_point_five.cpp
@@ -0,0 +1,6 @@
+#include "sqr_one_minus_x_to_power_of_minus_zero_point_five.h"
+
+double sqrtOneMinusXToPowerOfMinusZeroPointFive(double x, unsigned members, double error)
+{
+  return 0.0;
+}
---
+++ mishanina.elena/I6/sqr_one_minus_x_to_power_of_minus_zero_point_five.h
@@ -0,0 +1,6 @@
+#ifndef SQR_ONE_MINUS_X_TO_POWER_OF_MINUS_ZERO_POINT_FIVE_H
+#define SQR_ONE_MINUS_X_TO_POWER_OF_MINUS_ZERO_POINT_FIVE_H
+
+double sqrtOneMinusXToPowerOfMinusZeroPointFive(double x, unsigned members, double error);
+
+#endif

13d3cd2a707f48d454438c231daccb91a6f25274
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-07 23:18:14 +0300

    Fix function: printResult

--- mishanina.elena/I6/print_result.cpp
+++ mishanina.elena/I6/print_result.cpp
@@ -2,7 +2,8 @@
 
 void printResult(std::ostream& out, double x, unsigned members, double error)
 {
-  out << std::fixed << std::setw(5) << std::setprecision(2) << x << ' ';
-  out << std::setw(10) << std::setprecision(5) << sqrtOneMinusXToPowerOfMinusZeroPointFive(x, members, error) << ' ';
-  out << std::setw(10) << std::setprecision(5) << 1 / (sqrt(1 - x * x)) << '\n';
+  std::setprecision(5);
+  out << std::fixed << std::setw(5) << x << ' ';
+  out << std::setw(10) << sqrtOneMinusXToPowerOfMinusZeroPointFive(x, members, error) << ' ';
+  out << std::setw(10) << 1 / sqrt(1 - x * x) << '\n';
 }

d3a01e43f2bf1703db3436a54ab62f3bca28630c
KiraGelaz <kiragelaz@yandex.ru>
2023-01-07 15:43:40 +0300

    add second file

---
+++ kryuchkova.kira/T1/shape.h
@@ -0,0 +1,20 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include "base_types.h"
+
+namespace kiragelaz
+{
+  class Shape
+  {
+  public:
+    virtual double getArea() = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t point) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k);
+    virtual Shape * clone() const = 0;
+    virtual ~Shape() = default;
+  };
+}
+
+#endif

2986cb1980937857ed559caab7e1fdbcc8b79d21
AlcatraZ-1337 <sva104@mail.ru>
2023-01-07 14:01:58 +0000

    lab l1 complete

---
+++ styopin.vladislav/l1/Count.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#ifndef MAIN_CPP_COUNTCHANGES_H
+#define MAIN_CPP_COUNTCHANGES_H
+
+int CountChanges(int, int);
+#endif
+
+#ifndef MAIN_CPP_COUNTLOCALMIN_H
+#define MAIN_CPP_COUNTLOCALMIN_H
+
+void CountMin(unsigned int &countMin, int prev, int curr, int &checking);
+#endif

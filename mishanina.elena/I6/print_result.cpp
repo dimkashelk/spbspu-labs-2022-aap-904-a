85068c474e6fe2586d2d37c253f6cb624eafbd8e
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-07 20:33:18 +0300

    Create function: printResult

---
+++ mishanina.elena/I6/print_result.cpp
@@ -0,0 +1,5 @@
+#include "print_result.h"
+
+void printResult(std::ostream& out, double x, unsigned members, double error)
+{
+}
---
+++ mishanina.elena/I6/print_result.h
@@ -0,0 +1,7 @@
+#ifndef PRINT_RESULT_H
+#define PRINT_RESULT_H
+#include <iosfwd>
+
+void printResult(std::ostream& out, double x, unsigned members, double error);
+
+#endif

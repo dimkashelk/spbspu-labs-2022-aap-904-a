e6f96abc972f6c01fcf16e1fdc144afb7c6ee74b
Kirill Odintsov <ksodintsov03@gmail.com>
2023-01-07 04:24:36 +0300

    Concave edge case + fix

--- odintsov.kirill/T1/concave.cpp
+++ odintsov.kirill/T1/concave.cpp
@@ -1,5 +1,6 @@
 #include "concave.hpp"
 #include <stdexcept>
+#include "line.hpp"
 #include "triangle.hpp"
 #include "base-types.hpp"
 #include "minmax.hpp"
@@ -10,7 +11,8 @@ odintsov::Concave::Concave(const point_t& p1, const point_t& p2, const point_t& 
   p3(p3),
   p4(p4)
 {
-  if (!isTriangle(p1, p2, p3) || !isPointInsideTriangle(p4, p1, p2, p3)) {
+  if (!(isTriangle(p1, p2, p3) && isPointInsideTriangle(p4, p1, p2, p3)) ||
+      isPointOnLineSeg(p4, p1, p2) || isPointOnLineSeg(p4, p1, p3) || isPointOnLineSeg(p4, p2, p3)) {
     throw std::invalid_argument("points are set incorrectly");
   }
 }
--- odintsov.kirill/T1/nearlyEquals.hpp
+++ odintsov.kirill/T1/nearlyEquals.hpp
@@ -1,6 +1,8 @@
 #ifndef NEARLYEQUALS_HPP
 #define NEARLYEQUALS_HPP
 
+#include "base-types.hpp"
+
 namespace odintsov {
   bool nearlyEquals(double a, double b, double epsilon);
   bool nearlyEquals(const point_t& a, const point_t& b, double epsilon);

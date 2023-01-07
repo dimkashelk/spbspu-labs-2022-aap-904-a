b733dbcd65b2c3d4d71f75d1dff18929de834306
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-01-07 14:54:51 +0500

    Added class Shape

---
+++ fesenko.ivan/T1/shape.h
@@ -0,0 +1,13 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+class Shape {
+ public:
+  virtual double getArea() const = 0
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(double dx, double dy) = 0;
+  virtual void move(const point_t &pos) = 0;
+  virtual void scale(double k) = 0;
+
+  virtual ~Shape() = default;
+};
+#endif

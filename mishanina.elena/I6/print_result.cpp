22fad447d8589bfa46a20264d0fbd58776570325
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-01-07 20:59:51 +0300

    Include function sqrtOneMinusXToPowerOfMinusZeroPointFive to print_result

--- mishanina.elena/I6/print_result.cpp
+++ mishanina.elena/I6/print_result.cpp
@@ -3,6 +3,6 @@
 void printResult(std::ostream& out, double x, unsigned members, double error)
 {
   out << std::fixed << std::setw(5) << std::setprecision(2) << x;
-  out << std::setw(10) << std::setprecision(5);
+  out << std::setw(10) << std::setprecision(5) << sqrtOneMinusXToPowerOfMinusZeroPointFive(x, members, error);
   out << std::setw(10) << std::setprecision(5) << 1 / (std::sqrt(1 - x * x)) << '\n';
 }
--- mishanina.elena/I6/print_result.h
+++ mishanina.elena/I6/print_result.h
@@ -2,6 +2,7 @@
 #define PRINT_RESULT_H
 #include <iosfwd>
 #include <iomanip>
+#include "sqr_one_minus_x_to_power_of_minus_zero_point_five.h"
 
 void printResult(std::ostream& out, double x, unsigned members, double error);
 

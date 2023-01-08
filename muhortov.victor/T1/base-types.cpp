830d5cfb0e78d498c1310d9ff1f5b9b2e81de404
Victor Muhortov <viktormuhortov26@gmail.com>
2023-01-08 14:42:51 +0300

    Create some functions

--- muhortov.victor/T1/base-types.cpp
+++ muhortov.victor/T1/base-types.cpp
@@ -8,18 +8,42 @@ double calculatePointsDistance(const point_t &p1, const point_t &p2)
   return std::sqrt(line_x * line_x + line_y * line_y);
 }
 
-bool checkingConcave(const point_t &one, const point_t &two, const point_t &three, const point_t &four)
+point_t calculateScale(point_t position, point_t center, double k)
+{
+  position.x = k * (position.x - center.x) + center.x;
+  position.y = k * (position.y - center.y) + center.y;
+  return position;
+}
+
+point_t moveToDelta(point_t position, double delta_x, double delta_y)
+{
+  position.x += delta_x;
+  position.y += delta_y;
+  return position;
+}
+
+double calculateTriangleArea(const double &a, const double &b, const double &c)
+{
+  double p = (a + b + c) / 2;
+  return std::sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+bool checkingTriangle(const point_t &one, const point_t &two, const point_t &three)
 {
   double a = std::sqrt((three.x - one.x) * (three.x - one.x) + (three.y - one.y) * (three.y - one.y));
   double b = std::sqrt((two.x - one.x) * (two.x - one.x) + (two.y - one.y) * (two.y - one.y));
   double c = std::sqrt((three.x - two.x) * (three.x - two.x) + (three.y - two.y) * (three.y - two.y));
+  return a + b < c || a + c < b || b + c < a;
+}
+
+bool checkingConcave(const point_t &one, const point_t &two, const point_t &three, const point_t &four)
+{
   bool trOne = ((one.x - four.x) * (two.y - one.y) - (two.x - one.x) * (one.y - four.y)) > 0;
   bool trTwo = ((two.x - four.x) * (three.y - two.y) - (three.x - two.x) * (two.y - four.y)) > 0;
   bool trThree = ((three.x - four.x) * (one.y - three.y) - (one.x - three.x) * (three.y - four.y)) > 0;
 
-  bool first = a + b < c || a + c < b || b + c < a;
   bool second =  one.x == four.x || one.y == four.y;
   bool third = !((trOne && trTwo && trThree) || (!trOne && !trTwo && !trThree));
 
-  return (first || second || third);
+  return (checkingTriangle(one, two, three) || second || third);
 }
--- muhortov.victor/T1/concave.cpp
+++ muhortov.victor/T1/concave.cpp
@@ -34,11 +34,7 @@ double Concave::getArea() const
   double a2 = sides[3];
   double b2 = sides[4];
   double c2 = sides[5];
-  double p1 = (a1 + b1 + c1) / 2;
-  double p2 = (a2 + b2 + c2) / 2;
-  double s = std::sqrt(p1 * (p1 - a1) * (p1 - b1) * (p1 - c1));
-  double s1 = std::sqrt(p2 * (p2 - a2) * (p2 - b2) * (p2 - c2));
-  return s - s1;
+  return calculateTriangleArea(a1, b1, c1) - calculateTriangleArea(a2, b2, c2);
 }
 
 rectangle_t Concave::getFrameRect() const
@@ -52,27 +48,23 @@ rectangle_t Concave::getFrameRect() const
 
 void Concave::move(point_t position)
 {
-  double delta_x = position.x - getFrameRect().pos.x;
-  double delta_y = position.y - getFrameRect().pos.y;
-  move(delta_x, delta_y);
+  move(position.x - getFrameRect().pos.x, position.y - getFrameRect().pos.y);
 }
 
 void Concave::move(double delta_x, double delta_y)
 {
   for (point_t &position: positions)
   {
-    position.x += delta_x;
-    position.y += delta_y;
+    position = moveToDelta(position, delta_x, delta_y);
   }
 }
 
 void Concave::scaleWithoutCheck(double k)
 {
-  point_t center{getFrameRect().pos.x, getFrameRect().pos.y};
+  point_t center = getFrameRect().pos;
   for (point_t &position: positions)
   {
-    position.x = k * (position.x - center.x) + center.x;
-    position.y = k * (position.y - center.y) + center.y;
+    position = calculateScale(position, center, k);
   }
 }
 

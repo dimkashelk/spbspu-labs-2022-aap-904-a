2bfa9a0b9fb39a5c4db7aed70e026d959116e2b9
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-01-09 00:21:32 +0300

    Fixed small mistake

--- fesenko.ivan/T1/supportFunctions.h
+++ fesenko.ivan/T1/supportFunctions.h
@@ -3,7 +3,7 @@
 #include "shape.h"
 void movePoint(point_t &p, double dx, double dy);
 void movePointBecauseOfScale(point_t &p, point_t &zoomCenter, double k);
-void fullScale(Shape *shape, point_t &zomeCenter, double k);
+void fullScale(Shape *shape, point_t &zoomCenter, double k);
 bool isIntersectionOfSegments(const point_t &point1, const point_t &point2, const point_t &point3, const point_t &point4);
 point_t getIntersectionPoint(const point_t &point1, const point_t &point2, const point_t &point3, const point_t &point4);
 #endif

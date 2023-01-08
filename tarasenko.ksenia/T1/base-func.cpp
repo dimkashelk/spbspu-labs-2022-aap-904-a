88a99ac6e64ad5e87e5ec2a03d635d881ecefe5e
xeniaTa <kseniatarasenko1@gmail.com>
2023-01-08 19:39:25 +0300

    fix CG

--- tarasenko.ksenia/T1/base-func.cpp
+++ tarasenko.ksenia/T1/base-func.cpp
@@ -7,6 +7,6 @@ tarasenko::rectangle_t tarasenko::makeRectangle(point_t point_ld, point_t point_
   {
     throw std::invalid_argument("It isn't rectangle");
   }
-  rectangle_t rect{point_ru.x_ - point_ld.x_, point_ru.y_ - point_ld.y_,{(point_ru.x_ + point_ld.x_) / 2, (point_ru.y_ + point_ld.y_) / 2}};
+  rectangle_t rect{point_ru.x_ - point_ld.x_, point_ru.y_ - point_ld.y_, {(point_ru.x_ + point_ld.x_) / 2, (point_ru.y_ + point_ld.y_) / 2}};
   return rect;
 }

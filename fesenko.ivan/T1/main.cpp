d7ba0b9b820e1e3d1fb5a36161512d76890019b1
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-01-08 22:48:46 +0300

    Finished work with Concave

--- fesenko.ivan/T1/main.cpp
+++ fesenko.ivan/T1/main.cpp
@@ -2,6 +2,7 @@
 #include "shape.h"
 #include "workWithArray.h"
 #include "rectangle.h"
+#include "concave.h"
 
 int main()
 {
@@ -18,6 +19,24 @@ int main()
         std::cin >> point1.x >> point1.y >> point2.x >> point2.y;
         expandArray(shapes, size);
         shapes[size] = new Rectangle(point1, point2);
+        size++;
+      }
+      catch (const std::invalid_argument &e) {
+        std::cerr << e.what() << "\n";
+        badShape = true;
+      }
+      catch (...) {
+        std::cerr << "Error with rectangle\n";
+        deleteArray(shapes, size);
+        return 2;
+      }
+    }
+    if (figureName == "CONCAVE") {
+      try {
+        point_t point1, point2, point3, point4;
+        std::cin >> point1.x >> point1.y >> point2.x >> point2.y >> point3.x >> point3.y >> point4.x >> point4.y;
+        expandArray(shapes, size);
+        shapes[size] = new Concave(point1, point2, point3, point4);
         size++;
       }
       catch (const std::invalid_argument &e) {

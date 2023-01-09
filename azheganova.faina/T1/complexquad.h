431fd99e2963498185ff72cc1f6095b27d844461
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2023-01-09 21:49:55 +0300

    remove

--- azheganova.faina/T1/complexquad.h
+++ azheganova.faina/T1/complexquad.h
@@ -20,13 +20,8 @@ class Complexquad: virtual public Shape
     Shape * clone() const override;
     ~Complexquad() noexcept override;
   private:
-    point_t center;
-    Shape* triangle_1;
-    Shape* triangle_2;
-    point_t p1;
-    point_t p2;
-    point_t p3;
-    point_t p4;
+    point_t triangle1[3];
+    point_t triangle2[3];
 };
 
 #endif

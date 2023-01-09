945cf2302c0641a6204074add2501d85ac7a1ff0
Gulkov Mikhail <gulkov.mikhail@mail.ru>
2023-01-09 05:36:45 +0300

    fix makeRing

--- gulkov.mikhail/T1/MakeShapes.cpp
+++ gulkov.mikhail/T1/MakeShapes.cpp
@@ -40,7 +40,7 @@ Shape *makeConcave(std::istream &input)
   return new Concave({dots[0], dots[1]}, {dots[2], dots[3]}, {dots[4], dots[5]}, {dots[6], dots[7]});
 }
 
-Shape *makeRing(CompositeShape &compositeShape, std::istream &input)
+Shape *makeRing(std::istream &input)
 {
   double dots[4]{0};
   for (double &dot: dots)
--- gulkov.mikhail/T1/MakeShapes.hpp
+++ gulkov.mikhail/T1/MakeShapes.hpp
@@ -8,7 +8,7 @@
 Shape *makeRectangle(std::istream &input);
 Shape *makeEllipse(std::istream &input);
 Shape *makeConcave(std::istream &input);
-Shape *makeRing(CompositeShape &compositeShape, std::istream &input);
+Shape *makeRing(std::istream &input);
 scale_t getScale(std::istream &input);
 void checkInput(std::istream &input);
 

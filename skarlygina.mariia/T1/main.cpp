45d3f2bafd56c64680918a4f35f66ce760482b60
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-01-09 21:20:09 +0300

    one shape

--- skarlygina.mariia/T1/main.cpp
+++ skarlygina.mariia/T1/main.cpp
@@ -18,9 +18,7 @@ int main()
   Shape** array_figures = new Shape*[size];
   do
   {
-    Shape* rectangle = nullptr;
-    Shape* ellipse = nullptr;
-    Shape* ring = nullptr;
+    Shape* shape = nullptr;
     try
     {
       std::cin >> figure;
@@ -28,8 +26,8 @@ int main()
       {
         double parameters[4]{};
         std::cin >> parameters[0] >> parameters[1] >> parameters[2] >> parameters[3];
-        rectangle = new Rectangle({parameters[0], parameters[1]}, {parameters[2], parameters[3]});
-        Shape** extend_array = FigureArray::extendArray(array_figures, rectangle, size);
+        shape = new Rectangle({parameters[0], parameters[1]}, {parameters[2], parameters[3]});
+        Shape** extend_array = FigureArray::extendArray(array_figures, shape, size);
         delete[] array_figures;
         array_figures = extend_array;
         size++;
@@ -38,8 +36,8 @@ int main()
       {
         double parameters[4]{};
         std::cin >> parameters[0] >> parameters[1] >> parameters[2] >> parameters[3];
-        ring = new Ring({parameters[0], parameters[1]}, parameters[3], parameters[2]);
-        Shape** extend_array = FigureArray::extendArray(array_figures, ring, size);
+        shape = new Ring({parameters[0], parameters[1]}, parameters[3], parameters[2]);
+        Shape** extend_array = FigureArray::extendArray(array_figures, shape, size);
         delete[] array_figures;
         array_figures = extend_array;
         size++;
@@ -48,8 +46,8 @@ int main()
       {
         double parameters[4]{};
         std::cin >> parameters[0] >> parameters[1] >> parameters[2] >> parameters[3];
-        ellipse = new Ellipse({parameters[0], parameters[1]}, parameters[2], parameters[3]);
-        Shape** extend_array = FigureArray::extendArray(array_figures, ellipse, size);
+        shape = new Ellipse({parameters[0], parameters[1]}, parameters[2], parameters[3]);
+        Shape** extend_array = FigureArray::extendArray(array_figures, shape, size);
         delete[] array_figures;
         array_figures = extend_array;
         size++;
@@ -70,9 +68,7 @@ int main()
     catch (const std::invalid_argument& e)
     {
       std::cerr << e.what() << "\n";
-      delete ellipse;
-      delete ring;
-      delete rectangle;
+      delete shape;
       isCommand = false;
       continue;
     }

0500100c4987611c8fb061a9c4b0beaca7364503
Gulkov Mikhail <gulkov.mikhail@mail.ru>
2023-01-09 18:38:03 +0300

    Fix: SafeScale first, then move

--- gulkov.mikhail/T1/IsoScale.cpp
+++ gulkov.mikhail/T1/IsoScale.cpp
@@ -9,8 +9,8 @@ void doSafeIsoScale(Shape *shape, scale_t scale)
 void doIsoScale(Shape *shape, scale_t scale)
 {
   point_t pos = shape->getFrameRect().pos;
-  scale.scale <= 0.0 ? throw std::invalid_argument("Bad input, invalid scale value") : shape->move(scale.pos);
   shape->makeSafeScale(scale.scale);
+  shape->move(scale.pos);
   point_t offset = scaleDelta(pos, scale);
   shape->move(offset.x, offset.y);
 }

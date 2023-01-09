fdb29528c3ee75c75513feb94288e534f1269bba
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2023-01-09 18:32:47 +0300

    fixed isoscale

--- azheganova.faina/T1/isoscale.cpp
+++ azheganova.faina/T1/isoscale.cpp
@@ -5,7 +5,7 @@ void isoScale(Shape * shape, const point_t & position, double k)
   point_t position1 = shape->getFrameRect().pos;
   shape->move(position);
   point_t position2 = shape->getFrameRect().pos;
-  shape->scale(k);
+  shape->toscale(k);
   double dx = (position1.x - position2.x) * k;
   double dy = (position1.y - position2.y) * k;
   shape->move(dx, dy);

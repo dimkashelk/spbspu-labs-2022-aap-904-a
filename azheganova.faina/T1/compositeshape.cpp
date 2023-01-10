7929a3830aff0a8db6d0f253eab338236ce53498
azheganova.fl@edu.spbstu.ru <faina.azheganova@gmail.com>
2023-01-10 14:49:53 +0300

    fiXED

--- azheganova.faina/T1/compositeshape.cpp
+++ azheganova.faina/T1/compositeshape.cpp
@@ -186,8 +186,8 @@ void CompositeShape::isoScale1(point_t position, double k)
   move(position);
   point_t position2 = getFrameRect().pos;
   scale(k);
-  double dx = (position2.x - position1.x) * (-k);
-  double dy = (position2.y - position1.y) * (-k);
+  double dx = (position1.x - position2.x) * k;
+  double dy = (position1.y - position2.y) * k;
   move(dx, dy);
 }
 

d2748a6ecc3f558c759052094c300730a40c1529
Kozyrin Semen <garbagemilitia@gmail.com>
2023-01-19 01:58:31 +0300

    Deleting conflicting I3 files

--- kozyrin.semen/I3/StringOperations.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRINGOPERATIONS_H
-#define STRINGOPERATIONS_H
-#include <iostream>
-
-char* inputString(std::istream& stream, size_t& size);
-void switchCase(const char* src, char* dest);
-void threeMostCommon(const char* src, char* dest);
-
-#endif //STRINGOPERATIONS_H

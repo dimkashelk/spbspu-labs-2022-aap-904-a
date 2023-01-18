85cf7793ea49082d8370cd356a45bcb34433e925
Kozyrin Semen <garbagemilitia@gmail.com>
2023-01-18 23:25:21 +0300

    Added inputArray Implemented array too big error handling

--- kozyrin.semen/I4/ArrayOperations.h
+++ kozyrin.semen/I4/ArrayOperations.h
@@ -1,6 +1,9 @@
 #ifndef ARRAYOPERATIONS_H
 #define ARRAYOPERATIONS_H
+#include <cstddef>
+#include <iostream>
+#include <fstream>
 
-
+void inputArray(int* arr, size_t size, std::istream& stream = std::cin);
 
 #endif //ARRAYOPERATIONS_H
--- kozyrin.semen/I4/main.cpp
+++ kozyrin.semen/I4/main.cpp
@@ -1,12 +1,38 @@
-#include <iostream>
-#include <fstream>
+#include "ArrayOperations.h"
+
+void output(int* arr, size_t rows, size_t cols) //For debug
+{
+  int j = 0;
+  for (size_t i = 0; i < rows * cols; ++i) {
+    if (j == cols) {
+      j = 0;
+      std::cout << '\n';
+    }
+    std::cout << arr[i] << ' ';
+    j++;
+  }
+}
 
 void taskOne(std::ifstream& fin, std::ofstream& fout)
 {
+  int arr[1000]{0};
+  size_t rows = 0;
+  size_t cols = 0;
+  fin >> rows;
+  fin >> cols;
+
+  if (rows * cols > 1000) {
+    std::cerr << "Error: array too big";
+    return;
+  }
+  inputArray(arr, rows * cols, fin);
+
+  output(arr, rows, cols);
 }
 
 void taskTwo(std::ifstream& fin, std::ofstream& fout)
 {
+
 }
 
 int main(int argc, char* argv[])
@@ -20,8 +46,8 @@ int main(int argc, char* argv[])
     std::cerr << "Input file could not be opened";
     return 2;
   }
-  std::ofstream fout(argv[2]);
-  if (!fin.is_open()) {
+  std::ofstream fout(argv[3]);
+  if (!fout.is_open()) {
     std::cerr << "output file could not be opened";
     return 2;
   }

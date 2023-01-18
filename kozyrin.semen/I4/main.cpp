63fcf46a6d7e4499963d5afee47194b38113c676
Kozyrin Semen <garbagemilitia@gmail.com>
2023-01-19 00:45:55 +0300

    Changed task function return value Implemented dynamic array input, bad alloc error handling

--- kozyrin.semen/I4/main.cpp
+++ kozyrin.semen/I4/main.cpp
@@ -14,7 +14,7 @@ void output(int* arr, size_t rows, size_t cols) //For debug
   std::cout << '\n';
 }
 
-void taskOne(std::ifstream& fin, std::ofstream& fout)
+int taskOne(std::ifstream& fin, std::ofstream& fout)
 {
   int arr[1000]{0};
   size_t rows = 0;
@@ -24,17 +24,34 @@ void taskOne(std::ifstream& fin, std::ofstream& fout)
 
   if (rows * cols > 1000) {
     std::cerr << "Error: array too big";
-    return;
+    return 1;
   }
   inputArray(arr, rows * cols, fin);
 
   output(arr, rows, cols);
   fout << "Number of sorted rows: " << sortedRowsCount(arr, rows, cols);
+  return 0;
 }
 
-void taskTwo(std::ifstream& fin, std::ofstream& fout)
+int taskTwo(std::ifstream& fin, std::ofstream& fout)
 {
+  int* arr = nullptr;
+  size_t rows = 0;
+  size_t cols = 0;
+  fin >> rows;
+  fin >> cols;
 
+  try {
+    arr = new int[rows * cols];
+  }
+  catch (const std::bad_alloc & err) {
+    delete[] arr;
+    std::cerr << "Error: " << err.what();
+    return 1;
+  }
+  inputArray(arr, rows * cols, fin);
+  delete[] arr;
+  return 0;
 }
 
 int main(int argc, char* argv[])
@@ -54,6 +71,6 @@ int main(int argc, char* argv[])
     return 2;
   }
 
-  void (*choose[2])(std::ifstream& fin, std::ofstream& fout) = {taskOne, taskTwo};
-  choose[argv[1][0] - 49](fin, fout);
+  int (*choose[2])(std::ifstream& fin, std::ofstream& fout) = {taskOne, taskTwo};
+  return choose[argv[1][0] - 49](fin, fout);
 }

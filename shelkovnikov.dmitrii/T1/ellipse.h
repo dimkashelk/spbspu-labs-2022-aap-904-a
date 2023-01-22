d75f1e36fc0e1e8787a99a6dd2e90c26196c67a6
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-01-22 19:23:40 +0300

    copy override methods

--- shelkovnikov.dmitrii/T1/ellipse.h
+++ shelkovnikov.dmitrii/T1/ellipse.h
@@ -7,9 +7,15 @@ namespace dimkashelk
   class Ellipse: public Shape
   {
   public:
-
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t point) override;
+    void move(double delta_x, double delta_y) override;
+    void scale(double k) override;
+    void unsafeScale(double k) noexcept override;
+    Shape* clone() const override;
   private:
-    Triangle *triangle;
+    Triangle *triangles;
   };
 }
 #endif

fcc9a93c7916eb114ea682563593ce64cee3ab7b
Marinochka.n_n <marina-potapova04@mail.ru>
2023-01-26 21:30:13 +0300

    file elements.cpp complete

--- potapova.marina/I2/elements.cpp
+++ potapova.marina/I2/elements.cpp
@@ -2,7 +2,7 @@
 
 size_t potapova::findNegativeAfterMax(const int* array, size_t size)
 {
-  size_t max_element = 0;
+  int max_element = 0;
   for (size_t i = 0; i < size; i++) {
     if (max_element < array[i]) {
       max_element = array[i];
@@ -14,20 +14,18 @@ size_t potapova::findNegativeAfterMax(const int* array, size_t size)
       count_negative++;
     }
   }
-  std::cout << count_negative << "\n";
+  return count_negative;
 }
-
-size_t potapova::findRepeatingPositiveNumbers(const int* array, size_t size)
+bool potapova::findRepeatingPositiveNumbers(const int* array, size_t size)
 {
   size_t count_pairs = 0;
+  bool flag;
   for (size_t i = 0; i < size; i++) {
     for (size_t j = 1; j < size; j++) {
       if (array[i] > 0 && array[j] > 0 && array[i] < array[j] && array[i] == array[j]) {
-        count_pairs++;
+        return true;
       }
     }
-    if (count_pairs > 0) {
-      std::cout << "Yes" << "\n";
-    }
   }
+  return false;
 }

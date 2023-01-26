4ee67c226ff88fb075c6e49e81cdf7d9029e701f
Marinochka.n_n <marina-potapova04@mail.ru>
2023-01-26 19:18:15 +0300

    add first function, change the second one

--- potapova.marina/I2/elements.cpp
+++ potapova.marina/I2/elements.cpp
@@ -2,15 +2,27 @@
 
 size_t potapova::findNegativeAfterMax(const int* array, size_t size)
 {
-
+  size_t max_element = 0;
+  for (size_t i = 0; i < size; i++) {
+    if (max_element < array[i]) {
+      max_element = array[i];
+    }
+  }
+  size_t count_negative = 0;
+  for (size_t i = 0; i < size - 1; i++) {
+    if (array[i] == max_element && array[i + 1] < 0) {
+      count_negative++;
+    }
+  }
+  std::cout << count_negative << "\n";
 }
 
 size_t potapova::findRepeatingPositiveNumbers(const int* array, size_t size)
 {
-  size_t count_pairs = 0
+  size_t count_pairs = 0;
   for (size_t i = 0; i < size; i++) {
     for (size_t j = 1; j < size; j++) {
-      if (a[i] > 0 && a[j] > 0 && a[i] < a[j] && a[i] == a[j]) {
+      if (array[i] > 0 && array[j] > 0 && array[i] < array[j] && array[i] == array[j]) {
         count_pairs++;
       }
     }

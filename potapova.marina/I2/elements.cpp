61d2bb8ea9ac0a6659a32bbb13b27c53008e5226
Marinochka.n_n <marina-potapova04@mail.ru>
2023-01-27 15:13:54 +0300

    fix unused variable

--- potapova.marina/I2/elements.cpp
+++ potapova.marina/I2/elements.cpp
@@ -18,7 +18,6 @@ size_t potapova::findNegativeAfterMax(const int* array, size_t size)
 }
 bool potapova::findRepeatingPositiveNumbers(const int* array, size_t size)
 {
-  size_t count_pairs = 0;
   for (size_t i = 0; i < size; i++) {
     for (size_t j = 1; j < size; j++) {
       if (array[i] > 0 && array[j] > 0 && array[i] < array[j] && array[i] == array[j]) {

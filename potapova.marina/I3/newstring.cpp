7b60845b14845647f8b7fae2ba6be3ff4c61cee0
Marinochka.n_n <marina-potapova04@mail.ru>
2023-01-31 00:36:33 +0300

    write the second function

--- potapova.marina/I3/newstring.cpp
+++ potapova.marina/I3/newstring.cpp
@@ -1,11 +1,64 @@
 #include "newstring.h"
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 
-potapova::addThreeMostCommonSymbols()
+char * potapova::addThreeMostCommonSymbols()
 {
-
+  for (auto)
 }
-potapova::addAllLatinLetters()
+char * potapova::addAllLatinLetters(char * destination , const char * source1, const char * source2)
 {
+  char * j = destination;
+  for (const char * i = source1; *i != '\0'; i++)
+  {
+    if (std::isalpha(*i))
+    {
+      *(j + 1) = '\0';
+      *j = *i;
+      j++;
+    }
+  }
+  for (const char * i = source2; *i != '\0'; i++)
+  {
+    if (std::isalpha(*i))
+    {
+      *(j + 1) = '\0';
+      *j = *i;
+      j++;
+    }
+  std::sort(destination);
+  }
+
+  return destination;
+}
+potapova::receiveStringTypeC()
+{
+  size_t capasity = 10;
+  char * cstring = new char[capasity];
+  size_t size = 0;
+
+  std::cin >> std::noskipws;
+  do {
+    if (size == capasity) {
+      try {
+        char * newstr = new char[capasity + 20];
+        for (auto i = cstring, j = newstr; i != cstring + size; ++i, ++j) {
+          *j = *i;
+        }
+        delete [] cstring;
+        cstring = newstr;
+        capasity += 20;
+      } catch(const std::exception & e) {
+        delete [] cstring;
+        return 1;
+      }
+    }
+    std::cin >> cstring[size];
+  } while (std::cin && cstring[size++] != '\n');
+  cstring[size - 1] = '\0';
+
+  std::cout << cstring << "\n";
+  delete [] cstring;
 
 }

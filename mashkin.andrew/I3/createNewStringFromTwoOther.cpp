89fc401ab01ef1fc733ffdf699719957d4371e36
Машкин Андрей <beedzibood@gmail.com>
2023-01-31 13:04:43 +0300

    CG

--- mashkin.andrew/I3/createNewStringFromTwoOther.cpp
+++ mashkin.andrew/I3/createNewStringFromTwoOther.cpp
@@ -4,55 +4,55 @@
 
 char* createNewStringFromTwoOther(char* newStringFromTwoOther, const char* cstring1, const char* cstring2)
 {
-	size_t capacity = 10;
-	newStringFromTwoOther = new char[capacity];
-	size_t size = 0;
-	for (const char* i = cstring1; *i; i++)
-	{
-		if (std::isdigit(*i))
-		{
-			bool repetition = false;
-			for (char* j = newStringFromTwoOther; *j; j++)
-			{
-				if (*j == *i)
-				{
-					repetition = true;
-					break;
-				}
-			}
-			if (!repetition)
-			{
-				for (const char* j = cstring2; *j; j++)
-				{
-					if (*j == *i)
-					{
-						if (size == capacity)
-						{
-							try
-							{
-								char* newstr = new char[capacity + 10];
-								for (char* a = newStringFromTwoOther, *b = newstr; a != newStringFromTwoOther + size; ++a, ++b)
-								{
-									*b = *a;
-								}
-								delete[] newStringFromTwoOther;
-								newStringFromTwoOther = newstr;
-								capacity += 10;
-								delete[] newstr;
-							}
-							catch (...)
-							{
-								delete[] newStringFromTwoOther;
-								throw std::exception("Error");
-							}
-						}
-						newStringFromTwoOther[size] = *j;
-						size++;
-					}
-				}
-			}
-		}
-	}
-	newStringFromTwoOther[size] = '\0';
-	return newStringFromTwoOther;
+  size_t capacity = 10;
+  newStringFromTwoOther = new char[capacity];
+  size_t size = 0;
+  for (const char* i = cstring1; *i; i++)
+  {
+    if (std::isdigit(*i))
+    {
+      bool repetition = false;
+      for (char* j = newStringFromTwoOther; *j; j++)
+      {
+        if (*j == *i)
+        {
+          repetition = true;
+          break;
+        }
+      }
+      if (!repetition)
+      {
+        for (const char* j = cstring2; *j; j++)
+        {
+          if (*j == *i)
+          {
+            if (size == capacity)
+            {
+              try
+              {
+                char* newstr = new char[capacity + 10];
+                for (char* a = newStringFromTwoOther, *b = newstr; a != newStringFromTwoOther + size; ++a, ++b)
+                {
+                  *b = *a;
+                }
+                delete[] newStringFromTwoOther;
+                newStringFromTwoOther = newstr;
+                capacity += 10;
+                delete[] newstr;
+              }
+              catch (...)
+              {
+                delete[] newStringFromTwoOther;
+                throw std::exception("Error");
+              }
+            }
+            newStringFromTwoOther[size] = *j;
+            size++;
+          }
+        }
+      }
+    }
+  }
+  newStringFromTwoOther[size] = '\0';
+  return newStringFromTwoOther;
 }
--- mashkin.andrew/I3/main.cpp
+++ mashkin.andrew/I3/main.cpp
@@ -6,46 +6,46 @@
 
 int main()
 {
-	char* string1 = nullptr;
-	try
-	{
-		string1 = createCString();
-	}
-	catch (const std::exception& ex)
-	{
-		std::cout << ex.what() << "\n";
-		return 1;
-	}
-	char* string2 = nullptr;
-	try
-	{
-		string2 = createCString();
-	}
-	catch (const std::exception& ex)
-	{
-		std::cout << ex.what() << "\n";
-		return 1;
-	}
-	char* cstring = nullptr;
-	try
-	{
-		cstring = createCString();
-	}
-	catch (const std::exception& ex)
-	{
-		std::cout << ex.what() << "\n";
-		return 1;
-	}
-	char* newStringFromTwoOther = nullptr;
-	newStringFromTwoOther = createNewStringFromTwoOther(newStringFromTwoOther, string1, string2);
-	std::cout << newStringFromTwoOther << "\n";
-	char* fromThreeLetters = nullptr;
-	fromThreeLetters = createNewStringFromThreeOftenLetters(fromThreeLetters, cstring);
-	std::cout << fromThreeLetters << "\n";
-	delete[] string1;
-	delete[] string2;
-	delete[] cstring;
-	delete[] newStringFromTwoOther;
-	delete[] fromThreeLetters;
-	return 0;
+  char* string1 = nullptr;
+  try
+  {
+    string1 = createCString();
+  }
+  catch (const std::exception& ex)
+  {
+    std::cout << ex.what() << "\n";
+    return 1;
+  }
+  char* string2 = nullptr;
+  try
+  {
+    string2 = createCString();
+  }
+  catch (const std::exception& ex)
+  {
+    std::cout << ex.what() << "\n";
+    return 1;
+  }
+  char* cstring = nullptr;
+  try
+  {
+    cstring = createCString();
+  }
+  catch (const std::exception& ex)
+  {
+    std::cout << ex.what() << "\n";
+    return 1;
+  }
+  char* newStringFromTwoOther = nullptr;
+  newStringFromTwoOther = createNewStringFromTwoOther(newStringFromTwoOther, string1, string2);
+  std::cout << newStringFromTwoOther << "\n";
+  char* fromThreeLetters = nullptr;
+  fromThreeLetters = createNewStringFromThreeOftenLetters(fromThreeLetters, cstring);
+  std::cout << fromThreeLetters << "\n";
+  delete[] string1;
+  delete[] string2;
+  delete[] cstring;
+  delete[] newStringFromTwoOther;
+  delete[] fromThreeLetters;
+  return 0;
 }

b4851fd6181d27ac16047585d742b5b133823cf3
Машкин Андрей <beedzibood@gmail.com>
2023-01-31 12:28:42 +0300

    add two func: createNewStringFromThreeOfenLetters and createNewStringFromTwoOther

---
+++ mashkin.andrew/I3/createNewStringFromThreeOfenLetters.h
@@ -0,0 +1,5 @@
+#ifndef CREATENEWSTRINGFROMTHREEOFTENLETTERS_H
+#define CREATENEWSTRINGFROMTHREEOFTENLETTERS_H
+
+char* createNewStringFromThreeOftenLetters(char* fromThreeLetters, const char* cstring);
+#endif

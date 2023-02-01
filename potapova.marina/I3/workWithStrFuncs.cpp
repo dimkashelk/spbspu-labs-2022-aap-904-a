67722bc76d2c0105c1b85a17dc3443c790ccff21
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-01 15:45:33 +0300

    finish work with strings functions

--- potapova.marina/I3/workWithStrFuncs.cpp
+++ potapova.marina/I3/workWithStrFuncs.cpp
@@ -2,10 +2,31 @@
 #include <iostream>
 #include <cstddef>
 #include <exception>
+#include <cstdint>
+
+size_t capasity_block = 100;
 
 char* reallocMem(char* str, const size_t& size, const size_t& new_capasity)
 {
+  char* new_mem_block;
 
+  try
+  {
+    new_mem_block = new char[new_capacity];
+  }
+  catch (const std::bad_alloc & e)
+  {
+    delete[] str;
+    std::cerr << "Allocation error: " << e.what() << '\n';
+    std::exit(1);
+  }
+  for (auto old_char_ptr = str, new_char_ptr = new_mem_block; old_char_ptr < old_char_ptr + size; ++old_char_ptr, ++new_char_ptr)
+  {
+    *new_char_ptr = *old_char_ptr;
+  }
+  delete[] str;
+
+  return new_mem_block;
 }
 
 void pushBack(char*& str, size_t& str_size, size_t& str_capasity, const char& new_char)
@@ -19,6 +40,7 @@ void pushBack(char*& str, size_t& str_size, size_t& str_capasity, const char& ne
   str[str_size] = '\0';
 }
 
+std::pair<char*, char*> strCopy(const char* str);
 strCopy(const char* str)
 {
   char* result;
@@ -36,6 +58,8 @@ strCopy(const char* str)
 
   while (*str != '\0')
   {
-    pushBack()
+    pushBack(result, result_size, result_capasity, *str);
+    ++str;
   }
+  return std::pair<char*, char*>(result, result + result_size);
 }

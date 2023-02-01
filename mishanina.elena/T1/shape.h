38ad3d03024690535643518575006ace3df6bb82
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-02-01 04:03:09 +0300

    Implementation shape.h: class Shape

---
+++ mishanina.elena/T1/shape.h
@@ -0,0 +1,16 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+class Shape
+{
+public:
+	virtual std::string getName() const = 0;
+	virtual double getArea() const = 0;
+	virtual rectangle_t getFrameRect() const = 0;
+	virtual void move(point_t point) = 0;
+	virtual void move(double dx, double dy) = 0;
+	virtual void scale(double k) = 0;
+	virtual Shape* clone() const = 0;
+};
+
+#endif

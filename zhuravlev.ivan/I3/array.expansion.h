b97fe586f2a50e3c5f3c797d4d888afd1ecf18db
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-02-01 16:00:11 +0300

    fixing acceptance

--- zhuravlev.ivan/I3/array.expansion.h
+++ zhuravlev.ivan/I3/array.expansion.h
@@ -2,5 +2,5 @@
 #define SPBSPU_LABS_2022_AAP_904_A_ARRAY_EXPANSION_H
 #include <cstddef>
 
-void arrayExpansion(const char *str, size_t new_capacity);
+void arrayExpansion(char *str, size_t new_capacity);
 #endif

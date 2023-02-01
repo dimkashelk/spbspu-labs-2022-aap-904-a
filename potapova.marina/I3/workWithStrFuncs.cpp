f1ce401ddff6b6a189dc8eadeb0cb37938e382ee
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-01 14:31:28 +0300

    work with string functions

--- potapova.marina/I3/workWithStrFuncs.cpp
+++ potapova.marina/I3/workWithStrFuncs.cpp
@@ -1,7 +1,41 @@
 #include "workWithStrFuncs.h"
+#include <iostream>
+#include <cstddef>
+#include <exception>
 
-char * reallocMem(char * str, const std::size_t& size, const std::size_t& new_capasity)
+char* reallocMem(char* str, const size_t& size, const size_t& new_capasity)
 {
 
 }
 
+void pushBack(char*& str, size_t& str_size, size_t& str_capasity, const char& new_char)
+{
+  if (str_size == str_capasity)
+  {
+    str_capasity += capasity_block;
+    str = reallocMem(str, str_size, str_capasity);
+  }
+  str[str_size++] = new_char;
+  str[str_size] = '\0';
+}
+
+strCopy(const char* str)
+{
+  char* result;
+  try
+  {
+    result = new char[capasity_block];
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << "Allocation failed: " << e.what() << '\n';
+    std::exit(1);
+  }
+  size_t result_size = 0;
+  size_t result_capasity = capasity_block;
+
+  while (*str != '\0')
+  {
+    pushBack()
+  }
+}
--- potapova.marina/I3/workWithStrFuncs.h
+++ potapova.marina/I3/workWithStrFuncs.h
@@ -2,5 +2,11 @@
 #define WORKWITHSTRFUNCS_H
 
 #include <iostream>
+#include <algorithm>
+#include <utility>
+
+char* reallocMem(char* str, const size_t& size, const size_t new_capacity);
+void pushBack(char* str, size_t& str_size, size_t& str_capasity);
+
 
 #endif

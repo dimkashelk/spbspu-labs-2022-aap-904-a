b451188c02597e05fa260b3ed1cdcef56552a777
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-02-01 04:46:22 +0300

    Implementation base-types.h: add needed functions

---
+++ mishanina.elena/T1/base-types.cpp
@@ -0,0 +1,46 @@
+#include "base-types.h"
+
+double twoPointsDistance(const point_t& p1, const point_t& p2)
+{
+  return 0.0;
+}
+
+point_t getCenterRectangle(const point_t& ld_point, const point_t& ru_point)
+{
+  return point_t{ };
+}
+
+point_t getCenterParallelogram(const point_t& lu_point, const point_t& rd_point)
+{
+  return point_t{ };
+}
+
+bool isCorrectRectangle(const point_t& ld_point, const point_t& ru_point)
+{
+  return false;
+}
+
+point_t movePoint(const point_t point, const point_t dpoint)
+{
+  return point_t{ };
+}
+
+bool isCorrectParallelogram(const point_t& A, const point_t& B, const point_t& C)
+{
+  return false;
+}
+
+bool isCorrectTriangle(const point_t& A, const point_t& B, const point_t& C)
+{
+  return false;
+}
+
+point_t getCenterTriangle(const point_t& A, const point_t& B, const point_t& C)
+{
+  return point_t{ };
+}
+
+point_t scalePoint(const point_t point, const point_t pos, const double k)
+{
+  return point_t{ };
+}
--- mishanina.elena/T1/base-types.h
+++ mishanina.elena/T1/base-types.h
@@ -14,4 +14,15 @@ struct rectangle_t
   point_t pos;
 };
 
+double twoPointsDistance(const point_t& p1, const point_t& p2);
+point_t getCenterRectangle(const point_t& l_point, const point_t& r_point);
+bool isCorrectRectangle(const point_t& ld_point, const point_t& ru_point);
+point_t scalePoint(const point_t point, const point_t pos, const double k);
+point_t getCenterParallelogram(const point_t& lu_point, const point_t& rd_point);
+point_t movePoint(const point_t point, const point_t dpoint);
+bool isCorrectParallelogram(const point_t& A, const point_t& B, const point_t& C);
+bool isCorrectTriangle(const point_t& A, const point_t& B, const point_t& C);
+point_t getCenterTriangle(const point_t& A, const point_t& B, const point_t& C);
+
+
 #endif

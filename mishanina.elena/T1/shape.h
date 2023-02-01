2ebf28b32d2d0edaa88689de1f2bca8b6c0b6a17
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-02-01 05:43:56 +0300

    CG shape.h: margin

--- mishanina.elena/T1/shape.h
+++ mishanina.elena/T1/shape.h
@@ -4,13 +4,13 @@
 class Shape
 {
 public:
-	virtual std::string getName() const = 0;
-	virtual double getArea() const = 0;
-	virtual rectangle_t getFrameRect() const = 0;
-	virtual void move(point_t point) = 0;
-	virtual void move(double dx, double dy) = 0;
-	virtual void scale(double k) = 0;
-	virtual Shape* clone() const = 0;
+  virtual std::string getName() const = 0;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(point_t point) = 0;
+  virtual void move(double dx, double dy) = 0;
+  virtual void scale(double k) = 0;
+  virtual Shape* clone() const = 0;
 };
 
 #endif

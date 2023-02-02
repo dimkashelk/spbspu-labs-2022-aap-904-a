9bc79143209fa8307bb9ef2815f5a07c9a822c58
Vavericherry <vaverich3@inbox.ru>
2023-02-02 06:54:40 +0300

    Add main.cpp file. There is realization of input value of array

---
+++ averich.vladimir/I1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main()
+{
+  unsigned int value = 0;
+  unsigned int prevalue = 0;
+  unsigned int preprevalue = 0;
+  int countersum = 0;
+  int countermod = 0;
+  std::cin >> value;
+  if (!std::cin)
+  {
+   std::cout << "Error, you need to type an integer\n";
+   return 1;
+  }
+  while (value != 0)
+  {
+    preprevalue = prevalue;
+    prevalue = value;
+    std::cin >> value;
+    if (!std::cin)
+    {
+     std::cout << "Entered incorrect value\n";
+     return 1;
+    }
+  }
+  return 0;
+}

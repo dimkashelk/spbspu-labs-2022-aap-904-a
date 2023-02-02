ce39d2a9540838a77654972c6c7ab37337572d65
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-02 18:49:35 +0300

    inputStr complete

---
+++ potapova.marina/I3/inputStr.cpp
@@ -0,0 +1,25 @@
+#include "inputStr.h"
+#include <iostream>
+#include <cstddef>
+#include <exception>
+
+void inputStr(syze_t& size, char*& str)
+{
+  if (!(std::cin >> size))
+  {
+    std::cerr << "Input error\n";
+  }
+  try
+  {
+    str = new char[size + 1];
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << "Allocation failed: " << e.what() << '\n';
+    std::exit(1);
+  }
+  if (!(std::cin >> str))
+  {
+    std::cerr << "Input error\n";
+  }
+}
---
+++ potapova.marina/I3/inputStr.h
@@ -0,0 +1,6 @@
+#ifndef INPUTSTR_H
+#define INPUTSTR_H
+
+void inputStr(syze_t& size, char*& str)
+
+#endif

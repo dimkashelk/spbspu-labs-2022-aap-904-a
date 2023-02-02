79bc52f111fe7cfa408019b49530f6b03d705784
Засульский Егор <3756035@gmail.com>
2023-02-02 15:31:54 +0300

    6

--- zasulsky.egor/I3/extendString.cpp
+++ zasulsky.egor/I3/extendString.cpp
@@ -5,13 +5,14 @@
 
 char* zasulsky::extendString(const char* src, size_t capDiff)
 {
-  size_t size = 0;
+  size_t size = 1;
   for (const char* c = src; *c != '\0'; c++) {
     size++;
   }
   if (size > std::numeric_limits< size_t >::max() - capDiff) {
     throw std::overflow_error("String too large");
   }
+  std::cout << size << ' ';
   char* newstr = new char[size + capDiff];
   std::strcpy(newstr, src);
   return newstr;

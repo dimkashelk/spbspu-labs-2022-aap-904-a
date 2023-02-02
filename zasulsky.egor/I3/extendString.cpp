6cee1af8e18e968899694e60aec07f89fd73bdb6
Засульский Егор <3756035@gmail.com>
2023-02-02 14:56:13 +0300

    new attempt 3

--- zasulsky.egor/I3/extendString.cpp
+++ zasulsky.egor/I3/extendString.cpp
@@ -3,12 +3,16 @@
 #include <cstring>
 #include <limits>
 
-char* zasulsky::extendString(const char* src, size_t cap, size_t capDiff)
+char* zasulsky::extendString(const char* src, size_t capDiff)
 {
-  if (cap > std::numeric_limits< size_t >::max() - capDiff) {
+  size_t size = 0;
+  for (const char* c = src; *c != '\0'; c++) {
+    size++;
+  }
+  if (size > std::numeric_limits< size_t >::max() - capDiff) {
     throw std::overflow_error("String too large");
   }
-  char* newstr = new char[cap + capDiff];
+  char* newstr = new char[size + capDiff];
   std::strcpy(newstr, src);
   return newstr;
 }
--- zasulsky.egor/I3/extendString.hpp
+++ zasulsky.egor/I3/extendString.hpp
@@ -4,7 +4,7 @@
 #include <cstddef>
 
 namespace zasulsky {
-  char* extendString(const char* src, size_t cap, size_t newCap);
+  char* extendString(const char* src, size_t capDiff);
 }
 
 #endif

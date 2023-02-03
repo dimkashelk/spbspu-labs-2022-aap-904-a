cd05a9824dd4d263f6dc8bc977680f5ab2599ea9
Царева ЯД <tsarewaya@yandex.ru>
2023-02-03 22:32:28 +0300

    fix cg

--- tsareva.yana/I3/change_chars.h
+++ tsareva.yana/I3/change_chars.h
@@ -1,6 +1,5 @@
 #ifndef CHANGE_CHARS_H
 #define CHANGE_CHARS_H
 #include <cstddef>
-
-char* change_chars(char* res, char* str);
+char *change_chars(char* res, char* str);
 #endif
--- tsareva.yana/I3/golden_ratio.h
+++ tsareva.yana/I3/golden_ratio.h
@@ -1,6 +1,5 @@
 #ifndef GOLDEN_RATIO_H
 #define GOLDEN_RATIO_H
 #include <cstddef>
-
 size_t goldenRatio(size_t capasity);
 #endif

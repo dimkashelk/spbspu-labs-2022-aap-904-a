0d1d19a6ade995f67369bd9efba0007d58a771f2
Царева ЯД <tsarewaya@yandex.ru>
2023-02-04 00:18:38 +0300

    make fun

---
+++ tsareva.yana/I3/extend_string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "extend_string.h"
+#include <limits>
+#include <stdexcept>
+#include "golden_ratio.h"
+
+char *extendSrting(char* str_1, size_t & capacity_1, size_t size_1){
+
+}
+
---
+++ tsareva.yana/I3/extend_string.h
@@ -0,0 +1,5 @@
+#ifndef EXTEND_STRING_H
+#define EXTEND_STRING_H
+#include <cstddef>
+char *extendString(char* str_1, size_t & capacity_1, size_t size_1);
+#endif

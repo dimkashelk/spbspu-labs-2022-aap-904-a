c2dec42ddd85c573ae351f6c3db8b2b740246c01
Царева ЯД <tsarewaya@yandex.ru>
2023-02-04 00:22:51 +0300

    make limits and if

--- tsareva.yana/I3/extend_string.cpp
+++ tsareva.yana/I3/extend_string.cpp
@@ -5,5 +5,9 @@
 #include "golden_ratio.h"
 
 char *extendSrting(char* str_1, size_t & capacity_1, size_t size_1){
+  size_t max_size_t = std::numeric_limits< size_t >::max();
+  if (( max_size_t - (goldenRatio(capacity_1) - capacity_1)) >= capacity_1){
+    throw std::invalid_argument("to much");
+  }
 }
 

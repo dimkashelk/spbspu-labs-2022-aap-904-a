0ac134207bab9ba83b0bf3d81916d3ef2dd5956a
zzzhura <iQubert323@>
2023-02-03 17:32:23 +0300

    fixing acceptance

--- zhuravlev.ivan/I3/finder.three.same.elements.cpp
+++ zhuravlev.ivan/I3/finder.three.same.elements.cpp
@@ -41,7 +41,7 @@ char* finderThreeSameElements(char* destination, const char* str)
   *result_string = '\0';
   for (auto k = result_string; *k != '\0'; ++k)
   {
-    if (((int)(*k)) < ((int)*(k + 1)))
+    if ((*k) < *(k + 1)))
     {
       std::swap(*k, *(k + 1));
     }

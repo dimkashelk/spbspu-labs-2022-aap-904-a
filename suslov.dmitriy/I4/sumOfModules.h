ba64654877085e758fdcec3006214cb775f919d6
Disus <mita2004567@gmail.com>
2023-02-04 18:40:33 +0300

    fix

--- suslov.dmitriy/I4/sumOfModules.h
+++ suslov.dmitriy/I4/sumOfModules.h
@@ -2,5 +2,5 @@
 #define UNTITLED9_SUMOFMODULES_H
 int *scanMatrixTwo(const char *in_file, int *n, int *m);
 int *smoothedMatrix(int *matrix, int n, int m);
-int sum(int *matrix, int n, int m);
+int sum(int *matrix, int n);
 #endif

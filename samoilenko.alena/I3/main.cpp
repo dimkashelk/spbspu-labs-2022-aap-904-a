32b94c5871e03959542b79644473b72d644b03d1
Алена Самойленко <alenasamojlenko1@gmail.com>
2023-02-05 18:11:59 +0000

    Revert "I3 in progress"
    This reverts commit 515ae8f659b047804c50ee2ab85ee767c0f4beb6

--- samoilenko.alena/I3/main.cpp
+++ samoilenko.alena/I3/main.cpp
@@ -1,32 +1,5 @@
-#include<iostream>
-#include <fstream>
-#include "latin_letters.h"
-#include "most_common_characters.h"
 int main()
 {
-  std::ifstream input;
-  input.open("input.txt");
-  if (!input)
-  {
-    std::cout << "Error during opening the file";
-    return 1;
-  }
-  int max_len;
-  input >> max_len >> std::ws;
-  char* str = new char[max_len + 1];
-  input.getline(str, max_len);
-  char* res = new char[27];
-  printNewLatinLetters(res, str);
-  countMostCommonCharacters(res, str);
-  std::ofstream output;
-  output.open("output.txt");
-  if (!output)
-  {
-    std::cout << "Error during opening the file";
-  }
-  output << res;
-  input.close();
-  output.close();
-  delete[] str;
-  delete[] res;
+
+  return 0;
 }
--- samoilenko.alena/I3/most_common_characters.h
+++
@@ -1,5 +0,0 @@
-#ifndef MOST_COMMON_CHARACTERS_H
-#define MOST_COMMON_CHARACTERS_H
-
-char* countMostCommonCharacters(char*, const char*);
-#endif

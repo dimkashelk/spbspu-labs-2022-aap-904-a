108b5d663c7318e26b41f17a08cc6811a288ef0b
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-02-05 08:46:47 +0300

    Implementation printShapes.h

--- mishanina.elena/T1/printShapes.h
+++ mishanina.elena/T1/printShapes.h
@@ -1,4 +1,9 @@
 #ifndef PRINT_SHAPES_H
 #define PRINT_SHAPES_H
+#include "CompositeShape.h"
+#include <iomanip>
+#include <istream>
+
+void printShapes(std::ostream& out, const CompositeShape& compositeShape);
 
 #endif

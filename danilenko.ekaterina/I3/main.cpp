04f227e580a7e747b9d04262a0349fb4fc4b559d
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-02-06 15:51:13 +0300

    fix valgrind error

--- danilenko.ekaterina/I3/main.cpp
+++ danilenko.ekaterina/I3/main.cpp
@@ -21,7 +21,7 @@ int main()
       try
       {
         char *new_string = new char[capacity];
-        for (size_t i = 1; i < size1; i++)
+        for (size_t i = 0; i < size1; i++)
         {
           new_string[i] = source1[i];
         }

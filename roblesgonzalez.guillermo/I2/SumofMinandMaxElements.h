eb30c5a20fddc7ba781f172019595db1731ca986
guillermo9559 <javier@LAPTOP-D83M1BKQ.localdomain>
2023-02-06 20:34:16 +0300

    Lab 2

---
+++ roblesgonzalez.guillermo/I2/SumofMinandMaxElements.h
@@ -0,0 +1,5 @@
+#ifndef SPBSPU_LABS_2022_AAP_904_A_SUMOFMINANDMAXELEMENTS_H
+#define SPBSPU_LABS_2022_AAP_904_A_SUMOFMINANDMAXELEMENTS_H
+#include <cstddef>
+size_t SumofMinandMaxElements(const int *input_array, size_t array_size);
+#endif
---
+++ roblesgonzalez.guillermo/I2/count_ascending_pairs.cpp
@@ -0,0 +1,18 @@
+#include "count_ascending_pairs.h"
+
+size_t count_ascending_pairs(const int *input_array, size_t array_size)
+{
+ if (array_size < 2)
+ {
+  return 0;
+ }
+ size_t count = 0;
+ for (size_t i = 1; i < array_size; i++)
+ {
+  if (input_array[i - 1] < input_array[i])
+   {
+    count++;
+   }
+ }
+ return count;
+}

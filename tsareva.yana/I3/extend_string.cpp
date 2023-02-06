d4f8731542540fc028c911d34cd66cbd0c8cbf87
Царева ЯД <tsarewaya@yandex.ru>
2023-02-07 00:20:18 +0300

    fixed lab 4

--- tsareva.yana/I3/extend_string.cpp
+++ tsareva.yana/I3/extend_string.cpp
@@ -1,25 +1,15 @@
 #include "extend_string.h"
 #include <iostream>
-#include <limits>
 #include <stdexcept>
 #include "golden_ratio.h"
 
 char *extendString(char* str_1, size_t & capacity_1, size_t size_1)
 {
-  size_t max_size_t = std::numeric_limits< size_t >::max();
-  if ((max_size_t - (goldenRatio(capacity_1) - capacity_1)) <= capacity_1)
-  {
-    throw std::invalid_argument("to much");
-  }
-  else
-  {
-    capacity_1 = goldenRatio(capacity_1);
-  }
   if (capacity_1 < size_1)
   {
     throw std::invalid_argument("bruh");
   }
-  char* newstr = new char[capacity_1];
+  char* newstr = new char[goldenRatio(capacity_1)];
   auto* i = str_1;
   auto* j = newstr;
   while (i != str_1 + size_1)
--- tsareva.yana/I3/golden_ratio.cpp
+++ tsareva.yana/I3/golden_ratio.cpp
@@ -1,8 +1,15 @@
 #include "golden_ratio.h"
+#include <exception>
 #include <cstddef>
 #include <cmath>
 
-size_t goldenRatio(size_t capasity)
+size_t goldenRatio(size_t capacity)
 {
-  return (capasity * (1 + std::sqrt(5))) / 2;
+  size_t gRatioK = ((1 + std::sqrt(5)) / 2);
+  size_t max_size_t = std::numeric_limits< size_t >::max();
+  if ((max_size_t - gRatioK * capacity) > capacity)
+  {
+    capacity = gRatioK * capacity;
+  }
+  return capacity;
 }

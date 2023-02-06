eab77812256fb48ee600b6c48742c86f1e0a672e
Царева ЯД <tsarewaya@yandex.ru>
2023-02-07 01:25:46 +0300

    fixed lab 4 x6

--- tsareva.yana/I3/golden_ratio.cpp
+++ tsareva.yana/I3/golden_ratio.cpp
@@ -1,12 +1,17 @@
 #include "golden_ratio.h"
 #include <limits>
+#include <stdexcept>
 #include <cmath>
 
 size_t goldenRatio(size_t capacity)
 {
-  size_t gRatioK = ((1 + std::sqrt(5)) / 2);
+  const size_t gRatioK = ((1 + std::sqrt(5)) / 2);
   size_t max_size_t = std::numeric_limits< size_t >::max();
-  if ((max_size_t - ((gRatioK * capacity) - capacity)) > capacity)
+  if ((max_size_t - ((gRatioK * capacity) - capacity)) <= capacity)
+  {
+    throw std::invalid_argument("to much");
+  }
+  else
   {
     capacity = gRatioK * capacity;
   }

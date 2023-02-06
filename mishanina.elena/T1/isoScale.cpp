f1a93e90b3b331d7753b91dd9fde3f0f958d78fb
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-02-06 05:53:31 +0300

    Implementation isoScale.h

--- mishanina.elena/T1/isoScale.cpp
+++ mishanina.elena/T1/isoScale.cpp
@@ -1,1 +1,12 @@
 #include "isoScale.h"
+
+void isoScale(Shape* shape, const point_t& pos, double k)
+{
+  point_t pos1 = shape->getFrameRect().pos;
+  shape->move(pos);
+  point_t pos2 = shape->getFrameRect().pos;
+  shape->scale(k);
+  double dx = (pos1.x - pos2.x) * k;
+  double dy = (pos1.y - pos2.y) * k;
+  shape->move(dx, dy);
+}
--- mishanina.elena/T1/isoScale.h
+++ mishanina.elena/T1/isoScale.h
@@ -2,4 +2,6 @@
 #define ISO_SCALE_H
 #include "shape.h"
 
+void isoScale(Shape* shape, const point_t& pos, double k);
+
 #endif

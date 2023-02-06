38f52ca6334a03241de4196f4d03da7390355be9
Царева ЯД <tsarewaya@yandex.ru>
2023-02-07 00:44:24 +0300

    fixed lab 4 x3

--- tsareva.yana/I3/golden_ratio.cpp
+++ tsareva.yana/I3/golden_ratio.cpp
@@ -7,7 +7,7 @@ size_t goldenRatio(size_t capacity)
 {
   size_t gRatioK = ((1 + std::sqrt(5)) / 2);
   size_t max_size_t = std::numeric_limits< size_t >::max();
-  if ((max_size_t - gRatioK * capacity) > capacity)
+  if ((max_size_t - ((gRatioK * capacity) - capacity)) > capacity)
   {
     capacity = gRatioK * capacity;
   }

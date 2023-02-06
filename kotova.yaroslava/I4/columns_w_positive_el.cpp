58a4bdeda29666ef241ca07c4b4216f240be242f
Yaroslava <aroslavakotova@gmail.com>
2023-02-06 22:45:45 +0300

    changed 1 function, add 1 function

--- kotova.yaroslava/I4/columns_w_positive_el.cpp
+++ kotova.yaroslava/I4/columns_w_positive_el.cpp
@@ -1,18 +1,34 @@
 #include "columns_w_positive_el.h"
+bool isPositive(const int* matrix, size_t& column, size_t& line, size_t& number_column)
+{
+  bool flag = true;
+  size_t line_curr = 1;
+  size_t column_curr = 1;
+  for (size_t i = line_curr; i < line - 1; i++)
+  {
+    number_column = matrix[column_curr * i];
+    if (matrix[column_curr * line_curr + i] > 0)
+    {
+      flag = true;
+      line_curr++;
+    }
+    else
+    {
+      flag = false;
+      break;
+    }
+  }
+  return number_column;
+}
 size_t countPositiveElOfCol(const int* matrix, size_t column, size_t line)
 {
+  size_t columns_curr = 0;
   size_t count = 0;
-  for (size_t i = 0; i < column; i++)
+  for (size_t i = columns_curr; i < column - 1; i++)
   {
-    for (size_t j = 0; j < line; j++)
+    if (isPositive(matrix, column, line, columns_curr))
     {
-      for(size_t n = 0; n < line; n++)
-      {
-        if ((matrix[column * j + i] && matrix[column * n + i]) > 0 && j != n)
-        {
-          count++;
-        }
-      }
+      count++;
     }
   }
   return count;

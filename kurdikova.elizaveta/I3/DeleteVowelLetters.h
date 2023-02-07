b2a278a54d9ca3ea81ce2f8396910d06350632b9
Kurdikova Elizaveta <Kurdikova.es@edu.spbstu.ru>
2023-02-07 20:10:59 +0300

    Remove the function declaration in the header file

--- kurdikova.elizaveta/I3/DeleteVowelLetters.h
+++ kurdikova.elizaveta/I3/DeleteVowelLetters.h
@@ -1,6 +1,5 @@
 #ifndef DELETEVOWELLETTERS_H
 #define DELETEVOWELLETTERS_H
 
-bool isLetterVowel(char letter);
 char *deleteVowels(char *result1, const char *source);
 #endif
--- kurdikova.elizaveta/I3/TakeSimilarLettersInOrder.h
+++ kurdikova.elizaveta/I3/TakeSimilarLettersInOrder.h
@@ -1,6 +1,5 @@
 #ifndef TAKESIMILARLETTERSINORDER_H
 #define TAKESIMILARLETTERSINORDER_H
 
-bool isExistsInBoth(char letter1, char letter2);
 char *lettersInOrder(char *result2, const char *source1, const char *source2);
 #endif

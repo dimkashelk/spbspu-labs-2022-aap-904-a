4bc5936eced60dc0e791d2fb48792bec90924ced
egor.aksenov <aksenov200477@hmail.com>
2023-02-08 00:48:06 +0300

    delete useless functions

--- aksenov.egor/I1/main.cpp
+++ aksenov.egor/I1/main.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
-#include "notZero.h"
-#include "isMore.h"
 int main()
 {
   int value = 0;
   int predv = 0;
   int compare_count = 0;
   int minv = 0;
-  int min_count = 0;
+  unsigned int min_count = 0;
   do
   {
     predv = value;
     std::cin >> value;
-    if (std::cin)
+    if (predv < value)
     {
-      if (isMore(predv, value))
-      {
-        compare_count++;
-      }
-      if (minv == 0)
-      {
-        minv = value;
-      }
-      if (minv != 0 && value < minv && value != 0)
-      {
-        minv = value;
-        min_count = 0;
-      }
-      if (minv == value)
-      {
-        min_count++;
-      }
+      compare_count++;
+    }
+    if (minv == 0)
+    {
+      minv = value;
+    }
+    if (minv != 0 && value < minv && value != 0)
+    {
+      minv = value;
+      min_count = 0;
+    }
+    if (minv == value)
+    {
+      min_count++;
     }
   }
   while (value && std::cin);

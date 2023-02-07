c828d9c3f900d7fe783a6013289904ba9c986246
DiHASTRO <d87779007501d@gmail.com>
2023-02-07 19:00:38 +0300

    Refactored code by including a function

---
+++ maistrenko.dmitriy/S0/credentials.hpp
@@ -0,0 +1,13 @@
+#ifndef CREDENTIALS_HPP
+#define CREDENTIALS_HPP
+
+#include "credentials.hpp"
+#include <iosfwd>
+
+namespace maistrenko
+{
+  std::ostream& outCredentials(std::ostream& out);
+}
+
+#endif
+
--- maistrenko.dmitriy/S0/main.cpp
+++ maistrenko.dmitriy/S0/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include "credentials.hpp"
 
 int main()
 {
-  std::cout << "maistrenko.dmitriy\n";
+  maistrenko::outCredentials(std::cout) << '\n';
   return 0;
 }

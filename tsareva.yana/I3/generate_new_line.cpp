9ec31844b28ecdb16ea5163528045a927d3e6a9e
Царева ЯД <tsarewaya@yandex.ru>
2023-02-09 00:32:30 +0300

    fix lab

--- tsareva.yana/I3/generate_new_line.cpp
+++ tsareva.yana/I3/generate_new_line.cpp
@@ -1,44 +1,11 @@
 #include "generate_new_line.h"
+#include "check_similarity_letters.h"
 
 void generateNewLine(char * res, const char * str1, const char  * str2)
 {
   auto i = str1;
   auto j = str2;
-  bool noMatches = true;
-  while (*i)
-  {
-    noMatches = true;
-    while (*j && noMatches)
-    {
-      if (*i == *j)
-      {
-        noMatches = false;
-      }
-      j++;
-    }
-    if (noMatches)
-    {
-      *res++ = *i;
-    }
-    i++;
-  }
-
-  while (*j)
-  {
-    noMatches = true;
-    while (*i && noMatches)
-    {
-      if (*j == *i)
-      {
-        noMatches = false;
-      }
-      i++;
-    }
-    if (noMatches)
-    {
-      *res++ = *i;
-    }
-    j++;
-  }
+  char * res2 = checkSmilarityLetters(res, str1, str2);
+  checkSmilarityLetters(res2, str2, str1);
   *res = '\0';
 }

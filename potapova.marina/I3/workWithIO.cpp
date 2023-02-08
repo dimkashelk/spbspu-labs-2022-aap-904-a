ba8cccda4c66eb45f826ad5e44e9693f485c4ad9
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-08 23:08:03 +0300

    change input

--- potapova.marina/I3/workWithIO.cpp
+++ potapova.marina/I3/workWithIO.cpp
@@ -18,12 +18,10 @@ char* stdinGetLine()
   size_t result_capasity = capasity_block;
 
   char cur_char;
-  std::cin >> std::noskipws >> cur_char;
-  while (cur_char != '\n')
+  std::cin >> std::noskipws;
+  while (std::cin >> cur_char && cur_char != '\n')
   {
     pushBack(result, result_size, result_capasity, cur_char);
-
-    std::cin >> cur_char;
   }
 
   return result;

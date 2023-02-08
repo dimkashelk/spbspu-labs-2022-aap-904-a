7b9ffa60629514922834a83768f462717dd2e3a8
Царева ЯД <tsarewaya@yandex.ru>
2023-02-09 01:07:18 +0300

    fixed lab

--- tsareva.yana/I3/check_over_flow_size_t.cpp
+++ tsareva.yana/I3/check_over_flow_size_t.cpp
@@ -6,4 +6,3 @@ bool checkOverFlowSizeT(size_t first_addend, size_t second_addend)
   size_t max_size_t = std::numeric_limits< size_t>::max();
   return (max_size_t - second_addend >= first_addend);
 }
-

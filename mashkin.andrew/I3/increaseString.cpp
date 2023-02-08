f9dba69f8a8eace09e3657347958efd957208032
Машкин Андрей <beedzibood@gmail.com>
2023-02-09 01:41:47 +0300

    remove const

--- mashkin.andrew/I3/increaseString.cpp
+++ mashkin.andrew/I3/increaseString.cpp
@@ -1,10 +1,10 @@
 #include "increaseString.h"
 #include <cstddef>
 
-char* increaseString(const char* string1, const size_t capacity, const size_t size)
+char* increaseString(char* string1, const size_t capacity, const size_t size)
 {
   char* newStr = new char[capacity + 20];
-  for (const char* i = string1, *j = newStr; i != string1 + size; ++i, ++j)
+  for (char* i = string1, *j = newStr; i != string1 + size; ++i, ++j)
   {
     *j = *i;
   }

aca16397d13a69eb0ef4ed678ea38f4d2cd6d63a
Царева ЯД <tsarewaya@yandex.ru>
2023-02-08 23:43:55 +0300

    fix checkOverFlowSizeT

--- tsareva.yana/I3/check_over_flow_size_t.cpp
+++ tsareva.yana/I3/check_over_flow_size_t.cpp
@@ -1,15 +1,11 @@
 #include "check_over_flow_size_t.h"
 #include <limits>
 
-bool checkOverFlowSizeT(size_t first_addend, size_t second_addend)
+size_t checkOverFlowSizeT(size_t first_addend, size_t second_addend)
 {
   size_t max_size_t = std::numeric_limits< size_t>::max();
   if (max_size_t - second_addend >= first_addend)
   {
-    return true;
-  }
-  else
-  {
-    return false;
+    return first_addend + second_addend;
   }
 }
--- tsareva.yana/I3/check_over_flow_size_t.h
+++ tsareva.yana/I3/check_over_flow_size_t.h
@@ -1,5 +1,5 @@
 #ifndef CHECK_OVER_FLOW_SIZE_T_H
 #define CHECK_OVER_FLOW_SIZE_T_H
 #include <cstddef>
-bool checkOverFlowSizeT(size_t first_addend, size_t second_addend);
+size_t checkOverFlowSizeT(size_t first_addend, size_t second_addend);
 #endif

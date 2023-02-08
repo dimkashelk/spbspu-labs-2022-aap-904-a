5fa00e3802b2345ba354e2000215ae9475030775
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-02-08 14:19:10 +0300

    func latin letters and part of main are ready

--- samoilenko.alena/I3/latin_letters.cpp
+++ samoilenko.alena/I3/latin_letters.cpp
@@ -1,1 +1,28 @@
 #include "latin_letters.h"
+#include <iostream>
+#include <cstddef>
+
+char* createStringNewLatinLetters(char* destination, const char* source)
+{
+  size_t ascii[26] = {0};
+  size_t i = 0;
+  while (source[i] != '\0')
+  {
+    if (isalpha(source[i]))
+    {
+      ascii[size_t(tolower(source[i])) - 97]++;
+    }
+    i++;
+  }
+  size_t j = 0;
+  for (i = 0; i < 26; i++)
+  {
+    if (ascii[i] == 0)
+    {
+      destination[j] = char(i + 97);
+      j++;
+    }
+  }
+  destination[j] = '\0';
+  return destination;
+}

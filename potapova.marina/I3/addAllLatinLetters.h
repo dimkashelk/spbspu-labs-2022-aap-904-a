ab9ec87464cb89d541317e93f87bc89e96844597
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-08 19:36:50 +0300

    rm

--- potapova.marina/I3/addAllLatinLetters.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADDALLLATINLETTERS_H
-#define ADDALLLATINLETTERS_H
-
-#include <cctype>
-#include <algorithm>
-
-char* addAllLatinLetters(char* dest, const char* str1, const char* str2);
-
-#endif

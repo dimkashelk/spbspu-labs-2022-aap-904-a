859ee577364e5883dc0b39295ab321b60d760325
Царева ЯД <tsarewaya@yandex.ru>
2023-02-09 18:42:28 +0300

    made row matrix

--- tsareva.yana/I4/main.cpp
+++ tsareva.yana/I4/main.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
 #include<fstream>
+#include <cstddef>
 
 int main(int argc, char * argv[])
 {
   if (argc != 4)
   {
     std::cerr << "The arguments are not correct!\n";
+    return 1;
   }
-  std::ifstream input(argv[2]);
+  std::ifstream inputfile(argv[2]);
+  if (!inputfile)
+  {
+    std::cerr << "Error file\n";
+    return 1;
+  }
+  size_t row = 0;
+  inputfile >> row;
+  if (!inputfile)
+  {
+    std::cerr << "Incorrect arguments of the matrix\n";
+  }
 }

3fe1c1a82be32be2b7f745353f07c5e9fd828e6c
Lisa Lisovskaya <minstone1993@gmail.com>
2023-02-09 18:20:02 +0300

    CG naming fixed

---
+++ lisovskaya.lisa/I2/findArithmeticMean.h
@@ -0,0 +1,5 @@
+#ifndef SPBSPU_LABS_2022_APP_904_FINDARITHMETICMEAN_H
+#define SPBSPU_LABS_2022_APP_904_FINDARITHMETICMEAN_H
+#include <cstddef>
+double findArithmeticMean(const int *ar, size_t m);
+#endif

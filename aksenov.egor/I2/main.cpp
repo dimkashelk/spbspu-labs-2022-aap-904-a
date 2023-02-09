97bf83c03f495013066c02f835853dd5b56e7ba0
egor.aksenov <aksenov200477@hmail.com>
2023-02-09 16:38:56 +0300

    white space...

--- aksenov.egor/I2/main.cpp
+++ aksenov.egor/I2/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
       return 2;
     }
   std::cout << countAmountOfRightPairs(f_arr, fsize) << std::endl;
-  std::cout << isRepeatingPositiveNumbers(f_arr, fsize) << std::endl; 
+  std::cout << isRepeatingPositiveNumbers(f_arr, fsize) << std::endl;
     delete [] f_arr;
     input.close();
   }

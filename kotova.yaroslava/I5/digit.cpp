5acd043bf3c3b29e2de647c113f7bbcaacb5435c
Yaroslava <aroslavakotova@gmail.com>
2023-02-10 01:11:48 +0300

    add basic func

---
+++ kotova.yaroslava/I5/digit.cpp
@@ -0,0 +1,49 @@
+#include "digit.hpp"
+#include <cctype>
+bool isSign(char c)
+{
+  return (c == '+' || c == '-');
+}
+
+bool isDigit(char c)
+{
+  return (std::isdigit(c));
+}
+
+bool isDot(char c)
+{
+  return (c == '.');
+}
+
+bool isEnd(char c)
+{
+  return (c == '\0');
+}
+
+bool isE(char c)
+{
+  return (c == 'E');
+}
+
+bool continueWithDigit(const char* data)
+{
+  return isDigit(*data) && (continueWithDigit(data + 1) || isEnd(*(data + 1)));
+}
+
+bool isOrder(const char* data)
+{
+  return isE(*data) && (isSign(*(data + 1)) && continueWithDigit(data + 2)) || continueWithDigit(data + 1);
+}
+bool continueWithOrder(const char* data)
+{
+  return isOrder(data + 1) || (isDigit(*(data + 1)) && isOrder(data + 2));
+}
+bool isInteger(const char* data)
+{
+  return isDigit(*data) && ((continueWithDigit(data + 1) || continueWithOrder(data + 1)));
+}
+
+bool isFloat(const char* data)
+{
+  return isInteger(data) || (isSign(*data) && isInteger(data + 1));
+}
---
+++ kotova.yaroslava/I5/digit.hpp
@@ -0,0 +1,5 @@
+#ifndef DIGIT_H
+#define DIGIT_H
+
+bool isFloat(const char* data);
+#endif

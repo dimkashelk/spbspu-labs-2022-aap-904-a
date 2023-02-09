b762a25c2b221f6e06a33deb13914fb38064821d
alexg_29 <alexander.gar2901@gmail.com>
2023-02-09 19:16:24 +0300

    LAB I2 ! !

--- garzonnavarrete.alexander/I2/main.cpp
+++ garzonnavarrete.alexander/I2/main.cpp
@@ -4,26 +4,24 @@
 #include <ctime>
 using namespace std;
 
-void process_array(int* arr, int n) {
+void process_array(int* arr, int n){
   int max = INT_MIN;
   int min = INT_MAX;
   int imin = 0, imax = 0, suma = 0;
   double avg = 0.0;
-  for (size_t i = 0; i < n; ++i) {
-    if (arr[i] < min)
-    {
+  for (size_t i = 0; i < n; ++i){
+    if (arr[i] < min){
       min = arr[i];
       imin = i;
     }
-    if (arr[i] > max)
-    {
+    if (arr[i] > max){
       max = arr[i];
       imax = i;
     }
   }
   suma = imin + imax;
   int c = 0;
-  for (size_t i = 0; i < n; i+=2) {
+  for (size_t i = 0; i < n; i+=2){
     avg += arr[i];
     ++c;
   }
@@ -40,7 +38,7 @@ int main(int argc, char** argv)
   process_array(&(arr1[0]), 18);
   int n;
   cin >> n;
-  if (n <= 0) {
+  if (n <= 0){
      std::cerr << "Invalid array length." << endl;
      return -1;
   }
@@ -48,12 +46,12 @@ int main(int argc, char** argv)
   for(int i = 0; i < n; ++i) arr2[i] = rand();
   process_array(arr2, n);
   delete[] arr2;
-  if (argc > 0) {
+  if (argc > 0){
      ifstream ifs;
      ifs.open(argv[1]);
-     if (ifs) {
+     if (ifs){
         ifs >> n;
-        if (n <= 0) {
+        if (n <= 0){
            std::cerr << "Invalid array length specified in the file." << endl;
            return -2;
         }

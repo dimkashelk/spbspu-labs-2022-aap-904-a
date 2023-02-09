d3d4363fafa475d62060bfe1b992498f9a02b83b
Kozyrin Semen <garbagemilitia@gmail.com>
2023-02-09 11:34:45 +0300

    Implemented isReal()

---
+++ kozyrin.semen/I5/IsReal.cpp
@@ -0,0 +1,31 @@
+#include "isReal.h"
+
+bool isSign(char chr)
+{
+  return chr == '+' || chr == '-';
+}
+
+bool isDigit(char chr)
+{
+  return chr >= 48 && chr <= 57;
+}
+
+bool isUInt(char*& str)
+{
+  return isDigit(*str) && isDigit(*++str) && isUInt(str) || isDigit(*(str - 1));
+}
+
+bool isMantissa(char*& str)
+{
+  return str[0] == '.' && isUInt(++str);
+}
+
+bool isOrder(char* str)
+{
+  return *str == 'E' && (isSign(*++str) && isUInt(++str) || isUInt(str));
+}
+
+bool isReal(char* str)
+{
+  return isSign(str[0]) && isMantissa(++str) && isOrder(str) || isMantissa(str) && isOrder(str);
+}
---
+++ kozyrin.semen/I5/IsReal.h
@@ -0,0 +1,6 @@
+#ifndef ISREAL_H
+#define ISREAL_H
+
+bool isReal(char* str);
+
+#endif

abd298a08831a527275968faea751c5691f39641
Царева ЯД <tsarewaya@yandex.ru>
2023-02-09 19:40:04 +0300

    made algorithm fun counter...

--- tsareva.yana/I4/counter_column_witch_duplicate_elements.cpp
+++ tsareva.yana/I4/counter_column_witch_duplicate_elements.cpp
@@ -2,5 +2,25 @@
 
 size_t counterColumnWitchDuplicateElements(const int * array, const size_t row, size_t column)
 {
-
+  size_t counter_column = 0;
+  bool duplicate = true;
+  for (size_t i = 0; i < column; i++)
+  {
+    duplicate = true;
+    for (size_t j = 0; j < row; j++)
+    {
+      for (size_t k = 0; k < row; k++)
+      {
+        if ((array[column * j + i] != array[column * k +i]) && j != k)
+        {
+          duplicate = false;
+        }
+      }
+    }
+    if (duplicate == true)
+    {
+      counter_column++;
+    }
+  }
+  return counter_column
 }

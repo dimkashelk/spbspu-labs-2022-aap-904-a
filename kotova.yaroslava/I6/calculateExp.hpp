1a16842b0ac2eb80857c038ab87c98f9300a45d8
Yaroslava <aroslavakotova@gmail.com>
2023-02-11 01:22:35 +0300

    add function prototypes

---
+++ kotova.yaroslava/I6/calculateExp.hpp
@@ -0,0 +1,7 @@
+#ifndef EXP_HPP
+#define EXP_HPP
+#include <iosfwd>
+double exp(double x, double absError, unsigned numberMax);
+void printRow(std::ostream& out, double x, double absError, unsigned numberMax);
+void printTable(std::ostream& out, double left, double right, double step, double absError, unsigned numberMax);
+#endif

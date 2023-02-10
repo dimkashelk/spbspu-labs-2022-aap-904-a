023b1e9c405765b1fe2ce117fbb27fd947a00952
Shuzamada <shuzamada@inbox.ru>
2023-02-11 00:45:39 +0300

    factorial

---
+++ hrushchev.nikita/I6/factorial.cpp
@@ -0,0 +1,12 @@
+#include "factorial.hpp"
+#include <cstddef>
+
+double factorial(const size_t n) 
+{
+  double result = 1;
+  for (size_t i = 1; i <= n; ++i) 
+  {
+    result *= i;
+  }
+  return result;
+}

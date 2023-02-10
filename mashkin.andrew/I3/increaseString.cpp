2dd95a664a925d8b921e64dcc90f5701128607c6
Машкин Андрей <beedzibood@gmail.com>
2023-02-10 15:22:17 +0300

    transmit char* j from for(), because in for i can't

--- mashkin.andrew/I3/increaseString.cpp
+++ mashkin.andrew/I3/increaseString.cpp
@@ -4,7 +4,8 @@
 char* increaseString(const char* string1, const size_t capacity, const size_t size, const size_t increaseCapacity)
 {
   char* newStr = new char[capacity + increaseCapacity];
-  for (const char* i = string1, *j = newStr; i != string1 + size; ++i, ++j)
+  char* j = newStr;
+  for (const char* i = string1; i != string1 + size; ++i, ++j)
   {
     *j = *i;
   }

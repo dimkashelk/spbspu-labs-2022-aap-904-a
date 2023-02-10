8e1a0af946d2467080aa4182256ee0692650663c
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-02-10 11:58:30 +0300

    lab3

--- roblesgonzalez.guillermo/I3/main.cpp
+++ roblesgonzalez.guillermo/I3/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <cstring>
-#include "removeDuplicateSpaces.h"
+#include <cctype>
 #include "removeDigits.h"
-#include <limits>
-int main() {
-    const int MAX_LENGTH = 100;
-    char str[MAX_LENGTH];
-    std::cin.getline(str, MAX_LENGTH);
+#include "removeDuplicateSpaces.h"
 
-    size_t size1 = removeDuplicateSpaces(str, str);
-    if (size1 == std::numeric_limits<size_t>::max()) {
-        std::cerr << "Error: removeDuplicateSpaces failed" << '\n';
-        return 1;
+int main() {
+    char str[100];
+    std::cin.getline(str, 100);
+
+    int size1 = removeDuplicateSpaces(str, str);
+    if (static_cast<int>(size1) == -1) {
+        std::cerr << "Error: Insufficient memory" << '\n';
+        return 3;
     }
 
-    char res2[MAX_LENGTH];
-    size_t size2 = removeDigits(res2, str);
-    if (size2 == std::numeric_limits<size_t>::max()) {
-        std::cerr << "Error: removeDigits failed" << '\n';
-        return 2;
+    char res2[100];
+    int size2 = removeDigits(res2, str);
+    if (static_cast<int>(size2) == -1) {
+        std::cerr << "Error: Insufficient memory" << '\n';
+        return 3;
     }
 
     std::cout << res2 << '\n';

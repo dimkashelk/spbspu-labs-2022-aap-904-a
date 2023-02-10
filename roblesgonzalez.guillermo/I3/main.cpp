f9531d73ab54c5582c86ca6fdae704ddb9de2709
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-02-10 12:07:37 +0300

    lab 3

--- roblesgonzalez.guillermo/I3/main.cpp
+++ roblesgonzalez.guillermo/I3/main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
+#include <fstream>
 #include "removeDuplicateSpaces.h"
 #include "removeDigits.h"
 
 int main() {
-    char str[100];
-    std::cin.getline(str, 100);
-
-    char res1[100];
-    size_t size1 = removeDuplicateSpaces(res1, str);
-    if (size1 != static_cast<size_t>(-1)) {
-        std::cout << res1 << '\n';
-    } else {
-        std::cerr << "Memory allocation failed for `res1`" << '\n';
+    std::string str;
+    if (!std::getline(std::cin, str)) {
+        std::cerr << "Error: no input provided" << '\n';
+        return 1;
     }
 
-    char res2[27];
-    size_t size2 = removeDigits(res2, str);
-    if (size2 != static_cast<size_t>(-1)) {
+    try {
+        char res1[100];
+        int size1 = removeDuplicateSpaces(res1, str.c_str());
+        if (size1 == -1) {
+            std::cerr << "Error: not enough memory" << '\n';
+            return 2;
+        }
+        std::cout << res1 << '\n';
+    } catch (const std::bad_alloc &e) {
+        std::cerr << "Error: not enough memory" << '\n' << e.what();
+        return 2;
+    }
+
+    try {
+        char res2[27];
+        int size2 = removeDigits(res2, str.c_str());
+        if (size2 == -1) {
+            std::cerr << "Error: not enough memory" << '\n';
+            return 2;
+        }
         std::cout << res2 << '\n';
-    } else {
-        std::cerr << "Memory allocation failed for `res2`" << '\n';
+    } catch (const std::bad_alloc &e) {
+        std::cerr << "Error: not enough memory" << '\n' << e.what();
+        return 2;
     }
     return 0;
 }

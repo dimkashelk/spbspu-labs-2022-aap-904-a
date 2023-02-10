a244e7a60ff58bcad827a04084ab52a36b8c882f
Машкин Андрей <beedzibood@gmail.com>
2023-02-10 22:36:24 +0300

    corrected CG: remove const from a few args

--- mashkin.andrew/I3/increaseString.cpp
+++ mashkin.andrew/I3/increaseString.cpp
@@ -1,7 +1,7 @@
 #include "increaseString.h"
 #include <cstddef>
 
-char* increaseString(const char* string1, const size_t capacity, const size_t size, const size_t increaseCapacity)
+char* increaseString(const char* string1, size_t capacity, size_t size, size_t increaseCapacity)
 {
   char* newStr = new char[capacity + increaseCapacity];
   char* j = newStr;
--- mashkin.andrew/I3/increaseString.h
+++ mashkin.andrew/I3/increaseString.h
@@ -2,5 +2,5 @@
 #define INCREASESTRING_H
 #include <cstddef>
 
-char* increaseString(const char* string1, const size_t capacity, const size_t size, const size_t increaseCapacity);
+char* increaseString(const char* string1, size_t capacity, size_t size, size_t increaseCapacity);
 #endif

24deb50844baaf842d0a3fe589440f4995684323
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-02-10 12:46:47 +0300

    Lab 3

--- roblesgonzalez.guillermo/I3/main.cpp
+++ roblesgonzalez.guillermo/I3/main.cpp
@@ -1,40 +1,64 @@
-#include <fstream>
-#include <memory>
+#include <iostream>
+#include <cstddef>
+#include <cstring>
+#include <ctype.h>
+#include <limits.h>
 #include "removeDuplicateSpaces.h"
 #include "removeDigits.h"
 
-int main() {
-    std::string str;
-    if (!std::getline(std::cin, str)) {
-        std::cerr << "Error: no input provided" << '\n';
+int main()
+{
+  size_t cap = 10;
+  char* cstring = new char[cap];
+  size_t size = 0;
+  std::cin >> std::noskipws;
+  do
+  {
+    if (size == cap - 1)
+    {
+      try
+      {
+        cstring[cap - 1] = '\0';
+        char* newstr = new char[cap + 20];
+        std::strcpy(newstr, cstring);
+        delete [] cstring;
+        cstring = newstr;
+        cap += 20;
+      }
+      catch (...)
+      {
+        delete [] cstring;
         return 1;
+      }
     }
-
-    try {
-        char res1[100];
-        int size1 = removeDuplicateSpaces(res1, str.c_str());
-        if (size1 == -1) {
-            std::cerr << "Error: not enough memory" << '\n';
-            return 2;
-        }
-        std::cout << res1 << '\n';
-    } catch (const std::bad_alloc &e) {
-        std::cerr << "Error: not enough memory" << '\n' << e.what();
-        return 2;
-    }
-
-    try {
-        char res2[27];
-        int size2 = removeDigits(res2, str.c_str());
-        if (size2 == -1) {
-            std::cerr << "Error: not enough memory" << '\n';
-            return 2;
-        }
-        std::cout << res2 << '\n';
-    } catch (const std::bad_alloc &e) {
-        std::cerr << "Error: not enough memory" << '\n' << e.what();
-        return 2;
-    }
-
-    return 0;
+    std::cin >> cstring[size];
+  }
+  while (std::cin && cstring[size++] != '\n');
+  if (size == 0)
+  {
+    std::cerr << "Empty string\n";
+    delete [] cstring;
+    return 1;
+  }
+  else
+  {
+    cstring[size - 1] = '\0';
+  }
+  char* destination = nullptr;
+  try
+  {
+    destination = new char[size];
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cout << e.what() << '\n';
+    delete [] cstring;
+    return 1;
+  }
+  removeDuplicateSpaces(destination, cstring);
+  removeDigits(destination, destination);
+  std::cout << destination << '\n';
+  delete [] cstring;
+  delete [] destination;
+  return 0;
 }

e31e0bbe00065ef016a4e2f94be7ed10e1a2af48
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-02-11 19:19:18 +0300

    func latin letters in progress

--- samoilenko.alena/I3/latin_letters.cpp
+++ samoilenko.alena/I3/latin_letters.cpp
@@ -1,27 +1,23 @@
 #include "latin_letters.h"
-#include <iostream>
+#include <cctype>
 
 char* createStringNewLatinLetters(char* destination, const char* source)
 {
-  size_t ascii[26] = {0};
-  size_t i = 0;
-  while (source[i] != '\0')
+  size_t k = 0;
+  for (char i = 'a'; i <= 'z'; i++)
   {
-    if (isalpha(source[i]))
+    for (const char* j = source; *j != '\0'; j++)
     {
-      ascii[size_t(tolower(source[i])) - 97]++;
-    }
-    i++;
-  }
-  size_t j = 0;
-  for (i = 0; i < 26; i++)
-  {
-    if (ascii[i] == 0)
-    {
-      destination[j] = char(i + 97);
-      j++;
+      if (*j == i)
+      {
+        i++;
+      }
+      else
+      {
+        destination[k++] = i;
+        break;
+      }
     }
   }
-  destination[j] = '\0';
   return destination;
 }

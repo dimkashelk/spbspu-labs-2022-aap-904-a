d81b064bdd37f83d8b6b7546992b3752a0c430d0
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-02-11 15:33:17 +0300

    fix valgrind errors(3)

--- danilenko.ekaterina/I3/createStringFromInput.cpp
+++ danilenko.ekaterina/I3/createStringFromInput.cpp
@@ -40,8 +40,8 @@ char* createStringFromInput(std::istream& input)
 
   if (size1 <= 1)
   {
-    delete[] source1;
     throw std::logic_error("Empty string");
+    delete[] source1;
   }
   source1[size1 - 1] = '\0';
 

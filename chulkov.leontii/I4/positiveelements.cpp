6966013a6a36c58723b60c0eab52ab9b9314ec08
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-02-11 03:57:13 +0300

    lb4

---
+++ chulkov.leontii/I4/positiveelements.cpp
@@ -0,0 +1,23 @@
+#include <cstddef>
+
+size_t getPositiveEl(int* matrix, size_t countRows, size_t countColumns)
+{
+  size_t count = 0;
+  for (size_t row = 0; row < countRows; row++)
+  {
+    bool negative = true;
+    for (size_t column = 0; column < countColumns; column++)
+    {
+      if (matrix[row * column + column] < 0)
+      {
+        negative = false;
+        break;
+      }
+      if (negative)
+      {
+        count++;
+      }
+    }
+  }
+  return count;
+}
---
+++ chulkov.leontii/I4/positiveelements.h
@@ -0,0 +1,5 @@
+#ifndef POSITIVE_ELEMENTS
+#define POSITIVE_ELEMENTS
+#include <cstddef>
+int getPositiveEl(int* matrix, size_t countRows, size_t countColumns);
+#endif

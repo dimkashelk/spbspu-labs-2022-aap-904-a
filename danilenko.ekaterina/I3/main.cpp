864161dc3dce09f46792c6ef3df5f83d414e0a55
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-02-11 17:28:47 +0300

    fix valgring error

--- danilenko.ekaterina/I3/main.cpp
+++ danilenko.ekaterina/I3/main.cpp
@@ -17,8 +17,8 @@ int main()
   }
   catch (const std::exception& e)
   {
-    std::cout << e.what() << "\n";
     delete[] source1;
+    std::cout << e.what() << "\n";
     return 1;
   }
 
@@ -28,32 +28,32 @@ int main()
   try
   {
     destination1 = new char[capacity + 1];
+    destination1 = makeNewStringWithoutRepeat(destination1, source1, source2);
+    std::cout << "New string without repeating letters from two strings: " << destination1 << "\n";
+    delete[] destination1;
   }
   catch (const std::bad_alloc& e)
   {
-    std::cout << e.what() << '\n';
     delete[] source1;
+    std::cout << e.what() << '\n';
     return 1;
   }
-  destination1 = makeNewStringWithoutRepeat(destination1, source1, source2);
-  std::cout << "New string without repeating letters from two strings: " << destination1 << "\n";
-  delete[] destination1;
-
+  
   //2
   char* destination2 = nullptr;
   try
   {
     destination2 = new char[28];
+    destination2 = makeNewStringWithMissingLetters(destination2, source1);
+    std::cout << "New string with missing letters in first string: " << destination2;
+    delete[] destination2;
   }
   catch (const std::bad_alloc& e)
   {
-    std::cout << e.what() << '\n';
     delete[] source1;
+    std::cout << e.what() << '\n';
     return 1;
   }
-  destination2 = makeNewStringWithMissingLetters(destination2, source1);
-  std::cout << "New string with missing letters in first string: " << destination2;
-  delete[] destination2;
 
   delete[] source1;
   return 0;

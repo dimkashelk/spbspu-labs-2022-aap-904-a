76db68d9703ebc3edf7d7621dfa460f406a08d83
Машкин Андрей <beedzibood@gmail.com>
2023-02-12 13:11:07 +0300

    declared and defined functions (increaseString and createString) and used them in main

--- mashkin.andrew/I5/main.cpp
+++ mashkin.andrew/I5/main.cpp
@@ -1,4 +1,21 @@
+#include "createCString.h"
+#include <iostream>
+
 int main()
 {
+  char* string = nullptr;
+  size_t capacity = 10;
+  size_t size = 0;
+  size_t endIndex = 0;
+  try
+  {
+    string = createCString(std::cin, size, capacity);
+  }
+  catch (const std::exception& ex)
+  {
+    std::cout << ex.what() << "\n";
+    delete[] string;
+    return 1;
+  }
   return 0;
 }

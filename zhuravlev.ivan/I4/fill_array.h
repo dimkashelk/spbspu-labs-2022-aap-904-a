98f5597d30bdf77ef15514a2d7b4e4688d9b1cca
zzzhura <iQubert323@>
2023-02-12 13:51:19 +0300

    fixing build

--- zhuravlev.ivan/I4/fill_array.h
+++ zhuravlev.ivan/I4/fill_array.h
@@ -3,6 +3,5 @@
 #include <fstream>
 #include <cstddef>
 
-void fillDynArray(int array_for_filling, size_t nRows, size_t nColomns, std::ifstream& input);
-void fillIntegratedArray(int* arr, size_t nRow, size_t nColomn, std::ifstream& input);
+void fillArray(int* array_for_filling, size_t nRow, size_t nColomn, std::ifstream& input);
 #endif

724b96b3c8c512a9965cb0b5895e5fa976469507
Засульский Егор <3756035@gmail.com>
2023-02-12 23:31:47 +0300

    1 trial

---
+++ zasulsky.egor/I5/isExpression.cpp
@@ -0,0 +1,63 @@
+#include "isExpression.hpp"
+
+char* skipIdentifier(char* cptr)
+{
+  if (*cptr >= 'a' && *cptr <= 'e') {
+    return cptr + 1;
+  }
+  return nullptr;
+}
+
+char* skipDigit(char* cptr)
+{
+  if (*cptr >= '0' && *cptr <= '9') {
+    return cptr + 1;
+  }
+  return nullptr;
+}
+
+char* skipUnsigned(char* cptr)
+{
+  cptr = skipDigit(cptr);
+  if (cptr == nullptr) {
+    return nullptr;
+  }
+  char* testptr = skipUnsigned(cptr);
+  if (testptr == nullptr) {
+    return cptr;
+  }
+  return testptr;
+}
+
+char* skipModifier(char* cptr)
+{
+  char* testptr = skipUnsigned(cptr);
+  if (testptr == nullptr) {
+    testptr = skipIdentifier(cptr);
+  }
+  return testptr;
+}
+
+char* skipTerm(char* cptr)
+{
+  cptr = skipModifier(cptr);
+  if (cptr != nullptr && *cptr == '*') {
+    return skipTerm(cptr + 1);
+  }
+  return cptr;
+}
+
+char* skipExpression(char* cptr)
+{
+  cptr = skipTerm(cptr);
+  if (cptr != nullptr && (*cptr == '+' || *cptr == '-')) {
+    return skipExpression(cptr + 1);
+  }
+  return cptr;
+}
+
+bool zasulsky::isExpression(char* cstring)
+{
+  cstring = skipExpression(cstring);
+  return (cstring != nullptr && *cstring == '\0');
+}
---
+++ zasulsky.egor/I5/main.cpp
@@ -0,0 +1,12 @@
+#include <cstddef>
+#include <iostream>
+#include <tuple>
+#include "readString.hpp"
+#include "isExpression.hpp"
+
+int main()
+{
+  std::tuple< char*, size_t, size_t > stringData = zasulsky::readString(std::cin);
+  char* cstring = std::get< 0 >(stringData);
+  std::cout << std::boolalpha << zasulsky::isExpression(cstring) << '\n';
+}

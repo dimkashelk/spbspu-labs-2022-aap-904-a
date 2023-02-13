0aea83109c681f309f5e2ef212438ececbbfc1c2
Yaroslava <aroslavakotova@gmail.com>
2023-02-13 20:38:53 +0300

    change functions that related to the counting of positive elements

--- kotova.yaroslava/I4/columns_w_positive_el.cpp
+++ kotova.yaroslava/I4/columns_w_positive_el.cpp
@@ -1,21 +1,21 @@
 #include "columns_w_positive_el.h"
-bool isPositive(const int* matrix, size_t line, size_t& number_column)
+bool isPositive(const int* matrix, size_t line, size_t columns, size_t& numberColumn)
 {
   bool flag = true;
-  size_t line_curr = 1;
-  size_t column_curr = 1;
-  for (size_t i = line_curr; i < line - 1; i++)
+  size_t cnt = 0;
+  size_t column_curr = 0;
+  for (size_t i = 0; i < line; i++)
   {
-    number_column = matrix[column_curr * i];
-    if (matrix[column_curr * line_curr + i] > 0)
+    numberColumn = matrix[column_curr * i];
+    if (*(matrix + columns * (numberColumn-1) * i) > 0)
     {
       flag = true;
-      i++;
+      cnt++;
     }
     else
     {
       flag = false;
-      break;
+      return flag;
     }
   }
   return flag;
@@ -24,9 +24,9 @@ size_t countPositiveElOfCol(const int* matrix, size_t column, size_t line)
 {
   size_t columns_curr = 0;
   size_t count = 0;
-  for (size_t i = columns_curr; i < column - 1; i++)
+  for (size_t i = 0; i < column; i++)
   {
-    if (isPositive(matrix, line, columns_curr))
+    if (isPositive(matrix, line, column, columns_curr))
     {
       count++;
     }

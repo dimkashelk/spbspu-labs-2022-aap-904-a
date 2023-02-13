adff047ddc366b236a0262b92432330304f44f9a
Засульский Егор <3756035@gmail.com>
2023-02-13 12:43:51 +0300

    5 trial

--- zasulsky.egor/I5/main.cpp
+++ zasulsky.egor/I5/main.cpp
@@ -7,12 +7,13 @@
 int main()
 {
   std::tuple< char*, size_t, size_t > stringData = zasulsky::readString(std::cin);
+  char* cstring = std::get< 0 >(stringData);
   size_t size = std::get< 1 > (stringData);
   if (size == 1) {
     std::cout << "Empty string\n";
+    delete [] cstring;
     return 1;
   }
-  char* cstring = std::get< 0 >(stringData);
   std::cout << std::boolalpha << zasulsky::isExpression(cstring) << '\n';
   delete [] cstring;
 }

b482da85a53525e89ba070f264578df02556b106
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-02-15 15:31:01 +0300

    Splitting the func about remove duplicate letters into two parts

--- danilenko.ekaterina/I3/makeNewStringWithoutRepeat.cpp
+++ danilenko.ekaterina/I3/makeNewStringWithoutRepeat.cpp
@@ -2,21 +2,9 @@
 #include <cstddef>
 #include <cstring>
 
-char* makeNewStringWithoutRepeat(char* destination, const char* source1, const char* source2)
+char* removeDuplicateLetters(char* destination)
 {
-  size_t str_len1 = strlen(source1);
-  size_t str_len2 = strlen(source2);
-  size_t total_len = str_len1 + str_len2;
-
-  for (size_t i = 0; i < str_len1; i++)
-  {
-    destination[i] = source1[i];
-  }
-  for (size_t j = 0; j < str_len2; j++)
-  {
-    destination[str_len1 + j] = source2[j];
-  }
-
+  size_t total_len = strlen(destination);
   for (size_t i = 0; i < total_len; i++)
   {
     for (size_t j = i + 1; j < total_len; j++)
@@ -33,5 +21,26 @@ char* makeNewStringWithoutRepeat(char* destination, const char* source1, const c
     }
   }
   destination[total_len] = '\0';
+  return destination;
+}
+
+
+char* makeNewStringWithoutRepeat(char* destination, const char* source1, const char* source2)
+{
+  size_t str_len1 = strlen(source1);
+  size_t str_len2 = strlen(source2);
+  size_t total_len = str_len1 + str_len2;
+
+  for (size_t i = 0; i < str_len1; i++)
+  {
+    destination[i] = source1[i];
+  }
+  for (size_t j = 0; j < str_len2; j++)
+  {
+    destination[str_len1 + j] = source2[j];
+  }
+  destination[total_len] = '\0';
+  destination = removeDuplicateLetters(destination);
+
   return destination;
 }
--- danilenko.ekaterina/I3/makeNewStringWithoutRepeat.h
+++ danilenko.ekaterina/I3/makeNewStringWithoutRepeat.h
@@ -1,6 +1,5 @@
 #ifndef MAKENEWSTRINGWITHOUTREPEAT_H
 #define MAKENEWSTRINGWITHOUTREPEAT_H
 
-char *makeNewStringWithoutRepeat(char *destination, const char *source1, const char *source2);
-
+char* makeNewStringWithoutRepeat(char* destination, const char* source1, const char* source2);
 #endif

582ac76bd2d5cb06e06f63849b11eca3ae1abd7f
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-02-16 21:30:09 +0300

    Fix cg: createPoints.cpp

--- mishanina.elena/T1/createPoints.cpp
+++ mishanina.elena/T1/createPoints.cpp
@@ -3,7 +3,7 @@
 double* createCoordinates(std::istream& in, std::size_t size)
 {
   double *coordinates = new double[size];
-  for (int i = 0; i < size; ++i)
+  for (std::size_t i = 0; i < size; ++i)
   {
     in >> coordinates[i];
     if (!in)
@@ -19,7 +19,7 @@ point_t* fillPoints(double *coordinates, std::size_t size)
   point_t *points = new point_t[size];
   for (std::size_t i = 0; i < size; i++)
   {
-    for (int j = 0; j < size * 2; j+=2)
+    for (std::size_t j = 0; j < size * 2; j+=2)
     {
       points[i].x = coordinates[j];
       points[i].y = coordinates[j+1];

89db20a9538cf37bd41ea2288d25518b63d08939
egor.aksenov <aksenov200477@hmail.com>
2023-02-16 12:57:47 +0300

    fixing pointers

--- aksenov.egor/I2/main.cpp
+++ aksenov.egor/I2/main.cpp
@@ -20,38 +20,37 @@ int main(int argc, char *argv[])
     std::cout << "error input" << "\n";
     return 2;
   }
-  int *dynamic_arr = new int[line];
+  int *second_arr = new int[line];
   if (line > 0)
   {
-    dynamic_arr = fillArrayWithRandomNumbers(dynamic_arr, line);
-    std::cout << countAmountOfRightPairs(dynamic_arr, line) << "\n";
-    std::cout << isRepeatingPositiveNumbers(dynamic_arr, line) << "\n";
-    delete [] dynamic_arr;
+    second_arr = fillArrayWithRandomNumbers(second_arr, line);
+    std::cout << countAmountOfRightPairs(second_arr, line) << "\n";
+    std::cout << isRepeatingPositiveNumbers(second_arr, line) << "\n";
+    delete [] second_arr;
   }
   else
   {
     std::cout << "ERROR " << "\n";
-    delete [] dynamic_arr;
+    delete [] second_arr;
   }
   line = 0;
   if (argc != 2)
   {
     std::cerr << "Not correct amount of parameters" << "\n";
   }
-  int *f_arr = new int[line];
   std::string fname = argv[1];
   std::ifstream input(fname);
   try
   {
-    f_arr = readFile(input, line, f_arr);
+    second_arr = readFile(input, line, second_arr);
   }
   catch (const std::length_error & e)
   {
     std::cout << e.what() << "\n";
-    delete [] f_arr;
+    delete [] second_arr;
     return 2;
   }
-  std::cout << countAmountOfRightPairs(f_arr, line) << "\n";
-  std::cout << isRepeatingPositiveNumbers(f_arr, line) << "\n";
-  delete [] f_arr;
+  std::cout << countAmountOfRightPairs(second_arr, line) << "\n";
+  std::cout << isRepeatingPositiveNumbers(second_arr, line) << "\n";
+  delete [] second_arr;
 }

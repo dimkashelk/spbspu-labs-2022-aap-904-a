2baa21980352ea38551c8ec400ac096fdaf14c66
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-02-16 22:23:56 +0300

    Fix createPoints.cpp

--- mishanina.elena/T1/createPoints.cpp
+++ mishanina.elena/T1/createPoints.cpp
@@ -19,11 +19,26 @@ point_t* fillPoints(double *coordinates, std::size_t size)
   point_t *points = new point_t[size];
   for (std::size_t i = 0; i < size; i++)
   {
-    for (std::size_t j = 0; j < size * 2; j+=2)
+//    for (std::size_t j = 0; j < size * 2; j+=2)
+//    {
+//      points[i].x = coordinates[j];
+//      points[i].y = coordinates[j+1];
+//      break;
+//    }
+    if (i == 0)
     {
-      points[i].x = coordinates[j];
-      points[i].y = coordinates[j+1];
-      break;
+      points[i].x = coordinates[0];
+      points[i].y = coordinates[1];
+    }
+    else if (i == 1)
+    {
+      points[i].x = coordinates[2];
+      points[i].y = coordinates[3];
+    }
+    else if (i == 2)
+    {
+      points[i].x = coordinates[4];
+      points[i].y = coordinates[5];
     }
   }
   return points;

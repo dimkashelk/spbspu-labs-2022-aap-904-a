bc213e630a099ac15519d1b0f6300cb66c8edb10
Yaroslava <aroslavakotova@gmail.com>
2023-02-18 01:16:16 +0300

    fix 1 thread

--- kotova.yaroslava/I4/reading_elements.h
+++ kotova.yaroslava/I4/reading_elements.h
@@ -1,6 +1,5 @@
 #ifndef READING_ELEMENTS_H
 #define READING_ELEMENTS_H
-#include <fstream>
 #include <istream>
 void readElements(int* matrix, size_t column, size_t line, std::ifstream& file_matrix_in);
 #endif

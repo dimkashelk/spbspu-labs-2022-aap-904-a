30a4df6c2bbb2ba46027485b47b7fd30fdb42f6f
Царева ЯД <tsarewaya@yandex.ru>
2023-02-17 15:33:32 +0300

    make

--- tsareva.yana/I5/is_float.cpp
+++ tsareva.yana/I5/is_float.cpp
@@ -1,4 +1,25 @@
 #include "is_float.h"
+#include <iostream>
+bool isDigit(char c)
+{
+  return std::isdigit(c);
+}
+bool isSign(char c)
+{
+  return (c== '+' || c == '-');
+}
+bool isDot(char c)
+{
+  return (c == '.');
+}
+bool isE(char c)
+{
+  return (c == 'E');
+}
+bool isEndString(char c)
+{
+  return (c == '\0');
+}
 bool isFloat(const char * data)
 {
 

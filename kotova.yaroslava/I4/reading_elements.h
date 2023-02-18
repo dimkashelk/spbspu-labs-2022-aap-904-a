59c2932206da79d54caa68349e93205310a88271
Yaroslava <aroslavakotova@gmail.com>
2023-02-18 15:05:16 +0300

    Revert " fix build"
    This reverts commit b9bf06fa83621283bdbb86fff6d752155ff2ec15.

--- kotova.yaroslava/I4/reading_elements.h
+++ kotova.yaroslava/I4/reading_elements.h
@@ -1,5 +1,5 @@
 #ifndef READING_ELEMENTS_H
 #define READING_ELEMENTS_H
 #include <istream>
-void readElements(int* matrix, size_t column, size_t line, std::istream& file_matrix_in);
+void readElements(int* matrix, size_t column, size_t line, std::ifstream& file_matrix_in);
 #endif

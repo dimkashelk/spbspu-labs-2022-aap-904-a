9a1a2d22649e12cf5148aa172f236ac1c72a718c
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-02-18 16:50:06 +0300

    Fix fillPoints

--- mishanina.elena/T1/createPoints.cpp
+++ mishanina.elena/T1/createPoints.cpp
@@ -18,23 +18,10 @@ double *createCoordinates(std::istream& in, std::size_t size)
 point_t *fillPoints(double *coordinates, std::size_t size)
 {
   point_t *points = new point_t[size];
-  for (std::size_t i = 0; i < size; i++)
+  for (std::size_t i = 0, j = 0; i < size; i++, j+=2)
   {
-    if (i == 0)
-    {
-      points[i].x = coordinates[0];
-      points[i].y = coordinates[1];
-    }
-    else if (i == 1)
-    {
-      points[i].x = coordinates[2];
-      points[i].y = coordinates[3];
-    }
-    else if (i == 2)
-    {
-      points[i].x = coordinates[4];
-      points[i].y = coordinates[5];
-    }
+    points[i].x = coordinates[j];
+    points[i].y = coordinates[j + 1];
   }
   return points;
 }

0e30caeda6786cb6081c16f1222c5f62f4d8c778
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-02-19 17:55:57 +0300

    change func with string without common letters

--- danilenko.ekaterina/I3/makeNewStringWithoutRepeat.cpp
+++ danilenko.ekaterina/I3/makeNewStringWithoutRepeat.cpp
@@ -2,46 +2,44 @@
 #include <cstddef>
 #include <cstring>
 
-char* copyString(char* destination, const char* source, size_t index)
-{
-  size_t source_lenght = strlen(source);
-  for (size_t i = 0; i < source_lenght; i++)
-  {
-    destination[index + i] = source[i];
-  }
-  destination[source_lenght + index] = '\0';
-  return destination;
-}
-
-char* removeDuplicateLetters(char* destination)
-{
-  size_t total_len = strlen(destination);
-  for (size_t i = 0; i < total_len; i++)
-  {
-    for (size_t j = i + 1; j < total_len; j++)
-    {
-      if (destination[i] == destination[j])
-      {
-        total_len--;
-        for (size_t k = j; k < total_len - 1; k++)
-        {
-          destination[k] = destination[k + 1];
-        }
-        j--;
-      }
-    }
-  }
-  destination[total_len] = '\0';
-  return destination;
-}
-
 char* makeNewStringWithoutRepeat(char* destination, const char* source1, const char* source2)
 {
-  size_t str_len1 = strlen(source1);
+  size_t new_element = 0;
+  bool match = false;
 
-  destination = copyString(destination, source1, 0);
-  destination = copyString(destination, source2, str_len1);
-  destination = removeDuplicateLetters(destination);
+  for (size_t i = 0; source1[i] != '\0'; i++)
+  {
+    match = false;
+    for (size_t j = 0; source2[j] != '\0'; j++)
+    {
+      if (source1[i] == source2[j])
+      {
+        match = true;
+        break;
+      }
+    }
+    if (match == false)
+    {
+      destination[new_element++] = source1[i];
+    }
+  }
 
+  for (size_t i = 0; source2[i] != '\0'; i++)
+  {
+    match = false;
+    for (size_t j = 0; source1[j] != '\0'; j++)
+    {
+      if (source2[i] == source1[j])
+      {
+        match = true;
+        break;
+      }
+    }
+    if (match == false)
+    {
+      destination[new_element++] = source2[i];
+    }
+  }
+  destination[new_element] = '\0';
   return destination;
 }
--- danilenko.ekaterina/I3/makeNewStringWithoutRepeat.h
+++ danilenko.ekaterina/I3/makeNewStringWithoutRepeat.h
@@ -2,4 +2,5 @@
 #define MAKENEWSTRINGWITHOUTREPEAT_H
 
 char* makeNewStringWithoutRepeat(char* destination, const char* source1, const char* source2);
+
 #endif

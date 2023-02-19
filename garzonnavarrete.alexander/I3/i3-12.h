284411af039f542fc98ad20008afb8cb03241334
alexg_29 <alexander.gar2901@gmail.com>
2023-02-19 17:53:27 +0300

    LAB I3 DONE!

---
+++ garzonnavarrete.alexander/I3/i3-12.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+char* read_str(istream& ifs, char* dst);
+char* merge(char* dst, char* src1, char* src2);
---
+++ garzonnavarrete.alexander/I3/i3-3.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <locale.h>
+
+using namespace std;
+
+char* remove_roman(char* dst, char* src);

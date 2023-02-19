73a96e3e70790de5521e171288f72b6df9f8f30c
alexg_29 <alexander.gar2901@gmail.com>
2023-02-19 13:09:26 +0300

    Lab I2 fixed!

--- garzonnavarrete.alexander/I2/countAriNum.h
+++
@@ -1,5 +0,0 @@
-#ifndef SPBSPU_LABS_2022_AAP_904_A_COUNTARINUM_H
-#define SPBSPU_LABS_2022_AAP_904_A_COUNTARINUM_H
-#include <cstddef>
-double countAriNum(const int *input_array, size_t array_size);
-#endif
--- garzonnavarrete.alexander/I2/main.cpp
+++ garzonnavarrete.alexander/I2/main.cpp
@@ -1,82 +1,35 @@
 #include <iostream>
-#include "countAriNum.h"
-#include "countIndices.h"
-#include <stdexcept>
-#include <cstddef>
-#include <ctime>
-#include <fstream>
+#include <climits>
+#include <algorithm>
 
-int main(int argc, char *argv[]) {
+using namespace std;
 
-  if (argc > 2) {
-    std::cout << "Too many parameters\n";
-    return 1;
-  }
+int main()
+{
+  int arr[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20};
+  int max = INT_MIN;
+  int min = INT_MAX;
+  int suma = INT_MAX + INT_MIN;
+  double sum = 0.0;
 
-  int arr1[] = {2, 4, 6, 8, 10, 12, 14};
-  try {
-    std::cout << countIndices(arr1, 7) << std::endl;
-    std::cout << countAriNum(arr1, 7) << std::endl;
+  for (int i: arr) {
+    if (i < min) {
+      min = i;
+    }
+    if (i > max) {
+      max = i;
+    }
+    if (i) {
+      suma = min + max;
+    }
   }
-  catch (const std::overflow_error &e) {
-    std::cout << e.what();
-    return 2;
+  for (size_t i = 0; i < 18; i+=2) {
+    sum += arr[i];
   }
+    cout << "The min element is: " << min << endl;
+    cout << "The max element is: " << max << endl;
+    cout << "The sum between max and min is: " << suma << endl;
+    cout << "The arithmetic media is: " << sum / ((18 + 1)/2) << endl;
 
-  // Initialize array 2 and call the functions
-  size_t n = 0;
-  std::cin >> n;
-  if (n == 0) {
-    std::cout << "Array length cannot be 0\n";
-    return 2;
-  }
-
-  int *arr2 = new int[n];
-  std::srand(time(nullptr));
-  for (size_t i = 0; i < n; i++) {
-    arr2[i] = std::rand();
-  }
-  try {
-    std::cout << countIndices(arr2, n) << std::endl;
-    std::cout << countAriNum(arr2, n) << std::endl;
-  }
-  catch (const std::overflow_error &e) {
-    std::cout << e.what();
-    delete[] arr2;
-    return 2;
-  }
-  delete[] arr2;
-
-  if (argc == 1) {
-    std::cout << "No input file provided\n";
-    return 1;
-  }
-
-  std::ifstream in(argv[1]);
-  if (!in.is_open()) {
-    std::cout << "Error while opening file\n";
-    return 1;
-  }
-
-  size_t size = 0;
-  in >> size;
-  if (!in) {
-    std::cout << "Error reading file\n";
-    return 1;
-  }
-
-  if (size == 0) {
-    std::cout << "Array length from file cannot be 0\n";
-    return 2;
-  }
-
-  int *arr = new int[size];
-  for (size_t i = 0; i < size; i++) {
-    in >> arr[i];
-    if (!in) {
-      std::cout << "Error reading file\n";
-      delete[] arr;
-}
-}
-  return 1;
+  return 0;
 }

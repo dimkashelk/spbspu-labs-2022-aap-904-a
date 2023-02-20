fed7af40b8197c718b2dade495fdf933937a5dcb
Disus <mita2004567@gmail.com>
2023-02-21 00:51:25 +0300

    fix

--- suslov.dmitriy/I5/main.cpp
+++ suslov.dmitriy/I5/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include "scanStr.h"
+#include <scanStr.h>
 #include "isFloat.h"
 int main()
 {
   size_t c = 0;
   char *arr = scanStr(c, std::cin);
-  std::cout << std::boolalpha << real(arr) << "\n";
+  if (c > 0)
+  {
+    std::cout << std::boolalpha << real(arr) << "\n";
+  }
+  else
+  {
+    std::cerr << "Error";
+    delete[] arr;
+    return 1;
+  }
   delete[] arr;
   return 0;
 }

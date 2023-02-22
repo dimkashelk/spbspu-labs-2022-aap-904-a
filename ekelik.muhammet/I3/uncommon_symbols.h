f7e55143985bb37c76b9a83f38d0cc67b11d6229
mekel1k <mekel1k4444@gmail.com>
2023-02-22 21:55:36 +0300

    до комиссии

---
+++ ekelik.muhammet/I3/uncommon_symbols.h
@@ -0,0 +1,8 @@
+#ifndef UNCOMMON_SYMBOLS_H
+#define UNCOMMON_SYMBOLS_H
+
+#include <cstddef>
+
+char* find_uncommon_symbols(char* my_symbols, const char* source1);
+
+#endif
---
+++ ekelik.muhammet/common/my_String.cpp
@@ -0,0 +1,29 @@
+#include "my_String.h"
+#include <my_ext_String.h>
+
+char* makeMyString(size_t &size, size_t &capacity, std::istream &input)
+{
+  char* my_string = new char[capacity];
+  my_string[0] = '\n';
+
+  input >> std::noskipws;
+  do
+  {
+    if (size + 1 == capacity)
+    {
+      my_string[size] = '\0';
+      char* newstr = extendMyString(my_string, capacity);
+      delete[] my_string;
+      my_string = newstr;
+    }
+    input >> my_string[size];
+  }
+  while (input && my_string[size++] != '\n');
+
+  if (my_string[0] != '\n')
+  {
+    my_string[size - 1] = '\0';
+  }
+
+  return my_string;
+}

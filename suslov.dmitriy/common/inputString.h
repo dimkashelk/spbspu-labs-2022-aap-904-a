c9195f3ae20a12fe3d60cc3a74b0013ec1db538f
Disus <mita2004567@gmail.com>
2023-02-22 10:47:16 +0300

    fix

--- suslov.dmitriy/common/inputString.h
+++ suslov.dmitriy/common/inputString.h
@@ -1,5 +1,7 @@
 #ifndef SPBSPU_LABS_2022_AAP_904_A_INPUTSTRING_H
 #define SPBSPU_LABS_2022_AAP_904_A_INPUTSTRING_H
+#include <iostream>
+#include <cstddef>
 char *expandString(const char *oldString, size_t oldSize, size_t delta);
 char *scanStr(size_t &length, std::istream &in);
 #endif

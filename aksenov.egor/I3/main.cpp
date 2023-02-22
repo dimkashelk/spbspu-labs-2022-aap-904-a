b3f7eaa86e46ec0a716b7eef1763940562526c69
egor.aksenov <aksenov200477@hmail.com>
2023-02-22 03:47:53 +0300

    add function for deletting extra spaces

--- aksenov.egor/I3/main.cpp
+++ aksenov.egor/I3/main.cpp
@@ -1,9 +1,15 @@
 #include "isRepeatingElements.h"
 #include "readString.h"
+#include "deleteSpaces.h"
 #include <iostream>
 int main()
 {
   char *cstring = nullptr;
+  size_t size = 0;
   cstring = readString(std::cin);
   std::cout << std::boolalpha << isRepeatingElements(cstring);
+  char *strwithoutspaces = new char[size];
+  strwithoutspaces = deleteSpaces(strwithoutspaces, cstring);
+  std::cout << strwithoutspaces;
+  delete[] cstring;
 }

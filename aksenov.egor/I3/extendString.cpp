6dddbdfe37b90852e2919c6cc1fbb09d6231d622
egor.aksenov <aksenov200477@hmail.com>
2023-02-23 01:14:51 +0300

    more try/catch

--- aksenov.egor/I3/extendString.cpp
+++ aksenov.egor/I3/extendString.cpp
@@ -2,6 +2,10 @@
 #include <stdexcept>
 char *extendString(char *cstring, size_t size, size_t capacity)
 {
+  if (capacity < size)
+  {
+    throw std::invalid_argument("unreal");
+  }
   char *newstring = new char[capacity];
   for (auto i = cstring, j = newstring; i != cstring + size; ++i, ++j)
   {

8eb7fb9eb408999781a799f1168806a82018fcb4
Vavericherry <vaverich3@inbox.ru>
2023-02-24 23:46:55 +0300

    Fix pipelines

--- averich.vladimir/I2/main.cpp
+++ averich.vladimir/I2/main.cpp
@@ -7,8 +7,7 @@
 #include "generationdynarray.hpp"
 int main(int argc, char* argv[])
 {
-  const size_t sizeOfDefaultArray = 10;
-  int defaultArray[sizeOfDefaultArray] = { -5, 2, 1, 3, 2, 1, -2, 1, -3, -4 };
+  int defaultArray[10] = { -5, 2, 1, 3, 2, 1, -2, 1, -3, -4 };
   std::cout << "Count of triplets: " << isTriplet(defaultArray, sizeOfDefaultArray) << " in the static array" << std::endl;
   sort(defaultArray, sizeOfDefaultArray);
 

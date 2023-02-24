22b07aafdbbdccdcb00b4a006c69e8e772f13b56
Vavericherry <vaverich3@inbox.ru>
2023-02-24 19:30:05 +0300

    Fix Triplet function

--- averich.vladimir/I2/istriplet.cpp
+++ averich.vladimir/I2/istriplet.cpp
@@ -3,11 +3,15 @@
 #include <cstddef>
 int isTriplet(int arr[], size_t size)
 {
+  if (size < 3)
+  {
+    throw std::length_error("Error");
+  }
   size_t i = 0;
   size_t j = 1;
   size_t k = 2;
   int countOfTriplet = 0;
-  for (size_t l = 0; l < size - 2; l++)
+  while (k != size)
   {
    if (arr[i] > arr[j] && arr[j] > arr[k])
    {
--- averich.vladimir/I2/main.cpp
+++ averich.vladimir/I2/main.cpp
@@ -7,51 +7,62 @@
 #include "generationdynarray.hpp"
 int main(int argc, char* argv[])
 {
-  //good
   const size_t sizeOfDefaultArray = 10;
   int defaultArray[sizeOfDefaultArray] = { -5, 2, 1, 3, 2, 1, -2, 1, -3, -4 };
   std::cout << "Count of triplets: " << isTriplet(defaultArray, sizeOfDefaultArray) << " in the static array" << std::endl;
   sort(defaultArray, sizeOfDefaultArray);
-  //good
+
   size_t sizeOfDynArray = 0;
   std::cout << "Enter size of dynamic array: ";
   std::cin >> sizeOfDynArray;
   if (!std::cin)
   {
-    std::cerr << "Error input" << std::endl;
+    std::cout << "Error input" << std::endl;
     return 1;
   }
   int* dynArray = new int[sizeOfDynArray];
   if (sizeOfDynArray > 0)
   {
     dynArray = generationDynArray(sizeOfDynArray);
-    std::cout << "Count of triplets: " << isTriplet(dynArray, sizeOfDynArray) << " in the dynamic array" << std::endl;
+    try
+    {
+      std::cout << "Count of triplets: " << isTriplet(dynArray, sizeOfDynArray) << " in the dynamic array" << std::endl;
+    }
+    catch (std::length_error& e)
+    {
+      std::cout << e.what() << std::endl;
+    }
     sort(dynArray, sizeOfDynArray);
     delete[] dynArray;
   }
   else
   {
-    std::cerr << "Error" << std::endl;
+    std::cout << "Error" << std::endl;
+    return 1;
     delete[] dynArray;
   }
+
   if (argc != 2)
   {
-    std::cerr << "Error: incorrect file name";
+    std::cout << "Error: incorrect file name";
     return 1;
   }
   std::string fileName = argv[1];
   std::ifstream in(fileName);
   size_t sizeOfFileArray = 0;
+  in >> sizeOfFileArray;
   int* array = new int[sizeOfFileArray];
   try
   {
     int* fileArray = readTheArray(in, sizeOfFileArray, array);
     std::cout << "Count of triplets: " << isTriplet(fileArray, sizeOfFileArray) << " in the file array" << std::endl;
     sort(fileArray, sizeOfFileArray);
+    delete[] fileArray;
   }
   catch (const std::length_error& e)
   {
-    std::cerr << e.what() << std::endl;
+    std::cout << e.what() << std::endl;
+    delete[] array;
     return 1;
   }
   delete[] array;

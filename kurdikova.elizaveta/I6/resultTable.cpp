1080d3e6977cc84fc492855b02272fd0d3cbca9b
Kurdikova Elizaveta <Kurdikova.es@edu.spbstu.ru>
2023-02-25 16:47:07 +0300

    Init commit

---
+++ kurdikova.elizaveta/I6/resultTable.cpp
@@ -0,0 +1,34 @@
+#include "resultTable.h"
+#include <iomanip>
+#include <cmath>
+#include "naturalLogarithm.h"
+
+void printTableRow(std::ostream& out, const double x, const size_t numberMax, const double absError)
+{
+  out << std::left << std::setprecision(10) << "| " << std::setw(5) << x
+      << " | " << std::setw(14) << getNaturalLog(x, numberMax, absError)
+      << " | " << std::setw(23) << std::log((x + 1) / (x - 1)) << " |\n";
+}
+
+void printTable(std::ostream& out, const double left, const double right,
+                const double step, const size_t numberMax, const double absError)
+{
+  const double EPS = 0.1E-10;
+  out <<
+      "|-------|----------------|-------------------------|\n"
+      "|   X   |  Taylor's row  |  std::log((x+1)/(x-1))  |\n"
+      "|-------|----------------|-------------------------|\n";
+  for (double x = left; x < right; x += step)
+  {
+    printTableRow(out, x, numberMax, absError);
+    if (std::fabs(x + step - right) < EPS || x + step > right)
+    {
+      printTableRow(out, right, numberMax, absError);
+      out <<
+          "|_______|________________|_________________________|\n";
+      return;
+    }
+  }
+  out <<
+      "|_______|________________|_________________________|\n";
+}

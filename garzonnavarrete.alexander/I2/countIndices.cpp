961aca181192fa2cca5b627d8233de71e5b61e2b
alexg_29 <alexander.gar2901@gmail.com>
2023-02-27 17:52:34 +0300

    I2 LAB!

--- garzonnavarrete.alexander/I2/countIndices.cpp
+++ garzonnavarrete.alexander/I2/countIndices.cpp
@@ -7,8 +7,8 @@ size_t countIndices(const int *input_array, size_t array_size)
   {
     return 0;
   }
-  int min = std::numeric_limits<int>::max();
-  int max = std::numeric_limits<int>::min();
+  int min = std::numeric_limits< int >::max();
+  int max = std::numeric_limits< int >::min();
   for (size_t i = 0; i < array_size; i++)
   {
     if (input_array[i] < min)
--- garzonnavarrete.alexander/I2/main.cpp
+++ garzonnavarrete.alexander/I2/main.cpp
@@ -6,55 +6,53 @@
 #include "countAriNum.h"
 #include "countIndices.h"
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
   if (argc != 2)
   {
-    cout << "Incorrect number of parameters";
+    std::cout << "Incorrect number of parameters";
     return 1;
   }
   int arr1[] = {1, 3, 6, 7, 9, 7, 14};
   try
   {
-    cout << countAriNum(arr1, 7) << "\n";
-    cout << countIndices(arr1, 7) << "\n";
+    std::cout << countAriNum(arr1, 7) << "\n";
+    std::cout << countIndices(arr1, 7) << "\n";
   }
   catch (const std::runtime_error &e)
   {
-    cout << e.what();
+    std::cout << e.what();
     return 2;
   }
   size_t n = 0;
-  cin >> n;
+  std::cin >> n;
   int *arr2 = new int[n];
-  srand(time(nullptr));
+  std::srand(time(nullptr));
   for (size_t i = 0; i < n; i++)
   {
-    arr2[i] = rand();
+    arr2[i] = std::rand();
   }
   try
   {
-    cout << countAriNum(arr2, n) << "\n";
-    cout << countIndices(arr2, n) << "\n";
+    std::cout << countAriNum(arr2, n) << "\n";
+    std::cout << countIndices(arr2, n) << "\n";
   }
   catch (...)
   {
-    cout << "0\n";
+    std::cout << "0\n";
   }
   delete[] arr2;
-  ifstream in(argv[1]);
+  std::ifstream in(argv[1]);
   if (!in.is_open())
   {
-    cout << "Error while opening file\n";
+    std::cout << "Error while opening file\n";
     return 1;
   }
   size_t size = 0;
   in >> size;
   if (!in)
   {
-    cout << "Error reading file\n";
+    std::cout << "Error reading file\n";
     return 1;
   }
   int *arr = new int[size];
@@ -63,19 +61,19 @@ int main(int argc, char *argv[])
     in >> arr[i];
     if (!in)
     {
-      cout << "Error reading file\n";
+      std::cout << "Error reading file\n";
       delete[] arr;
       return 1;
     }
   }
   try
   {
-    cout << countAriNum(arr, size) << "\n";
-    cout << countIndices(arr, size) << "\n";
+    std::cout << countAriNum(arr, size) << "\n";
+    std::cout << countIndices(arr, size) << "\n";
   }
   catch (...)
   {
-    cout << "0\n";
+    std::cout << "0\n";
   }
   delete[] arr;
   return 0;

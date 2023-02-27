43fe2d44a31a5f1e256558a1401e6be71923d91b
mekel1k <mekel1k4444@gmail.com>
2023-02-28 02:11:59 +0300

    oldu gibi

--- ekelik.muhammet/I3/uncommonSymbols.cpp
+++ ekelik.muhammet/I3/uncommonSymbols.cpp
@@ -1,19 +1,16 @@
-
 #include "uncommonSymbols.h"
 #include <cstring>
 
-char* findUncommonSymbols(char* mySymbols, const char* source1)
+char* findUncommonSymbols(char* mySymbols, const char* source1, const char* source2)
 {
-  const char* source2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
   size_t sizeUnique = 0;
   bool isCommon = false;
 
-  while (*source1)
+  for (; *source1; source1++)
   {
-    isCommon = false;
-    for (size_t i = 0; i < std::strlen(source2); ++i)
+    for (const char* j = source2; *j; j++)
     {
-      if (*source1 == source2[i])
+      if (*source1 == *j)
       {
         isCommon = true;
         break;
@@ -22,9 +19,9 @@ char* findUncommonSymbols(char* mySymbols, const char* source1)
     if (!isCommon)
     {
       mySymbols[sizeUnique] = *source1;
-      ++sizeUnique;
+      sizeUnique++;
     }
-    ++source1;
+    isCommon = false;
   }
   mySymbols[sizeUnique] = '\0';
   return mySymbols;

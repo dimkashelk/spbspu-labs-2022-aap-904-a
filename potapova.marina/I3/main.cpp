085a0630d6c18f66feef2a6d1bfa74a55e2c9fdd
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-28 00:12:40 +0300

    change input

--- potapova.marina/I3/main.cpp
+++ potapova.marina/I3/main.cpp
@@ -4,55 +4,38 @@
 
 int main()
 {
-  const char* input_str;
-  try
-  {
-    input_str = getLineFromFile();
-  }
-  catch (const std::bad_alloc& e)
-  {
-    std::cerr << "Allocation failed: " << e.what() << '\n';
-    return 1;
-  }
-  if (input_str == nullptr)
-  {
-    std::cerr << "Empty input\n";
-    return 1;
-  }
-  char most_common_syms[4];
-  std::cout << getMostCommonSyms(most_common_syms, input_str) << '\n';
-  delete[] input_str;
+    const char* input_str;
+    size_t str1_size;
+    try
+    {
+        input_str = getLineFromFile();
+    }
+    catch (const std::bad_alloc& e)
+    {
+        std::cerr << "Allocation failed: " << e.what() << '\n';
+        return 1;
+    }
+    if (input_str == nullptr)
+    {
+        std::cerr << "Empty input\n";
+        return 1;
+    }
+    char most_common_syms[4];
+    std::cout << getMostCommonSyms(most_common_syms, input_str) << '\n';
 
-  size_t str1_size;
-  char* str1;
-  size_t str2_size;
-  char* str2;
-  try
-  {
-    str1 = getLineFromFile(&str1_size);
-    str2 = getLineFromFile(&str2_size);
-  }
-  catch(const std::bad_alloc& e)
-  {
-    std::cerr << "Allocation failed: " << e.what() << '\n';
-  }
-  if (str1 == nullptr || str2 == nullptr)
-  {
-    std::cerr << "One of the strings is empty";
-  }
-  char* result;
-  try
-  {
-    result = new char[str1_size + str2_size + 1];
-  }
-  catch (const std::bad_alloc& e)
-  {
-    std::cerr << "Allocation failed: " << e.what() << '\n';
-    return 1;
-  }
-  std::cout << addAllLatinLets(result, str1, str2) << '\n';
-  delete[] str1;
-  delete[] str2;
-  delete[] result;
-  return 0;
+    const char str2[] = "all";
+    char* all_latin_lets;
+    try
+    {
+        all_latin_lets = new char[str1_size + sizeof(str2)];
+    }
+    catch (const std::bad_alloc& e)
+    {
+        std::cerr << "Allocation failed: " << e.what() << '\n';
+        return 1;
+    }
+    std::cout << addAllLatinLets(all_latin_lets, input_str, str2) << '\n';
+    delete[] input_str;
+    delete[] all_latin_lets;
+    return 0;
 }

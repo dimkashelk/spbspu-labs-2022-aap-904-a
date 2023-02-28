67c5f74cdb8bdecc7386320f0221e7a6eef6e2f4
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-02-28 21:01:59 +0300

    Fix Function3

--- chulkov.leontii/I4/positiveelements.cpp
+++ chulkov.leontii/I4/positiveelements.cpp
@@ -1,21 +1,24 @@
 #include "positiveelements.h"
 #include <cstddef>
 
+bool containsOnlyPositive(const int* row, size_t countColumns)
+{
+  for (size_t column = 0; column < countColumns; column++)
+  {
+    if (row[column] < 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
 size_t countPositiveOnlyRows(const int* matrix, size_t countRows, size_t countColumns)
 {
   size_t count = 0;
   for (size_t row = 0; row < countRows; row++)
   {
-    bool negative = true;
-    for (size_t column = 0; column < countColumns; column++)
-    {
-      if (matrix[countRows * row + column] < 0)
-      {
-        negative = false;
-        break;
-      }
-    }
-    if (negative)
+    if (containsOnlyPositive(matrix + countColumns * row, countColumns))
     {
       count++;
     }

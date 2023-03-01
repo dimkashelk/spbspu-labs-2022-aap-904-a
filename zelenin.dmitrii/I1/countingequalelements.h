91eef091b8469fd3cbf634e78b3ffb9a3132b2a0
Зеленин Дмитрий <mitya.zelenin12@mail.ru>
2023-03-01 03:14:20 +0300

    Counting Equal Elements and Pythagorean threes

---
+++ zelenin.dmitrii/I1/countingequalelements.h
@@ -0,0 +1,6 @@
+#ifndef COUNTINGEQUALELEMENTS_H
+#define COUNTINGEQUALELEMENTS_H
+
+void countingEqualElements(int& value, int& pred_value, int& equal_element_counter, int& max_equal_element);
+
+#endif
---
+++ zelenin.dmitrii/I1/countingpythagoreanthrees.h
@@ -0,0 +1,6 @@
+#ifndef COUNTINGPYTHAGOREANTHREES_H
+#define COUNTINGPYTHAGOREANTHREES_H
+
+void countingPythagoreanThrees(int& value, int& program_launch_number, int& count_of_pythagorean_threes, int& x, int& y, int& z);
+
+#endif

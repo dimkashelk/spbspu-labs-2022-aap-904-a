071f10b9d9b05bf9a3630b585a883f43c1fe73c7
bobrovskaya <bobrovskaya.ma@edu.spbpu>
2023-03-01 23:23:59 +0300

    new commt

--- bobrovskaya.maria/I3/main.cpp
+++ bobrovskaya.maria/I3/main.cpp
@@ -5,10 +5,15 @@
 
 int main()
 {
-  char * source1 = nullptr;
+  char *source1 = nullptr;
   try
   {
-    source1= createCString(std::cin);
+    source1 = createCString(std::cin);
+    if (*source1 == '\0')
+    {
+      delete[] source1;
+      throw std::logic_error("Empty string");
+    }
   }
   catch (const std::exception& e)
   {
@@ -18,13 +23,13 @@ int main()
   size_t countDifLett = countDifLetters(source1);
   std::cout << countDifLett << "\n";
 
-  char source2[]= "adfgvbh";
-  char *destination= nullptr;
+  char source2[] = "adfgvbh";
+  char *destination = nullptr;
   try
   {
     destination = new char[100];
   }
-  catch (const std::bad_alloc& e)
+  catch(const std::bad_alloc& e)
   {
     delete[] source1;
     std::cerr << e.what() << '\n';
@@ -32,7 +37,8 @@ int main()
   }
   destination = newStringIndeticalCharacters(destination, source1, source2);
   std::cout << destination << "\n";
+
   delete[] destination;
-  delete [] source1;
+  delete[] source1;
   return 0;
 }

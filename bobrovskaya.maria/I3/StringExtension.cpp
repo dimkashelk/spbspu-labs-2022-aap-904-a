692f7437815750dfb7a50808be121b4fea02fdbb
bobrovskaya <bobrovskaya.ma@edu.spbpu>
2023-03-02 23:33:13 +0300

    CG fixed

--- bobrovskaya.maria/I3/StringExtension.cpp
+++ bobrovskaya.maria/I3/StringExtension.cpp
@@ -1,6 +1,6 @@
 #include "StringExtension.h"
 
-char* stringExtension(char *source1, size_t &capacity, size_t size)
+char *stringExtension(char *source1, size_t &capacity, size_t size)
 {
   char *newstr = new char[capacity + 20];
   for (auto i = source1, j = newstr; i != source1 + size; i++, j++)

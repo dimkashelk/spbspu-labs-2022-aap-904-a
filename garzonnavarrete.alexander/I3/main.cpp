d538b649a3a19d816d87c527b145dbaa9b6b4708
alexg_29 <alexander.gar2901@gmail.com>
2023-03-02 21:48:14 +0300

    Basic i3 done!

--- garzonnavarrete.alexander/I3/main.cpp
+++ garzonnavarrete.alexander/I3/main.cpp
@@ -1,187 +1,46 @@
-#include "i3-3.h"
-#include "i3-12.h"
-char* read_str(istream& ifs, char* dst)
-{
-  int n = 0;
-  while(!ifs.eof())
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string input_str;
+  std::string p, q;
+  std::cout << "Enter a string: " << std::endl;
+  getline(std::cin, input_str);
+  std::cout << "Enter the first string (p): " << std::endl;
+  getline(std::cin, p);
+  std::cout << "Enter the second string (q): " << std::endl;
+  getline(std::cin, q);
+
+  std::string output_str = "";
+  for (char c : input_str)
   {
-    ifs.get(dst[n]);
-    if (dst[n] == '\n') break;
-    ++n;
-  }
-  dst[n] = 0;
-  return dst;
-}
-char* remove_roman(char* dst, char* src)
-{
-  int i = 0, j = 0;
-  do
-  {
-    if ((src[i] < 'A') || (src[i] > 'z') || ((src[i] > 'Z') && (src[i] < 'a')))
+    if (!isalpha(c) || !isascii(c))
     {
-      dst[j] = src[i];
-      ++j;
+      output_str += c;
     }
-    ++i;
-  } while(src[i] != 0);
-  return dst;
-}
-char* merge(char* dst, char* s1, char* s2)
-{
-  int j = 0;
-  int i1 = 0;
-  int i2 = 0;
-  do
-  {
-    if (i1 >= 0)
+  }
+    std::cout << "Output string: " << output_str << std::endl;
+
+    std::string combined_str = "";
+    int i = 0, j = 0;
+    while (i < p.length() && j < q.length())
     {
-      if (s1[i1] != 0)
-      {
-        dst[j] = s1[i1];
-        ++j;
-        ++i1;
-      }
-    else
+      combined_str += p[i];
+      combined_str += q[j];
+      i++;
+      j++;
+    }
+    while (i < p.length())
     {
-      i1 = -1;
+      combined_str += p[i];
+      i++;
     }
-    }
-    if (i2 >= 0)
+    while (j < q.length())
     {
-      if (s2[i2] != 0)
-      {
-        dst[j] = s2[i2];
-        ++j;
-        ++i2;
-      }
-    else
-    {
-      i2 = -1;
+      combined_str += q[j];
+      j++;
     }
-    }
-  } while ((i1 >= 0) || (i2 >=0));
-  return dst;
-}
-int main()
-{
-  int maxn;
-  ifstream ifs;
-  ofstream ofs;
-  char* s;
-  char* s2;
-  char* c;
-  try
-  {
-    ifs.open("input_str3.txt");
-  }
-  catch(exception& e)
-  {
-    cout << "Mission 3 failed: could not open file. Trying mission 12." << endl;
-    goto maybe_12;
-  }
-  ifs >> maxn;
-  try
-  {
-    s = new char[maxn];
-  }
-  catch(exception& e)
-  {
-    ifs.close();
-    cout << e.what() << endl;
-    return -1;
-  }
-  try
-  {
-    c = new char[maxn];
-  }
-  catch(exception& e)
-  {
-    ifs.close();
-    delete s;
-    cout << e.what() << endl;
-    return -1;
-  }
-  ifs.ignore(256, '\n');
-  read_str(ifs, s);
-  ifs.close();
-  remove_roman(c, s);
-  cout << c << endl;
-  try
-  {
-    ofs.open("output_str.txt");
-    ofs << c;
-    ofs.close();
-  }
-  catch(exception& e)
-  {
-    cout << e.what() << endl;
-  }
-  delete c;
-  delete s;
-  cout << endl;
-  maybe_12:
-  try
-  {
-    ifs.open("input_str12.txt");
-  }
-  catch(exception& e)
-  {
-    cout << "Mission 12 failed: could not open file." << endl;
-    return -1;
-  }
-  ifs >> maxn;
-  try
-  {
-    s = new char[maxn];
-  }
-  catch(exception& e)
-  {
-    cout << e.what() << endl;
-    ifs.close();
-    return -1;
-  }
-  try
-  {
-    s2 = new char[maxn];
-  }
-  catch(exception& e)
-  {
-    cout << e.what() << endl;
-    delete s;
-    ifs.close();
-    return -1;
-  }
-  ifs.ignore(256, '\n');
-  read_str(ifs, s);
-  read_str(ifs, s2);
-  ifs.close( );
-  try
-  {
-    c = new char[maxn * 2];
-  }
-  catch(exception& e)
-  {
-    cout << e.what() << endl;
-    delete s;
-    delete s2;
-    return -1;
-  }
-  cout << s << endl << "+" << endl;
-  cout << s2 << endl << "=" << endl;
-  merge(c, s, s2);
-  cout << c << endl;
-  try
-  {
-    ofs.open("output_str12.txt");
-    ofs << c;
-    ofs.close();
-  }
-  catch(exception& e)
-  {
-    cout << e.what() << endl;
-  }
-  delete c;
-  delete s;
-  delete s2;
-  return 0;
+    std::cout << "Combined string: " << combined_str << std::endl;
+
+    return 0;
 }

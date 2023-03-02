cf6efeebcbf5753ca7f13e13c99cb8ccf93c0720
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-02 22:32:23 +0300

    checking for opening

--- danilenko.ekaterina/I4/main.cpp
+++ danilenko.ekaterina/I4/main.cpp
@@ -1,11 +1,20 @@
 ﻿#include <iostream>
+#include <fstream>
 
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
   if (argc != 4)
   {
     std::cout << "Check parameters";
     return 1;
   }
+
+  std::ifstream inp_file(argv[2]);
+  if (!inp_file.is_open())
+  {
+    std::cout << "Error. File not open" << '\n';
+    return 1;
+  }
+
   return 0;
 }

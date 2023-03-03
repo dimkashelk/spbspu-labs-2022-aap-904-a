903b6b9e085de660e55221767b9d50a4284cda65
bobrovskaya <bobrovskaya.ma@edu.spbpu>
2023-03-04 00:22:52 +0300

    change const StringExtension

--- bobrovskaya.maria/I3/StringExtension.h
+++ bobrovskaya.maria/I3/StringExtension.h
@@ -2,6 +2,6 @@
 #define I3_STRINGEXTENSION_H
 #include <cstddef>
 
-char *stringExtension(char *source1, size_t &capacity, size_t size);
+char *stringExtension(const char *source1, size_t &capacity, size_t size);
 
 #endif

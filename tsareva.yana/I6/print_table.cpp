2c9d3af1e8752e1435acb69e0ed0ae6dc28821e4
Царева ЯД <tsarewaya@yandex.ru>
2023-03-03 22:52:49 +0300

    description fun printRowInTable

--- tsareva.yana/I6/print_table.cpp
+++ tsareva.yana/I6/print_table.cpp
@@ -1,9 +1,13 @@
 #include "print_table.h"
 #include <iomanip>
+#include <cmath>
+#include "arccos.h"
 
 void printRowInTable(std::ostream & out, double x, unsigned members, double absError)
 {
-
+  out << std::setw(5) << x << ' ';
+  out << std::setw(10) << std::setprecision(5) << arcCos(x, absError, members) << ' ';
+  out << std::setw(10) << std::setprecision(5) << std::acos(x) << '\n';
 }
 
 void printTable(std::ostream & out, double left_limit, double right_limit, const double step, unsigned members, const double absError)

b5e9ce03160db1272a26e7eb8c36271996dafb50
Aksenov Egor <aksenov200477@gmail.com>
2023-03-03 15:46:25 +0300

    new lab

---
+++ aksenov.egor/I4/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <cstring>
+#include "CollsWithZero.h"
+#include "calculateModulsSum.h"
+int main(int argc, char *argv[])
+{
+  if (argc != 4)
+  {
+    std::cout << "Invalid arguments" << "\n";
+    return 1;
+  }
+  std::ifstream input(argv[2]);
+  if (!input.is_open())
+  {
+    std::cout << "Input error" << "\n";
+    return 1;
+  }
+  size_t r = 0;
+  size_t c = 0;
+  input >> r >> c;
+  if (!input)
+  {
+    std::cout << "Input error" << "\n";
+    return 1;
+  }
+  std::ofstream output(argv[3]);
+  if (!output.is_open())
+  {
+    std::cout << "Output file is not opened" << "\n";
+    return 1;
+  }
+  if (std::strcmp(argv[1], "1") == 0)
+  {
+    int arr[1000];
+    if (r * c > 1000)
+    {
+      std::cout << "> 1000" << "\n";
+      return 1;
+    }
+    for (size_t i = 0; i < r * c; i++)
+    {
+      input >> arr[i];
+      if (!input)
+      {
+        std::cout << "Input error" << "\n";
+        return 1;
+      }
+    }
+    output << countColsWithZero(arr, r, c);
+  }
+  else if (std::strcmp(argv[1], "2") == 0)
+  {
+    int *matrix = new int [r * c];
+    for (size_t i = 0; i < r * c; i++)
+    {
+      input >> matrix[i];
+      if (!input)
+      {
+        std::cout << "Input error" << "\n";
+        delete[] matrix;
+        return 1;
+      }
+    }
+    int *smoothed_matrix = countSmoothedMatrix(matrix, r, c);
+    if (smoothed_matrix != nullptr)
+    {
+      output << countSum(smoothed_matrix, r);
+      delete[] smoothed_matrix;
+    }
+    delete[] matrix;
+  }
+  else
+  {
+    std::cout << "Error" << "\n";
+    return 1;
+  }
+  return 0;
+}

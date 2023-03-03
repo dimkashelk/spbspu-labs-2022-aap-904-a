162dc95d63c4766dc7dab8ab28dfc231da3bdc41
Aksenov Egor <aksenov200477@gmail.com>
2023-03-03 23:35:04 +0300

    create a logic for functions

--- aksenov.egor/I5/isFloat.cpp
+++ aksenov.egor/I5/isFloat.cpp
@@ -1,5 +1,11 @@
 #include "isFloat.h"
 #include <cctype>
+bool continueWithDigit(const char *symbol);
+bool continueWithSign(const char *symbol);
+bool continueWihtPoint(const char *symbol);
+bool continueWithE(const char *symbol);
+bool continueFirstDigitPart(const char *symbol);
+bool continueSecondDigitPart(const char *symbol);
 bool isDigit(char symbol)
 {
   return std::isdigit(symbol);
@@ -21,3 +27,33 @@ bool isE(char symbol)
   return (symbol == 'E');
 }
 
+bool continueWithDigit(const char *symbol)
+{
+ return isDigit(*symbol) && (continueWithDigit(symbol + 1) || continueWithE(symbol + 1) || continueWithDot(symbol + 1));
+}
+bool continueWithSign(const char *symbol)
+{
+  return isSign(*symbol) && continueSecondDigitPart(symbol + 1);
+}
+bool continueWihtPoint(const char *symbol)
+{
+  return isPoint(*symbol) && continueFirstDigitPart(symbol + 1);
+}
+bool continueWithE(const char *symbol)
+{
+  return isE(*symbol) && continueSecondDigitPart(symbol + 1);
+}
+bool continueFirstDigitPart(const char *symbol)
+{
+  return isDigit(*symbol) && (continueFirstDigitPart(symbol + 1) || continueWithE(symbol + 1));
+}
+bool continueSecondDigitPart(const char *symbol)
+{
+  return isDigit(*symbol) && (continueSecondDigitPart(symbol + 1) || isEnd(*(symbol + 1)));
+}
+bool isFloat(const char *expr)
+{
+  bool firstOption = isSign(*symbol) && continueWithDigit(symbol + 1);
+  bool secondOption = isDigit(*symbol) && (continueWithDigit(symbol + 1) || continueWithE(symbol + 1) || continueWithPoint(symbol + 1));
+  return firstOption || secondOption;
+}

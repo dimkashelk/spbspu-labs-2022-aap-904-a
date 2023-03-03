cc0773f5be7471ed6e00d221ad7bd49188628a15
alexg_29 <alexander.gar2901@gmail.com>
2023-03-03 20:25:52 +0300

    I3!

--- garzonnavarrete.alexander/I3/main.cpp
+++ garzonnavarrete.alexander/I3/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <string>
 
-std::string removeNonAlphaASCII(std::string str)
-{
+std::string removeNonAlphaASCII(std::string str) {
   std::string output_str = "";
-  for (char c : str)
-  {
-    if (isalpha(c) && isascii(c))
-    {
+  for (char c : str) {
+    if (isalpha(c) && isascii(c)) {
       output_str += c;
     }
   }
@@ -16,33 +13,20 @@ std::string removeNonAlphaASCII(std::string str)
 
 int main() {
   std::string input_str;
-  std::string q;
-
   std::cout << "Enter a string: " << std::endl;
   getline(std::cin, input_str);
-
-  q = "qwerrtyy";
-  if (input_str.empty())
-  {
+  if (input_str.empty()) {
     std::cerr << "Error: Input strings cannot be empty" << std::endl;
     return 1;
   }
+
   std::string output_str = removeNonAlphaASCII(input_str);
   std::cout << "Output string: " << output_str << std::endl;
+
   std::string combined_str = "";
-  size_t len = std::min(input_str.length(), p.length());
-  for (size_t i = 0; i < len; i++)
-  {
+  size_t len = input_str.length();
+  for (size_t i = 0; i < len; i++) {
     combined_str += input_str[i];
-    combined_str += p[i];
-  }
-  if (input_str.length() > len)
-  {
-    combined_str += input_str.substr(len);
-  }
-  if (p.length() > len)
-  {
-    combined_str += p.substr(len);
   }
   std::cout << "Combined string: " << combined_str << std::endl;
   return 0;

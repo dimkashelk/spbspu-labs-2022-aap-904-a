aacf1e9e5ac9efac9e31714474c400f427f62a23
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-03 20:48:12 +0300

    fix valgrind errors(7)

--- danilenko.ekaterina/I4/main.cpp
+++ danilenko.ekaterina/I4/main.cpp
@@ -16,37 +16,40 @@ int main(int argc, char* argv[])
   size_t rows = 0;
   size_t columns = 0;
   std::ifstream inp_file(argv[2]);
-  inp_file >> rows >> columns;
   if (!inp_file.is_open())
   {
     std::cout << "Error. File not open" << '\n';
     return 1;
   }
+  inp_file >> rows;
+  inp_file >> columns;
+  if (!inp_file)
+  {
+    std::cout << "Error. File not open";
+    return 1;
+  }
   std::ofstream out_file(argv[3]);
   if (!out_file.is_open())
   {
-    std::cout << "error output file could not be opened\n";
-    return 1;
-  }
-  if (rows * columns > 1000)
-  {
-    std::cout << "Error. Too much data" << '\n';
+    std::cout << "Error. Output file could not be opened";
     return 1;
   }
 
-  if (!strcmp(argv[1], "1"))
+  if (!std::strcmp(argv[1], "1"))
   {
     int matrix[1000];
-    for (size_t i = 0; i < rows; i++)
+    if (rows * columns > 1000)
     {
-      for (size_t j = 0; j < columns; j++)
+      std::cout << "Error. Too much data" << '\n';
+      return 1;
+    }
+    for (size_t i = 0; i < rows * columns; i++)
+    {
+      inp_file >> matrix[i];
+      if (!inp_file)
       {
-        inp_file >> matrix[rows * i + j];
-        if (!inp_file)
-        {
-          std::cout << "Error. File not open";
-          return 1;
-        }
+        std::cout << "Error. File not open";
+        return 1;
       }
     }
     size_t result = countColumnsWithAscElements(matrix, rows, columns);
@@ -57,14 +60,19 @@ int main(int argc, char* argv[])
       return 1;
     }
   }
-  else if (!strcmp(argv[1], "2"))
+  else if (!std::strcmp(argv[1], "2"))
   {
+    if ((rows * columns) == 0)
+    {
+      out_file << "0\n";
+      return 0;
+    }
     int* matrix2 = new int [rows * columns];
     for (size_t i = 0; i < rows; i++)
     {
       for (size_t j = 0; j < columns; j++)
       {
-        inp_file >> matrix2[i * rows + j];
+        inp_file >> matrix2[rows * i + j];
         if (!inp_file)
         {
           std::cout << "Error. File not open";

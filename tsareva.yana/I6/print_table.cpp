2f7771f81161cb74ed8beefccfc11f5228f2678e
Царева ЯД <tsarewaya@yandex.ru>
2023-03-03 21:31:37 +0300

    make fun printRowInTable

--- tsareva.yana/I6/print_table.cpp
+++ tsareva.yana/I6/print_table.cpp
@@ -1,4 +1,10 @@
 #include "print_table.h"
+
+void printRowInTable(std::ostream, double x, unsigned members, double absError)
+{
+
+}
+
 void printTable(std::ostream & out, double left_limit, double right_limit, const double step, unsigned members, const double absError)
 {
 

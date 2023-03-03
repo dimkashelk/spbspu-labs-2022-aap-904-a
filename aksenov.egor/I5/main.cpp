fd631af1a507789c71d8aade07299c02dca6235e
Aksenov Egor <aksenov200477@gmail.com>
2023-03-04 00:40:02 +0300

    fill main.cpp

--- aksenov.egor/I5/main.cpp
+++ aksenov.egor/I5/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <readString.h>
+#include "isFloat.h"
 int main()
 {
-  
+  size_t size = 0;
+  size_t increaseCapacity = 20;
+  char *cstr = new char[size];
+  try
+  {
+    cstr = readString(std::cin, size, increaseCapacity);
+  }
+  catch (const std::runtime_error &e)
+  {
+    std::cout << e.what() << "\n";
+    return 1;
+  }
+  std::cout << std::boolalpha << isFloat(cstr) << "\n";
+  delete[] cstr;
 }

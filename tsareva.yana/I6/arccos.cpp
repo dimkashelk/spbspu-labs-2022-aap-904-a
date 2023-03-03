264a61a13c5a1bf2dd2c281698f2a1cf74f6f3fa
Царева ЯД <tsarewaya@yandex.ru>
2023-03-03 21:43:35 +0300

    make variables in fun arccos

--- tsareva.yana/I6/arccos.cpp
+++ tsareva.yana/I6/arccos.cpp
@@ -2,5 +2,10 @@
 
 double arcCos(const x, double absError, unsigned max_member)
 {
-
+  const double pi = 3.14;
+  double sum = pi / 2;
+  double next = x;
+  unsigned members = 1;
+  double numerator = 0.0;
+  double denominator = 0.0;
 }

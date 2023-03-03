c70ba56cf5e8d0d2a078a0193b6053d1c43dbb2c
Царева ЯД <tsarewaya@yandex.ru>
2023-03-03 21:24:51 +0300

    description main

--- tsareva.yana/I6/main.cpp
+++ tsareva.yana/I6/main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-
+  double left_limit = 0.0;
+  double right_limit = 0.0;
+  unsigned members = 0;
+  std::cin >> left_limit >> right_limit >> members;
+  if (!std::cin || left_limit >= right_limit || left_limit < -1 || right_limit > 1)
+  {
+    std::cout << "Error... Incorrect arguments\n";
+  }
+  const double step = 0.02;
+  const double absError = 0.00001;
   return 0;
 }

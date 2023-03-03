601d015cb3db116b4889c86941de34370e067ddf
Vavericherry <vaverich3@inbox.ru>
2023-03-03 13:50:47 +0300

    Fix condition of triplet function

--- averich.vladimir/I2/istriplet.cpp
+++ averich.vladimir/I2/istriplet.cpp
@@ -8,11 +8,14 @@ int isTriplet(const int arr[], size_t size)
     throw std::length_error("Error triplet");
   }
   int countOfTriplet = 0;
-  for (size_t i = 0; i != size; i++)
+  for (size_t i = 2; i < size; i++)
   {
-    if (arr[i] > arr[i + 1] && arr[i + 1] > arr[i + 2])
+    if (arr[i] < arr[i - 1])
     {
-      countOfTriplet += 1;
+      if (arr[i - 1] < arr[i - 2])
+      {
+        countOfTriplet += 1;
+      }
     }
   }
   return countOfTriplet;

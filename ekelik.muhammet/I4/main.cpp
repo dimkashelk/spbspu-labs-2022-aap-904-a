2613eb0353dc36d8d2170039add31726edef9db6
mekel1k <mekel1k4444@gmail.com>
2023-03-03 19:48:32 +0300

    gunceleme

--- ekelik.muhammet/I4/main.cpp
+++ ekelik.muhammet/I4/main.cpp
@@ -12,40 +12,30 @@ int main(int argc, char** argv)
     std::cout << "Error!";
     return 1;
   }
-  if (!std::strcmp(argv[1], "1"))
+  
+  std::ifstream inputFile;
+  inputFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+  
+  try
   {
-    std::ifstream inputFile;
-    inputFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
-    try
-    {
-      inputFile.open(argv[2]);
-    }
-    catch (const std::exception& e)
-    {
-      std::cout << e.what() << "\n";
-      return 1;
-    }
-    int staticMatrix[1000];
+    inputFile.open(argv[2]);
+
     size_t numRows = 0, numCols = 0;
-    try
+    int* matrix = nullptr;
+    bool use_static_matrix = std::strcmp(argv[1], "1") == 0;
+
+    if (use_static_matrix)
     {
-      inputFile >> numRows;
-      inputFile >> numCols;
-    }
-    catch (const std::exception& e)
-    {
-      inputFile.close();
-      std::cout << e.what() << "\n";
-      return 1;
-    }
-    if (numRows * numCols > 1000)
-    {
-      inputFile.close();
-      std::cout << numRows * numCols << " > " << 1000 << "\n";
-      return 1;
-    }
-    try
-    {
+      int staticMatrix[1000];
+      inputFile >> numRows >> numCols;
+
+      if (numRows * numCols > 1000)
+      {
+        inputFile.close();
+        std::cout << numRows * numCols << " > " << 1000 << "\n";
+        return 1;
+      }
+
       for (size_t i = 0; i < numRows; i++)
       {
         for (size_t j = 0; j < numCols; j++)
@@ -53,87 +43,40 @@ int main(int argc, char** argv)
           inputFile >> staticMatrix[numRows * i + j];
         }
       }
+
+      matrix = staticMatrix;
     }
-    catch (const std::exception& e)
+    else
     {
-      inputFile.close();
-      std::cout << e.what() << "\n";
-      return 1;
-    }
-    inputFile.close();
-    std::ofstream outputFile;
-    outputFile.exceptions(std::ofstream::badbit | std::ofstream::failbit);
-    try
-    {
-      outputFile.open(argv[3]);
-    }
-    catch (const std::exception& e)
-    {
-      std::cout << e.what() << "\n";
-      return 1;
-    }
-    outputFile << countRowsWithEqualSum(staticMatrix, numRows, numCols) << "\n";
-    outputFile << findLongestSeries(staticMatrix, numRows, numCols) << "\n";
-    outputFile.close();
-    return 0;
-  }
-  else if (!std::strcmp(argv[1], "2"))
-  {
-    std::ifstream inputFile;
-    inputFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
-    try
-    {
-      inputFile.open(argv[2]);
-    }
-    catch (const std::exception& e)
-    {
-      std::cout << e.what() << "\n";
-      return 1;
-    }
-    size_t numRows = 0, numCols = 0;
-    int* dynamicMatrix = nullptr;
-    try
-    {
-      inputFile >> numRows;
-      inputFile >> numCols;
-      dynamicMatrix = new int[numRows * numCols];
+      inputFile >> numRows >> numCols;
+      matrix = new int[numRows * numCols];
+
       for (size_t i = 0; i < numRows; i++)
       {
         for (size_t j = 0; j < numCols; j++)
         {
-          inputFile >> dynamicMatrix[numRows * i + j];
+          inputFile >> matrix[numRows * i + j];
         }
       }
     }
-    catch (const std::exception& e)
-    {
-      delete[] dynamicMatrix;
-      inputFile.close();
-      std::cout << e.what() << "\n";
-      return 1;
-    }
+
     inputFile.close();
+
     std::ofstream outputFile;
     outputFile.exceptions(std::ofstream::badbit | std::ofstream::failbit);
-    try
-    {
-      outputFile.open(argv[3]);
-    }
-    catch (const std::exception& e)
-    {
-      delete[] dynamicMatrix;
-      std::cout << e.what() << "\n";
-      return 1;
-    }
-    outputFile << countRowsWithEqualSum(dynamicMatrix, numRows, numCols) << "\n";
-    outputFile << findLongestSeries(dynamicMatrix, numRows, numCols) << "\n";
+    outputFile.open(argv[3]);
+
+    outputFile << countRowsWithEqualSum(matrix, numRows, numCols) << "\n";
+    outputFile << findLongestSeries(matrix, numRows, numCols) << "\n";
     outputFile.close();
-    delete[] dynamicMatrix;
+
+    if (!use_static_matrix) delete[] matrix;
+
     return 0;
   }
-  else
+  catch (const std::exception& e)
   {
-    std::cout << "Error!";
+    std::cout << e.what() << "\n";
     return 1;
   }
 }

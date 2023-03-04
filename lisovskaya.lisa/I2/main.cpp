e967c60985d2c485f7e5bff78b3b871013398af9
Lisa Lisovskaya <minstone1993@gmail.com>
2023-03-05 02:49:22 +0300

    acceptance error fixed

--- lisovskaya.lisa/I2/main.cpp
+++ lisovskaya.lisa/I2/main.cpp
@@ -8,11 +8,6 @@
 #include "readArray.h"
 int main(int argc, char *argv[])
 {
-  if (argc > 2)
-  {
-    std::cerr << "Not correct amount of parametrs" << "\n";
-    return 1;
-  }
   std::srand(std::time(nullptr));
   int arr1[] = {2, 5, 3, 4, 6, 7, 1};
   std::cout << counterBeforeMin(arr1, 7) << std::endl;
@@ -25,41 +20,49 @@ int main(int argc, char *argv[])
     return 2;
   }
   int *array2 = new int[n];
-  array2 = generateRand(array2, n);
-  try
+  if (n > 0)
   {
-    std::cout << counterBeforeMin(array2, n) << "\n";
-    std::cout << findArithmeticMean(array2, n) << "\n";
+    array2 = generateRand(array2, n);
+    try
+    {
+      std::cout << counterBeforeMin(array2, n) << "\n";
+      std::cout << findArithmeticMean(array2, n) << "\n";
+      delete[] array2;
+    }
+    catch (const std::runtime_error &e)
+    {
+      std::cout << 0 << "\n" << 0;
+      delete[] array2;
+      return 2;
+    }
   }
-  catch (const std::runtime_error &e)
+  else
   {
-    std::cout << 0 << "\n" << 0;
+    std::cout << "Dynamic array error" << "\n";
     delete[] array2;
-    return 2;
   }
-  delete[] array2;
+  if (argc != 2)
+  {
+    std::cerr << "Not correct amount of parametrs" << "\n";
+    return 1;
+  }
   int size = 0;
   std::ifstream in(argv[1]);
   in >> size;
   if (!in)
   {
     std::cerr << "error reading file" << "\n";
-    return 2;
+    return 1;
   }
   if (size > 0)
   {
     int *array3 = new int[size];
+    array3 = readArray(in, size, array3);
     try
     {
-      array3 = readArray(in, size, array3);
       std::cout << counterBeforeMin(array3, size) << "\n";
       std::cout << findArithmeticMean(array3, size) << "\n";
-    }
-    catch (const std::runtime_error &e)
-    {
-      std::cout << e.what();
       delete[] array3;
-      return 2;
     }
     catch (const std::length_error &e)
     {

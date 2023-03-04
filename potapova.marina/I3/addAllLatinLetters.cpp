c64b30482e2122f77cc1c3e059c79000bfe90e84
Потапова <marina-potapova04@mail.ru>
2023-03-04 15:09:17 +0300

    CG

--- potapova.marina/I3/addAllLatinLetters.cpp
+++ potapova.marina/I3/addAllLatinLetters.cpp
@@ -13,18 +13,20 @@ bool isInStr(const char* str, const char c)
   }
   return false;
 }
+
 void strSort(char* str)
 {
   for(char* cur_sym_ptr = str + 1; *cur_sym_ptr != '\0'; ++cur_sym_ptr)
-	{
-		char* before_sym_ptr = cur_sym_ptr - 1;
-		while(before_sym_ptr >= str && *before_sym_ptr > *(before_sym_ptr + 1))
-		{
-			std::swap(*before_sym_ptr, *(before_sym_ptr + 1));
-			--before_sym_ptr;
-		}
-	}
+  {
+    char* before_sym_ptr = cur_sym_ptr - 1;
+    while(before_sym_ptr >= str && *before_sym_ptr > *(before_sym_ptr + 1))
+    {
+      std::swap(*before_sym_ptr, *(before_sym_ptr + 1));
+      --before_sym_ptr;
+    }
+  }
 }
+
 char* addLatinLetsFromStr(const char* const all_latin_lets, char* all_latin_lets_end_ptr, const char* str)
 {
   for (; *str != '\0'; ++str)

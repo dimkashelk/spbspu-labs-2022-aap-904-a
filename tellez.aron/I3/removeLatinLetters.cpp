c010f97923e722a99be79afbcc47d42ed3d06ae2
aront14 <arontellezc14@gmail.com>
2023-03-04 16:19:28 +0300

    fixing

--- tellez.aron/I3/removeLatinLetters.cpp
+++ tellez.aron/I3/removeLatinLetters.cpp
@@ -2,11 +2,8 @@
 #include <cctype>
 char* removeLatinLetters(char *destination, const char *source)
 {
-<<<<<<< HEAD
   char *src = const_cast< char* >(source);
-=======
   const char *src = source;
->>>>>>> master
   char *dst = destination;
   while (*src != '\0')
   {

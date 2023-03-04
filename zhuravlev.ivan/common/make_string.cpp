ce0c834566696409d8ba3d0d1f2a6008bf580939
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-03-04 14:20:07 +0300

    added file with realisation with fundament

---
+++ zhuravlev.ivan/common/make_string.cpp
@@ -0,0 +1,12 @@
+#include "make_string.h"
+#include <iostream>
+#include <stdexcept>
+
+char* makeCstring(std::istream& input)
+{
+  size_t capacity = 10;
+  size_t new_capacity = 0;
+  char* cstring = new char[capacity];
+  size_t size = 0;
+  return cstring;
+}

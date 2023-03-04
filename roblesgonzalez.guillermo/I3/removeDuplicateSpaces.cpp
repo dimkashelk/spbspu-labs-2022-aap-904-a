72215ae1b5d72278b7644edf38790fcc9cd0538a
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-04 10:17:52 +0300

    Lab 3 Possible Fix

--- roblesgonzalez.guillermo/I3/removeDuplicateSpaces.cpp
+++ roblesgonzalez.guillermo/I3/removeDuplicateSpaces.cpp
@@ -1,38 +1,39 @@
 #include "removeDuplicateSpaces.h"
 #include <cctype>
-char* removeDuplicateSpaces2(char *destination, const char *source)
+
+char* removeDuplicateSpaces2(char* destination, const char* source)
 {
   size_t size = 0;
   size_t d = 0;
-  bool last_char_was_space = false;
+
   while (std::isspace(source[d]))
   {
     d++;
   }
+
   while (source[d])
   {
     if (std::isspace(source[d]))
     {
-      if (!last_char_was_space)
+      if (d > 0 && !std::isspace(source[d - 1]))
       {
         destination[size] = ' ';
         size++;
       }
-      last_char_was_space = true;
       d++;
     }
     else
     {
       destination[size] = source[d];
       size++;
-      last_char_was_space = false;
-      d++;
     }
+    d++;
   }
-  if (size > 0 && std::isspace(destination[size - 1]))
+  while (size > 0 && std::isspace(destination[size - 1]))
   {
     size--;
   }
+
   destination[size] = '\0';
   return destination;
 }

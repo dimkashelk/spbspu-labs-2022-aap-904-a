d08577851eb505a99bb4d2a4a31a32b0b6ce1169
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-04 18:12:52 +0300

    .

---
+++ roblesgonzalez.guillermo/I3/removeDigits.cpp
@@ -0,0 +1,21 @@
+#include "removeDigits.h"
+#include <cctype>
+size_t removeDigits(char * destination, const char * source)
+{
+  if (destination == nullptr || source == nullptr)
+  {
+    return -1;
+  }
+  char *result = destination;
+  while (*source != '\0')
+  {
+    if (!std::isdigit(*source))
+    {
+      *destination = *source;
+      destination++;
+    }
+    source++;
+  }
+  *destination = '\0';
+  return (destination - result);
+}

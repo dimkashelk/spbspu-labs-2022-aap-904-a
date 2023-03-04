56d0a72cef3fbc971cf81744cd26cdeb714b7c08
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-03-04 19:30:32 +0300

    fixing tests

--- zhuravlev.ivan/I3/main.cpp
+++ zhuravlev.ivan/I3/main.cpp
@@ -3,49 +3,14 @@
 #include <cstring>
 #include "finder_three_same_elements.h"
 #include "finder_same_elements_in_two_string.h"
-#include "string_expansion.h"
+#include </Users/ivanzhuravlev/CLionProjects/spbspu-labs-2022-aap-904-a/zhuravlev.ivan/common/makeString.h>
 
 int main()
 {
-  size_t capacity = 10;
-  size_t new_capacity = 0;
-  char* cstring = new char[capacity];
-  size_t size = 0;
+  char* cstring = makeCstring(std::cin);
+  size_t size = sizeof(cstring);
   size_t size_of_static = 18;
   const char* static_string = "ABCDFEabcdef135790";
-  cstring[0] = '\0';
-  std::cin >> std::noskipws;
-  do
-  {
-    if (size + 1 == capacity)
-    {
-      try
-      {
-        cstring[size] = '\0';
-        new_capacity += 20;
-        char* new_str = nullptr;
-        char* prev_cstring = cstring;
-        new_str = stringExpansion(cstring, capacity, new_capacity);
-        delete[] prev_cstring;
-        cstring = new_str;
-        capacity += new_capacity;
-      }
-      catch (const std::bad_alloc& e)
-      {
-        delete[] cstring;
-        return 1;
-      }
-    }
-    std::cin >> cstring[size];
-  }
-  while (std::cin && cstring[size++] != '\n');
-  if (cstring[0] == '\n' || size == 0)
-  {
-    delete[] cstring;
-    std::cerr << "Empty string!" << "\n";
-    return 2;
-  }
-  cstring[size - 1] = '\0';
   try
   {
     char* destination = new char[size + size_of_static];
--- zhuravlev.ivan/I3/string_expansion.cpp
+++
@@ -1,9 +0,0 @@
-#include "string_expansion.h"
-#include <cstring>
-
-char* stringExpansion(const char* str, const size_t capacity, const size_t new_capacity)
-{
-  char* new_str = new char[capacity + new_capacity];
-  std::strcpy(new_str, str);
-  return new_str;
-}

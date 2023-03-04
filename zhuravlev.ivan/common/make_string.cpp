b8bcc6035d1af389a3385d0ec5f15530441fc6b1
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-03-04 14:29:06 +0300

    added full realisation of make_string.cpp

--- zhuravlev.ivan/common/make_string.cpp
+++ zhuravlev.ivan/common/make_string.cpp
@@ -8,5 +8,37 @@ char* makeCstring(std::istream& input)
   size_t new_capacity = 0;
   char* cstring = new char[capacity];
   size_t size = 0;
+  cstring[0] = '\0';
+  input >> std::noskipws;
+  do
+  {
+    if (size + 1 == capacity)
+    {
+      try
+      {
+        cstring[size] = '\0';
+        new_capacity += 20;
+        char* new_str = nullptr;
+        char* prev_cstring = cstring;
+        new_str = stringExpansion(cstring, capacity, new_capacity);
+        delete[] prev_cstring;
+        cstring = new_str;
+        capacity += new_capacity;
+      }
+      catch (const std::bad_alloc& e)
+      {
+        delete[] cstring;
+        throw;
+      }
+      input >> cstring[size];
+    }
+  }
+  while (std::cin && cstring[size++] != '\n');
+  if (cstring[0] == '\n' || size == 0)
+  {
+    delete[] cstring;
+    throw std::logic_error("String is empty");
+  }
+  cstring[size - 1] = '\0';
   return cstring;
 }

bfa8fbff26843716ab92887b32b49418b9bc3229
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-04 18:17:16 +0300

    ..

---
+++ roblesgonzalez.guillermo/I3/removeDuplicateSpaces.cpp
@@ -0,0 +1,35 @@
+#include "removeDuplicateSpaces.h"
+#include <cctype>
+char* removeDuplicateSpaces2(char* destination, const char* source)
+{
+  size_t size = 0;
+  size_t d = 0;
+  while (std::isspace(source[d]))
+  {
+    d++;
+  }
+  while (source[d])
+  {
+    if (std::isspace(source[d]))
+    {
+      if (d > 0 && !std::isspace(source[d - 1]))
+      {
+        destination[size] = ' ';
+        size++;
+      }
+      d++;
+    }
+    else
+    {
+      destination[size] = source[d];
+      size++;
+    }
+    d++;
+  }
+  while (size > 0 && std::isspace(destination[size - 1]))
+  {
+    size--;
+  }
+  destination[size] = '\0';
+  return destination;
+}

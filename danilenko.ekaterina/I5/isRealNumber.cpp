3399d571c13b1339ce67b6c7b4d9a98fac8d8611
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-06 00:26:56 +0300

    fix valgrind (2)

--- danilenko.ekaterina/I5/isRealNumber.cpp
+++ danilenko.ekaterina/I5/isRealNumber.cpp
@@ -34,7 +34,7 @@ bool isOrder(const char* data)
 
 bool isUnsignInteger(const char* data)
 {
-  return (isDigit(*data) && isEnd(*data)) || (isDigit(*data) && (isUnsignInteger(data + 1) || isOrder(data + 1) || isEnd(*(data + 1))));
+  return isDigit(*data) || (isDigit(*data) && (isUnsignInteger(data + 1) || isOrder(data + 1) || isEnd(*(data + 1))));
 }
 
 bool isMantissa(const char* data)

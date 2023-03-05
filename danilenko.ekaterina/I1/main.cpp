5e0760bd403e7c7c3bb960ad49f708e770bee347
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-05 16:48:33 +0300

    ....

--- danilenko.ekaterina/I1/main.cpp
+++ danilenko.ekaterina/I1/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include "QuantityLocalMin.h"
 #include "QuantitySignChange.h"
 
--- danilenko.ekaterina/S0/main.cpp
+++ danilenko.ekaterina/S0/main.cpp
@@ -4,4 +4,3 @@ int main()
 {
   std::cout << "danilenko.ekaterina\n";
 }
-

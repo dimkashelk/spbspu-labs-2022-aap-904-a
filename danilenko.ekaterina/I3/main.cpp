9b17fac4f758f95d0c4552b33c2c8bd884093115
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-05 16:05:38 +0300

    .

--- danilenko.ekaterina/I3/main.cpp
+++ danilenko.ekaterina/I3/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cstddef>
 #include <cstring>
 #include "makeNewStringWithoutRepeat.h"
--- danilenko.ekaterina/I4/main.cpp
+++ danilenko.ekaterina/I4/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cstddef>
 #include <fstream>
 #include <cstring>

0692d7cb7064e7e08ab215cbc7125734e5b6316d
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-05 23:46:15 +0300

    fix build (1)

--- danilenko.ekaterina/I3/createStringFromInput.h
+++
@@ -1,7 +0,0 @@
-#ifndef CREATESTRINGFROMINPUT_H
-#define CREATESTRINGFROMINPUT_H
-#include <istream>
-
-char* createStringFromInput(std::istream& input);
-
-#endif
--- danilenko.ekaterina/I3/makeStringExtention.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAKESTRINGEXTENTION_H
-#define MAKESTRINGEXTENTION_H
-#include <cstddef>
-
-char* makeStringExtention(const char* source1, size_t capacity, size_t size, size_t capacity_extention);
-
-#endif

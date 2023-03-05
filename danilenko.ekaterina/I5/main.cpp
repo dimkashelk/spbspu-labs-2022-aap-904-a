9118feafa49e2359fbab8d94b135147d4bee250f
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-05 23:37:58 +0300

    make main file

---
+++ danilenko.ekaterina/I5/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstddef>
+#include <stdexcept>
+#include <createStringFromInput.h>
+#include "isRealNumber.h"
+
+int main()
+{
+  char* string = nullptr;
+  std::cout << "Enter string: ";
+  try
+  {
+    string = createStringFromInput(std::cin);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what() << "\n";
+    delete[] string;
+    return 1;
+  }
+  std::cout << std::boolalpha << isRealNumber(string) << '\n';
+  delete[] string;
+  return 0;
+}

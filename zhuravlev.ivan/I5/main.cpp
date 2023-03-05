d7a199a4c17294a33b48e83976ccd7181ed02e2b
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-03-05 14:08:00 +0300

    fixing lab i3 and i5

--- zhuravlev.ivan/I5/main.cpp
+++ zhuravlev.ivan/I5/main.cpp
@@ -11,7 +11,7 @@ int main()
   }
   catch (const std::exception& e)
   {
-    std::cerr << e.what();
+    std::cout << e.what();
     return 1;
   }
   std::cout << std::boolalpha << isFloat(cstring) << "\n";

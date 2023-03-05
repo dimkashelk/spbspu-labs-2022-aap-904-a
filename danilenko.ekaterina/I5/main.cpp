9879c8db79c3bb5b0ebe58cda92f2e47beb8285b
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-06 01:17:50 +0300

    fix valgrind (7)

--- danilenko.ekaterina/I5/main.cpp
+++ danilenko.ekaterina/I5/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <cstddef>
 #include <stdexcept>
-#include <createStringFromInput.h>
+#include "createStringFromInput.h"
 #include "isRealNumber.h"
 
 int main()
@@ -15,7 +15,6 @@ int main()
   catch (const std::exception& e)
   {
     std::cout << e.what() << "\n";
-    delete[] string;
     return 1;
   }
   std::cout << std::boolalpha << isRealNumber(string) << '\n';

f558f2cae2581c989ac3ac8e285da06e94604779
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-05 23:38:51 +0300

    make func und header with real number recursion

---
+++ danilenko.ekaterina/I5/isRealNumber.cpp
@@ -0,0 +1,48 @@
+#include "isRealNumber.h"
+#include <iomanip>
+
+bool isUnsignInteger(const char* data);
+bool isEnd(char s)
+{
+  return s == '\0';
+}
+
+bool isSign(char s)
+{
+  return s == '+' || s == '-';
+}
+
+bool isDot(char s)
+{
+  return s == '.';
+}
+
+bool isE(char s)
+{
+  return s == 'E';
+}
+
+bool isDigit(char s)
+{
+  return std::isdigit(s);
+}
+
+bool isOrder(const char* data)
+{
+  return (isE(*data) && isSign(*(data + 1)) && isUnsignInteger(data + 2)) || (isE(*data) && isUnsignInteger(data + 1));
+}
+
+bool isUnsignInteger(const char* data)
+{
+  return (isDigit(*data) && isEnd(*data)) || (isDigit(*data) && (isUnsignInteger(data + 1) || isOrder(data + 1) || isEnd(*(data + 1))));
+}
+
+bool isMantissa(const char* data)
+{
+  return (isDot(*data) && isUnsignInteger(data + 1)) || isUnsignInteger(data);
+}
+
+bool isRealNumber(const char* data)
+{
+  return isEnd(*data) || isMantissa(data) || (isSign(*data) && isMantissa(data + 1));
+}

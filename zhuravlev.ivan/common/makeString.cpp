4032136559c7e3f60e89c1cc29b1bef0b2e75706
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-03-06 15:03:59 +0300

    fixing build fail

--- zhuravlev.ivan/common/makeString.cpp
+++ zhuravlev.ivan/common/makeString.cpp
@@ -36,7 +36,7 @@ char* makeCstring(size_t& capacity, size_t& size, std::istream& input)
   if (cstring[0] == '\n' || size == 0)
   {
     delete[] cstring;
-    throw std::logic_error('Empty string');
+    std::cerr << "String is empty\n";
   }
   cstring[size - 1] = '\0';
   return cstring;

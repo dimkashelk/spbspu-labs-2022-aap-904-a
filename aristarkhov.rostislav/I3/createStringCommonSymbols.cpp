c4f40866d824ca44689e60e1743efb49dbb54445
Aristarkhov Rostislav <aristarhov.ro@edu.spbstu.ru>
2023-03-06 16:55:27 +0300

    fix build try1

--- aristarkhov.rostislav/I3/createStringCommonSymbols.cpp
+++ aristarkhov.rostislav/I3/createStringCommonSymbols.cpp
@@ -1,7 +1,8 @@
 #include "createStringCommonSymbols.h"
 #include <cctype>
+#include <cstddef>
 
-size_t defineCountCommonSymbols(const char* source, const char p)
+size_t defineNumberCommonSymbols(const char* source, const char p)
 {
   size_t count = 0;
   for (size_t i = 0; source[i] != '\0'; i++)
@@ -19,9 +20,9 @@ char* createStringCommonSymbols(char* destination2, const char* source_1, const 
   size_t res = 0;
   for (size_t i = 0; source_1[i] != '\0'; i++)
   {
-    if (defineCountCommonSymbols(source_1 + i + 1, source_1[i]) == 0)
+    if (defineNumberCommonSymbols(source_1 + i + 1, source_1[i]) == 0)
     {
-      if (defineCountCommonSymbols(source_2, source_1[i]) > 0)
+      if (defineNumberCommonSymbols(source_2, source_1[i]) > 0)
       {
         destination2[res++] = source_1[i];
       }

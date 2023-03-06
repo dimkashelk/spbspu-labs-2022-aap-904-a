1d9031d0d7b78717f0d85b659e54d06f23be8367
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-06 16:40:28 +0300

    fix build

--- danilenko.ekaterina/I3/main.cpp
+++ danilenko.ekaterina/I3/main.cpp
@@ -10,12 +10,12 @@ int main()
   size_t capacity = 10;
   size_t size = 0;
   size_t new_capacity = capacity + 20;
-  char* source1 = nullptr;
+  char* string = nullptr;
   std::cout << "Enter string: ";
 
   try
   {
-    source1 = createStringFromInput(std::cin);
+    string = createStringFromInput(std::cin, string, size, capacity);
     if (size > new_capacity)
     {
       throw std::length_error("Long input string");
@@ -23,26 +23,26 @@ int main()
   }
   catch (const std::exception& e)
   {
-    delete[] source1;
+    delete[] string;
     std::cout << e.what() << "\n";
     return 1;
   }
 
   //1
   char source2[] = "abcd";
-  size_t size1 = std::strlen(source1);
-  size_t size2 = std::strlen(source1);
+  size_t size1 = std::strlen(string);
+  size_t size2 = std::strlen(source2);
   size_t string_size1 = size1 + size2 + 1;
   char* destination1 = nullptr;
   try
   {
     destination1 = new char[string_size1 + 20];
-    destination1 = makeNewStringWithoutRepeat(destination1, source1, source2);
+    destination1 = makeNewStringWithoutRepeat(destination1, string, source2);
   }
   catch (const std::bad_alloc& e)
   {
     delete[] destination1;
-    delete[] source1;
+    delete[] string;
     std::cout << e.what() << '\n';
     return 1;
   }
@@ -53,12 +53,12 @@ int main()
   try
   {
     destination2 = new char[26];
-    destination2 = makeNewStringWithMissingLetters(destination2, source1);
+    destination2 = makeNewStringWithMissingLetters(destination2, string);
   }
   catch (const std::bad_alloc& e)
   {
     delete[] destination2;
-    delete[] source1;
+    delete[] string;
     std::cout << e.what() << '\n';
     return 1;
   }
@@ -66,6 +66,6 @@ int main()
 
   delete[] destination1;
   delete[] destination2;
-  delete[] source1;
+  delete[] string;
   return 0;
 }

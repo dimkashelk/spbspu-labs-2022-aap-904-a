db5040d4d1fcb523697a380ccca3d6cfee095e01
mekel1k <mekel1k4444@gmail.com>
2023-03-06 20:46:30 +0300

    :/

--- ekelik.muhammet/I4/readMatrix.cpp
+++ ekelik.muhammet/I4/readMatrix.cpp
@@ -6,7 +6,10 @@ void readMatrix(std::ifstream& inputFile, int* matrix, size_t numRows, size_t nu
   {
     for (size_t j = 0; j < numCols; j++)
     {
-      inputFile >> matrix[numCols * i + j];
+      if (!(inputFile >> *(matrix + i * numCols + j)))
+      {
+        throw std::runtime_error("Error: Failed to read matrix element!");
+      }
     }
   }
 }

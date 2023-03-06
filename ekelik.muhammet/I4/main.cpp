efbe272045271a073f7359300a4616468bbb8117
mekel1k <mekel1k4444@gmail.com>
2023-03-06 20:35:14 +0300

    fixed2

--- ekelik.muhammet/I4/main.cpp
+++ ekelik.muhammet/I4/main.cpp
@@ -80,7 +80,7 @@ int main(int argc, char** argv)
     try
     {
       inputFile >> numRows >> numCols;
-      if (numRows == 0 || numCols == 0 || numRows * numCols > 1000)
+      if (numRows == 0 || numCols == 0)
       {
         inputFile.close();
         std::cout << "Error: Invalid matrix dimensions!\n";

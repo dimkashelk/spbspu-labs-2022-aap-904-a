da28870d86d48d1467977efc9a9ceee42f7b9039
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-08 02:33:16 +0300

    Lab 4

--- roblesgonzalez.guillermo/I4/main.cpp
+++ roblesgonzalez.guillermo/I4/main.cpp
@@ -8,11 +8,13 @@
 void readArray(int* arr, size_t rows, size_t cols, std::ifstream& inFile) {
     for (size_t i = 0; i < rows * cols; i++) {
         if (!(inFile >> arr[i])) {
-            throw std::runtime_error("Error: File read error");
+            delete[] arr;
+            throw std::ios_base::failure("Error: File read error");
         }
     }
-    delete[] arr;
 }
+
+
 int main(int argc, char* argv[]) {
     try {
         if (argc != 4) {
@@ -20,22 +22,22 @@ int main(int argc, char* argv[]) {
         }
         std::ifstream inFile(argv[2]);
         if (!inFile.is_open()) {
-            throw std::runtime_error("Error: Input file could not be opened");
+            throw std::ios_base::failure("Error: Input file could not be opened");
         }
         std::ofstream outFile(argv[3]);
         if (!outFile.is_open()) {
-            throw std::runtime_error("Error: Output file could not be opened");
+            throw std::ios_base::failure("Error: Output file could not be opened");
         }
         size_t rows = 0;
         size_t cols = 0;
         inFile >> rows >> cols;
         if (!inFile) {
-            throw std::runtime_error("Error: File read error");
+            throw std::ios_base::failure("Error: File read error");
         }
         if (rows * cols > 1000) {
             throw std::invalid_argument("Error: Array too large");
         }
-        int* arr = new int[rows * cols];
+        int *arr = new int[rows * cols];
         if (std::strcmp(argv[1], "1") == 0) {
             readArray(arr, rows, cols, inFile);
             outFile << countGrowingCols(arr, rows, cols) << "\n";
@@ -49,9 +51,8 @@ int main(int argc, char* argv[]) {
         } else if (std::strcmp(argv[1], "3") == 0) {
             char arr[1000];
             for (size_t i = 0; i < rows * cols; i++) {
-                inFile.exceptions(std::ios::failbit);
                 if (!(inFile >> arr[i])) {
-                    throw std::runtime_error("Error: File read error");
+                    throw std::ios_base::failure("Error: File read error");
                 }
             }
         } else {
@@ -60,11 +61,12 @@ int main(int argc, char* argv[]) {
         }
         delete[] arr;
         if (!outFile) {
-            throw std::runtime_error("Error: File write error");
+            throw std::ios_base::failure("Error: File write error");
         }
-    } catch (const std::exception& ex) {
+    } catch (const std::exception &ex) {
         std::cout << ex.what() << "\n";
         return 1;
     }
-    return 0;
+
+
 }

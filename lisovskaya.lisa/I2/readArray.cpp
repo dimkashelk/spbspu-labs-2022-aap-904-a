99f4a755db304fd28b2bb3fd8891c912cd632f9e
Lisa Lisovskaya <minstone1993@gmail.com>
2023-03-07 11:00:07 +0300

    flags for the stream fixed

--- lisovskaya.lisa/I2/readArray.cpp
+++ lisovskaya.lisa/I2/readArray.cpp
@@ -5,15 +5,7 @@ std::istream & readArray(std::istream & in, size_t & line, int *array3)
   for (size_t i = 0; i < line; ++i)
   {
     in >> array3[i];
-    if (in.bad())
-    {
-      return in;
-    }
-    if (in.eof())
-    {
-      return in;
-    }
-    if (in.fail())
+    if (!in)
     {
       return in;
     }

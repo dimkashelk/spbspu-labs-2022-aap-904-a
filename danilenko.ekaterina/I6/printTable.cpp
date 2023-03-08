0ced92de429595b108c4cbfa8613745b25202048
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-08 20:48:52 +0300

    drafts for the table output func

---
+++ danilenko.ekaterina/I6/printTable.cpp
@@ -0,0 +1,12 @@
+#include "printTable.h"
+#include <ostream>
+
+void printTableRow(std::ostream& out, double x, double error, unsigned k)
+{
+  
+}
+
+void printTable(std::ostream& out, double l, double r, double s, double error, unsigned k)
+{
+
+}
---
+++ danilenko.ekaterina/I6/printTable.h
@@ -0,0 +1,6 @@
+#ifndef PRINTTABLE_H
+#define PRINTTABLE_H
+
+void printTable(std::ostream& out, double l, double r, double s, double error, unsigned k);
+
+#endif

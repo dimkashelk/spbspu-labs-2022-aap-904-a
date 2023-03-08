bf78867bdb0d740b62152654fc3f73de3e7767df
alexg_29 <alexander.gar2901@gmail.com>
2023-03-08 22:36:07 +0300

    Lab Despacito!

--- garzonnavarrete.alexander/I3/StringPyQ.cpp
+++ garzonnavarrete.alexander/I3/StringPyQ.cpp
@@ -3,29 +3,18 @@
 
 char *getSumOfStrings(char *arr_sum, const char *cstring_p, const char *cstring_q)
 {
-  size_t size_sum = 0;
-  for (size_t i = 0; *cstring_p || *cstring_q; i++)
+  char *result = arr_sum;
+  while (*cstring_p || *cstring_q)
   {
-    if ((i % 2 == 0 && *cstring_p) || !(*cstring_q))
+    if (*cstring_p)
     {
-      arr_sum[i] = *cstring_p;
-      size_sum++;
-      if (!(*cstring_p))
-      {
-        cstring_q++;
-      }
-      else
-      {
-        cstring_p++;
-      }
+      *result++ = *cstring_p++;
     }
-    else if ((i % 2 != 0 && *cstring_q) || !(*cstring_p))
+    if (*cstring_q)
     {
-      arr_sum[i] = *cstring_q;
-      size_sum++;
-      cstring_q++;
+      *result++ = *cstring_q++;
     }
   }
-  arr_sum[size_sum] = '\0';
+  *result = '\0';
   return arr_sum;
 }

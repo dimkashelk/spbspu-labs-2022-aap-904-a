1f692be09f17a3a513dc4290a8cfdc8e6b7f501f
bobrovskaya <bobrovskaya.ma@edu.spbpu>
2023-03-08 18:08:55 +0300

    fixed build error

--- bobrovskaya.maria/I6/printTable.cpp
+++ bobrovskaya.maria/I6/printTable.cpp
@@ -7,7 +7,7 @@ out << std::setw(5) << x << ' ';
 out << std::setw(10) << std::setprecision(5) << exponenta(x, absError, numberMax) << ' ';
 out << std::setw(10) << std::setprecision(5) << std::exp(x * (-1)) <<  '\n';
 }
-void printTable(std::ostream& out, double left, double right, double step, double absError, unsigned numberMax)
+void printTable(std::ostream &out, double left, double right, double step, double absError, unsigned numberMax)
 {
   for (double x = left; x + step <= right; x += step)
   {

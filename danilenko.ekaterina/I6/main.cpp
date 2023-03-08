a481ec244ea12dfa51db4abe71c910090f8fbf2b
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-08 21:51:16 +0300

    update main func with the addition of input data and table output

--- danilenko.ekaterina/I6/main.cpp
+++ danilenko.ekaterina/I6/main.cpp
@@ -1,20 +1,41 @@
-﻿// I6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include "printTable.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  double left = 0.0;
+  double right = 0.0;
+  size_t numberMax = 0;
+  const double absError = 0.00000001;//??????
+  const double k = 0.1;
+
+  std::cout << "Enter left and right border: ";
+  std::cin >> left >> right;
+  if (!std::cin)
+  {
+    std::cerr << "\nError!\n";
+    return 1;
+  }
+  if (left >= right)
+  {
+    std::cerr << "\nError! Incorrect interval\n";
+    return 1;
+  }
+  std::cout << "Enter the maximum number of variables: ";
+  std::cin >> numberMax;
+  if (!std::cin)
+  {
+    std::cerr << "\nError!\n";
+    return 1;
+  }
+  try
+  {
+    printTable(std::cout, left, right, numberMax, absError, k);
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
+  return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.

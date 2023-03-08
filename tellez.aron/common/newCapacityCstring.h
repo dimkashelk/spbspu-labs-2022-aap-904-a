46eb3db41efbdf8eb699449b75d5eb6f2408dc2e
aront14 <arontellezc14@gmail.com>
2023-03-08 17:16:19 +0300

    common folder created3.0

--- tellez.aron/common/newCapacityCstring.h
+++ tellez.aron/common/newCapacityCstring.h
@@ -1,5 +1,5 @@
 #ifndef NEWCAPACITYCSTRING_H
 #define NEWCAPACITYCSTRING_H
 #include <cstddef>
-char* makeNewCapacityCString(char* cstring, size_t& capacity, size_t size)
+char* makeNewCapacityCString(char* cstring, size_t& capacity, size_t size);
 #endif

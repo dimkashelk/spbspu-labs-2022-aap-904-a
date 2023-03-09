c8e1e892df066293e12799ecd461f8f1b7383664
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-03-09 23:32:40 +0300

    second func completed

--- samoilenko.alena/I4/main.cpp
+++ samoilenko.alena/I4/main.cpp
@@ -1,4 +1,5 @@
 #include "columns_without_null_elements.h"
+#include "rows_without_same_elements.h"
 
 int main(int argc, char* argv[])
 {
--- samoilenko.alena/I4/rows_without_same_elements.cpp
+++ samoilenko.alena/I4/rows_without_same_elements.cpp
@@ -3,6 +3,25 @@
 size_t countRowsWithoutSameElements(const int* matrix, size_t row, size_t col)
 {
   size_t count = 0;
-
+  for (size_t i = 0; i < row; i++)
+  {
+    bool b = false;
+    for (size_t j = 0; j < col; j++)
+    {
+      if (matrix[col * j + i] != matrix[col * j + i + 1])
+      {
+        b = true;
+      }
+      else
+      {
+        b = false;
+        break;
+      }
+    }
+    if (b == true)
+    {
+      count++;
+    }
+  }
   return count;
 }

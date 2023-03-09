c47540b64685ea41440bd2e978be8622d5dd518c
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-03-09 21:52:03 +0300

    first func in process

--- samoilenko.alena/I4/columns_without_null_elements.cpp
+++ samoilenko.alena/I4/columns_without_null_elements.cpp
@@ -3,6 +3,25 @@
 int printColumnsWithoutNull(const int* matrix, size_t row, size_t col)
 {
   int count = 0;
-
+  for (size_t i = 0; i < col; i++)
+  {
+    bool qu = false;
+    for (size_t j = 0; j < row; j++)
+    {
+      if (matrix[j][i] != 0)
+      {
+        qu = true;
+      }
+      else
+      {
+        qu = false;
+        break;
+      }
+    }
+    if (qu == 1)
+    {
+      count++;
+    }
+  }
   return count;
 }

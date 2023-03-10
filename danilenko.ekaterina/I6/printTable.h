fce8aeaeadb9df311f742842241e115fefc15d1e
Даниленко Екатерина Алексеевна <danilenko.ea@edu.spbstu.ru>
2023-03-10 23:22:25 +0300

    little miss fix

--- danilenko.ekaterina/I6/printTable.h
+++ danilenko.ekaterina/I6/printTable.h
@@ -2,7 +2,6 @@
 #define PRINTTABLE_H
 #include <ostream>
 
-void printTableRow(std::ostream& out, double x, double absError, unsigned members);
 void printTable(std::ostream& out, double left, double right, size_t members, double absError, double step);
 
 #endif

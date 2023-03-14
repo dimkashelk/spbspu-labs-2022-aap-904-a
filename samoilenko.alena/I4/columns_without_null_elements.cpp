65ef75bb1f8a1d5ec9cc5794085ac71c5ee01a25
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-03-15 00:09:06 +0300

    old names

--- samoilenko.alena/I4/columns_without_null_elements.cpp
+++ samoilenko.alena/I4/columns_without_null_elements.cpp
@@ -5,15 +5,15 @@ size_t countColumnsWithoutNull(const int* matrix, size_t row, size_t col)
   int count = 0;
   for (size_t i = 0; i < col; i++)
   {
-    bool colWithoutNullt = false;
+    bool colWithoutNull = false;
     for (size_t j = 0; j < row; j++)
     {
       if (matrix[col * j + i] != 0)
       {
-        colWithoutNullt = true;
+        colWithoutNull = true;
       }
     }
-    if (!colWithoutNullt)
+    if (!colWithoutNull)
     {
       count++;
     }

6e98af7df12e5e2a77e23791f288b40ed943ecb5
aront14 <arontellezc14@gmail.com>
2023-03-15 19:04:20 +0300

    try3.0

--- tellez.aron/I6/printTable.cpp
+++ tellez.aron/I6/printTable.cpp
@@ -5,17 +5,13 @@
 
 void printRow(std::ostream& out, double x, double absError, unsigned int numberMax)
 {
-  out << std::setw(10) << std::fixed << std::setprecision(5) << x << " ";
-  out << std::setw(15) << std::scientific << std::setprecision(5)
-      << exp(x, absError, numberMax) << " "
-      << std::exp(x * (-1)) << "\n";
+  out << std::setw(5) << x << " "<< std::setprecision(5);
+  out << std::setw(10) << exp(x, absError, numberMax) << " ";
+  out << std::setw(10) << std::exp(x * (-1)) << "\n";
 }
 
 void printTable(std::ostream& out, double left, double right, double step, double absError, unsigned int numberMax)
 {
-  out << std::setw(10) << std::left << "x" << " ";
-  out << std::setw(15) << std::left << "exp(x)" << " ";
-  out << std::setw(15) << std::left << "exp(-x)" << "\n";
   for (double x = left; x <= right; x += step)
   {
     printRow(out, x, absError, numberMax);
